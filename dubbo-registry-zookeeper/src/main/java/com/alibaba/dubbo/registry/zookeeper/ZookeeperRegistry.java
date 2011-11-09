fa49421c84baa9eef2c5d710f9a1da1f14c58468
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2011-11-09 09:33:39 +0000

    修改zookeeper会话过期重连
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@216 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-registry-zookeeper/src/main/java/com/alibaba/dubbo/registry/zookeeper/ZookeeperRegistry.java
+++ dubbo-registry-zookeeper/src/main/java/com/alibaba/dubbo/registry/zookeeper/ZookeeperRegistry.java
@@ -19,12 +19,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
 
@@ -52,71 +54,99 @@ public class ZookeeperRegistry extends FailbackRegistry {
     
     private final boolean       auth;
 
-    private final ZooKeeper     zookeeper;
+    private final ReentrantLock zookeeperLock = new ReentrantLock();
+
+    private volatile ZooKeeper  zookeeper;
 
     public ZookeeperRegistry(URL url) {
         super(url);
         try {
-            String address = url.getAddress();
-            String backup = url.getParameter(Constants.BACKUP_KEY);
-            if (backup != null && backup.length() > 0) {
-                address = address + "," + backup;
-            }
-            this.zookeeper = new ZooKeeper(address, url.getPositiveParameter(
-                    Constants.TIMEOUT_KEY, 5000), new Watcher() {
-                public void process(WatchedEvent event) {
-                    try {
-                        if (event.getType() != EventType.NodeChildrenChanged) {
-                            return;
-                        }
-                        String path = event.getPath();
-                        if (path == null || path.length() == 0) {
-                            return;
-                        }
-                        List<String> providers = zookeeper.getChildren(path, true);
-                        String service = path;
-                        int i = service.lastIndexOf(SEPARATOR);
-                        if (i >= 0) {
-                            service = service.substring(i + 1);
-                        }
-                        service = URL.decode(service);
-                        for (Map.Entry<String, Set<NotifyListener>> entry : getSubscribed().entrySet()) {
-                            String key = entry.getKey();
-                            URL subscribe = URL.valueOf(key);
-                            String subscribeService = subscribe.getServiceName();
-                            if (service.equals(subscribeService)) {
-                                List<URL> list = toUrls(subscribe, providers);
-                                if (logger.isInfoEnabled()) {
-                                    logger.info("Zookeeper service changed, service: " + service + ", urls: " + list);
-                                }
-                                for (NotifyListener listener : entry.getValue()) {
-                                    ZookeeperRegistry.this.notify(subscribe, listener, list);
-                                }
-                            }
-                        }
-                    } catch (Throwable e) {
-                        logger.error(e.getMessage(), e);
-                    }
-                }
-            });
+            initZookeeper(url);
             this.auth = url.getUsername() != null && url.getUsername().length() > 0 
                     && url.getPassword() != null && url.getPassword().length() > 0;
-            if (auth) {
-                zookeeper.addAuthInfo(url.getUsername(), url.getPassword().getBytes());
-            }
             String group = url.getParameter(Constants.GROUP_KEY);
             if (group != null && group.length() > 0) {
                 group = SEPARATOR + group;
                 this.root = group;
-                if (zookeeper.exists(group, false) == null) {
-                    zookeeper.create(group, new byte[0], auth ? Ids.CREATOR_ALL_ACL : Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-                }
             } else {
                 this.root = "";
             }
         } catch (Exception e) {
             throw new IllegalStateException(e.getMessage(), e);
         }
+    }
+    
+    private void initZookeeper(final URL url) {
+        zookeeperLock.lock();
+        try {
+            ZooKeeper zk = this.zookeeper;
+            if (zk == null || zk.getState() == null || ! zk.getState().isAlive()) {
+                this.zookeeper = createZookeeper(url);
+            }
+            if (zk != null) {
+                zk.close();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        } finally {
+            zookeeperLock.unlock();
+        }
+    }
+    
+    private ZooKeeper createZookeeper(final URL url) throws Exception {
+        String address = url.getAddress();
+        String backup = url.getParameter(Constants.BACKUP_KEY);
+        if (backup != null && backup.length() > 0) {
+            address = address + "," + backup;
+        }
+        ZooKeeper zk = new ZooKeeper(address, url.getPositiveParameter(
+                Constants.TIMEOUT_KEY, Integer.MAX_VALUE), new Watcher() {
+            public void process(WatchedEvent event) {
+                try {
+                    if (event.getState() == KeeperState.Expired) {
+                        initZookeeper(url);
+                    }
+                    if (event.getType() != EventType.NodeChildrenChanged) {
+                        return;
+                    }
+                    String path = event.getPath();
+                    if (path == null || path.length() == 0) {
+                        return;
+                    }
+                    ZooKeeper zk = ZookeeperRegistry.this.zookeeper;
+                    List<String> providers = zk.getChildren(path, true);
+                    String service = path;
+                    int i = service.lastIndexOf(SEPARATOR);
+                    if (i >= 0) {
+                        service = service.substring(i + 1);
+                    }
+                    service = URL.decode(service);
+                    for (Map.Entry<String, Set<NotifyListener>> entry : getSubscribed().entrySet()) {
+                        String key = entry.getKey();
+                        URL subscribe = URL.valueOf(key);
+                        String subscribeService = subscribe.getServiceName();
+                        if (service.equals(subscribeService)) {
+                            List<URL> list = toUrls(subscribe, providers);
+                            if (logger.isInfoEnabled()) {
+                                logger.info("Zookeeper service changed, service: " + service + ", urls: " + list);
+                            }
+                            for (NotifyListener listener : entry.getValue()) {
+                                ZookeeperRegistry.this.notify(subscribe, listener, list);
+                            }
+                        }
+                    }
+                } catch (Throwable e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+        });
+        if (auth) {
+            zk.addAuthInfo(url.getUsername(), url.getPassword().getBytes());
+        }
+        if (root != null && root.length() > 0 && zk.exists(root, false) == null) {
+            zk.create(root, new byte[0], auth ? Ids.CREATOR_ALL_ACL : Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        }
+        return zk;
     }
 
     public boolean isAvailable() {
