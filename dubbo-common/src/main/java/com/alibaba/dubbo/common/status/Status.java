42e7ca2144a99e61393f935705adebb76ac7c8fd
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2011-11-24 17:45:11 +0000

    修改Status增加description
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@402 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/status/Status.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/status/Status.java
@@ -32,19 +32,21 @@ public class Status {
     private final Level level;
 
     private final String message;
+
+    private final String description;
     
     public Status(Level level){
-        this(level, null);
+        this(level, null, null);
     }
 
-    /**
-     * 
-     * @param level
-     * @param message
-     */
     public Status(Level level, String message){
+        this(level, message, null);
+    }
+    
+    public Status(Level level, String message, String description){
         this.level = level;
         this.message = message;
+        this.description = description;
     }
     
     public Level getLevel() {
@@ -53,6 +55,10 @@ public class Status {
     
     public String getMessage() {
         return message;
+    }
+    
+    public String getDescription() {
+        return description;
     }
 
 }
