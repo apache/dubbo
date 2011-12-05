4664df02cd8764e846ea7656c420337e67715062
chao.liuc <chao.liuc@1a56cb94-b969-4eaa-88fa-be21384802f2>
2011-12-05 04:00:15 +0000

    DUBBO-117 	registry notify 防御性容错
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@517 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-registry/src/main/java/com/alibaba/dubbo/registry/support/RegistryDirectory.java
+++ dubbo-registry/src/main/java/com/alibaba/dubbo/registry/support/RegistryDirectory.java
@@ -155,11 +155,11 @@ public class RegistryDirectory<T> extends AbstractDirectory<T> implements Notify
             }
             // state change
             {
-                //如果计算错误，则不进行处理.
-//                if (newUrlInvokerMap == null || newUrlInvokerMap.size() ==0 ){
-//                    logger.error(new IllegalStateException(""));
-//                    return ;
-//                }
+//                如果计算错误，则不进行处理.
+                if (newUrlInvokerMap == null || newUrlInvokerMap.size() == 0 ){
+                    logger.error(new IllegalStateException("urls to invokers error .urls.size :"+urls.size() + ", invoker.size :0"));
+                    return ;
+                }
                 this.methodInvokerMap = newMethodInvokerMap;
                 this.urlInvokerMap = newUrlInvokerMap;
                 if(routers != null){ // null - do nothing
