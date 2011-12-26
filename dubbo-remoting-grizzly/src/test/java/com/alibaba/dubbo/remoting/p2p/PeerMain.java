a820cae7301fdcdd8dde5eb2e1caf7a8d7dedb19
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2011-12-26 09:43:46 +0000

    DUBBO-156 拆分remoting的p2p子模块
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@730 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-remoting-grizzly/src/test/java/com/alibaba/dubbo/remoting/p2p/PeerMain.java
+++ dubbo-remoting-grizzly/src/test/java/com/alibaba/dubbo/remoting/p2p/PeerMain.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.dubbo.remoting;
+package com.alibaba.dubbo.remoting.p2p;
 
 import java.util.Collection;
 
