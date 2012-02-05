4eeb430a7eb8a4b1aa7d02bb9c0d81e249d22738
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-02-05 09:49:05 +0000

    DUBBO-207 打印容错的异常日志，防止掩盖问题
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@897 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-config/src/main/java/com/alibaba/dubbo/config/AbstractConfig.java
+++ dubbo-config/src/main/java/com/alibaba/dubbo/config/AbstractConfig.java
@@ -340,7 +340,7 @@ public abstract class AbstractConfig implements Serializable {
                 try {
                     String name = method.getName();
                     if ((name.startsWith("get") || name.startsWith("is")) 
-                            && ! "getClass".equals(name) && ! "get".equals(name)&& ! "is".equals(name)
+                            && ! "getClass".equals(name) && ! "get".equals(name) && ! "is".equals(name)
                             && Modifier.isPublic(method.getModifiers()) 
                             && method.getParameterTypes().length == 0
                             && isPrimitive(method.getReturnType())) {
@@ -362,6 +362,7 @@ public abstract class AbstractConfig implements Serializable {
             buf.append(" />");
             return buf.toString();
         } catch (Throwable t) { // 防御性容错
+            logger.warn(t.getMessage(), t);
             return super.toString();
         }
     }
