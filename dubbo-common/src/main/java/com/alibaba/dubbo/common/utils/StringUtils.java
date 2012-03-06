c2ae319958bf9aca78ae0f701f2e02629e3341aa
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-03-06 21:39:12 +0000

    DUBBO-242 配置解析支持"-"分隔的属性，自动转为驼峰命名的setter
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@1159 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/StringUtils.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/StringUtils.java
@@ -387,6 +387,31 @@ public final class StringUtils
 		}
 		return buf.toString();
 	}
+	
+	public static String camelToSplitName(String camelName, String split) {
+	    if (camelName == null || camelName.length() == 0) {
+	        return camelName;
+	    }
+	    StringBuilder buf = null;
+	    for (int i = 0; i < camelName.length(); i ++) {
+	        char ch = camelName.charAt(i);
+	        if (ch >= 'A' && ch <= 'Z') {
+	            if (buf == null) {
+	                buf = new StringBuilder();
+	                if (i > 0) {
+	                    buf.append(camelName.substring(0, i));
+	                }
+	            }
+	            if (i > 0) {
+	                buf.append(split);
+	            }
+	            buf.append(Character.toLowerCase(ch));
+	        } else if (buf != null) {
+	            buf.append(ch);
+	        }
+	    }
+	    return buf == null ? camelName : buf.toString();
+	}
 
 	private StringUtils(){}
 }
