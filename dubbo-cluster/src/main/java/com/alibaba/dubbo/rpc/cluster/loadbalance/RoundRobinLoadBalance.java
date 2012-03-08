7f93d4da9eaa179f3435fe3c9b6a1bbd2421e73a
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-03-08 06:38:24 +0000

    DUBBO-222 从Dubbo1.0的代码中迁移带权重轮循逻辑到2.0
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@1202 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/RoundRobinLoadBalance.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/RoundRobinLoadBalance.java
@@ -15,6 +15,7 @@
  */
 package com.alibaba.dubbo.rpc.cluster.loadbalance;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -35,8 +36,38 @@ public class RoundRobinLoadBalance extends AbstractLoadBalance {
     
     private final ConcurrentMap<String, AtomicPositiveInteger> sequences = new ConcurrentHashMap<String, AtomicPositiveInteger>();
 
+    private final ConcurrentMap<String, AtomicPositiveInteger> weightSequences = new ConcurrentHashMap<String, AtomicPositiveInteger>();
+
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, Invocation invocation) {
         String key = invokers.get(0).getInterface().getName() + "." + invocation.getMethodName();
+        int length = invokers.size(); // 总个数
+        int maxWeight = 0; // 最大权重
+        int minWeight = Integer.MAX_VALUE; // 最小权重
+        for (int i = 0; i < length; i++) {
+            int weight = getWeight(invokers.get(i), invocation);
+            maxWeight = Math.max(maxWeight, weight); // 累计最大权重
+            minWeight = Math.min(minWeight, weight); // 累计最小权重
+        }
+        if (maxWeight > 0 && minWeight < maxWeight) { // 权重不一样
+            AtomicPositiveInteger weightSequence = weightSequences.get(key);
+            if (weightSequence == null) {
+                weightSequences.putIfAbsent(key, new AtomicPositiveInteger());
+                weightSequence = weightSequences.get(key);
+            }
+            int w = weightSequence.getAndIncrement() % maxWeight;
+            List<Invoker<T>> weightInvokers = new ArrayList<Invoker<T>>();
+            for (Invoker<T> invoker : invokers) { // 筛选权重大于当前权重基数的Invoker
+                if (getWeight(invoker, invocation) > w) {
+                    weightInvokers.add(invoker);
+                }
+            }
+            if (weightInvokers.size() == 1) {
+                return weightInvokers.get(0);
+            } else if (weightInvokers.size() > 1) {
+                invokers = weightInvokers;
+                length = invokers.size();
+            }
+        }
         AtomicPositiveInteger sequence = sequences.get(key);
         if (sequence == null) {
             sequences.putIfAbsent(key, new AtomicPositiveInteger());
