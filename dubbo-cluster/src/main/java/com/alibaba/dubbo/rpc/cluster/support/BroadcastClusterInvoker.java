e8c73f16540054431706322aafc494eb8508a9d8
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-03-12 07:18:31 +0000

    DUBBO-264 支持boardcast的cluster
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@1255 1a56cb94-b969-4eaa-88fa-be21384802f2

---
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/support/BroadcastClusterInvoker.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 1999-2012 Alibaba Group.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.dubbo.rpc.cluster.support;
+
+import java.util.List;
+
+import com.alibaba.dubbo.rpc.Invocation;
+import com.alibaba.dubbo.rpc.Invoker;
+import com.alibaba.dubbo.rpc.Result;
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.alibaba.dubbo.rpc.RpcException;
+import com.alibaba.dubbo.rpc.cluster.Directory;
+import com.alibaba.dubbo.rpc.cluster.LoadBalance;
+
+/**
+ * BroadcastClusterInvoker
+ * 
+ * @author william.liangf
+ */
+public class BroadcastClusterInvoker<T> extends AbstractClusterInvoker<T>{
+
+    public BroadcastClusterInvoker(Directory<T> directory) {
+        super(directory);
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public Result doInvoke(final Invocation invocation, List<Invoker<T>> invokers, LoadBalance loadbalance) throws RpcException {
+        checkInvokers(invokers, invocation);
+        RpcContext.getContext().setInvokers((List)invokers);
+        Result result = null;
+        for (Invoker<T> invoker: invokers) {
+            result = invoker.invoke(invocation);
+        }
+        return result;
+    }
+
+}
