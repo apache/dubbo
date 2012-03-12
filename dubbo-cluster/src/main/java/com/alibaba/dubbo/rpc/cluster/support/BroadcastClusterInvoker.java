8b9f693f797a6176452dc0faa3b921e84a7f26c8
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-03-12 07:43:50 +0000

    DUBBO-264 修改异常处理
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@1257 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/support/BroadcastClusterInvoker.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/support/BroadcastClusterInvoker.java
@@ -17,6 +17,8 @@ package com.alibaba.dubbo.rpc.cluster.support;
 
 import java.util.List;
 
+import com.alibaba.dubbo.common.logger.Logger;
+import com.alibaba.dubbo.common.logger.LoggerFactory;
 import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Result;
@@ -30,7 +32,9 @@ import com.alibaba.dubbo.rpc.cluster.LoadBalance;
  * 
  * @author william.liangf
  */
-public class BroadcastClusterInvoker<T> extends AbstractClusterInvoker<T>{
+public class BroadcastClusterInvoker<T> extends AbstractClusterInvoker<T> {
+    
+    private static final Logger logger = LoggerFactory.getLogger(BroadcastClusterInvoker.class);
 
     public BroadcastClusterInvoker(Directory<T> directory) {
         super(directory);
@@ -47,6 +51,10 @@ public class BroadcastClusterInvoker<T> extends AbstractClusterInvoker<T>{
                 result = invoker.invoke(invocation);
             } catch (RpcException e) {
                 exception = e;
+                logger.warn(e.getMessage(), e);
+            } catch (Throwable e) {
+                exception = new RpcException(e.getMessage(), e);
+                logger.warn(e.getMessage(), e);
             }
         }
         if (exception != null) {
