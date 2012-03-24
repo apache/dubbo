b547a2b8a02ec768c7d95cadd9fa10d4921fa1ec
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-03-24 14:33:33 +0000

    修改NotifyListener注释
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@1473 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-registry/dubbo-registry-api/src/main/java/com/alibaba/dubbo/registry/NotifyListener.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/com/alibaba/dubbo/registry/NotifyListener.java
@@ -26,11 +26,18 @@ import com.alibaba.dubbo.common.URL;
  * @author william.liangf
  */
 public interface NotifyListener {
-    
+
     /**
-     * 当收到服务变更通知时触发
-     * @param urls 含义同{@link com.alibaba.dubbo.registry.RegistryService#register(URL, NotifyListener)}的urls参数。
+     * 当收到服务变更通知时触发。
+     * 
+     * 通知需处理契约：<br>
+     * 1. 总是以服务接口为维度全量通知，即不会通知一个服务的部分数据，用户不需要对比上一次通知结果
+     * 2. 允许不同类型的数据分开通知，比如：providers, consumers, routes, overrides，允许只通知其中一种类型，但该类型的数据必须是全量的，不是增量的。
+     * 3. 如果一种类型的数据为空，需通知一个noprovider, noconsumer, noroute, nooverride协议的标识性URL数据。
+     * 4. 通知者(即注册中心实现)需保证通知的顺序，比如：单线程推送，队列串行化，带版本对比。
+     * 
+     * @param urls 含义同{@link com.alibaba.dubbo.registry.RegistryService#register(URL, NotifyListener)}的url参数。
      */
     void notify(List<URL> urls);
-    
+
 }
