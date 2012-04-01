f9906f4ff97edae47518d13d647087b36115430d
ding.lid <ding.lid@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-04-01 03:22:58 +0000

    添加PojoUtils UT
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@1498 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-common/src/test/java/com/alibaba/dubbo/common/utils/PojoUtilsTest.java
+++ dubbo-common/src/test/java/com/alibaba/dubbo/common/utils/PojoUtilsTest.java
@@ -83,6 +83,21 @@ public class PojoUtilsTest {
         assertObject(new Person());
         assertObject(new SerializablePerson());
     }
+    
+    @Test
+    public void test_Map_List_pojo() throws Exception {
+        Map<String, List<Object>> map = new HashMap<String, List<Object>>();
+        
+        List<Object> list = new ArrayList<Object>();
+        list.add(new Person());
+        list.add(new SerializablePerson());
+        
+        map.put("k", list);
+        
+        Object generalize = PojoUtils.generalize(map);
+        Object realize = PojoUtils.realize(generalize, Map.class);
+        assertEquals(map, realize);
+    }
 
     @Test
     public void test_PrimitiveArray() throws Exception {
