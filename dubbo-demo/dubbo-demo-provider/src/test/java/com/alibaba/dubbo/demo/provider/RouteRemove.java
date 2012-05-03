ae2664700f853c1e0045f95e64f934268883d369
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-05-03 02:14:25 +0000

    修改demo
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@1617 1a56cb94-b969-4eaa-88fa-be21384802f2

---
+++ dubbo-demo/dubbo-demo-provider/src/test/java/com/alibaba/dubbo/demo/provider/RouteRemove.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 1999-2011 Alibaba Group.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.dubbo.demo.provider;
+
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+import com.alibaba.dubbo.common.logger.Logger;
+import com.alibaba.dubbo.common.logger.LoggerFactory;
+import com.alibaba.dubbo.common.utils.ConfigUtils;
+import com.alibaba.dubbo.common.utils.NetUtils;
+import com.alibaba.dubbo.demo.DemoService;
+import com.alibaba.dubbo.registry.Registry;
+import com.alibaba.dubbo.registry.RegistryFactory;
+
+/**
+ * RegistryContainer
+ * 
+ * @author william.liangf
+ */
+public class RouteRemove {
+
+    private static final Logger logger = LoggerFactory.getLogger(RouteRemove.class);
+
+    public static final String REGISTRY_ADDRESS = "dubbo.registry.address";
+
+    private final RegistryFactory registryFactory = ExtensionLoader.getExtensionLoader(RegistryFactory.class).getAdaptiveExtension();
+
+    private Registry registry;
+
+    public Registry getRegistry() {
+        return registry;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        RouteRemove c = new RouteRemove();
+        c.start();
+        Thread.sleep(500);
+        c.stop();
+    }
+
+    public void start() {
+        String url = ConfigUtils.getProperty(REGISTRY_ADDRESS);
+        if (url == null || url.length() == 0) {
+            throw new IllegalArgumentException("Please set java start argument: -D" + REGISTRY_ADDRESS + "=zookeeper://127.0.0.1:2181");
+        }
+        URL registryUrl = URL.valueOf(url).addParameter(Constants.CHECK_KEY, String.valueOf(false));
+        registry = registryFactory.getRegistry(registryUrl);
+        URL routeUrl = URL.valueOf("route://" + NetUtils.getLocalHost() + "/" + DemoService.class.getName() + "?category=routes&version=1.0.0&dynamic=false&priority=1&name=aaa&rule=" + URL.encode("consumer.host=" + NetUtils.getLocalHost() + " => provider.host=" + NetUtils.getLocalHost()));
+        registry.unregister(routeUrl);
+    }
+
+    public void stop() {
+        try {
+            registry.destroy();
+        } catch (Throwable e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+}
