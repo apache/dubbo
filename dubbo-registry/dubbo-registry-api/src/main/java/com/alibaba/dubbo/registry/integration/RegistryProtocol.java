b098098529877768eb9beeea3a02045ac0941bf4
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-05-09 02:14:39 +0000

    DUBBO-343 还原提供方订阅override信息
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@1695 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-registry/dubbo-registry-api/src/main/java/com/alibaba/dubbo/registry/integration/RegistryProtocol.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/com/alibaba/dubbo/registry/integration/RegistryProtocol.java
@@ -152,9 +152,9 @@ public class RegistryProtocol implements Protocol {
         registry.register(registedProviderUrl);
         // 订阅override数据
         // FIXME 提供者订阅时，会影响同一JVM即暴露服务，又引用同一服务的的场景，因为subscribed以服务名为缓存的key，导致订阅信息覆盖。
-        /*registry.subscribe(registedProviderUrl.setProtocol(Constants.PROVIDER_PROTOCOL)
+        registry.subscribe(registedProviderUrl.setProtocol(Constants.PROVIDER_PROTOCOL)
                 .addParameters(Constants.CATEGORY_KEY, Constants.CONFIGURATORS_CATEGORY, 
-                        Constants.CHECK_KEY, String.valueOf(false)), listener);*/
+                        Constants.CHECK_KEY, String.valueOf(false)), listener);
         return registry;
     }
 
