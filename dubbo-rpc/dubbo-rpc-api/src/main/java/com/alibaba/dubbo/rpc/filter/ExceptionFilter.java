7602104698c995cec69abe19ef6c476124111e56
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-05-10 14:54:11 +0000

    DUBBO-377 ExceptionFilter不过滤java标准包中的异常类型
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@1718 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ExceptionFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ExceptionFilter.java
@@ -55,6 +55,11 @@ public class ExceptionFilter implements Filter {
             if (result.hasException() && GenericService.class != invoker.getInterface()) {
                 try {
                     Throwable exception = result.getException();
+                    // 如果是checked异常，直接抛出
+                    if (! (exception instanceof RuntimeException)) {
+                        return result;
+                    }
+                    // 在方法签名上有声明，直接抛出
                     try {
                         Method method = invoker.getInterface().getMethod(invocation.getMethodName(), invocation.getParameterTypes());
                         Class<?>[] exceptionClassses = method.getExceptionTypes();
@@ -66,14 +71,20 @@ public class ExceptionFilter implements Filter {
                     } catch (NoSuchMethodException e) {
                         return result;
                     }
-                    //在package里有类，直接抛出
+                    // 未在方法签名上定义的异常，在服务器端打印ERROR日志
+                    logger.error("Got unchecked and undeclared service method exception: " + exception.getClass().getName() + ": " + exception.getMessage(), exception);
+                    // 异常类和接口类在同一jar包里，直接抛出
                     String serviceFile = ReflectUtils.getCodeBase(invoker.getInterface());
                     String exceptionFile = ReflectUtils.getCodeBase(exception.getClass());
                     if (serviceFile == null || exceptionFile == null || serviceFile.equals(exceptionFile)){
                         return result;
                     }
-                    //除上面二种情况外，server端打印日志，并包装成runtimeException抛给客户端
-                    logger.error("Got unchecked and undeclare service method invoke exception: " + exception.getMessage(), exception);
+                    // 是JDK自带的异常，直接抛出
+                    String className = exception.getClass().getName();
+                    if (className.startsWith("java.") || className.startsWith("javax.")) {
+                        return result;
+                    }
+                    // 否则，包装成RuntimeException抛给客户端
                     return new RpcResult(new RuntimeException(StringUtils.toString(exception)));
                 } catch (Throwable e) {
                     logger.warn(e.getMessage(), e);
