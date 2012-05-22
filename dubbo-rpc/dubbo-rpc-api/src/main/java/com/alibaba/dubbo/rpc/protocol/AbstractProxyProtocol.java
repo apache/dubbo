d2a89fe24be8e72cd017336c82bb05afc8703335
kimi <kimi@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-05-22 11:05:33 +0000

    添加一个方法供子类获取 ProxyFactory
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@1841 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/protocol/AbstractProxyProtocol.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/protocol/AbstractProxyProtocol.java
@@ -1,18 +1,19 @@
 /*
  * Copyright 1999-2012 Alibaba Group.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.alibaba.dubbo.rpc.protocol;
 
 import java.util.List;
@@ -52,6 +53,10 @@ public abstract class AbstractProxyProtocol extends AbstractProtocol {
 
     public void setProxyFactory(ProxyFactory proxyFactory) {
         this.proxyFactory = proxyFactory;
+    }
+
+    public ProxyFactory getProxyFactory() {
+        return proxyFactory;
     }
 
     public <T> Exporter<T> export(final Invoker<T> invoker) throws RpcException {
