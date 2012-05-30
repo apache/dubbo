8588780ac738d4f970345ca93813c2179815e483
william.liangf <william.liangf@1a56cb94-b969-4eaa-88fa-be21384802f2>
2012-05-30 10:56:41 +0000

    DUBBO-396 增加单元测试
    git-svn-id: http://code.alibabatech.com/svn/dubbo/trunk@1888 1a56cb94-b969-4eaa-88fa-be21384802f2

--- dubbo-common/src/test/java/com/alibaba/dubbo/common/extensionloader/ExtensionLoaderTest.java
+++ dubbo-common/src/test/java/com/alibaba/dubbo/common/extensionloader/ExtensionLoaderTest.java
@@ -495,4 +495,22 @@ public class ExtensionLoaderTest {
         Assert.assertTrue(list.get(1).getClass() == OrderActivateExtImpl2.class);
     }
 
+    @Test
+    public void testLoadDefaultActivateExtension() throws Exception {
+        // test default
+        URL url = URL.valueOf("test://localhost/test?ext=order1,default");
+        List<ActivateExt1> list = ExtensionLoader.getExtensionLoader(ActivateExt1.class)
+                .getActivateExtension(url, "ext", "default_group");
+        Assert.assertEquals(2, list.size());
+        Assert.assertTrue(list.get(0).getClass() == OrderActivateExtImpl1.class);
+        Assert.assertTrue(list.get(1).getClass() == ActivateExt1Impl1.class);
+        
+        url = URL.valueOf("test://localhost/test?ext=default,order1");
+        list = ExtensionLoader.getExtensionLoader(ActivateExt1.class)
+                .getActivateExtension(url, "ext", "default_group");
+        Assert.assertEquals(2, list.size());
+        Assert.assertTrue(list.get(0).getClass() == ActivateExt1Impl1.class);
+        Assert.assertTrue(list.get(1).getClass() == OrderActivateExtImpl1.class);
+    }
+
 }
