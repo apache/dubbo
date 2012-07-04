81ca7e9f785941688912dc1f6b6eb720b10d35a2
liangfei0201 <liangfei0201@gmail.com>
2012-07-04 20:16:22 +0800

    add logback container

---
+++ dubbo-container/dubbo-container-logback/src/test/java/com/alibaba/dubbo/container/logback/LogbackContainerTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 1999-2011 Alibaba Group.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.dubbo.container.logback;
+
+import org.junit.Test;
+
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+import com.alibaba.dubbo.common.logger.Logger;
+import com.alibaba.dubbo.common.logger.LoggerFactory;
+import com.alibaba.dubbo.container.Container;
+
+/**
+ * StandaloneContainerTest
+ * 
+ * @author {@link "mailto:qq55355383@gmail.com" "yi.tong"}
+ * @date May 30, 2012
+ */
+public class LogbackContainerTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(LogbackContainerTest.class);
+
+	@Test
+	public void testContainer() {
+		LogbackContainer container = (LogbackContainer) ExtensionLoader.getExtensionLoader(Container.class)
+				.getExtension("logback");
+		container.start();
+
+		logger.debug("Test debug:" + this.getClass().getName());
+		logger.warn("Test warn:" + this.getClass().getName());
+		logger.info("Test info:" + this.getClass().getName());
+		logger.error("Test error:" + this.getClass().getName());
+
+		container.stop();
+	}
+
+}
