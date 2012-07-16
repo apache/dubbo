d10b6568a566b21feb12cebf9a4a4810fca3fa47
oldrat <oldratlee@gmail.com>
2012-07-16 19:12:01 +0800

    添加toString方法

--- dubbo-config/dubbo-config-api/src/main/java/com/alibaba/dubbo/config/utils/ReferenceConfigCache.java
+++ dubbo-config/dubbo-config-api/src/main/java/com/alibaba/dubbo/config/utils/ReferenceConfigCache.java
@@ -146,4 +146,10 @@ public class ReferenceConfigCache {
             destroyKey(key);
         }
     }
+
+    @Override
+    public String toString() {
+        return "ReferenceConfigCache(name: " + name
+                + ")";
+    }
 }
