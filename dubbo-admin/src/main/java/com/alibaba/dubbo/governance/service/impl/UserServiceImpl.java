4a57a672817adbf77b6ddb0a5205ee5bbb20f06d
liangfei0201 <liangfei0201@gmail.com>
2012-07-17 20:04:04 +0800

    DUBBO-489 增加简单权限

--- dubbo-admin/src/main/java/com/alibaba/dubbo/governance/service/impl/UserServiceImpl.java
+++ dubbo-admin/src/main/java/com/alibaba/dubbo/governance/service/impl/UserServiceImpl.java
@@ -20,6 +20,7 @@ import java.util.Map;
 
 import com.alibaba.dubbo.governance.service.UserService;
 import com.alibaba.dubbo.registry.common.domain.User;
+import com.alibaba.dubbo.registry.common.util.Coder;
 
 /**
  * IBatisUserService
@@ -28,17 +29,49 @@ import com.alibaba.dubbo.registry.common.domain.User;
  */
 public class UserServiceImpl extends AbstractService implements UserService {
 
+	private String rootPassword;
+
+	public void setRootPassword(String password) {
+		this.rootPassword = (password == null ? "" : password);
+	}
+
+	private String guestPassword;
+
+	public void setGuestPassword(String password) {
+		this.guestPassword = (password == null ? "" : password);
+	}
+
+    public User findUser(String username) {
+    	if ("guest".equals(username)) {
+    		User user = new User();
+            user.setUsername(username);
+            user.setPassword(Coder.encodeMd5(username + ":" + User.REALM + ":" + guestPassword));
+            user.setName(username);
+            user.setRole(User.GUEST);
+            user.setEnabled(true);
+            user.setLocale("zh");
+            user.setServicePrivilege("");
+            return user;
+    	} else if ("root".equals(username)) {
+    		User user = new User();
+            user.setUsername(username);
+            user.setPassword(Coder.encodeMd5(username + ":" + User.REALM + ":" + rootPassword));
+            user.setName(username);
+            user.setRole(User.ROOT);
+            user.setEnabled(true);
+            user.setLocale("zh");
+            user.setServicePrivilege("*");
+            return user;
+    	}
+    	return null;
+    }
+
     public List<User> findAllUsers() {
         // TODO Auto-generated method stub
         return null;
     }
 
     public Map<String, User> findAllUsersMap() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public User findUser(String username) {
         // TODO Auto-generated method stub
         return null;
     }
