f32e7c5557bc14ee5892cb3bd4693456c8503c3a
oldrat <oldratlee@gmail.com>
2012-07-24 10:52:03 +0800

    异常信息加强 和 bug fix DUBBO-510 Provider调用有Error时，Provider端没有日志报警

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ExceptionFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ExceptionFilter.java
@@ -34,8 +34,17 @@ import com.alibaba.dubbo.rpc.service.GenericService;
 
 /**
  * ExceptionInvokerFilter
+ * <p>
+ * 功能：
+ * <ol>
+ * <li>不期望的异常打ERROR日志（Provider端）<br>
+ *     不期望的日志即是，没有的接口上声明的Unchecked异常。
+ * <li>异常不在API包中，则Wrap一层RuntimeException。<br>
+ *     RPC对于第一层异常会直接序列化传输(Cause异常会String化)，避免异常在Client出不能反序列化问题。
+ * </ol>
  * 
  * @author william.liangf
+ * @author ding.lid
  */
 @Activate(group = Constants.PROVIDER)
 public class ExceptionFilter implements Filter {
@@ -56,8 +65,9 @@ public class ExceptionFilter implements Filter {
             if (result.hasException() && GenericService.class != invoker.getInterface()) {
                 try {
                     Throwable exception = result.getException();
+
                     // 如果是checked异常，直接抛出
-                    if (! (exception instanceof RuntimeException)) {
+                    if (! (exception instanceof RuntimeException) && (exception instanceof Exception)) {
                         return result;
                     }
                     // 在方法签名上有声明，直接抛出
@@ -72,8 +82,12 @@ public class ExceptionFilter implements Filter {
                     } catch (NoSuchMethodException e) {
                         return result;
                     }
+
                     // 未在方法签名上定义的异常，在服务器端打印ERROR日志
-                    logger.error("Got unchecked and undeclared exception which called by " + RpcContext.getContext().getRemoteHost() + ". service: " + invoker.getInterface().getName() + ", method: " + invocation.getMethodName() + ", exception: " + exception.getClass().getName() + ": " + exception.getMessage(), exception);
+                    logger.error("Got unchecked and undeclared exception which called by " + RpcContext.getContext().getRemoteHost()
+                            + ". service: " + invoker.getInterface().getName() + ", method: " + invocation.getMethodName()
+                            + ", exception: " + exception.getClass().getName() + ": " + exception.getMessage(), exception);
+
                     // 异常类和接口类在同一jar包里，直接抛出
                     String serviceFile = ReflectUtils.getCodeBase(invoker.getInterface());
                     String exceptionFile = ReflectUtils.getCodeBase(exception.getClass());
@@ -89,16 +103,21 @@ public class ExceptionFilter implements Filter {
                     if (exception instanceof RpcException) {
                         return result;
                     }
+
                     // 否则，包装成RuntimeException抛给客户端
                     return new RpcResult(new RuntimeException(StringUtils.toString(exception)));
                 } catch (Throwable e) {
-                    logger.warn(e.getMessage(), e);
+                    logger.warn("Fail to ExceptionFilter when called by " + RpcContext.getContext().getRemoteHost()
+                            + ". service: " + invoker.getInterface().getName() + ", method: " + invocation.getMethodName()
+                            + ", exception: " + e.getClass().getName() + ": " + e.getMessage(), e);
                     return result;
                 }
             }
             return result;
         } catch (RuntimeException e) {
-            logger.error("Got unchecked and undeclared exception which called by " + RpcContext.getContext().getRemoteHost() + ". service: " + invoker.getInterface().getName() + ", method: " + invocation.getMethodName() + ", exception: " + e.getClass().getName() + ": " + e.getMessage(), e);
+            logger.error("Got unchecked and undeclared exception which called by " + RpcContext.getContext().getRemoteHost()
+                    + ". service: " + invoker.getInterface().getName() + ", method: " + invocation.getMethodName()
+                    + ", exception: " + e.getClass().getName() + ": " + e.getMessage(), e);
             throw e;
         }
     }
