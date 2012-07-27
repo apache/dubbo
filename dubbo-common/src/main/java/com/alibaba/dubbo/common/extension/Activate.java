2b774739d769157c1793bd1bdca9d30dedd231e2
oldrat <oldratlee@gmail.com>
2012-07-27 13:01:36 +0800

    Activate添加JavaDoc

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/extension/Activate.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/extension/Activate.java
@@ -23,8 +23,11 @@ import java.lang.annotation.Target;
 
 /**
  * Activate.
- * 
+ * 用于给{@link com.alibaba.dubbo.common.extension.ExtensionLoader}的{@link ExtensionLoader#getActivateExtension}
+ * 方法提供信息，决定此方法是否把扩展返回。
+ *
  * @author william.liangf
+ * @author ding.lid
  * @export
  */
 @Documented
@@ -32,8 +35,22 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Activate {
 
+    /**
+     * Group过滤条件。包含{@link ExtensionLoader#getActivateExtension}的group参数给的值，则返回扩展。
+     * <br />
+     * 如没有Group设置，则不过滤。
+     */
     String[] group() default {};
 
+    /**
+     * Key过滤条件。包含{@link ExtensionLoader#getActivateExtension}的URL的参数Key中有，则返回扩展。
+     * <p />
+     * 示例：<br/>
+     * 注解的值 <code>@Activate("cache,validatioin")</code>，
+     * 则{@link ExtensionLoader#getActivateExtension}的URL的参数有<code>cache</code>Key，或是<code>validatioin</code>则返回扩展。
+     * <br/>
+     * 如没有设置，则不过滤。
+     */
     String[] value() default {};
 
     String[] before() default {};
