d1259a4d8738ec740db56e1c7e5a0026be52a4d9
Jinkai Ma <majk@vip.qq.com>
2014-11-14 10:41:15 +0800

    consumer demo without spring xml config and dubbo annotation config

---
+++ dubbo-demo/dubbo-demo-consumer/src/main/java/dubbo/spring/javaconfig/DubboDemoConsumerConfig.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2006-2014 handu.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dubbo.spring.javaconfig;
+
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import com.alibaba.dubbo.config.spring.AnnotationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Jinkai.Ma
+ */
+@Configuration
+public class DubboDemoConsumerConfig {
+
+    public static final String APPLICATION_NAME = "consumer-of-helloworld-app";
+
+    public static final String REGISTRY_ADDRESS = "zookeeper://127.0.0.1:2181";
+
+    public static final String ANNOTATION_PACKAGE = "com.alibaba.dubbo.demo.consumer";
+
+    @Bean
+    public ApplicationConfig applicationConfig() {
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        applicationConfig.setName(APPLICATION_NAME);
+        return applicationConfig;
+    }
+
+    @Bean
+    public RegistryConfig registryConfig() {
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setAddress(REGISTRY_ADDRESS);
+        return registryConfig;
+    }
+
+    @Bean
+    public AnnotationBean annotationBean() {
+        AnnotationBean annotationBean = new AnnotationBean();
+        annotationBean.setPackage(ANNOTATION_PACKAGE);
+        return annotationBean;
+    }
+}
