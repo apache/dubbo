af87ae29a89c05ed9fc1db2e8385a53a9d06af8a
zgmzyr <84169381@qq.com>
2015-05-15 10:29:15 +0800

    修复当value == null &&　entry.getValue() ＝= null 时 语句if(value == null && entry.getValue() != null || !value.equals(entry.getValue())) 报NullPointerException

--- dubbo-common/src/test/java/com/alibaba/dubbo/common/URLTest.java
+++ dubbo-common/src/test/java/com/alibaba/dubbo/common/URLTest.java
@@ -622,5 +622,14 @@ public class URLTest {
         assertEquals("localhost", url.getHost());
         assertTrue(url.isLocalHost());
     }
+    
+    @Test
+    public void testAddParameters() throws Exception {
+        URL url = URL.valueOf("dubbo://127.0.0.1:20880");
+        Map<String, String> parameters = new HashMap<String, String>();
+        parameters.put("version", null);
+        url.addParameters(parameters);
+        
+    }
 
 }
