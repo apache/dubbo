6b9452afb330194279e1998bc03a1159895c89e9
Ian Luo <ian.luo@gmail.com>
2016-06-12 15:28:13 +0800

    avoid NPE when date is null

--- hessian-lite/src/main/java/com/alibaba/com/caucho/hessian/io/JavaDeserializer.java
+++ hessian-lite/src/main/java/com/alibaba/com/caucho/hessian/io/JavaDeserializer.java
@@ -601,7 +601,7 @@ public class JavaDeserializer extends AbstractMapDeserializer {
 
       try {
         java.util.Date date = (java.util.Date) in.readObject();
-        value = new java.sql.Date(date.getTime());
+        if (date != null) value = new java.sql.Date(date.getTime());
 
         _field.set(obj, value);
       } catch (Exception e) {
