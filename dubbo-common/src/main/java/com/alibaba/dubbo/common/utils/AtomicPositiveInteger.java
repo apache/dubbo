97ac7ce0af84ef30c52a179680e7620ace48fd54
jiachun.fjc <jiachun.fjc@alibaba-inc.com>
2016-11-25 01:47:00 +0800

    less memory used & provides better perf

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/AtomicPositiveInteger.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/AtomicPositiveInteger.java
@@ -134,7 +134,7 @@ public class AtomicPositiveInteger extends Number {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + Integer.hashCode(get());
+        result = prime * result + get();
         return result;
     }
 
