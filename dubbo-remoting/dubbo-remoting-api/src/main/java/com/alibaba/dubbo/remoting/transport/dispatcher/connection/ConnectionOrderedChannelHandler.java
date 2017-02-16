9ee16c673df9b0103647c5319c0f1efa62ad9627
stoneapple <736551961@qq.com>
2017-02-17 00:42:13 +0800

    Fix 当提供方线程池满时，请求拒绝却并没有立即返回

--- dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/transport/dispatcher/connection/ConnectionOrderedChannelHandler.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/transport/dispatcher/connection/ConnectionOrderedChannelHandler.java
@@ -17,6 +17,7 @@ package com.alibaba.dubbo.remoting.transport.dispatcher.connection;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -28,6 +29,8 @@ import com.alibaba.dubbo.remoting.Channel;
 import com.alibaba.dubbo.remoting.ChannelHandler;
 import com.alibaba.dubbo.remoting.ExecutionException;
 import com.alibaba.dubbo.remoting.RemotingException;
+import com.alibaba.dubbo.remoting.exchange.Request;
+import com.alibaba.dubbo.remoting.exchange.Response;
 import com.alibaba.dubbo.remoting.transport.dispatcher.ChannelEventRunnable;
 import com.alibaba.dubbo.remoting.transport.dispatcher.WrappedChannelHandler;
 import com.alibaba.dubbo.remoting.transport.dispatcher.ChannelEventRunnable.ChannelState;
@@ -75,6 +78,19 @@ public class ConnectionOrderedChannelHandler extends WrappedChannelHandler {
         try {
             cexecutor.execute(new ChannelEventRunnable(channel, handler, ChannelState.RECEIVED, message));
         } catch (Throwable t) {
+        	//fix 线程池满了拒绝调用不返回，导致消费者一直等待超时
+        	if(message instanceof Request &&
+        			t instanceof RejectedExecutionException){
+        		Request request = (Request)message;
+        		if(request.isTwoWay()){
+        			String msg = "Server side("+url.getIp()+","+url.getPort()+") threadpool is exhausted ,detail msg:"+t.getMessage();
+        			Response response = new Response(request.getId(), request.getVersion());
+        			response.setStatus(Response.SERVER_THREADPOOL_EXHAUSTED_ERROR);
+        			response.setErrorMessage(msg);
+        			channel.send(response);
+        			return;
+        		}
+        	}
             throw new ExecutionException(message, channel, getClass() + " error when process received event .", t);
         }
     }
