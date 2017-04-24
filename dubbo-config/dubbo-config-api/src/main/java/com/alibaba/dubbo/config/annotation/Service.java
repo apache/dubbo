3945b83956debcf2127606ef6d88a6d29a080094
teaey <masfay@163.com>
2017-04-24 11:08:35 +0800

    Solve the conflict between @Service and @Transactional

--- dubbo-config/dubbo-config-api/src/main/java/com/alibaba/dubbo/config/annotation/Service.java
+++ dubbo-config/dubbo-config-api/src/main/java/com/alibaba/dubbo/config/annotation/Service.java
@@ -15,11 +15,7 @@
  */
 package com.alibaba.dubbo.config.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * Service
@@ -30,6 +26,7 @@ import java.lang.annotation.Target;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
+@Inherited
 public @interface Service {
 
     Class<?> interfaceClass() default void.class;
