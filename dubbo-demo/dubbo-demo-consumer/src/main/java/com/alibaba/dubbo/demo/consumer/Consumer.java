30980b23c16fb37b28bd1a8542ae3867cb4b7ff5
Zephery <1570631036@qq.com>
2017-08-31 21:13:21 +0800

    mydubbo

--- dubbo-demo/dubbo-demo-consumer/src/main/java/com/alibaba/dubbo/demo/consumer/Consumer.java
+++ dubbo-demo/dubbo-demo-consumer/src/main/java/com/alibaba/dubbo/demo/consumer/Consumer.java
@@ -1,7 +1,7 @@
 package com.alibaba.dubbo.demo.consumer;
 
 import com.alibaba.dubbo.demo.DemoService;
-
+import org.I0Itec.zkclient.ZkClient;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -17,5 +17,14 @@ public class Consumer {
         String hello = demoService.sayHello("world"); // 执行远程方法
 
         System.out.println(hello); // 显示调用结果
+        ZkClient zkClient = new ZkClient("123.206.28.24", 2181);
+//        zkClient.createPersistent("dubbo");
+        zkClient.createPersistent("/demo");
+        try {
+            Thread.sleep(100000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(zkClient.toString());
     }
 }
