3df74dcb7ee268c57b17b8a211d8dbbaacb4b76b
ken.lj <ken.lj.hz@gmail.com>
2017-10-25 19:31:23 +0800

    Fixd #672 Monitor blocks when registry unreachable

--- dubbo-monitor/dubbo-monitor-api/src/main/java/com/alibaba/dubbo/monitor/support/AbstractMonitorFactory.java
+++ dubbo-monitor/dubbo-monitor-api/src/main/java/com/alibaba/dubbo/monitor/support/AbstractMonitorFactory.java
@@ -17,6 +17,9 @@ package com.alibaba.dubbo.monitor.support;
 
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.common.logger.Logger;
+import com.alibaba.dubbo.common.logger.LoggerFactory;
+import com.alibaba.dubbo.common.utils.NamedThreadFactory;
 import com.alibaba.dubbo.monitor.Monitor;
 import com.alibaba.dubbo.monitor.MonitorFactory;
 import com.alibaba.dubbo.monitor.MonitorService;
@@ -24,7 +27,13 @@ import com.alibaba.dubbo.monitor.MonitorService;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -33,12 +42,21 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author william.liangf
  */
 public abstract class AbstractMonitorFactory implements MonitorFactory {
+    private static final Logger logger = LoggerFactory.getLogger(AbstractMonitorFactory.class);
 
     // 注册中心获取过程锁
     private static final ReentrantLock LOCK = new ReentrantLock();
 
     // 注册中心集合 Map<RegistryAddress, Registry>
     private static final Map<String, Monitor> MONITORS = new ConcurrentHashMap<String, Monitor>();
+
+    private static final Map<String, Future<Monitor>> MONITOR_CREATORS = new ConcurrentHashMap<String, Future<Monitor>>();
+
+    private static final ExecutorService monitorScanner = Executors.newFixedThreadPool(1, new NamedThreadFactory("DubboMonitorScanner", true));
+
+    static {
+        monitorScanner.submit(new MonitorScanTask());
+    }
 
     public static Collection<Monitor> getMonitors() {
         return Collections.unmodifiableCollection(MONITORS.values());
@@ -50,14 +68,26 @@ public abstract class AbstractMonitorFactory implements MonitorFactory {
         LOCK.lock();
         try {
             Monitor monitor = MONITORS.get(key);
-            if (monitor != null) {
+            if (monitor != null || MONITOR_CREATORS.get(key) != null) {
                 return monitor;
             }
-            monitor = createMonitor(url);
-            if (monitor == null) {
-                throw new IllegalStateException("Can not create monitor " + url);
+
+            final URL monitorUrl = url;
+            FutureTask<Monitor> task = new FutureTask<Monitor>(new MonitorCreator(monitorUrl));
+            Thread thread = new Thread(task);
+            thread.setName("DubboMointorCreator-thread-1");
+            thread.setDaemon(true);
+            thread.start();
+            try {
+                System.out.println("main: " + System.currentTimeMillis());
+                monitor = task.get(10, TimeUnit.MILLISECONDS);
+            } catch (Throwable t) {
+                MONITOR_CREATORS.put(key, task);
             }
-            MONITORS.put(key, monitor);
+            if (monitor != null) {
+                MONITORS.put(key, monitor);
+            }
+
             return monitor;
         } finally {
             // 释放锁
@@ -66,5 +96,48 @@ public abstract class AbstractMonitorFactory implements MonitorFactory {
     }
 
     protected abstract Monitor createMonitor(URL url);
+
+    static class MonitorScanTask implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                for (Map.Entry<String, Future<Monitor>> entry : MONITOR_CREATORS.entrySet()) {
+                    System.out.println(MONITOR_CREATORS.size());
+                    String key = entry.getKey();
+                    Future<Monitor> future = MONITOR_CREATORS.get(key);
+                    if (future != null) {
+                        try {
+                            Monitor monitor = future.get(10, TimeUnit.MILLISECONDS);
+                            System.out.println(monitor);
+                            MONITORS.put(key, monitor);
+                            MONITOR_CREATORS.remove(key);
+                        } catch (Throwable t) {
+                            logger.info(t);
+                        }
+                    }
+                }
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+
+    class MonitorCreator implements Callable<Monitor> {
+
+        private URL url;
+
+        public MonitorCreator(URL url) {
+            this.url = url;
+        }
+
+        @Override
+        public Monitor call() throws Exception {
+            Monitor monitor = AbstractMonitorFactory.this.createMonitor(url);
+            System.out.println("thread: " + System.currentTimeMillis());
+            return monitor;
+        }
+    }
 
 }
