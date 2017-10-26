ffcee9b761f1aa65a42849ab1255b5252a805090
zhangyinyue <zhangyinyue@globalegrow.com>
2017-10-26 18:11:31 +0800

    增加MethodValidated注解的测试用例  及对MethodValidated使用场景和用法的说明注释

---
+++ dubbo-filter/dubbo-filter-validation/src/main/java/com/alibaba/dubbo/validation/MethodValidated.java
@@ -0,0 +1,24 @@
+package com.alibaba.dubbo.validation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 方法分组验证注解
+ * 使用场景：当调用某个方法时，需要检查多个分组，可以在接口方法上加上该注解
+ * 用法 在接口方法上增加注解，如：
+ *  @MethodValidated({ValidationService.Save.class, ValidationService.Update.class})
+ *  void relatedQuery(ValidationParameter parameter);
+ * 表示relatedQuery这个方法需要同时检查Save和Update这两个分组
+ * @author: zhangyinyue
+ * @Createdate: 2017年10月10日 16:34
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MethodValidated {
+    Class<?>[] value() default {};
+}
