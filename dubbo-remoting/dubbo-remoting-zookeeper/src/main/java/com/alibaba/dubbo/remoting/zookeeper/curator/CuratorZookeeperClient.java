b27e1ffd0f23473eeb2b5b4aebaee9178fae78d8
ken.lj <ken.lj.hz@gmail.com>
2017-10-31 20:14:47 +0800

    Merge pull request #790 from chickenlj:bugfix_zookeeper_block
    Optimize zookeeper block logic on app startup

--- dubbo-remoting/dubbo-remoting-zookeeper/src/main/java/com/alibaba/dubbo/remoting/zookeeper/curator/CuratorZookeeperClient.java
+++ dubbo-remoting/dubbo-remoting-zookeeper/src/main/java/com/alibaba/dubbo/remoting/zookeeper/curator/CuratorZookeeperClient.java
@@ -27,7 +27,7 @@ public class CuratorZookeeperClient extends AbstractZookeeperClient<CuratorWatch
         try {
             CuratorFrameworkFactory.Builder builder = CuratorFrameworkFactory.builder()
                     .connectString(url.getBackupAddress())
-                    .retryPolicy(new RetryNTimes(Integer.MAX_VALUE, 1000))
+                    .retryPolicy(new RetryNTimes(1, 1000))
                     .connectionTimeoutMs(5000);
             String authority = url.getAuthority();
             if (authority != null && authority.length() > 0) {
