e08c4f5dc2740f042e0fefcfc59da02507284472
mercyblitz <mercyblitz@gmail.com>
2017-11-08 16:06:13 +0800

    Add compatibility for Spring 3.2.x to resolve #17

--- dubbo-test/dubbo-test-compatibility/dubbo-test-spring3/src/main/java/com/alibaba/dubbo/test/provider/DefaultDemoService.java
+++ dubbo-test/dubbo-test-compatibility/dubbo-test-spring3/src/main/java/com/alibaba/dubbo/test/provider/DefaultDemoService.java
@@ -1,4 +1,4 @@
-package com.alibaba.dubbo.test;
+package com.alibaba.dubbo.test.provider;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.alibaba.dubbo.demo.DemoService;
@@ -9,7 +9,12 @@ import com.alibaba.dubbo.demo.DemoService;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 2.5.8
  */
-@Service
+@Service(
+        version = "2.5.8",
+        application = "dubbo-annotation-provider",
+        protocol = "dubbo",
+        registry = "my-registry"
+)
 public class DefaultDemoService implements DemoService {
 
     @Override
