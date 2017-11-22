70532ff61b53988b9b7cec1c23e99ccb9ced58e0
杨浩 <takeseem@gmail.com>
2017-11-22 22:54:43 +0800

    GenericJSONConverter.json(Locale) will StackOverflowError #906

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/json/GenericJSONConverter.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/json/GenericJSONConverter.java
@@ -27,6 +27,7 @@ import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
@@ -371,6 +372,8 @@ public class GenericJSONConverter implements JSONConverter {
                     writeValue(item, jb, writeClass);
             }
             jb.arrayEnd();
+        } else if (obj instanceof Locale) {
+            jb.valueString(obj.toString());
         } else {
             jb.objectBegin();
 
