04964f7a68c02cd4a71a254a9abdf39f9fd37b68
shichao <834466168@qq.com>
2017-11-22 16:43:05 +0800

    1、修复hessian 重载方法报错问题 2、添加log4j2的支持 3、修复hessian attachment 隐形参数失效问题

--- dubbo-demo/dubbo-demo-consumer/src/main/java/com/alibaba/dubbo/demo/consumer/Consumer.java
+++ dubbo-demo/dubbo-demo-consumer/src/main/java/com/alibaba/dubbo/demo/consumer/Consumer.java
@@ -1,11 +1,7 @@
 package com.alibaba.dubbo.demo.consumer;
 
 import com.alibaba.dubbo.demo.DemoService;
-
-import com.alibaba.dubbo.rpc.RpcContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.Date;
 
 /**
  * Created by ken.lj on 2017/7/31.
@@ -17,13 +13,7 @@ public class Consumer {
         context.start();
 
         DemoService demoService = (DemoService) context.getBean("demoService"); // 获取远程服务代理
-        RpcContext.getContext().setAttachment("userId", "123");
-        String hello = demoService.sayHello("world", new Date()); // 执行远程方法
-
-        System.out.println(hello); // 显示调用结果
-
-        RpcContext.getContext().setAttachment("userId", "321");
-        hello = demoService.sayHello("world", new Date(System.currentTimeMillis() - 1000000)); // 执行远程方法
+        String hello = demoService.sayHello("world"); // 执行远程方法
 
         System.out.println(hello); // 显示调用结果
     }
