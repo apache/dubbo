2c325b26389de18ef095bdc14eb5cb644f541c19
stevenys <33243764+stevenys@users.noreply.github.com>
2017-11-23 17:48:28 +0800

    保留hash ring的本意，lastEntry()回退为firstEntry()

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
@@ -102,7 +102,7 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
         private Invoker<T> selectForKey(long hash) {
             Map.Entry<Long, Invoker<T>> entry = virtualInvokers.tailMap(hash, true).firstEntry();
         	if (entry == null) {
-        		entry = virtualInvokers.lastEntry();
+        		entry = virtualInvokers.firstEntry();
         	}
         	return entry.getValue();
         }
