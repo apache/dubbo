1b0a30f3ccd93e5e701635894e2fe81b7bb8522b
plan454 <xia_harry@163.com>
2017-11-30 20:36:01 +0800

    Merge pull request #947 from plan454:master
    DemoService got the ipv6 ip when running under some network stack.

--- dubbo-demo/dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
+++ dubbo-demo/dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Provider {
 
     public static void main(String[] args) throws Exception {
+        System.setProperty("java.net.preferIPv4Stack", "true"); //防止无线网络自动返回IPv6地址
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-provider.xml"});
         context.start();
 
