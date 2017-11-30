6b2fa866a40d422058c72cea5c315806a27ff1f3
Mercy <mercyblitz@gmail.com>
2017-11-30 15:04:43 +0800

    2.5.8 (#979)
    * Update version to 2.5.8
    
    * Update version to 2.5.8
    
    * Add compatibility for Spring 3.2.x to resolve #17
    
    * Add compatibility for Spring 3.2.x to resolve #17
    
    * Add compatibility for Spring 3.2.x to resolve #17
    
    * Add final to BEAN_NAME field
    
    * Fixed #883
    
    * Add @EnableDubboConfigBinding to fix #12

--- dubbo-config/dubbo-config-spring/src/test/java/com/alibaba/dubbo/config/spring/context/annotation/consumer/ConsumerConfiguration.java
+++ dubbo-config/dubbo-config-spring/src/test/java/com/alibaba/dubbo/config/spring/context/annotation/consumer/ConsumerConfiguration.java
@@ -4,8 +4,10 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.config.spring.api.DemoService;
 import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author ken.lj
@@ -16,6 +18,7 @@ import org.springframework.context.annotation.ImportResource;
         basePackageClasses = ConsumerConfiguration.class
 )
 @ImportResource("META-INF/spring/dubbo-annotation-consumer.xml")
+@PropertySource("META-INF/default.properties")
 public class ConsumerConfiguration {
 
     @Reference(version = "2.5.7", url = "dubbo://127.0.0.1:12345")
@@ -28,4 +31,54 @@ public class ConsumerConfiguration {
     public void setDemoService(DemoService demoService) {
         this.demoService = demoService;
     }
+
+
+    @Bean
+    public Child c() {
+        return new Child();
+    }
+
+    public static abstract class Ancestor {
+
+        @Reference(version = "2.5.7", url = "dubbo://127.0.0.1:12345")
+        private DemoService demoServiceFromAncestor;
+
+        public DemoService getDemoServiceFromAncestor() {
+            return demoServiceFromAncestor;
+        }
+
+        public void setDemoServiceFromAncestor(DemoService demoServiceFromAncestor) {
+            this.demoServiceFromAncestor = demoServiceFromAncestor;
+        }
+    }
+
+    public static abstract class Parent extends Ancestor {
+
+        private DemoService demoServiceFromParent;
+
+        public DemoService getDemoServiceFromParent() {
+            return demoServiceFromParent;
+        }
+
+        @Reference(version = "2.5.7", url = "dubbo://127.0.0.1:12345")
+        public void setDemoServiceFromParent(DemoService demoServiceFromParent) {
+            this.demoServiceFromParent = demoServiceFromParent;
+        }
+
+    }
+
+    public static class Child extends Parent {
+
+        @Reference(version = "2.5.7", url = "dubbo://127.0.0.1:12345")
+        private DemoService demoServiceFromChild;
+
+        public DemoService getDemoServiceFromChild() {
+            return demoServiceFromChild;
+        }
+
+        public void setDemoServiceFromChild(DemoService demoServiceFromChild) {
+            this.demoServiceFromChild = demoServiceFromChild;
+        }
+    }
+
 }
