c501f6eb7ef82a051a87cc994ba021cbc08a5748
mercyblitz <mercyblitz@gmail.com>
2017-12-12 11:12:44 +0800

    Update Testcases using Annotation as configuration

--- dubbo-config/dubbo-config-spring/src/test/java/com/alibaba/dubbo/config/spring/context/annotation/consumer/ConsumerConfiguration.java
+++ dubbo-config/dubbo-config-spring/src/test/java/com/alibaba/dubbo/config/spring/context/annotation/consumer/ConsumerConfiguration.java
@@ -1,5 +1,7 @@
 package com.alibaba.dubbo.config.spring.context.annotation.consumer;
 
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.config.spring.api.DemoService;
 import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
@@ -10,15 +12,45 @@ import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author ken.lj
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @date 2017/11/3
  */
 @Configuration("consumerConfiguration")
 @DubboComponentScan(
         basePackageClasses = ConsumerConfiguration.class
 )
-@ImportResource("META-INF/spring/dubbo-annotation-consumer.xml")
 @PropertySource("META-INF/default.properties")
 public class ConsumerConfiguration {
+
+    /**
+     * 当前应用配置，替代 XML 方式配置：
+     * <prev>
+     * &lt;dubbo:application name="dubbo-annotation-consumer"/&gt;
+     * </prev>
+     *
+     * @return {@link ApplicationConfig} Bean
+     */
+    @Bean("dubbo-annotation-consumer")
+    public ApplicationConfig applicationConfig() {
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        applicationConfig.setName("dubbo-annotation-consumer");
+        return applicationConfig;
+    }
+
+    /**
+     * 当前连接注册中心配置，替代 XML 方式配置：
+     * <prev>
+     * &lt;dubbo:registry address="N/A"/&gt;
+     * </prev>
+     *
+     * @return {@link RegistryConfig} Bean
+     */
+    @Bean
+    public RegistryConfig registryConfig() {
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setAddress("N/A");
+        return registryConfig;
+    }
 
     @Reference(version = "2.5.7", url = "dubbo://127.0.0.1:12345")
     private DemoService demoService;
