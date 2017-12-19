720a3afe31d883c16778b910ece6b02da3c5215a
Ian Luo <ian.luo@gmail.com>
2017-12-19 17:03:56 +0800

    #1056: I18N effort for dubbo code base: dubbo-common

--- dubbo-common/src/test/java/com/alibaba/dubbo/common/store/support/SimpleDataStoreTest.java
+++ dubbo-common/src/test/java/com/alibaba/dubbo/common/store/support/SimpleDataStoreTest.java
@@ -1,17 +1,18 @@
 /*
- * Copyright 1999-2011 Alibaba Group.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.alibaba.dubbo.common.store.support;
 
@@ -20,9 +21,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-/**
- * @author <a href="mailto:ding.lid@alibaba-inc.com">ding.lid</a>
- */
 public class SimpleDataStoreTest {
     SimpleDataStore dataStore = new SimpleDataStore();
 
