a62421b5c9a2fd3162fd6de2267c7e1f5d36e8aa
Ian Luo <ian.luo@gmail.com>
2017-12-20 15:14:25 +0800

    #1067: I18N effort for dubbo code base - dubbo-rpc (part4)

--- dubbo-rpc/dubbo-rpc-api/src/test/java/com/alibaba/dubbo/rpc/support/BlockMyInvoker.java
+++ dubbo-rpc/dubbo-rpc-api/src/test/java/com/alibaba/dubbo/rpc/support/BlockMyInvoker.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.dubbo.rpc.support;
 
 import com.alibaba.dubbo.common.URL;
@@ -6,10 +22,6 @@ import com.alibaba.dubbo.rpc.Result;
 import com.alibaba.dubbo.rpc.RpcException;
 import com.alibaba.dubbo.rpc.RpcResult;
 
-/**
- * @author ken.lj
- * @date 2017/10/24
- */
 public class BlockMyInvoker<T> extends MyInvoker<T> {
 
     private long blockTime = 100;
