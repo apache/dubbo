bc1f4ebe3c68fc050145f1555146dad8c2d4636c
WangXin <lovepoem@hotmail.com>
2017-12-19 21:43:18 -0600

    #1055 Translate the comments of dubbo-remoting module (#1065)

--- dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/buffer/HeapChannelBufferFactory.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/buffer/HeapChannelBufferFactory.java
@@ -1,11 +1,12 @@
 /*
- * Copyright 1999-2012 Alibaba Group.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,7 +20,7 @@ package com.alibaba.dubbo.remoting.buffer;
 import java.nio.ByteBuffer;
 
 /**
- * @author <a href="mailto:gang.lvg@alibaba-inc.com">kimi</a>
+ *
  */
 public class HeapChannelBufferFactory implements ChannelBufferFactory {
 
--- dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/exchange/Response.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/exchange/Response.java
@@ -1,12 +1,13 @@
 /*
- * Copyright 1999-2011 Alibaba Group.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *  
- *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,9 +18,6 @@ package com.alibaba.dubbo.remoting.exchange;
 
 /**
  * Response
- *
- * @author qian.lei
- * @author william.liangf
  */
 public class Response {
 
@@ -70,12 +68,12 @@ public class Response {
     /**
      * internal server error.
      */
-    public static final byte CLIENT_ERROR      = 90;
-    
+    public static final byte CLIENT_ERROR = 90;
+
     /**
      * server side threadpool exhausted and quick return.
      */
-    public static final byte SERVER_THREADPOOL_EXHAUSTED_ERROR      = 100;
+    public static final byte SERVER_THREADPOOL_EXHAUSTED_ERROR = 100;
 
     private long mId = 0;
 
