1094a2c195ff1b24ac55dd13eb794659f160b761
Ian Luo <ian.luo@gmail.com>
2017-12-22 15:45:53 +0800

    #1087: I18N effort for dubbo code base - dubbo-config(part2)

--- dubbo-config/dubbo-config-spring/src/test/java/com/alibaba/dubbo/config/spring/context/annotation/consumer/ConsumerConfiguration.java
+++ dubbo-config/dubbo-config-spring/src/test/java/com/alibaba/dubbo/config/spring/context/annotation/consumer/ConsumerConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.dubbo.config.spring.context.annotation.consumer;
 
 import com.alibaba.dubbo.config.ApplicationConfig;
@@ -10,11 +26,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
-/**
- * @author ken.lj
- * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
- * @date 2017/11/3
- */
 @Configuration("consumerConfiguration")
 @DubboComponentScan(
         basePackageClasses = ConsumerConfiguration.class
@@ -23,7 +34,7 @@ import org.springframework.context.annotation.PropertySource;
 public class ConsumerConfiguration {
 
     /**
-     * 当前应用配置，替代 XML 方式配置：
+     * Current application configuration, to replace XML config:
      * <prev>
      * &lt;dubbo:application name="dubbo-annotation-consumer"/&gt;
      * </prev>
@@ -38,7 +49,7 @@ public class ConsumerConfiguration {
     }
 
     /**
-     * 当前连接注册中心配置，替代 XML 方式配置：
+     * Current registry center configuration, to replace XML config:
      * <prev>
      * &lt;dubbo:registry address="N/A"/&gt;
      * </prev>
