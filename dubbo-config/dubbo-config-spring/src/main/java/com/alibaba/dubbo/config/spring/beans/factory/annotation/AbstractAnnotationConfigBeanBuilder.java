6a83ab5041772887a970997e1b11d1d1445a3249
Ian Luo <ian.luo@gmail.com>
2017-12-22 15:45:53 +0800

    #1087: I18N effort for dubbo code base - dubbo-config(part2)

--- dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/beans/factory/annotation/AbstractAnnotationConfigBeanBuilder.java
+++ dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/beans/factory/annotation/AbstractAnnotationConfigBeanBuilder.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.dubbo.config.spring.beans.factory.annotation;
 
 import com.alibaba.dubbo.config.*;
@@ -14,8 +30,6 @@ import static com.alibaba.dubbo.config.spring.util.BeanFactoryUtils.getOptionalB
 
 /**
  * Abstract Configurable {@link Annotation} Bean Builder
- *
- * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 2.5.7
  */
 abstract class AbstractAnnotationConfigBeanBuilder<A extends Annotation, B extends AbstractInterfaceConfig> {
--- dubbo-config/dubbo-config-spring/src/test/java/com/alibaba/dubbo/config/spring/context/annotation/provider/ProviderConfiguration.java
+++ dubbo-config/dubbo-config-spring/src/test/java/com/alibaba/dubbo/config/spring/context/annotation/provider/ProviderConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.dubbo.config.spring.context.annotation.provider;
 
 import com.alibaba.dubbo.config.ApplicationConfig;
@@ -16,18 +32,13 @@ import org.springframework.transaction.TransactionException;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-/**
- * @author ken.lj
- * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
- * @date 2017/11/3
- */
 @DubboComponentScan(basePackages = "com.alibaba.dubbo.config.spring.context.annotation.provider")
 @PropertySource("META-INF/default.properties")
 @EnableTransactionManagement
 public class ProviderConfiguration {
 
     /**
-     * 当前应用配置，替代 XML 方式配置：
+     * Current application configuration, to replace XML config:
      * <prev>
      * &lt;dubbo:application name="dubbo-annotation-provider"/&gt;
      * </prev>
@@ -42,7 +53,7 @@ public class ProviderConfiguration {
     }
 
     /**
-     * 当前连接注册中心配置，替代 XML 方式配置：
+     * Current registry center configuration, to replace XML config:
      * <prev>
      * &lt;dubbo:registry id="my-registry" address="N/A"/&gt;
      * </prev>
@@ -57,7 +68,7 @@ public class ProviderConfiguration {
     }
 
     /**
-     * 当前连接注册中心配置，替代 XML 方式配置：
+     * Current protocol configuration, to replace XML config:
      * <prev>
      * &lt;dubbo:protocol name="dubbo" port="12345"/&gt;
      * </prev>
