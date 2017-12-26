139a8586ea586b2e69023c415a8dbcb44e1b8af7
mercyblitz <mercyblitz@gmail.com>
2017-12-26 20:40:45 +0800

    DubboConfigConfiguration's Single and Multiple to be public classes, alibaba/dubbo#1122

--- dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/context/annotation/DubboConfigConfiguration.java
+++ dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/context/annotation/DubboConfigConfiguration.java
@@ -48,7 +48,7 @@ public class DubboConfigConfiguration {
             @EnableDubboConfigBinding(prefix = "dubbo.provider", type = ProviderConfig.class),
             @EnableDubboConfigBinding(prefix = "dubbo.consumer", type = ConsumerConfig.class)
     })
-    static class Single {
+    public static class Single {
 
     }
 
@@ -64,7 +64,7 @@ public class DubboConfigConfiguration {
             @EnableDubboConfigBinding(prefix = "dubbo.provider", type = ProviderConfig.class, multiple = true),
             @EnableDubboConfigBinding(prefix = "dubbo.consumer", type = ConsumerConfig.class, multiple = true)
     })
-    static class Multiple {
+    public static class Multiple {
 
     }
 }
