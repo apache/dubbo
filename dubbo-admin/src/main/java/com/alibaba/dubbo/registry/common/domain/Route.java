dca5adbe6eb4b025ef7815cc18098c7c2a0fb268
ken.lj <ken.lj.hz@gmail.com>
2017-12-26 23:46:42 +0800

    I18N effort for dubbo-admin, #1124

--- dubbo-admin/src/main/java/com/alibaba/dubbo/registry/common/domain/Route.java
+++ dubbo-admin/src/main/java/com/alibaba/dubbo/registry/common/domain/Route.java
@@ -1,17 +1,18 @@
-/**
- * Project: dubbo.registry-1.1.0-SNAPSHOT
- * <p>
- * File Created at 2010-4-15
- * $Id: Route.java 184666 2012-07-05 11:13:17Z tony.chenl $
- * <p>
- * Copyright 2008 Alibaba.com Croporation Limited.
- * All rights reserved.
- * <p>
- * This software is the confidential and proprietary information of
- * Alibaba Company. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Alibaba.com.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.alibaba.dubbo.registry.common.domain;
 
@@ -23,7 +24,6 @@ import java.util.List;
 /**
  * Route
  *
- * @author william.liangf
  */
 public class Route extends Entity {
 
@@ -45,7 +45,7 @@ public class Route extends Entity {
     public static final String KEY_PROVIDER_HOST = "provider.host";
     public static final String KEY_PROVIDER_PORT = "provider.port";
     private static final long serialVersionUID = -7630589008164140656L;
-    private long parentId; //默认为0
+    private long parentId; //default 0
 
     private String name;
 
