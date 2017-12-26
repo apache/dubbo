adfbff386e166c308eae9e089b3d1aff05eed703
mercyblitz <mercyblitz@gmail.com>
2017-12-26 23:50:03 +0800

    To resolve alibaba/dubbo #1121

--- dubbo-config/dubbo-config-spring/src/test/java/com/alibaba/dubbo/config/spring/beans/factory/annotation/ReferenceAnnotationBeanPostProcessorTest.java
+++ dubbo-config/dubbo-config-spring/src/test/java/com/alibaba/dubbo/config/spring/beans/factory/annotation/ReferenceAnnotationBeanPostProcessorTest.java
@@ -17,15 +17,21 @@
 package com.alibaba.dubbo.config.spring.beans.factory.annotation;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.config.spring.ReferenceBean;
 import com.alibaba.dubbo.config.spring.api.DemoService;
 import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Collection;
+
+import static com.alibaba.dubbo.config.spring.beans.factory.annotation.ReferenceAnnotationBeanPostProcessor.BEAN_NAME;
 
 /**
  * {@link ReferenceAnnotationBeanPostProcessor} Test
@@ -36,17 +42,16 @@ public class ReferenceAnnotationBeanPostProcessorTest {
 
     private static final String PROVIDER_LOCATION = "META-INF/spring/dubbo-provider.xml";
 
+    @Before
+    public void before() {
+        // Starts Provider
+        new ClassPathXmlApplicationContext(PROVIDER_LOCATION);
+    }
+
     @Test
     public void test() throws Exception {
 
-
-        // Starts Provider
-        new ClassPathXmlApplicationContext(PROVIDER_LOCATION);
-
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(TestBean.class);
-
-        context.refresh();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestBean.class);
 
         TestBean testBean = context.getBean(TestBean.class);
 
@@ -62,6 +67,31 @@ public class ReferenceAnnotationBeanPostProcessorTest {
         Assert.assertEquals("annotation:Mercy", demoService.sayName("Mercy"));
 
         context.close();
+
+    }
+
+    /**
+     * Test on {@link ReferenceAnnotationBeanPostProcessor#getReferenceBeans()}
+     */
+    @Test
+    public void testGetReferenceBeans() {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestBean.class);
+
+        ReferenceAnnotationBeanPostProcessor beanPostProcessor = context.getBean(BEAN_NAME,
+                ReferenceAnnotationBeanPostProcessor.class);
+
+        Collection<ReferenceBean<?>> referenceBeans = beanPostProcessor.getReferenceBeans();
+
+        Assert.assertEquals(1, referenceBeans.size());
+
+        ReferenceBean<?> referenceBean = referenceBeans.iterator().next();
+
+        TestBean testBean = context.getBean(TestBean.class);
+
+        Assert.assertEquals(referenceBean.get(), testBean.getDemoServiceFromAncestor());
+        Assert.assertEquals(referenceBean.get(), testBean.getDemoServiceFromParent());
+        Assert.assertEquals(referenceBean.get(), testBean.getDemoService());
 
     }
 
