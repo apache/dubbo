985fc99a9db56ace287993c7c281479eb90a0161
qinliujie <liujie.qlj@alibaba-inc.com>
2018-01-09 19:21:18 +0800

    bugfix:fix redis and multicast offline fail

--- dubbo-registry/dubbo-registry-multicast/src/main/java/com/alibaba/dubbo/registry/multicast/MulticastRegistry.java
+++ dubbo-registry/dubbo-registry-multicast/src/main/java/com/alibaba/dubbo/registry/multicast/MulticastRegistry.java
@@ -338,6 +338,13 @@ public class MulticastRegistry extends FailbackRegistry {
                 if (urls != null) {
                     urls.remove(url);
                 }
+                if (urls == null || urls.isEmpty()){
+                    if (urls == null){
+                        urls = new ConcurrentHashSet<URL>();
+                    }
+                    URL empty = url.setProtocol(Constants.EMPTY_PROTOCOL);
+                    urls.add(empty);
+                }
                 List<URL> list = toList(urls);
                 for (NotifyListener listener : entry.getValue()) {
                     notify(key, listener, list);
