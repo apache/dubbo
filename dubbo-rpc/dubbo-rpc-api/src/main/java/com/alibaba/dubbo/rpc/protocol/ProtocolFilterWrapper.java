b4961fd61dc44723ca847b19b19a850131b379ac
rushsky518 <973416205@qq.com>
2018-02-03 08:02:28 +0000

    Update ProtocolFilterWrapper.java
    对 filters.size()>0 的判断是冗余的。

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/protocol/ProtocolFilterWrapper.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/protocol/ProtocolFilterWrapper.java
@@ -46,38 +46,37 @@ public class ProtocolFilterWrapper implements Protocol {
     private static <T> Invoker<T> buildInvokerChain(final Invoker<T> invoker, String key, String group) {
         Invoker<T> last = invoker;
         List<Filter> filters = ExtensionLoader.getExtensionLoader(Filter.class).getActivateExtension(invoker.getUrl(), key, group);
-        if (filters.size() > 0) {
-            for (int i = filters.size() - 1; i >= 0; i--) {
-                final Filter filter = filters.get(i);
-                final Invoker<T> next = last;
-                last = new Invoker<T>() {
+            
+        for (int i = filters.size() - 1; i >= 0; i--) {
+            final Filter filter = filters.get(i);
+            final Invoker<T> next = last;
+            last = new Invoker<T>() {
 
-                    public Class<T> getInterface() {
-                        return invoker.getInterface();
-                    }
+                public Class<T> getInterface() {
+                    return invoker.getInterface();
+                }
 
-                    public URL getUrl() {
-                        return invoker.getUrl();
-                    }
+                public URL getUrl() {
+                    return invoker.getUrl();
+                }
 
-                    public boolean isAvailable() {
-                        return invoker.isAvailable();
-                    }
+                public boolean isAvailable() {
+                    return invoker.isAvailable();
+                }
 
-                    public Result invoke(Invocation invocation) throws RpcException {
-                        return filter.invoke(next, invocation);
-                    }
+                public Result invoke(Invocation invocation) throws RpcException {
+                    return filter.invoke(next, invocation);
+                }
 
-                    public void destroy() {
-                        invoker.destroy();
-                    }
+                public void destroy() {
+                    invoker.destroy();
+                }
 
-                    @Override
-                    public String toString() {
-                        return invoker.toString();
-                    }
-                };
-            }
+                @Override
+                public String toString() {
+                    return invoker.toString();
+                }
+            };
         }
         return last;
     }
