f7572ab21134f133067f0e31278f3b50f7b919d1
杨浩 <takeseem@gmail.com>
2018-03-09 10:46:57 +0800

    Fix #1411 Java Locale use '_' split language, country, variant.

--- hessian-lite/src/test/java/com/alibaba/com/caucho/hessian/io/LocaleSerializerTest.java
+++ hessian-lite/src/test/java/com/alibaba/com/caucho/hessian/io/LocaleSerializerTest.java
@@ -1,41 +1,29 @@
 package com.alibaba.com.caucho.hessian.io;
 
-import com.alibaba.com.caucho.hessian.io.base.SerializeTestBase;
-import junit.framework.TestCase;
-import org.junit.Test;
-
+import java.io.IOException;
 import java.util.Locale;
 
+import org.junit.Test;
+
+import com.alibaba.com.caucho.hessian.io.base.SerializeTestBase;
+
+import junit.framework.TestCase;
+
 public class LocaleSerializerTest extends SerializeTestBase {
+
+    /** {@linkplain LocaleSerializer#writeObject(Object, AbstractHessianOutput)} */
     @Test
-    public void hessian2() throws Exception {
-        Locale locale = new Locale("zh");
-        Locale result = baseHession2Serialize(locale);
-        TestCase.assertEquals(locale, result);
-        locale = new Locale("zh", "CN");
-        result = baseHession2Serialize(locale);
-        TestCase.assertEquals(locale, result);
-        locale = new Locale("zh", "CN", "GBK");
-        result = baseHession2Serialize(locale);
-        TestCase.assertEquals(locale, result);
-        locale = new Locale("zh-hant", "CN");
-        result = baseHession2Serialize(locale);
-        TestCase.assertEquals(locale, result);
+    public void locale() throws IOException {
+        assertLocale(null);
+        assertLocale(new Locale(""));
+        assertLocale(new Locale("zh"));
+        assertLocale(new Locale("zh", "CN"));
+        assertLocale(new Locale("zh-hant", "CN"));
+        assertLocale(new Locale("zh-hant", "CN", "GBK"));
     }
 
-    @Test
-    public void hessian1() throws Exception {
-        Locale locale = new Locale("zh");
-        Locale result = baseHessionSerialize(locale);
-        TestCase.assertEquals(locale, result);
-        locale = new Locale("zh", "CN");
-        result = baseHessionSerialize(locale);
-        TestCase.assertEquals(locale, result);
-        locale = new Locale("zh", "CN", "GBK");
-        result = baseHessionSerialize(locale);
-        TestCase.assertEquals(locale, result);
-        locale = new Locale("zh-hant", "CN");
-        result = baseHessionSerialize(locale);
-        TestCase.assertEquals(locale, result);
+    private void assertLocale(Locale locale) throws IOException {
+        TestCase.assertEquals(locale, baseHession2Serialize(locale));
+        TestCase.assertEquals(locale, baseHessionSerialize(locale));
     }
 }
