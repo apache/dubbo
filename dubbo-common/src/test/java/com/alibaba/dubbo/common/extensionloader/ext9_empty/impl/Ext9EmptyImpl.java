c0424aa77716ac73e267da940191f1b0ee54873f
ken.lj <ken.lj.hz@gmail.com>
2018-03-09 16:06:13 +0800

    Add unit test for ExtensionLoader

---
+++ dubbo-common/src/test/java/com/alibaba/dubbo/common/extensionloader/ext9_empty/impl/Ext9EmptyImpl.java
@@ -0,0 +1,10 @@
+package com.alibaba.dubbo.common.extensionloader.ext9_empty.impl;
+
+import com.alibaba.dubbo.common.extensionloader.ext9_empty.Ext9Empty;
+
+public class Ext9EmptyImpl implements Ext9Empty {
+    @Override
+    public void empty() {
+
+    }
+}
