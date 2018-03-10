377853e0486dc10c1350396212f6520d8c11f175
qct <quchentao@163.com>
2018-03-10 08:14:10 +0000

    fixes #1089, make ExecutionDispatcher meets dubbo-user-book

--- dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/transport/dispatcher/execution/ExecutionChannelHandler.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/transport/dispatcher/execution/ExecutionChannelHandler.java
@@ -36,41 +36,29 @@ public class ExecutionChannelHandler extends WrappedChannelHandler {
     }
 
     @Override
-    public void connected(Channel channel) throws RemotingException {
-        executor.execute(new ChannelEventRunnable(channel, handler, ChannelState.CONNECTED));
-    }
-
-    @Override
-    public void disconnected(Channel channel) throws RemotingException {
-        executor.execute(new ChannelEventRunnable(channel, handler, ChannelState.DISCONNECTED));
-    }
-
-    @Override
     public void received(Channel channel, Object message) throws RemotingException {
-    	try {
-            executor.execute(new ChannelEventRunnable(channel, handler, ChannelState.RECEIVED, message));
-        } catch (Throwable t) {
-            //TODO A temporary solution to the problem that the exception information can not be sent to the opposite end after the thread pool is full. Need a refactoring
-            //fix The thread pool is full, refuses to call, does not return, and causes the consumer to wait for time out
-        	if(message instanceof Request &&
-        			t instanceof RejectedExecutionException){
-        		Request request = (Request)message;
-        		if(request.isTwoWay()){
-        			String msg = "Server side("+url.getIp()+","+url.getPort()+") threadpool is exhausted ,detail msg:"+t.getMessage();
-        			Response response = new Response(request.getId(), request.getVersion());
-        			response.setStatus(Response.SERVER_THREADPOOL_EXHAUSTED_ERROR);
-        			response.setErrorMessage(msg);
-        			channel.send(response);
-        			return;
-        		}
-        	}
-            throw new ExecutionException(message, channel, getClass() + " error when process received event .", t);
+        if (message instanceof Request && !((Request) message).isHeartbeat()) {
+            try {
+                executor.execute(new ChannelEventRunnable(channel, handler, ChannelState.RECEIVED, message));
+            } catch (Throwable t) {
+                //TODO A temporary solution to the problem that the exception information can not be sent to the opposite end after the thread pool is full. Need a refactoring
+                //fix The thread pool is full, refuses to call, does not return, and causes the consumer to wait for time out
+                if (t instanceof RejectedExecutionException) {
+                    Request request = (Request) message;
+                    if (request.isTwoWay()) {
+                        String msg = "Server side(" + url.getIp() + "," + url.getPort()
+                            + ") threadpool is exhausted ,detail msg:" + t.getMessage();
+                        Response response = new Response(request.getId(), request.getVersion());
+                        response.setStatus(Response.SERVER_THREADPOOL_EXHAUSTED_ERROR);
+                        response.setErrorMessage(msg);
+                        channel.send(response);
+                        return;
+                    }
+                }
+                throw new ExecutionException(message, channel, getClass() + " error when process received event .", t);
+            }
+        } else {
+            handler.received(channel, message);
         }
     }
-
-    @Override
-    public void caught(Channel channel, Throwable exception) throws RemotingException {
-        executor.execute(new ChannelEventRunnable(channel, handler, ChannelState.CAUGHT, exception));
-    }
-
 }
