730bda79b8a36bb43d7bcccda0a82b2b17005d9f
YunaiV <>
2018-03-27 19:31:03 +0800

    ThreadPool

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/threadpool/ThreadPool.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/threadpool/ThreadPool.java
@@ -25,6 +25,8 @@ import java.util.concurrent.Executor;
 
 /**
  * ThreadPool
+ *
+ * 线程池接口
  */
 @SPI("fixed")
 public interface ThreadPool {
--- dubbo-common/src/main/java/com/alibaba/dubbo/common/threadpool/support/fixed/FixedThreadPool.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/threadpool/support/fixed/FixedThreadPool.java
@@ -31,14 +31,21 @@ import java.util.concurrent.TimeUnit;
 /**
  * Creates a thread pool that reuses a fixed number of threads
  *
+ * 固定大小线程池，启动时建立线程，不关闭，一直持有。(缺省)
+ *
  * @see java.util.concurrent.Executors#newFixedThreadPool(int)
  */
 public class FixedThreadPool implements ThreadPool {
 
+    @Override
     public Executor getExecutor(URL url) {
+        // 线程名
         String name = url.getParameter(Constants.THREAD_NAME_KEY, Constants.DEFAULT_THREAD_NAME);
+        // 线程数
         int threads = url.getParameter(Constants.THREADS_KEY, Constants.DEFAULT_THREADS);
+        // 队列数
         int queues = url.getParameter(Constants.QUEUES_KEY, Constants.DEFAULT_QUEUES);
+        // 创建执行器
         return new ThreadPoolExecutor(threads, threads, 0, TimeUnit.MILLISECONDS,
                 queues == 0 ? new SynchronousQueue<Runnable>() :
                         (queues < 0 ? new LinkedBlockingQueue<Runnable>()
