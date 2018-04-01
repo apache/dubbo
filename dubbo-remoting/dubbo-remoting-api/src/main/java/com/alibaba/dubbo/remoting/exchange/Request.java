9d1c8752d8cd0dd152ca91dd6c49af2944f31278
YunaiV <>
2018-04-01 15:35:03 +0800

    美滋滋 标记这个类，看完了

--- dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/exchange/Request.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/exchange/Request.java
@@ -22,25 +22,53 @@ import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Request.
+ *
+ * 请求
  */
 public class Request {
 
+    /**
+     * 事件 - 心跳
+     */
     public static final String HEARTBEAT_EVENT = null;
-
+    /**
+     * 事件 - 只读
+     */
     public static final String READONLY_EVENT = "R";
 
+    /**
+     * 请求编号自增序列
+     */
     private static final AtomicLong INVOKE_ID = new AtomicLong(0);
 
+    /**
+     * 请求编号
+     */
     private final long mId;
-
+    /**
+     * Dubbo 版本
+     */
     private String mVersion;
-
+    /**
+     * 是否需要响应
+     *
+     * true-需要
+     * false-不需要
+     */
     private boolean mTwoWay = true;
-
+    /**
+     * 是否是事件。例如，心跳事件。
+     */
     private boolean mEvent = false;
-
+    /**
+     * 是否异常的请求。
+     *
+     * 在消息解析的时候，会出现。
+     */
     private boolean mBroken = false;
-
+    /**
+     * 数据
+     */
     private Object mData;
 
     public Request() {
--- dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/exchange/Response.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/exchange/Response.java
@@ -18,11 +18,18 @@ package com.alibaba.dubbo.remoting.exchange;
 
 /**
  * Response
+ *
+ * 响应
  */
 public class Response {
 
+    /**
+     * 事件 - 心跳
+     */
     public static final String HEARTBEAT_EVENT = null;
-
+    /**
+     * 事件 - 只读
+     */
     public static final String READONLY_EVENT = "R";
 
     /**
@@ -75,16 +82,31 @@ public class Response {
      */
     public static final byte SERVER_THREADPOOL_EXHAUSTED_ERROR = 100;
 
+    /**
+     * 响应编号
+     *
+     * 一个 {@link Request#mId} 和 {@link Response#mId} 一一对应。
+     */
     private long mId = 0;
-
+    /**
+     * 版本
+     */
     private String mVersion;
-
+    /**
+     * 状态
+     */
     private byte mStatus = OK;
-
+    /**
+     * 是否事件
+     */
     private boolean mEvent = false;
-
+    /**
+     * 错误消息
+     */
     private String mErrorMsg;
-
+    /**
+     * 结果
+     */
     private Object mResult;
 
     public Response() {
