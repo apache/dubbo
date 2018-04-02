369ab096053b3a151798bd21ceab74992b4e5c47
YunaiV <>
2018-04-03 01:05:17 +0800

    2018_12_16_精尽 Dubbo 源码解析 —— NIO 服务器（六）之 Netty4 实现.md

--- dubbo-remoting/dubbo-remoting-netty4/src/main/java/com/alibaba/dubbo/remoting/transport/netty4/NettyTransporter.java
+++ dubbo-remoting/dubbo-remoting-netty4/src/main/java/com/alibaba/dubbo/remoting/transport/netty4/NettyTransporter.java
@@ -23,8 +23,14 @@ import com.alibaba.dubbo.remoting.RemotingException;
 import com.alibaba.dubbo.remoting.Server;
 import com.alibaba.dubbo.remoting.Transporter;
 
+/**
+ * 基于 Netty4 的网络传输实现类
+ */
 public class NettyTransporter implements Transporter {
 
+    /**
+     * 拓展名
+     */
     public static final String NAME = "netty4";
 
     public Server bind(URL url, ChannelHandler listener) throws RemotingException {
