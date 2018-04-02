2c3ff37ff5a8342051636de6bba9ec10fd3aa124
YunaiV <>
2018-04-02 22:19:05 +0800

    美滋滋 标记这个类，看完了

--- dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/buffer/ChannelBufferFactory.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/buffer/ChannelBufferFactory.java
@@ -18,12 +18,15 @@ package com.alibaba.dubbo.remoting.buffer;
 
 import java.nio.ByteBuffer;
 
+/**
+ * 通道 Buffer 工厂接口
+ */
 public interface ChannelBufferFactory {
 
     ChannelBuffer getBuffer(int capacity);
 
     ChannelBuffer getBuffer(byte[] array, int offset, int length);
 
-    ChannelBuffer getBuffer(ByteBuffer nioBuffer);
+    ChannelBuffer getBuffer(ByteBuffer nioBuffer); // java.nio.ByteBuffer
 
 }
