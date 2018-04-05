c623429e1a99353cd1ea90f95677bc156c70896f
Huang YunKun <htynkn@gmail.com>
2018-04-05 11:51:44 +0800

    add unit test for netty4 remoting (#1509)

--- dubbo-remoting/dubbo-remoting-grizzly/src/main/test/com/alibaba/dubbo/remoting/transport/grizzly/GrizzlyTransporterTest.java
+++ dubbo-remoting/dubbo-remoting-grizzly/src/main/test/com/alibaba/dubbo/remoting/transport/grizzly/GrizzlyTransporterTest.java
@@ -14,41 +14,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.dubbo.remoting.http.tomcat;
+package com.alibaba.dubbo.remoting.transport.grizzly;
 
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.common.utils.NetUtils;
-import com.alibaba.dubbo.remoting.http.HttpHandler;
-import com.alibaba.dubbo.remoting.http.HttpServer;
-import org.apache.http.client.fluent.Request;
+import com.alibaba.dubbo.remoting.Server;
+import com.alibaba.dubbo.remoting.transport.ChannelHandlerAdapter;
 import org.junit.Test;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-
-public class TomcatHttpBinderTest {
+public class GrizzlyTransporterTest {
     @Test
-    public void shouldAbleHandleRequestForTomcatBinder() throws Exception {
+    public void shouldAbleToBindGrizzly() throws Exception {
         int port = NetUtils.getAvailablePort();
         URL url = new URL("http", "localhost", port,
                 new String[]{Constants.BIND_PORT_KEY, String.valueOf(port)});
 
-        HttpServer httpServer = new TomcatHttpBinder().bind(url, new HttpHandler() {
-            @Override
-            public void handle(HttpServletRequest request, HttpServletResponse response) throws IOException {
-                response.getWriter().write("Tomcat");
-            }
-        });
+        Server server = new GrizzlyTransporter().bind(url, new ChannelHandlerAdapter());
 
-        String response = Request.Get(url.toJavaURL().toURI()).execute().returnContent().asString();
-
-        assertThat(response, is("Tomcat"));
-
-        httpServer.close();
+        assertThat(server.isBound(), is(true));
     }
 }
