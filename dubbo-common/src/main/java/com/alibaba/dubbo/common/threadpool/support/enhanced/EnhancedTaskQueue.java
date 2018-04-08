9a57a8cc487c2d66527584132e1056bf96d50941
xiuyuhang <442367943@qq.com>
2018-04-08 17:20:01 +0800

    Extension: Enhanced Thread Pool A thread pool that can provide faster processing speeds when there are more tasks (of course it consumes more resources) * When the number of tasks exceeds the core size, a new thread is first started to execute the task instead of putting it into the queue. * When the number of tasks is lower than the core size for a long time, the core size threads are maintained and redundant threads are recycled. * Compared to the fixed pool：When there are more tasks, provide more workers to handle the tasks. * Compared to the cached pool：The task queue in the cached pool is actually a SynchronousQueue and does not have the ability to cache tasks. * Whether to fail fail or put into a queue when a thread runs out：Both are feasible and need to consider which way should be applied according to the business scenario. Delayed scenarios are not allowed. Failfast is more reasonable than queues. However, if there is a certain tolerance for delays, queues are more reasonable than failfast.

---
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/threadpool/support/enhanced/EnhancedTaskQueue.java
@@ -0,0 +1,56 @@
+package com.alibaba.dubbo.common.threadpool.support.enhanced;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+
+/**
+ * enhanced task queue in the enhanced thread pool
+ */
+public class EnhancedTaskQueue<R extends Runnable> extends LinkedBlockingQueue<Runnable> {
+
+    private static final long serialVersionUID = -2635853580887179627L;
+
+    private EnhancedThreadPoolExecutor executor;
+
+    public EnhancedTaskQueue(int capacity) {
+        super(capacity);
+    }
+
+    public void setExecutor(EnhancedThreadPoolExecutor exec) {
+        executor = exec;
+    }
+
+    @Override
+    public boolean offer(Runnable runnable) {
+        if (executor == null) {
+            throw new RejectedExecutionException("enhanced queue does not have executor !");
+        }
+        int currentPoolThreadSize = executor.getPoolSize();
+        //have free worker. put task into queue to let the worker deal with task.
+        if (executor.getSubmittedTaskCount() < currentPoolThreadSize) {
+            return super.offer(runnable);
+        }
+
+        // return false to let executor create new worker.
+        if (currentPoolThreadSize < executor.getMaximumPoolSize()) {
+            return false;
+        }
+
+        //currentPoolThreadSize >= max
+        return super.offer(runnable);
+    }
+
+    /**
+     * retry offer task
+     *
+     * @param o task
+     * @return offer success or not
+     * @throws RejectedExecutionException if executor is terminated.
+     */
+    public boolean retryOffer(Runnable o) {
+        if (executor.isShutdown()) {
+            throw new RejectedExecutionException("Executor is shutdown !");
+        }
+        return super.offer(o);
+    }
+}
