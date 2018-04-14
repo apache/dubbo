a00cc5ba9a2301704973ee5fcb2593b718e0375b
YunaiV <>
2018-04-14 12:30:01 +0800

    AccessLogFilter

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/AccessLogFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/AccessLogFilter.java
@@ -58,74 +58,132 @@ import java.util.concurrent.TimeUnit;
  *    &lt;appender-ref ref="foo" /&gt;
  * &lt;/logger&gt;
  * </pre></code>
+ *
+ */
+/**
+ * 记录 Service 的 Access Log。
+ *
+ * 记录服务的访问日志的过滤器实现类。
+ *
+ * <p>
+ * 使用的Logger key是<code><b>dubbo.accesslog</b></code>。
+ * 如果想要配置Access Log只出现在指定的Appender中，可以在Log4j中注意配置上additivity。配置示例:
+ * <code>
+ * <pre>
+ * &lt;logger name="<b>dubbo.accesslog</b>" <font color="red">additivity="false"</font>&gt;
+ *    &lt;level value="info" /&gt;
+ *    &lt;appender-ref ref="foo" /&gt;
+ * &lt;/logger&gt;
+ * </pre></code>
+ *
+ * @author ding.lid
  */
 @Activate(group = Constants.PROVIDER, value = Constants.ACCESS_LOG_KEY)
 public class AccessLogFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(AccessLogFilter.class);
 
+    /**
+     * 访问日志在 {@link LoggerFactory} 中的日志名
+     */
     private static final String ACCESS_LOG_KEY = "dubbo.accesslog";
 
+    /**
+     * 访问日志的文件后缀
+     */
     private static final String FILE_DATE_FORMAT = "yyyyMMdd";
-
+    /**
+     * 日历的时间格式化
+     */
     private static final String MESSAGE_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
-
+    /**
+     * 队列大小，即 {@link #logQueue} 值的大小
+     */
     private static final int LOG_MAX_BUFFER = 5000;
-
+    /**
+     * 日志输出频率，单位：毫秒。仅适用于 {@link #logFuture}
+     */
     private static final long LOG_OUTPUT_INTERVAL = 5000;
 
+    /**
+     * 日志队列
+     *
+     * key：访问日志名
+     * value：日志集合
+     */
     private final ConcurrentMap<String, Set<String>> logQueue = new ConcurrentHashMap<String, Set<String>>();
-
+    /**
+     * 定时任务线程池
+     */
     private final ScheduledExecutorService logScheduled = Executors.newScheduledThreadPool(2, new NamedThreadFactory("Dubbo-Access-Log", true));
-
+    /**
+     * 记录日志任务
+     */
     private volatile ScheduledFuture<?> logFuture = null;
 
+    /**
+     * 初始化任务
+     */
     private void init() {
         if (logFuture == null) {
             synchronized (logScheduled) {
-                if (logFuture == null) {
+                if (logFuture == null) { // 双重锁，避免重复初始化
                     logFuture = logScheduled.scheduleWithFixedDelay(new LogTask(), LOG_OUTPUT_INTERVAL, LOG_OUTPUT_INTERVAL, TimeUnit.MILLISECONDS);
                 }
             }
         }
     }
 
+    /**
+     * 添加日志内容到日志队列
+     *
+     * @param accesslog 日志文件
+     * @param logmessage 日志内容
+     */
     private void log(String accesslog, String logmessage) {
+        // 初始化
         init();
+        // 获得队列，以文件名为 Key
         Set<String> logSet = logQueue.get(accesslog);
         if (logSet == null) {
             logQueue.putIfAbsent(accesslog, new ConcurrentHashSet<String>());
             logSet = logQueue.get(accesslog);
         }
+        // 若未超过队列大小，添加到队列中
         if (logSet.size() < LOG_MAX_BUFFER) {
             logSet.add(logmessage);
         }
     }
 
+    @Override
     @SuppressWarnings("Duplicates")
     public Result invoke(Invoker<?> invoker, Invocation inv) throws RpcException {
         try {
+            // 记录访问日志的文件名
             String accesslog = invoker.getUrl().getParameter(Constants.ACCESS_LOG_KEY);
             if (ConfigUtils.isNotEmpty(accesslog)) {
+                // 服务的名字、版本、分组信息
                 RpcContext context = RpcContext.getContext();
                 String serviceName = invoker.getInterface().getName();
                 String version = invoker.getUrl().getParameter(Constants.VERSION_KEY);
                 String group = invoker.getUrl().getParameter(Constants.GROUP_KEY);
+                // 拼接日志内容
                 StringBuilder sn = new StringBuilder();
-                sn.append("[").append(new SimpleDateFormat(MESSAGE_DATE_FORMAT).format(new Date())).append("] ").append(context.getRemoteHost()).append(":").append(context.getRemotePort())
-                        .append(" -> ").append(context.getLocalHost()).append(":").append(context.getLocalPort())
+                sn.append("[").append(new SimpleDateFormat(MESSAGE_DATE_FORMAT).format(new Date())).append("] ") // 时间
+                        .append(context.getRemoteHost()).append(":").append(context.getRemotePort()) // 调用方地址
+                        .append(" -> ").append(context.getLocalHost()).append(":").append(context.getLocalPort()) // 本地地址
                         .append(" - ");
-                if (null != group && group.length() > 0) {
+                if (null != group && group.length() > 0) { // 分组
                     sn.append(group).append("/");
                 }
-                sn.append(serviceName);
-                if (null != version && version.length() > 0) {
+                sn.append(serviceName); // 服务名
+                if (null != version && version.length() > 0) { // 版本
                     sn.append(":").append(version);
                 }
                 sn.append(" ");
-                sn.append(inv.getMethodName());
+                sn.append(inv.getMethodName()); // 方法名
                 sn.append("(");
-                Class<?>[] types = inv.getParameterTypes();
+                Class<?>[] types = inv.getParameterTypes(); // 参数类型
                 if (types != null && types.length > 0) {
                     boolean first = true;
                     for (Class<?> type : types) {
@@ -138,13 +196,15 @@ public class AccessLogFilter implements Filter {
                     }
                 }
                 sn.append(") ");
-                Object[] args = inv.getArguments();
+                Object[] args = inv.getArguments(); // 参数值
                 if (args != null && args.length > 0) {
                     sn.append(JSON.toJSONString(args));
                 }
                 String msg = sn.toString();
+                // 【方式一】使用日志组件，例如 Log4j 等写
                 if (ConfigUtils.isDefault(accesslog)) {
                     LoggerFactory.getLogger(ACCESS_LOG_KEY + "." + invoker.getInterface().getName()).info(msg);
+                // 【方式二】异步输出到指定文件
                 } else {
                     log(accesslog, msg);
                 }
@@ -152,17 +212,24 @@ public class AccessLogFilter implements Filter {
         } catch (Throwable t) {
             logger.warn("Exception in AcessLogFilter of service(" + invoker + " -> " + inv + ")", t);
         }
+        // 服务调用
         return invoker.invoke(inv);
     }
 
+    /**
+     * 日志任务
+     */
     private class LogTask implements Runnable {
+
+        @Override
         public void run() {
             try {
-                if (logQueue != null && logQueue.size() > 0) {
+                if (logQueue.size() > 0) {
                     for (Map.Entry<String, Set<String>> entry : logQueue.entrySet()) {
                         try {
                             String accesslog = entry.getKey();
                             Set<String> logSet = entry.getValue();
+                            // 获得日志文件
                             File file = new File(accesslog);
                             File dir = file.getParentFile();
                             if (null != dir && !dir.exists()) {
@@ -171,25 +238,25 @@ public class AccessLogFilter implements Filter {
                             if (logger.isDebugEnabled()) {
                                 logger.debug("Append log to " + accesslog);
                             }
+                            // 归档历史日志文件，例如： `accesslog` => `access.20181023`
                             if (file.exists()) {
                                 String now = new SimpleDateFormat(FILE_DATE_FORMAT).format(new Date());
-                                String last = new SimpleDateFormat(FILE_DATE_FORMAT).format(new Date(file.lastModified()));
+                                String last = new SimpleDateFormat(FILE_DATE_FORMAT).format(new Date(file.lastModified())); // 最后修改时间
                                 if (!now.equals(last)) {
                                     File archive = new File(file.getAbsolutePath() + "." + last);
                                     file.renameTo(archive);
                                 }
                             }
+                            // 输出日志到指定文件
                             FileWriter writer = new FileWriter(file, true);
                             try {
-                                for (Iterator<String> iterator = logSet.iterator();
-                                     iterator.hasNext();
-                                     iterator.remove()) {
-                                    writer.write(iterator.next());
-                                    writer.write("\r\n");
+                                for (Iterator<String> iterator = logSet.iterator(); iterator.hasNext(); iterator.remove()) {
+                                    writer.write(iterator.next()); // 写入一行日志
+                                    writer.write("\r\n"); // 换行
                                 }
-                                writer.flush();
+                                writer.flush(); // 刷盘
                             } finally {
-                                writer.close();
+                                writer.close(); // 关闭
                             }
                         } catch (Exception e) {
                             logger.error(e.getMessage(), e);
@@ -200,6 +267,7 @@ public class AccessLogFilter implements Filter {
                 logger.error(e.getMessage(), e);
             }
         }
+
     }
 
 }
