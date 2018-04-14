089860feb5b850688bf9ca47abf46e0675a86a32
YunaiV <>
2018-04-14 22:51:19 +0800

    LimitFilter

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ActiveLimitFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ActiveLimitFilter.java
@@ -29,28 +29,37 @@ import com.alibaba.dubbo.rpc.RpcStatus;
 /**
  * LimitInvokerFilter
  *
+ * 每服务消费者每服务每方法最大并发调用数限制的过滤器实现类
  */
 @Activate(group = Constants.CONSUMER, value = Constants.ACTIVES_KEY)
 public class ActiveLimitFilter implements Filter {
 
+    @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         URL url = invoker.getUrl();
         String methodName = invocation.getMethodName();
+        // 获得服务提供者每服务每方法最大可并行执行请求数
         int max = invoker.getUrl().getMethodParameter(methodName, Constants.ACTIVES_KEY, 0);
+        // 获得 RpcStatus 对象，基于服务 URL + 方法维度
         RpcStatus count = RpcStatus.getStatus(invoker.getUrl(), invocation.getMethodName());
         if (max > 0) {
+            // 获得超时值
             long timeout = invoker.getUrl().getMethodParameter(invocation.getMethodName(), Constants.TIMEOUT_KEY, 0);
             long start = System.currentTimeMillis();
-            long remain = timeout;
+            long remain = timeout; // 剩余可等待时间
             int active = count.getActive();
+            // 超过最大可并行执行请求数，等待
             if (active >= max) {
-                synchronized (count) {
+                synchronized (count) { // 通过锁，有且仅有一个在等待。
+                    // 循环，等待可并行执行请求数
                     while ((active = count.getActive()) >= max) {
+                        // 等待，直到超时，或者被唤醒
                         try {
                             count.wait(remain);
                         } catch (InterruptedException e) {
                         }
-                        long elapsed = System.currentTimeMillis() - start;
+                        // 判断是否没有剩余时长了，抛出 RpcException 异常
+                        long elapsed = System.currentTimeMillis() - start; // 本地等待时长
                         remain = timeout - elapsed;
                         if (remain <= 0) {
                             throw new RpcException("Waiting concurrent invoke timeout in client-side for service:  "
@@ -65,16 +74,21 @@ public class ActiveLimitFilter implements Filter {
         }
         try {
             long begin = System.currentTimeMillis();
+            // 调用开始的计数
             RpcStatus.beginCount(url, methodName);
             try {
+                // 服务调用
                 Result result = invoker.invoke(invocation);
+                // 调用结束的计数（成功）
                 RpcStatus.endCount(url, methodName, System.currentTimeMillis() - begin, true);
                 return result;
             } catch (RuntimeException t) {
+                // 调用结束的计数（失败）
                 RpcStatus.endCount(url, methodName, System.currentTimeMillis() - begin, false);
                 throw t;
             }
         } finally {
+            // 唤醒等待的相同服务的相同方法的请求
             if (max > 0) {
                 synchronized (count) {
                     count.notify();
--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ExecuteLimitFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ExecuteLimitFilter.java
@@ -19,56 +19,59 @@ package com.alibaba.dubbo.rpc.filter;
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.common.extension.Activate;
-import com.alibaba.dubbo.rpc.Filter;
-import com.alibaba.dubbo.rpc.Invocation;
-import com.alibaba.dubbo.rpc.Invoker;
-import com.alibaba.dubbo.rpc.Result;
-import com.alibaba.dubbo.rpc.RpcException;
-import com.alibaba.dubbo.rpc.RpcStatus;
+import com.alibaba.dubbo.rpc.*;
 
 import java.util.concurrent.Semaphore;
 
 /**
  * ThreadLimitInvokerFilter
  *
+ * 服务提供者每服务每方法最大可并行执行请求数的过滤器实现类
  */
 @Activate(group = Constants.PROVIDER, value = Constants.EXECUTES_KEY)
 public class ExecuteLimitFilter implements Filter {
 
+    @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         URL url = invoker.getUrl();
         String methodName = invocation.getMethodName();
-        Semaphore executesLimit = null;
-        boolean acquireResult = false;
+        Semaphore executesLimit = null; // 信号量
+        boolean acquireResult = false; // 是否获得信号量
+        // 获得服务提供者每服务每方法最大可并行执行请求数
         int max = url.getMethodParameter(methodName, Constants.EXECUTES_KEY, 0);
         if (max > 0) {
+            // 获得 RpcStatus 对象，基于服务 URL + 方法维度
             RpcStatus count = RpcStatus.getStatus(url, invocation.getMethodName());
+            // 获得信号量。若获得不到，抛出异常。
 //            if (count.getActive() >= max) {
             /**
              * http://manzhizhen.iteye.com/blog/2386408
              * use semaphore for concurrency control (to limit thread number)
              */
             executesLimit = count.getSemaphore(max);
-            if(executesLimit != null && !(acquireResult = executesLimit.tryAcquire())) {
+            if (executesLimit != null && !(acquireResult = executesLimit.tryAcquire())) {
                 throw new RpcException("Failed to invoke method " + invocation.getMethodName() + " in provider " + url + ", cause: The service using threads greater than <dubbo:service executes=\"" + max + "\" /> limited.");
             }
         }
         long begin = System.currentTimeMillis();
         boolean isSuccess = true;
+        // 调用开始的计数
         RpcStatus.beginCount(url, methodName);
         try {
-            Result result = invoker.invoke(invocation);
-            return result;
+            // 服务调用
+            return invoker.invoke(invocation);
         } catch (Throwable t) {
-            isSuccess = false;
+            isSuccess = false; // 标记失败
             if (t instanceof RuntimeException) {
                 throw (RuntimeException) t;
             } else {
                 throw new RpcException("unexpected exception when ExecuteLimitFilter", t);
             }
         } finally {
+            // 调用结束的计数（成功）（失败）
             RpcStatus.endCount(url, methodName, System.currentTimeMillis() - begin, isSuccess);
-            if(acquireResult) {
+            // 释放信号量
+            if (acquireResult) {
                 executesLimit.release();
             }
         }
