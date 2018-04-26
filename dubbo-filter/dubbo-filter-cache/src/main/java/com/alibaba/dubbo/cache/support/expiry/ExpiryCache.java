f08ccb77fdf3bb8a7fbb5f5d652b3ea64824bcd3
谢志春 zhichun.xie <zhichun.xie@weimob.com>
2018-04-26 23:59:16 +0800

    ExpiryCache - With the characteristic of expiration time

--- dubbo-filter/dubbo-filter-cache/src/main/java/com/alibaba/dubbo/cache/support/expiry/ExpiryCache.java
+++ dubbo-filter/dubbo-filter-cache/src/main/java/com/alibaba/dubbo/cache/support/expiry/ExpiryCache.java
@@ -12,9 +12,9 @@ public class ExpiryCache implements Cache {
     private final Map<Object, Object> store;
 
     public ExpiryCache(URL url) {
-        // 缓存时间
+        // cache time (second)
         final int secondsToLive = url.getParameter("cache.seconds", 180);
-        // 缓存检查的时间间隔
+        // Cache check interval (second)
         final int intervalSeconds = url.getParameter("cache.interval", 1);
         ExpiryMap<Object, Object> expiryMap = new ExpiryMap<Object, Object>(secondsToLive, intervalSeconds);
         expiryMap.getExpireThread().startExpiryIfNotStarted();
