e85e9771196aa04234f5dac9692b0f5f8047556a
Huang YunKun <htynkn@gmail.com>
2018-04-28 20:26:14 +0800

    fix * imports issue

---
+++ dubbo-common/src/test/java/com/alibaba/dubbo/common/logger/LoggerFactoryTest.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.dubbo.common.logger;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class LoggerFactoryTest {
+    @Test
+    public void testLoggerLevel() {
+        LoggerFactory.setLevel(Level.INFO);
+        Level level = LoggerFactory.getLevel();
+
+        assertThat(level, is(Level.INFO));
+    }
+
+    @Test
+    public void testGetLogFile() {
+        LoggerFactory.setLoggerAdapter("slf4j");
+        File file = LoggerFactory.getFile();
+
+        assertThat(file, is(nullValue()));
+    }
+
+    @Test
+    public void testAllLogLevel() {
+        for (Level targetLevel : Level.values()) {
+            LoggerFactory.setLevel(targetLevel);
+            Level level = LoggerFactory.getLevel();
+
+            assertThat(level, is(targetLevel));
+        }
+    }
+
+    @Test
+    public void testGetLogger() {
+        Logger logger1 = LoggerFactory.getLogger(this.getClass());
+        Logger logger2 = LoggerFactory.getLogger(this.getClass());
+
+        assertThat(logger1, is(logger2));
+    }
+}
