a42fa2de5ba27ffaff7f16e9cc1758bb23949ee1
liaozan <378024053@qq.com>
2018-05-09 17:26:53 +0800

    Remove unnecessary null check.

--- dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/AnnotationBean.java
+++ dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/AnnotationBean.java
@@ -89,23 +89,23 @@ public class AnnotationBean extends AbstractConfig implements DisposableBean, Be
         if (annotationPackage == null || annotationPackage.length() == 0) {
             return;
         }
-        if (beanFactory instanceof BeanDefinitionRegistry) {
-            try {
-                // init scanner
-                Class<?> scannerClass = ReflectUtils.forName("org.springframework.context.annotation.ClassPathBeanDefinitionScanner");
-                Object scanner = scannerClass.getConstructor(new Class<?>[]{BeanDefinitionRegistry.class, boolean.class}).newInstance(beanFactory, true);
-                // add filter
-                Class<?> filterClass = ReflectUtils.forName("org.springframework.core.type.filter.AnnotationTypeFilter");
-                Object filter = filterClass.getConstructor(Class.class).newInstance(Service.class);
-                Method addIncludeFilter = scannerClass.getMethod("addIncludeFilter", ReflectUtils.forName("org.springframework.core.type.filter.TypeFilter"));
-                addIncludeFilter.invoke(scanner, filter);
-                // scan packages
-                String[] packages = Constants.COMMA_SPLIT_PATTERN.split(annotationPackage);
-                Method scan = scannerClass.getMethod("scan", String[].class);
-                scan.invoke(scanner, new Object[]{packages});
-            } catch (Throwable e) {
-                // spring 2.0
-            }
+	    if (beanFactory instanceof BeanDefinitionRegistry) {
+		    try {
+			    // init scanner
+			    Class<?> scannerClass = ReflectUtils.forName("org.springframework.context.annotation.ClassPathBeanDefinitionScanner");
+			    Object scanner = scannerClass.getConstructor(new Class<?>[]{BeanDefinitionRegistry.class, boolean.class}).newInstance(new Object[]{(BeanDefinitionRegistry) beanFactory, true});
+			    // add filter
+			    Class<?> filterClass = ReflectUtils.forName("org.springframework.core.type.filter.AnnotationTypeFilter");
+			    Object filter = filterClass.getConstructor(Class.class).newInstance(Service.class);
+			    Method addIncludeFilter = scannerClass.getMethod("addIncludeFilter", ReflectUtils.forName("org.springframework.core.type.filter.TypeFilter"));
+			    addIncludeFilter.invoke(scanner, filter);
+			    // scan packages
+			    String[] packages = Constants.COMMA_SPLIT_PATTERN.split(annotationPackage);
+			    Method scan = scannerClass.getMethod("scan", new Class<?>[]{String[].class});
+			    scan.invoke(scanner, new Object[]{packages});
+		    } catch (Throwable e) {
+			    // spring 2.0
+		    }
         }
     }
 
@@ -296,12 +296,10 @@ public class AnnotationBean extends AbstractConfig implements DisposableBean, Be
 	            if (reference.application().length() > 0) {
                     referenceConfig.setApplication(applicationContext.getBean(reference.application(), ApplicationConfig.class));
                 }
-	            reference.module();
-	            if (reference.module().length() > 0) {
+                if (reference.module() != null && reference.module().length() > 0) {
                     referenceConfig.setModule(applicationContext.getBean(reference.module(), ModuleConfig.class));
                 }
-	            reference.consumer();
-	            if (reference.consumer().length() > 0) {
+                if (reference.consumer() != null && reference.consumer().length() > 0) {
                     referenceConfig.setConsumer(applicationContext.getBean(reference.consumer(), ConsumerConfig.class));
                 }
                 try {
