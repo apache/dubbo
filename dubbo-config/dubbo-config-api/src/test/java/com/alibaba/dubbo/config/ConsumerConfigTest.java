75664c3bfb2f937c1934c1b8bb7e8cb99e45bbb5
beiwei30 <ian.luo@gmail.com>
2018-05-09 16:40:06 +0800

    make test stable

--- dubbo-config/dubbo-config-api/src/test/java/com/alibaba/dubbo/config/ConsumerConfigTest.java
+++ dubbo-config/dubbo-config-api/src/test/java/com/alibaba/dubbo/config/ConsumerConfigTest.java
@@ -27,6 +27,7 @@ public class ConsumerConfigTest {
     @Test
     public void testTimeout() throws Exception {
         try {
+            System.clearProperty("sun.rmi.transport.tcp.responseTimeout");
             ConsumerConfig consumer = new ConsumerConfig();
             consumer.setTimeout(10);
             assertThat(consumer.getTimeout(), is(10));
