616f6290a2b4e33fe96b241a10a82568d192a57b
JackCaptain1015 <15700094730@163.com>
2018-05-12 20:48:17 +0800

    Such logic already exists in findAnnotationName (#1750)

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/extension/ExtensionLoader.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/extension/ExtensionLoader.java
@@ -665,12 +665,7 @@ public class ExtensionLoader<T> {
             if (name == null || name.length() == 0) {
                 name = findAnnotationName(clazz);
                 if (name == null || name.length() == 0) {
-                    if (clazz.getSimpleName().length() > type.getSimpleName().length()
-                            && clazz.getSimpleName().endsWith(type.getSimpleName())) {
-                        name = clazz.getSimpleName().substring(0, clazz.getSimpleName().length() - type.getSimpleName().length()).toLowerCase();
-                    } else {
-                        throw new IllegalStateException("No such extension name for the class " + clazz.getName() + " in the config " + resourceURL);
-                    }
+                    throw new IllegalStateException("No such extension name for the class " + clazz.getName() + " in the config " + resourceURL);
                 }
             }
             String[] names = NAME_SEPARATOR.split(name);
