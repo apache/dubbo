07320f8a44775c550765c9a4720cee97b3bdd8df
beiwei30 <ian.luo@gmail.com>
2018-05-14 14:47:38 +0800

    change url test package

--- dubbo-config/dubbo-config-api/src/test/java/com/alibaba/dubbo/config/url/InvokerSideConfigUrlTest.java
+++ dubbo-config/dubbo-config-api/src/test/java/com/alibaba/dubbo/config/url/InvokerSideConfigUrlTest.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.dubbo.config.url.test;
+package com.alibaba.dubbo.config.url;
 
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.logger.Logger;
@@ -137,23 +137,18 @@ public class InvokerSideConfigUrlTest extends UrlTestBase {
 
     @BeforeClass
     public static void start() {
-
         //RegistryController.startRegistryIfAbsence(1);
     }
 
 
     @Before
     public void setUp() {
-
         initServConf();
         initRefConf();
-
-        return;
     }
 
     @After()
     public void teardown() {
-
         //RegistryServer.reloadCache();
     }
 
@@ -171,9 +166,7 @@ public class InvokerSideConfigUrlTest extends UrlTestBase {
     @Ignore("parameter on register center will not be merged any longer with query parameter request from the consumer")
     @Test
     public void regConfForConsumerUrlTest() {
-
         verifyInvokerUrlGeneration(regConfForConsumer, regConfForConsumerTable);
-
     }
 
     // ======================================================
--- dubbo-config/dubbo-config-api/src/test/java/com/alibaba/dubbo/config/url/UrlTestBase.java
+++ dubbo-config/dubbo-config-api/src/test/java/com/alibaba/dubbo/config/url/UrlTestBase.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.dubbo.config.url.test;
+package com.alibaba.dubbo.config.url;
 
 
 import com.alibaba.dubbo.common.logger.Logger;
@@ -29,9 +29,9 @@ import com.alibaba.dubbo.config.ServiceConfig;
 import com.alibaba.dubbo.config.api.DemoService;
 import com.alibaba.dubbo.config.provider.impl.DemoServiceImpl;
 
-import org.junit.Assert;
-
 import java.util.Arrays;
+
+import static junit.framework.TestCase.fail;
 
 @SuppressWarnings("unused")
 public class UrlTestBase {
@@ -205,7 +205,7 @@ public class UrlTestBase {
         }
 
         if (!failLog.equals(FAILLOG_HEADER)) {
-            Assert.fail(failLog);
+            fail(failLog);
         }
     }
 
