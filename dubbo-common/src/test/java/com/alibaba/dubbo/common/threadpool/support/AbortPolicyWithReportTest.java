59c7819f929d600cec3870d9431be846006d512b
beiwei30 <ian.luo@gmail.com>
2018-05-15 10:38:42 +0800

    reformat the code, and move the test into the correct package

--- dubbo-common/src/test/java/com/alibaba/dubbo/common/threadpool/support/AbortPolicyWithReportTest.java
+++ dubbo-common/src/test/java/com/alibaba/dubbo/common/threadpool/support/AbortPolicyWithReportTest.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.dubbo.common.threadpool;
+package com.alibaba.dubbo.common.threadpool.support;
 
 import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.common.threadpool.support.AbortPolicyWithReport;
@@ -37,11 +37,11 @@ public class AbortPolicyWithReportTest {
                     System.out.println("hello");
                 }
             }, (ThreadPoolExecutor) Executors.newFixedThreadPool(1));
-        }catch (RejectedExecutionException rj){
-
+        } catch (RejectedExecutionException rj) {
+            // ignore
         }
 
-        Thread.currentThread().sleep(1000);
+        Thread.sleep(1000);
 
     }
 }
