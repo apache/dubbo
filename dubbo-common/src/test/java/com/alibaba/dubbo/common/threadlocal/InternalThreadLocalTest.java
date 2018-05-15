64bc20ca63dd8f9cec86227af9f6260cbd81923d
xiuyuhang <442367943@qq.com>
2018-05-15 16:48:49 +0800

    Unit test for InternalThreadLocal Fix bug in method removeAll

--- dubbo-common/src/test/java/com/alibaba/dubbo/common/threadlocal/InternalThreadLocalTest.java
+++ dubbo-common/src/test/java/com/alibaba/dubbo/common/threadlocal/InternalThreadLocalTest.java
@@ -62,6 +62,21 @@ public class InternalThreadLocalTest {
     }
 
     @Test
+    public void testRemoveAll() throws InterruptedException {
+        final InternalThreadLocal<Integer> internalThreadLocal = new InternalThreadLocal<Integer>();
+        internalThreadLocal.set(1);
+        Assert.assertTrue("set failed", internalThreadLocal.get() == 1);
+
+        final InternalThreadLocal<String> internalThreadLocalString = new InternalThreadLocal<String>();
+        internalThreadLocalString.set("value");
+        Assert.assertTrue("set failed", "value".equals(internalThreadLocalString.get()));
+
+        InternalThreadLocal.removeAll();
+        Assert.assertTrue("removeAll failed!", internalThreadLocal.get() == null);
+        Assert.assertTrue("removeAll failed!", internalThreadLocalString.get() == null);
+    }
+
+    @Test
     public void testSize() throws InterruptedException {
         final InternalThreadLocal<Integer> internalThreadLocal = new InternalThreadLocal<Integer>();
         internalThreadLocal.set(1);
