4550d56d87be57a156898f2f4288e7156b7ff835
Ian Luo <ian.luo@gmail.com>
2018-05-16 10:10:21 +0800

    #1682: Enhance the test coverage part-4: dubbo-common/src/main/java/com/alibaba/dubbo/common/status(store|threadpoolutils) modules (#1806)

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/ConfigUtils.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/ConfigUtils.java
@@ -226,7 +226,7 @@ public class ConfigUtils {
                     input.close();
                 }
             } catch (Throwable e) {
-                logger.warn("Failed to load " + fileName + " file from " + fileName + "(ingore this file): " + e.getMessage(), e);
+                logger.warn("Failed to load " + fileName + " file from " + fileName + "(ignore this file): " + e.getMessage(), e);
             }
             return properties;
         }
@@ -261,7 +261,7 @@ public class ConfigUtils {
             try {
                 properties.load(ClassHelper.getClassLoader().getResourceAsStream(fileName));
             } catch (Throwable e) {
-                logger.warn("Failed to load " + fileName + " file from " + fileName + "(ingore this file): " + e.getMessage(), e);
+                logger.warn("Failed to load " + fileName + " file from " + fileName + "(ignore this file): " + e.getMessage(), e);
             }
             return properties;
         }
@@ -284,7 +284,7 @@ public class ConfigUtils {
                     }
                 }
             } catch (Throwable e) {
-                logger.warn("Fail to load " + fileName + " file from " + url + "(ingore this file): " + e.getMessage(), e);
+                logger.warn("Fail to load " + fileName + " file from " + url + "(ignore this file): " + e.getMessage(), e);
             }
         }
 
@@ -312,6 +312,7 @@ public class ConfigUtils {
             try {
                 timeout = Integer.parseInt(value);
             } catch (Exception e) {
+                // ignore
             }
         } else {
             value = ConfigUtils.getProperty(Constants.SHUTDOWN_WAIT_SECONDS_KEY);
@@ -319,6 +320,7 @@ public class ConfigUtils {
                 try {
                     timeout = Integer.parseInt(value) * 1000;
                 } catch (Exception e) {
+                    // ignore
                 }
             }
         }
