f1f0cf07ffe2399fbe8fbab0f33dd278c2c59b36
ken.lj <ken.lj.hz@gmail.com>
2018-05-30 18:27:50 +0800

    make DubboAsync work at source compile phase.

--- dubbo-plugin/dubbo-async-processer/src/main/java/com/alibaba/dubbo/config/async/DubboAsync.java
+++ dubbo-plugin/dubbo-async-processer/src/main/java/com/alibaba/dubbo/config/async/DubboAsync.java
@@ -16,9 +16,17 @@
  */
 package com.alibaba.dubbo.config.async;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  *
  */
-
+@Documented
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
 public @interface DubboAsync {
 }
