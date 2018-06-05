42ba20ed43331f0e3e1c4a4ea75fcb34f564b796
yizhenqiang <manzhizhen@163.com>
2018-06-05 15:59:29 +0800

    Merge pull request #1453, restore the bug that attachment has not been updated in the RpcContext when the Dubbo built-in retry mechanism is triggered.

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/protocol/AbstractInvoker.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/protocol/AbstractInvoker.java
@@ -136,7 +136,14 @@ public abstract class AbstractInvoker<T> implements Invoker<T> {
         }
         Map<String, String> context = RpcContext.getContext().getAttachments();
         if (context != null) {
-            invocation.addAttachmentsIfAbsent(context);
+            /**
+             * invocation.addAttachmentsIfAbsent(context){@link RpcInvocation#addAttachmentsIfAbsent(Map)}should not be used here,
+             * because the {@link RpcContext#setAttachment(String, String)} is passed in the Filter when the call is triggered
+             * by the built-in retry mechanism of the Dubbo. The attachment to update RpcContext will no longer work, which is
+             * a mistake in most cases (for example, through Filter to RpcContext output traceId and spanId and other information).
+             * yizhenqiang 20180310
+             */
+            invocation.addAttachments(context);
         }
         if (getUrl().getMethodParameter(invocation.getMethodName(), Constants.ASYNC_KEY, false)) {
             invocation.setAttachment(Constants.ASYNC_KEY, Boolean.TRUE.toString());
