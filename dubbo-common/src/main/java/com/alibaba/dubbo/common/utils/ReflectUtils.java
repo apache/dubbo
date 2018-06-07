2db9a0b033f3f992207729f568e76b50b5cd64f0
beiwei30 <ian.luo@gmail.com>
2018-06-07 14:22:27 +0800

    #1682: Enhance the test coverage part-4 : dubbo-common/src/main/java/com/alibaba/dubbo/common/status(store|threadpoolutils) modules

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/ReflectUtils.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/ReflectUtils.java
@@ -166,30 +166,15 @@ public final class ReflectUtils {
      */
     public static boolean isCompatible(Class<?> c, Object o) {
         boolean pt = c.isPrimitive();
-        if (o == null)
+        if (o == null) {
             return !pt;
+        }
 
         if (pt) {
-            if (c == int.class)
-                c = Integer.class;
-            else if (c == boolean.class)
-                c = Boolean.class;
-            else if (c == long.class)
-                c = Long.class;
-            else if (c == float.class)
-                c = Float.class;
-            else if (c == double.class)
-                c = Double.class;
-            else if (c == char.class)
-                c = Character.class;
-            else if (c == byte.class)
-                c = Byte.class;
-            else if (c == short.class)
-                c = Short.class;
+            c = getBoxedClass(c);
         }
-        if (c == o.getClass())
-            return true;
-        return c.isInstance(o);
+
+        return c == o.getClass() || c.isInstance(o);
     }
 
     /**
