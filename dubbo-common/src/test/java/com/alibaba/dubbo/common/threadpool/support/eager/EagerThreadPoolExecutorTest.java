a3f22ba2310e5d29b6067587840895caec8b0602
bohr.qiu <bohr.qiu@gmail.com>
2018-06-11 18:58:00 +0800

    delete unused test

--- dubbo-common/src/test/java/com/alibaba/dubbo/common/threadpool/support/eager/EagerThreadPoolExecutorTest.java
+++ dubbo-common/src/test/java/com/alibaba/dubbo/common/threadpool/support/eager/EagerThreadPoolExecutorTest.java
@@ -102,6 +102,5 @@ public class EagerThreadPoolExecutorTest {
                 executorService.getClass()
                         .getSimpleName()
                         .equals("EagerThreadPoolExecutor"));
-        executorService.execute(() ->{ throw new RuntimeException("x");});
     }
 }
