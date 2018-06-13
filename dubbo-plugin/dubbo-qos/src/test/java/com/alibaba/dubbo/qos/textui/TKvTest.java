d116fbc2a70752473b859d5d33223fd647cff7f0
Ian Luo <ian.luo@gmail.com>
2018-06-13 14:33:24 +0800

    #1689: Enhance the test coverage part-10 : dubbo-plugin module, unit (#1936)
    test for textui

--- dubbo-plugin/dubbo-qos/src/test/java/com/alibaba/dubbo/qos/textui/TKvTest.java
+++ dubbo-plugin/dubbo-qos/src/test/java/com/alibaba/dubbo/qos/textui/TKvTest.java
@@ -18,22 +18,43 @@ package com.alibaba.dubbo.qos.textui;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+
 public class TKvTest {
     @Test
-    public void outPutTest() {
-
-        final TKv tKv = new TKv(new TTable.ColumnDefine(TTable.Align.RIGHT), new TTable.ColumnDefine(10, false, TTable.Align.LEFT));
+    public void test1() {
+        TKv tKv = new TKv(new TTable.ColumnDefine(TTable.Align.RIGHT), new TTable.ColumnDefine(10, false, TTable.Align.LEFT));
         tKv.add("KEY-1", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
         tKv.add("KEY-2", "1234567890");
         tKv.add("KEY-3", "1234567890");
 
-        final TTable tTable = new TTable(new TTable.ColumnDefine[]{
+        TTable tTable = new TTable(new TTable.ColumnDefine[]{
                 new TTable.ColumnDefine(),
                 new TTable.ColumnDefine(20, false, TTable.Align.LEFT)
         });
 
-        tTable.addRow("OPTIONS", tKv.rendering());
+        String kv = tKv.rendering();
+        assertThat(kv, containsString("ABCDEFGHIJ\n"));
+        assertThat(kv, containsString("KLMNOPQRST\n"));
+        assertThat(kv, containsString("UVWXYZ\n"));
 
-        System.out.println(tTable.rendering());
+        tTable.addRow("OPTIONS", kv);
+        String table = tTable.rendering();
+        assertThat(table, containsString("|OPTIONS|"));
+        assertThat(table, containsString("|KEY-3"));
+
+        System.out.println(table);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        TKv tKv = new TKv();
+        tKv.add("KEY-1", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        tKv.add("KEY-2", "1234567890");
+        tKv.add("KEY-3", "1234567890");
+        String kv = tKv.rendering();
+        assertThat(kv, containsString("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        System.out.println(kv);
     }
 }
