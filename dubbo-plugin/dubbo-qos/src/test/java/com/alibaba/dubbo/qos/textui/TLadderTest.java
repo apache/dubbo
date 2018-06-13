19799a4681b10d649784b52745a64f4bebe52ab6
beiwei30 <ian.luo@gmail.com>
2018-06-13 13:59:47 +0800

    #1689: Enhance the test coverage part-10 : dubbo-plugin module, unit     test for textui

--- dubbo-plugin/dubbo-qos/src/test/java/com/alibaba/dubbo/qos/textui/TLadderTest.java
+++ dubbo-plugin/dubbo-qos/src/test/java/com/alibaba/dubbo/qos/textui/TLadderTest.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,14 +14,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba;
+package com.alibaba.dubbo.qos.textui;
 
 import org.junit.Test;
 
-public class StringSplitTest {
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class TLadderTest {
     @Test
-    public void stringSplitTest(){
-        String str = new String("?\\\\      helloworld ac");
-        String[] result = str.split("(?<![\\\\]) ");
+    public void testRendering() throws Exception {
+        TLadder ladder = new TLadder();
+        ladder.addItem("1");
+        ladder.addItem("2");
+        ladder.addItem("3");
+        ladder.addItem("4");
+        String result = ladder.rendering();
+        String expected = "1\n" +
+                "  `-2\n" +
+                "    `-3\n" +
+                "      `-4\n";
+        assertThat(result, equalTo(expected));
+        System.out.println(result);
     }
 }
