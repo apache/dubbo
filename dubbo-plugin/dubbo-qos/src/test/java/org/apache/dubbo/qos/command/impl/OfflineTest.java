f186f2bc167f151dc15e025c03cca6da8c11c464
Ian Luo <ian.luo@gmail.com>
2018-06-25 14:31:23 +0800

    [dubbo-1689]: Enhance the test coverage part-10 : dubbo-plugin module (#1980)
    *     #1689: Enhance the test coverage part-10 : dubbo-plugin module
    
    *     #1689: Enhance the test coverage part-10 : dubbo-plugin module
    
    * fix unit test failure

--- dubbo-plugin/dubbo-qos/src/test/java/org/apache/dubbo/qos/command/impl/OfflineTest.java
+++ dubbo-plugin/dubbo-qos/src/test/java/org/apache/dubbo/qos/command/impl/OfflineTest.java
@@ -2,32 +2,27 @@ package org.apache.dubbo.qos.command.impl;
 
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.config.model.ApplicationModel;
-import org.apache.dubbo.config.model.ConsumerModel;
 import org.apache.dubbo.config.model.ProviderModel;
 import org.apache.dubbo.qos.command.CommandContext;
-import org.apache.dubbo.registry.integration.RegistryDirectory;
+import org.apache.dubbo.registry.Registry;
 import org.apache.dubbo.registry.support.ProviderConsumerRegTable;
 import org.apache.dubbo.registry.support.ProviderInvokerWrapper;
 import org.apache.dubbo.rpc.Invoker;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import java.util.Map;
-
 import static org.apache.dubbo.registry.support.ProviderConsumerRegTable.getProviderInvoker;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class LsTest {
+public class OfflineTest {
     @Test
     public void testExecute() throws Exception {
-        ConsumerModel consumerModel = mock(ConsumerModel.class);
-        when(consumerModel.getServiceName()).thenReturn("org.apache.dubbo.FooService");
         ProviderModel providerModel = mock(ProviderModel.class);
         when(providerModel.getServiceName()).thenReturn("org.apache.dubbo.BarService");
-        ApplicationModel.initConsumerModel("org.apache.dubbo.FooService", consumerModel);
         ApplicationModel.initProviderModel("org.apache.dubbo.BarService", providerModel);
 
         Invoker providerInvoker = mock(Invoker.class);
@@ -42,20 +37,18 @@ public class LsTest {
             wrapper.setReg(true);
         }
 
-        Invoker consumerInvoker = mock(Invoker.class);
-        URL consumerUrl = mock(URL.class);
-        when(consumerUrl.getServiceKey()).thenReturn("org.apache.dubbo.FooService");
-        when(consumerUrl.toFullString()).thenReturn("dubbo://localhost:8888/org.apache.dubbo.FooService");
-        when(consumerInvoker.getUrl()).thenReturn(consumerUrl);
-        RegistryDirectory directory = mock(RegistryDirectory.class);
-        Map invokers = Mockito.mock(Map.class);
-        when(invokers.size()).thenReturn(100);
-        when(directory.getUrlInvokerMap()).thenReturn(invokers);
-        ProviderConsumerRegTable.registerConsumer(consumerInvoker, registryUrl, consumerUrl, directory);
+        Registry registry = mock(Registry.class);
+        TestRegistryFactory.registry = registry;
 
-        Ls ls = new Ls();
-        String output = ls.execute(mock(CommandContext.class), null);
-        assertThat(output, containsString("org.apache.dubbo.FooService|100"));
-        assertThat(output, containsString("org.apache.dubbo.BarService| Y"));
+        Offline offline = new Offline();
+        String output = offline.execute(mock(CommandContext.class), new String[]{"org.apache.dubbo.BarService"});
+        assertThat(output, containsString("OK"));
+        Mockito.verify(registry).unregister(providerUrl);
+        for (ProviderInvokerWrapper wrapper : getProviderInvoker("org.apache.dubbo.BarService")) {
+            assertThat(wrapper.isReg(), is(false));
+        }
+
+        output = offline.execute(mock(CommandContext.class), new String[]{"org.apache.dubbo.FooService"});
+        assertThat(output, containsString("service not found"));
     }
 }
