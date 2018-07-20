9ad92279093bd01ccf13dce1a72e4aa3b2d5c891
时无两丶 <442367943@qq.com>
2018-07-20 10:58:44 +0800

    * Improve AsyncContext docs (#2101)
    * Make the future final which is in the constructor of AsyncRpcResult because of using in a inner class.

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/AsyncContext.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/AsyncContext.java
@@ -27,6 +27,11 @@ import java.util.concurrent.CompletableFuture;
  */
 public interface AsyncContext {
 
+    /**
+     * get the internal future which is binding to this async context
+     *
+     * @return the internal future
+     */
     CompletableFuture getInternalFuture();
 
     /**
@@ -51,5 +56,31 @@ public interface AsyncContext {
      */
     void start();
 
+    /**
+     * Signal RpcContext switch.
+     * Use this method to switch RpcContext from a Dubbo thread to a new thread created by the user.
+     *
+     * Note that you should use it in a new thread like this:
+     * <code>
+     * public class AsyncServiceImpl implements AsyncService {
+     *     public String sayHello(String name) {
+     *         final AsyncContext asyncContext = RpcContext.startAsync();
+     *         new Thread(() -> {
+     *
+     *             // right place to use this method
+     *             asyncContext.signalContextSwitch();
+     *
+     *             try {
+     *                 Thread.sleep(500);
+     *             } catch (InterruptedException e) {
+     *                 e.printStackTrace();
+     *             }
+     *             asyncContext.write("Hello " + name + ", response from provider.");
+     *         }).start();
+     *         return null;
+     *     }
+     * }
+     * </code>
+     */
     void signalContextSwitch();
 }
