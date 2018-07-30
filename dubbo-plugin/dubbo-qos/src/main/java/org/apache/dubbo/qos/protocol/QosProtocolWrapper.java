c7632dd593ca1b90c4264038233e3538e77cfb98
Huxing Zhang <huxing.zhang@gmail.com>
2018-07-30 16:00:22 +0800

    Add log message if qos is not enabled.

--- dubbo-plugin/dubbo-qos/src/main/java/org/apache/dubbo/qos/protocol/QosProtocolWrapper.java
+++ dubbo-plugin/dubbo-qos/src/main/java/org/apache/dubbo/qos/protocol/QosProtocolWrapper.java
@@ -85,6 +85,9 @@ public class QosProtocolWrapper implements Protocol {
         try {
             boolean qosEnable = url.getParameter(QOS_ENABLE,true);
             if (!qosEnable) {
+                logger.info("qos won't be started because it is disabled. " +
+                        "Please check dubbo.application.qos.enable is configured either in system property, " +
+                        "dubbo.properties or XML/spring-boot configuration.");
                 return;
             }
 
