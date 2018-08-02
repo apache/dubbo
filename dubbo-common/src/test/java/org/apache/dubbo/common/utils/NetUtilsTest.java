9de876b74ee92ea1b3bf711b247715178a1e0781
Huxing Zhang <huxing.zhang@gmail.com>
2018-08-02 10:34:08 +0800

    Improve UT.

--- dubbo-common/src/test/java/org/apache/dubbo/common/utils/NetUtilsTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/utils/NetUtilsTest.java
@@ -185,12 +185,13 @@ public class NetUtilsTest {
 
     @Test
     public void testIsValidV6Address() {
+        String saved = System.getProperty("java.net.preferIPv6Addresses");
         System.setProperty("java.net.preferIPv6Addresses", "true");
         InetAddress address = NetUtils.getLocalAddress();
         if (address instanceof Inet6Address) {
             assertThat(NetUtils.isValidV6Address((Inet6Address) address), equalTo(true));
         }
-        System.setProperty("java.net.preferIPv6Addresses", "false");
+        System.setProperty("java.net.preferIPv6Addresses", saved);
     }
 
     /**
