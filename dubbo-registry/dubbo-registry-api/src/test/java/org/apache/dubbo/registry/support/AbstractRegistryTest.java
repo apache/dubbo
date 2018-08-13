d90f1615f2296a80044db6bfd14905b71e782b04
binss <i@binss.me>
2018-08-13 19:17:07 +0800

    Fix format problem of AbstractRegistryTest

--- dubbo-registry/dubbo-registry-api/src/test/java/org/apache/dubbo/registry/support/AbstractRegistryTest.java
+++ dubbo-registry/dubbo-registry-api/src/test/java/org/apache/dubbo/registry/support/AbstractRegistryTest.java
@@ -41,22 +41,28 @@ public class AbstractRegistryTest {
         URL url = URL.valueOf("dubbo://" + NetUtils.getLocalAddress().getHostName() + ":2233");
         testUrl = URL.valueOf("http://1.2.3.4:9090/registry?check=false&file=N/A&interface=com.test");
 
-        //init the object
+        // init the object
         abstractRegistry = new AbstractRegistry(url) {
             @Override
             public boolean isAvailable() {
                 return false;
             }
         };
-        //init notify listener
+        // init notify listener
         listener = urls -> notifySuccess = true;
-        //notify flag
+        // notify flag
         notifySuccess = false;
     }
 
+    /**
+     * Test method for
+     * {@link org.apache.dubbo.registry.support.AbstractRegistry#register(URL)}.
+     *
+     * @throws Exception
+     */
     @Test
-    public void registerTest() {
-        //check parameters
+    public void testRegister() {
+        // check parameters
         try {
             abstractRegistry.register(null);
             Assert.fail();
@@ -67,14 +73,20 @@ public class AbstractRegistryTest {
         int beginSize = abstractRegistry.getRegistered().size();
         abstractRegistry.register(testUrl);
         Assert.assertEquals(beginSize + 1, abstractRegistry.getRegistered().size());
-        //check register when the url is the same
+        // check register when the url is the same
         abstractRegistry.register(testUrl);
         Assert.assertEquals(beginSize + 1, abstractRegistry.getRegistered().size());
     }
 
+    /**
+     * Test method for
+     * {@link org.apache.dubbo.registry.support.AbstractRegistry#unregister(URL)}.
+     *
+     * @throws Exception
+     */
     @Test
-    public void unregisterTest() {
-        //check parameters
+    public void testUnregister() {
+        // check parameters
         try {
             abstractRegistry.unregister(null);
             Assert.fail();
@@ -91,23 +103,29 @@ public class AbstractRegistryTest {
         Assert.assertEquals(beginSize - 1, abstractRegistry.getRegistered().size());
     }
 
+    /**
+     * Test method for
+     * {@link org.apache.dubbo.registry.support.AbstractRegistry#subscribe(URL, NotifyListener)}.
+     *
+     * @throws Exception
+     */
     @Test
-    public void subscribeTest() {
-        //check parameters
+    public void testSubscribe() {
+        // check parameters
         try {
             abstractRegistry.subscribe(null, listener);
             Assert.fail();
         } catch (Exception e) {
             Assert.assertTrue(e instanceof IllegalArgumentException);
         }
-        //check parameters
+        // check parameters
         try {
             abstractRegistry.subscribe(testUrl, null);
             Assert.fail();
         } catch (Exception e) {
             Assert.assertTrue(e instanceof IllegalArgumentException);
         }
-        //check parameters
+        // check parameters
         try {
             abstractRegistry.subscribe(null, null);
             Assert.fail();
@@ -120,23 +138,29 @@ public class AbstractRegistryTest {
         Assert.assertTrue(abstractRegistry.getSubscribed().get(testUrl).contains(listener));
     }
 
+    /**
+     * Test method for
+     * {@link org.apache.dubbo.registry.support.AbstractRegistry#unsubscribe(URL, NotifyListener)}.
+     *
+     * @throws Exception
+     */
     @Test
-    public void unsubscribeTest() {
-        //check parameters
+    public void testUnsubscribe() {
+        // check parameters
         try {
             abstractRegistry.unsubscribe(null, listener);
             Assert.fail();
         } catch (Exception e) {
             Assert.assertTrue(e instanceof IllegalArgumentException);
         }
-        //check parameters
+        // check parameters
         try {
             abstractRegistry.unsubscribe(testUrl, null);
             Assert.fail();
         } catch (Exception e) {
             Assert.assertTrue(e instanceof IllegalArgumentException);
         }
-        //check parameters
+        // check parameters
         try {
             abstractRegistry.unsubscribe(null, null);
             Assert.fail();
@@ -149,8 +173,12 @@ public class AbstractRegistryTest {
         Assert.assertFalse(abstractRegistry.getSubscribed().get(testUrl).contains(listener));
     }
 
+    /**
+     * Test method for
+     * {@link org.apache.dubbo.registry.support.AbstractRegistry#recover()}.
+     */
     @Test
-    public void recoverTest() throws Exception {
+    public void testRecover() throws Exception {
         abstractRegistry.register(testUrl);
         abstractRegistry.subscribe(testUrl, listener);
         abstractRegistry.recover();
@@ -160,8 +188,12 @@ public class AbstractRegistryTest {
         Assert.assertTrue(abstractRegistry.getSubscribed().get(testUrl).contains(listener));
     }
 
+    /**
+     * Test method for
+     * {@link org.apache.dubbo.registry.support.AbstractRegistry#notify(List)}.
+     */
     @Test
-    public void notifyTest() {
+    public void testNotify() {
         abstractRegistry.subscribe(testUrl, listener);
         List<URL> urls = new ArrayList<>();
         urls.add(testUrl);
@@ -171,16 +203,22 @@ public class AbstractRegistryTest {
         Assert.assertTrue(notifySuccess);
     }
 
+    /**
+     * Test method for
+     * {@link org.apache.dubbo.registry.support.AbstractRegistry#notify(URL, NotifyListener, List)}.
+     *
+     * @throws Exception
+     */
     @Test
-    public void notify2Test() {
-        //check parameters
+    public void testNotify2() {
+        // check parameters
         try {
             abstractRegistry.notify(null, null, null);
             Assert.fail();
         } catch (Exception e) {
             Assert.assertTrue(e instanceof IllegalArgumentException);
         }
-        //check parameters
+        // check parameters
         try {
             abstractRegistry.notify(testUrl, null, null);
             Assert.fail();
