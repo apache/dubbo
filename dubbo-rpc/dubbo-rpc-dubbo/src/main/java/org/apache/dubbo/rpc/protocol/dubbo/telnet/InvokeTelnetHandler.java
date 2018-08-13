1bfeefdf0941e1a8c6e599d1c7a6661f6577f6e2
unknown <guorong.guorongzhu@hz.ali.com>
2018-08-13 21:09:32 +0800

    du xiu team

--- dubbo-rpc/dubbo-rpc-dubbo/src/main/java/org/apache/dubbo/rpc/protocol/dubbo/telnet/InvokeTelnetHandler.java
+++ dubbo-rpc/dubbo-rpc-dubbo/src/main/java/org/apache/dubbo/rpc/protocol/dubbo/telnet/InvokeTelnetHandler.java
@@ -60,7 +60,7 @@ public class InvokeTelnetHandler implements TelnetHandler {
         for (int i = 0; i < types.length; i++) {
             Class<?> type = types[i];
             Object arg = args.get(i);
-            //du xiu  team
+            //judge every arg by  du xiu  team
             if (arg == null) {
                 return false;
             }
