75a0742d305e18034b0789d10447033d007d7a60
binss <i@binss.me>
2018-08-14 10:23:52 +0800

    Improve existing tests and add multi thread tests

--- dubbo-registry/dubbo-registry-api/src/test/java/org/apache/dubbo/registry/support/AbstractRegistryTest.java
+++ dubbo-registry/dubbo-registry-api/src/test/java/org/apache/dubbo/registry/support/AbstractRegistryTest.java
@@ -21,10 +21,12 @@ import org.apache.dubbo.common.utils.NetUtils;
 import org.apache.dubbo.registry.NotifyListener;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * AbstractRegistryTest
@@ -35,11 +37,13 @@ public class AbstractRegistryTest {
     private NotifyListener listener;
     private AbstractRegistry abstractRegistry;
     private boolean notifySuccess;
+    private int threadNumber;
 
     @Before
     public void init() {
         URL url = URL.valueOf("dubbo://" + NetUtils.getLocalAddress().getHostName() + ":2233");
         testUrl = URL.valueOf("http://1.2.3.4:9090/registry?check=false&file=N/A&interface=com.test");
+        threadNumber = 100;
 
         // init the object
         abstractRegistry = new AbstractRegistry(url) {
@@ -52,6 +56,11 @@ public class AbstractRegistryTest {
         listener = urls -> notifySuccess = true;
         // notify flag
         notifySuccess = false;
+    }
+
+    @After
+    public void after() {
+        abstractRegistry.destroy();
     }
 
     /**
@@ -79,6 +88,41 @@ public class AbstractRegistryTest {
     }
 
     /**
+     * Multi thread test method for
+     * {@link org.apache.dubbo.registry.support.AbstractRegistry#register(URL)}.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testRegisterMultiThread() {
+        int threadBeginSize = abstractRegistry.getRegistered().size();
+        CountDownLatch countDownLatch = new CountDownLatch(threadNumber);
+        Thread[] threads = new Thread[threadNumber];
+        for (int i = 0; i < threadNumber; i++){
+            URL url = URL.valueOf("dubbo://" + NetUtils.getLocalAddress().getHostName() + i);
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    abstractRegistry.register(url);
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        for (int i = 0; i < threadNumber; i++){
+            threads[i].run();
+        }
+
+        try {
+            // wait for all thread done
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(threadBeginSize + threadNumber, abstractRegistry.getRegistered().size());
+    }
+
+    /**
      * Test method for
      * {@link org.apache.dubbo.registry.support.AbstractRegistry#unregister(URL)}.
      *
@@ -93,14 +137,62 @@ public class AbstractRegistryTest {
         } catch (Exception e) {
             Assert.assertTrue(e instanceof IllegalArgumentException);
         }
-        // check if unregister url successfully
-        abstractRegistry.register(testUrl);
+
         int beginSize = abstractRegistry.getRegistered().size();
+        // test unregister before register
         abstractRegistry.unregister(testUrl);
-        Assert.assertEquals(beginSize - 1, abstractRegistry.getRegistered().size());
-        // check if unregister a not exist url successfully
+        Assert.assertEquals(beginSize, abstractRegistry.getRegistered().size());
+
+        // test register then unregister
+        abstractRegistry.register(testUrl);
+        Assert.assertEquals(beginSize + 1, abstractRegistry.getRegistered().size());
         abstractRegistry.unregister(testUrl);
-        Assert.assertEquals(beginSize - 1, abstractRegistry.getRegistered().size());
+        Assert.assertEquals(beginSize, abstractRegistry.getRegistered().size());
+        // test double unregister after register
+        abstractRegistry.unregister(testUrl);
+        Assert.assertEquals(beginSize, abstractRegistry.getRegistered().size());
+    }
+
+    /**
+     * Multi thread test method for
+     * {@link org.apache.dubbo.registry.support.AbstractRegistry#unregister(URL)}.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testUnregisterMultiThread() {
+        int threadBeginSize = abstractRegistry.getRegistered().size();
+        CountDownLatch countDownLatch = new CountDownLatch(threadNumber);
+        Thread[] threads = new Thread[threadNumber];
+
+        for(int i = 0; i < threadNumber; i++){
+            URL url = URL.valueOf("dubbo://" + NetUtils.getLocalAddress().getHostName() + i);
+            abstractRegistry.register(url);
+        }
+        Assert.assertEquals(threadBeginSize + threadNumber, abstractRegistry.getRegistered().size());
+
+        // unregister by multi thread
+        for (int i = 0; i < threadNumber; i++){
+            URL url = URL.valueOf("dubbo://" + NetUtils.getLocalAddress().getHostName() + i);
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    abstractRegistry.unregister(url);
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        for(int i = 0; i < threadNumber; i++){
+            threads[i].run();
+        }
+
+        try {
+            // wait for all thread done
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(threadBeginSize, abstractRegistry.getRegistered().size());
     }
 
     /**
@@ -133,9 +225,48 @@ public class AbstractRegistryTest {
             Assert.assertTrue(e instanceof IllegalArgumentException);
         }
         // check if subscribe successfully
+        Assert.assertNull(abstractRegistry.getSubscribed().get(testUrl));
         abstractRegistry.subscribe(testUrl, listener);
         Assert.assertNotNull(abstractRegistry.getSubscribed().get(testUrl));
         Assert.assertTrue(abstractRegistry.getSubscribed().get(testUrl).contains(listener));
+    }
+
+    /**
+     * Multi thread test method for
+     * {@link org.apache.dubbo.registry.support.AbstractRegistry#subscribe(URL, NotifyListener)}.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testSubscribeMultiThread() {
+        CountDownLatch countDownLatch = new CountDownLatch(threadNumber);
+        Thread[] threads = new Thread[threadNumber];
+        NotifyListener[] listeners = new NotifyListener[threadNumber];
+        for (int i = 0; i<threadNumber; i++) {
+            listeners[i] = urls->notifySuccess = true;
+        }
+        for (int i = 0; i<threadNumber; i++) {
+            NotifyListener notifyListener = listeners[i];
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    abstractRegistry.subscribe(testUrl, notifyListener);
+                    countDownLatch.countDown();
+                }
+            });
+            threads[i].run();
+        }
+
+        try {
+            // wait for all thread done
+            countDownLatch.await();
+            for (int i = 0; i < threadNumber; i++) {
+                Assert.assertNotNull(abstractRegistry.getSubscribed().get(testUrl));
+                Assert.assertTrue(abstractRegistry.getSubscribed().get(testUrl).contains(listeners[i]));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -167,10 +298,52 @@ public class AbstractRegistryTest {
         } catch (Exception e) {
             Assert.assertTrue(e instanceof IllegalArgumentException);
         }
+
+        Assert.assertNull(abstractRegistry.getSubscribed().get(testUrl));
         // check if unsubscribe successfully
         abstractRegistry.subscribe(testUrl, listener);
         abstractRegistry.unsubscribe(testUrl, listener);
+        // Since we have subscribe testUrl, here should return a empty set instead of null
+        Assert.assertNotNull(abstractRegistry.getSubscribed().get(testUrl));
         Assert.assertFalse(abstractRegistry.getSubscribed().get(testUrl).contains(listener));
+    }
+
+    /**
+     * Multi thread test method for
+     * {@link org.apache.dubbo.registry.support.AbstractRegistry#unsubscribe(URL, NotifyListener)}.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testUnsubscribeMultiThread() {
+        CountDownLatch countDownLatch = new CountDownLatch(threadNumber);
+        Thread[] threads = new Thread[threadNumber];
+        NotifyListener[] listeners = new NotifyListener[threadNumber];
+        for (int i = 0; i < threadNumber; i++) {
+            listeners[i] = urls->notifySuccess=true;
+            abstractRegistry.subscribe(testUrl, listeners[i]);
+        }
+        for (int i = 0; i < threadNumber; i++){
+            NotifyListener notifyListener = listeners[i];
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    abstractRegistry.unsubscribe(testUrl, notifyListener);
+                    countDownLatch.countDown();
+                }
+            });
+            threads[i].run();
+        }
+
+        try {
+            // wait for all thread done
+            countDownLatch.await();
+            for(int i = 0; i < threadNumber; i++) {
+                Assert.assertFalse(abstractRegistry.getSubscribed().get(testUrl).contains(listener));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -179,6 +352,12 @@ public class AbstractRegistryTest {
      */
     @Test
     public void testRecover() throws Exception {
+        // test recover nothing
+        abstractRegistry.recover();
+        Assert.assertFalse(abstractRegistry.getRegistered().contains(testUrl));
+        Assert.assertNull(abstractRegistry.getSubscribed().get(testUrl));
+
+        // test recover
         abstractRegistry.register(testUrl);
         abstractRegistry.subscribe(testUrl, listener);
         abstractRegistry.recover();
@@ -195,7 +374,16 @@ public class AbstractRegistryTest {
     @Test
     public void testNotify() {
         abstractRegistry.subscribe(testUrl, listener);
+        // check parameters
+        Assert.assertFalse(notifySuccess);
+        abstractRegistry.notify(null);
+        Assert.assertFalse(notifySuccess);
+
         List<URL> urls = new ArrayList<>();
+        Assert.assertFalse(notifySuccess);
+        abstractRegistry.notify(urls);
+        Assert.assertFalse(notifySuccess);
+
         urls.add(testUrl);
         // check if notify successfully
         Assert.assertFalse(notifySuccess);
@@ -210,7 +398,7 @@ public class AbstractRegistryTest {
      * @throws Exception
      */
     @Test
-    public void testNotify2() {
+    public void testNotifyThreeArgs() {
         // check parameters
         try {
             abstractRegistry.notify(null, null, null);
@@ -225,6 +413,17 @@ public class AbstractRegistryTest {
         } catch (Exception e) {
             Assert.assertTrue(e instanceof IllegalArgumentException);
         }
+        // check parameters
+        try {
+            abstractRegistry.notify(null, listener, null);
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof IllegalArgumentException);
+        }
+
+        Assert.assertFalse(notifySuccess);
+        abstractRegistry.notify(testUrl, listener, null);
+        Assert.assertFalse(notifySuccess);
 
         List<URL> urls = new ArrayList<>();
         urls.add(testUrl);
