0212e0f61d25cf5695e505da5337aa59b444d881
huchuanling <1248940905@qq.com>
2018-08-14 12:09:27 +0800

    fix issue3

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/ServiceConfig.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/ServiceConfig.java
@@ -205,8 +205,8 @@ public class ServiceConfig<T> extends AbstractServiceConfig {
             return;
         }
 
+        appendProperties(this);
         if (delay != null && delay > 0) {
-            appendProperties(this);
             delayExportExecutor.schedule(new Runnable() {
                 @Override
                 public void run() {
@@ -310,7 +310,7 @@ public class ServiceConfig<T> extends AbstractServiceConfig {
         checkApplication();
         checkRegistry();
         checkProtocol();
-        appendProperties(this);
+//        appendProperties(this);
         checkStubAndMock(interfaceClass);
         if (path == null || path.length() == 0) {
             path = interfaceName;
