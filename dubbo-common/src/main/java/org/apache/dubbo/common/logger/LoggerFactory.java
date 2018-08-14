f6dbac583b42815771b7fba029120fafccddb064
wp170770 <wp170770@cainiao.com>
2018-08-14 11:06:22 +0800

    fix bug 1713

--- dubbo-common/src/main/java/org/apache/dubbo/common/logger/LoggerFactory.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/logger/LoggerFactory.java
@@ -20,6 +20,7 @@ import org.apache.dubbo.common.extension.ExtensionLoader;
 import org.apache.dubbo.common.logger.jcl.JclLoggerAdapter;
 import org.apache.dubbo.common.logger.jdk.JdkLoggerAdapter;
 import org.apache.dubbo.common.logger.log4j.Log4jLoggerAdapter;
+import org.apache.dubbo.common.logger.log4j2.Log4j2LoggerAdapter;
 import org.apache.dubbo.common.logger.slf4j.Slf4jLoggerAdapter;
 import org.apache.dubbo.common.logger.support.FailsafeLogger;
 
@@ -47,6 +48,8 @@ public class LoggerFactory {
             setLoggerAdapter(new Log4jLoggerAdapter());
         } else if ("jdk".equals(logger)) {
             setLoggerAdapter(new JdkLoggerAdapter());
+        } else if ("log4j2".equals(logger)) {
+            setLoggerAdapter(new Log4j2LoggerAdapter());
         } else {
             try {
                 setLoggerAdapter(new Log4jLoggerAdapter());
@@ -57,7 +60,11 @@ public class LoggerFactory {
                     try {
                         setLoggerAdapter(new JclLoggerAdapter());
                     } catch (Throwable e3) {
-                        setLoggerAdapter(new JdkLoggerAdapter());
+                        try {
+                            setLoggerAdapter(new JdkLoggerAdapter());
+                        } catch (Throwable e4) {
+                            setLoggerAdapter(new Log4j2LoggerAdapter());
+                        }
                     }
                 }
             }
