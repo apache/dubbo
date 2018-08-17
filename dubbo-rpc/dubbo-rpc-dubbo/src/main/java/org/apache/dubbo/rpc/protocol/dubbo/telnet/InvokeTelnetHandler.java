cccd9d575dac0e81907bf56473c4455c942caee7
Michael Chow <mizhoux@gmail.com>
2018-08-17 11:12:21 +0800

    fix telnet invoke NPE #2218 (#2273)
    * fix issue #2218
    * add some unit tests

--- dubbo-rpc/dubbo-rpc-dubbo/src/main/java/org/apache/dubbo/rpc/protocol/dubbo/telnet/InvokeTelnetHandler.java
+++ dubbo-rpc/dubbo-rpc-dubbo/src/main/java/org/apache/dubbo/rpc/protocol/dubbo/telnet/InvokeTelnetHandler.java
@@ -60,6 +60,19 @@ public class InvokeTelnetHandler implements TelnetHandler {
         for (int i = 0; i < types.length; i++) {
             Class<?> type = types[i];
             Object arg = args.get(i);
+
+            if (arg == null) {
+                // if the type is primitive, the method to invoke will cause NullPointerException definitely
+                // so we can offer a specified error message to the invoker in advance and avoid unnecessary invoking
+                if (type.isPrimitive()) {
+                    throw new NullPointerException(String.format(
+                            "The type of No.%d parameter is primitive(%s), but the value passed is null.", i + 1, type.getName()));
+                }
+
+                // if the type is not primitive, we choose to believe what the invoker want is a null value
+                continue;
+            }
+
             if (ReflectUtils.isPrimitive(arg.getClass())) {
                 if (!ReflectUtils.isPrimitive(type)) {
                     return false;
