853e3f2780d2e066f5a73833f3aaa1c052890a27
zhangyunlong <496933982@qq.com>
2018-08-27 22:21:44 +0800

    ReferenceConfigCache缓存功能修改

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/utils/ReferenceConfigCache.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/utils/ReferenceConfigCache.java
@@ -39,6 +39,7 @@ public class ReferenceConfigCache {
      * <p>
      * key example: <code>group1/org.apache.dubbo.foo.FooService:1.0.0</code>.
      */
+    private static final String SPLIT_TAG="---";
     public static final KeyGenerator DEFAULT_KEY_GENERATOR = new KeyGenerator() {
         @Override
         public String generateKey(ReferenceConfig<?> referenceConfig) {
@@ -61,12 +62,26 @@ public class ReferenceConfigCache {
             }
             return ret.toString();
         }
+
+        @Override
+        public String generateKey(String version, String url, String name) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(url);
+            builder.append(SPLIT_TAG);
+            builder.append(name);
+            builder.append(SPLIT_TAG);
+            builder.append(version);
+            builder.append(SPLIT_TAG);
+            return builder.toString();
+        }
     };
     static final ConcurrentMap<String, ReferenceConfigCache> cacheHolder = new ConcurrentHashMap<String, ReferenceConfigCache>();
     private final String name;
     private final KeyGenerator generator;
     ConcurrentMap<String, ReferenceConfig<?>> cache = new ConcurrentHashMap<String, ReferenceConfig<?>>();
 
+    //存储自定以字段到ReferenceConfig的映射
+    ConcurrentMap<String, ReferenceConfig<?>> keyCache = new ConcurrentHashMap<>();
     private ReferenceConfigCache(String name, KeyGenerator generator) {
         this.name = name;
         this.generator = generator;
@@ -115,12 +130,50 @@ public class ReferenceConfigCache {
         return (T) config.get();
     }
 
+    public <T> void  put(String version,String url,String name,ReferenceConfig<T> referenceConfig){
+        if(StringUtils.isBlank(version)){
+            throw new IllegalArgumentException("version is null");
+        }
+        if(StringUtils.isBlank(url)){
+            throw new IllegalArgumentException("url is null");
+        }
+        if(StringUtils.isBlank(name)){
+            throw new IllegalArgumentException("name is null");
+        }
+
+        String key = generator.generateKey(referenceConfig);
+        String strKey = generator.generateKey(version,url,name);
+        keyCache.put(strKey,referenceConfig);
+        cache.putIfAbsent(key, referenceConfig);
+    }
+
+    public <T> T getFromStrKey(String version,String url,String name) {
+        String strKey = generator.generateKey(version,url,name);
+        ReferenceConfig<?> config = keyCache.get(strKey);
+        if (config != null) {
+            return (T) config.get();
+        }
+
+        return null;
+    }
+
+    public Boolean isCacheExists(String version,String url,String name){
+        String strKey = generator.generateKey(version,url,name);
+        ReferenceConfig<?> config = keyCache.get(strKey);
+        return config!=null;
+    }
     void destroyKey(String key) {
         ReferenceConfig<?> config = cache.remove(key);
         if (config == null) return;
         config.destroy();
     }
 
+    public void destory(String version,String url,String name){
+        String strKey = generator.generateKey(version,url,name);
+        ReferenceConfig<?> config = keyCache.remove(strKey);
+        if (config == null) return;
+        config.destroy();
+    }
     /**
      * clear and destroy one {@link ReferenceConfig} in the cache.
      *
@@ -149,5 +202,6 @@ public class ReferenceConfigCache {
 
     public static interface KeyGenerator {
         String generateKey(ReferenceConfig<?> referenceConfig);
+        String generateKey(String version,String url,String name);
     }
 }
