c0be5e536e13e686f0251f2d88d4d6bcfe9406bc
ken.lj <ken.lj.hz@gmail.com>
2018-09-07 16:17:16 +0800

    Add demo change for tagrouter

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/AbstractReferenceConfig.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/AbstractReferenceConfig.java
@@ -61,6 +61,9 @@ public abstract class AbstractReferenceConfig extends AbstractInterfaceConfig {
     // group
     protected String group;
 
+    // enable dynamic tag
+    protected String enabledynamictag;
+
     public Boolean isCheck() {
         return check;
     }
