7d5731924e1bdeeea5572a2480838fd86f3a42d6
Song Kun <songkun.fp@gmail.com>
2018-09-19 07:35:10 +0800

    Fix typos in serialization unit tests (#2523)

--- dubbo-serialization/dubbo-serialization-jdk/src/test/java/org/apache/dubbo/common/serialize/serialization/CompactedJavaSerializationTest.java
+++ dubbo-serialization/dubbo-serialization-jdk/src/test/java/org/apache/dubbo/common/serialize/serialization/CompactedJavaSerializationTest.java
@@ -19,7 +19,7 @@ package org.apache.dubbo.common.serialize.serialization;
 
 import org.apache.dubbo.common.serialize.java.CompactedJavaSerialization;
 
-public class CompactedJavaSerializationTest extends AbstractSerializationPersionFailTest {
+public class CompactedJavaSerializationTest extends AbstractSerializationPersonFailTest {
     {
         serialization = new CompactedJavaSerialization();
     }
