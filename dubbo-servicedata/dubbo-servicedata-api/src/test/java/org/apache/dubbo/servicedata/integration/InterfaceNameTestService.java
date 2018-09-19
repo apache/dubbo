de598acae1058fc25ee8cdd1875e4a65e3df1cc7
cvictory <shenglicao2@gmail.com>
2018-09-19 19:19:59 +0800

    add serviceStoreService test and modify the code

---
+++ dubbo-servicedata/dubbo-servicedata-api/src/test/java/org/apache/dubbo/servicedata/integration/InterfaceNameTestService.java
@@ -0,0 +1,9 @@
+package org.apache.dubbo.servicedata.integration;
+
+/**
+ * @author cvictory ON 2018/9/19
+ */
+public interface InterfaceNameTestService {
+
+    public void test();
+}
---
+++ dubbo-servicedata/dubbo-servicedata-api/src/test/java/org/apache/dubbo/servicedata/integration/InterfaceNameTestService2.java
@@ -0,0 +1,9 @@
+package org.apache.dubbo.servicedata.integration;
+
+/**
+ * @author cvictory ON 2018/9/19
+ */
+public interface InterfaceNameTestService2 {
+
+    public void test2();
+}
