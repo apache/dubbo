428bcb7d0dac20101e146eed1470526d28b8bb26
manzhizhen <manzhizhen@163.com>
2018-10-09 14:38:25 +0800

    Fixed 2.6.x branch a minor issue with doConnect not using getConnectTimeout() in NettyClient，like  #2595

--- dubbo-remoting/dubbo-remoting-mina/src/main/java/com/alibaba/dubbo/remoting/transport/mina/MinaClient.java
+++ dubbo-remoting/dubbo-remoting-mina/src/main/java/com/alibaba/dubbo/remoting/transport/mina/MinaClient.java
@@ -78,7 +78,7 @@ public class MinaClient extends AbstractClient {
             cfg.setThreadModel(ThreadModel.MANUAL);
             cfg.getSessionConfig().setTcpNoDelay(true);
             cfg.getSessionConfig().setKeepAlive(true);
-            int timeout = getTimeout();
+            int timeout = getConnectTimeout();
             cfg.setConnectTimeout(timeout < 1000 ? 1 : timeout / 1000);
             // set codec.
             connector.getFilterChain().addLast("codec", new ProtocolCodecFilter(new MinaCodecAdapter(getCodec(), getUrl(), this)));
@@ -135,10 +135,10 @@ public class MinaClient extends AbstractClient {
             }
         });
         try {
-            finish.await(getTimeout(), TimeUnit.MILLISECONDS);
+            finish.await(getConnectTimeout(), TimeUnit.MILLISECONDS);
         } catch (InterruptedException e) {
             throw new RemotingException(this, "client(url: " + getUrl() + ") failed to connect to server " + getRemoteAddress() + " client-side timeout "
-                    + getTimeout() + "ms (elapsed: " + (System.currentTimeMillis() - start)
+                    + getConnectTimeout() + "ms (elapsed: " + (System.currentTimeMillis() - start)
                     + "ms) from netty client " + NetUtils.getLocalHost() + " using dubbo version "
                     + Version.getVersion() + ", cause: " + e.getMessage(), e);
         }
