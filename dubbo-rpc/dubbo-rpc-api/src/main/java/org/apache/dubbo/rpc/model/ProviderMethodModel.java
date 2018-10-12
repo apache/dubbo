45b3ab4436b4763e1890ded0728137de95df699d
Jeff-Lv <lrq315@gmail.com>
2018-10-12 11:40:11 +0800

    Merge pull request #2636, move the classes of model from config to rpc-api for reusing.

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/model/ProviderMethodModel.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/model/ProviderMethodModel.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.dubbo.config.model;
+package org.apache.dubbo.rpc.model;
 
 import java.lang.reflect.Method;
 
