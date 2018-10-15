1ef12208df62c7f23e13b0bd024955ea9f552f43
时无两丶 <442367943@qq.com>
2018-10-15 21:35:36 +0800

    Optimize ut for serialization model. (#2632)
    * 1. Integrate the unit test of the serialization module, code reuse.
    2. Remove redundant unit tests and pojo classes.
    3. Remove the useless pojo class in the common module.
    
    * fix ci fail
    
    * fix ci fail
    
    * fix ci fail
    
    * fix ci fail
    
    * remove author info

---
+++ dubbo-serialization/dubbo-serialization-test/src/test/java/org/apache/dubbo/common/serialize/support/SerializableClassRegistryTest.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.dubbo.common.serialize.support;
+
+import org.apache.dubbo.common.serialize.model.SerializablePerson;
+import org.apache.dubbo.common.serialize.model.person.Phone;
+import org.junit.Test;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class SerializableClassRegistryTest {
+    @Test
+    public void testAddClasses() {
+        SerializableClassRegistry.registerClass(SerializablePerson.class);
+        SerializableClassRegistry.registerClass(Phone.class);
+
+        Map<Class, Object> registeredClasses = SerializableClassRegistry.getRegisteredClasses();
+        assertThat(registeredClasses.size(), equalTo(2));
+    }
+}
