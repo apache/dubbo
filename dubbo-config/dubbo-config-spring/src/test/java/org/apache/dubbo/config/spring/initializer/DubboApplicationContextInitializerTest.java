3214b2b84c4960999ab141ec0205a641b9e57013
Ian Luo <ian.luo@gmail.com>
2018-10-16 16:16:54 +0800

    prototype for issue2570 (#2640)
    * #2570: dubbo all in one fail to start from a tomcat server when spring framework is absent
    
    * add comments and fix unit test
    
    * add license header
    
    * update comments in unit tests

--- dubbo-config/dubbo-config-spring/src/test/java/org/apache/dubbo/config/spring/initializer/DubboApplicationContextInitializerTest.java
+++ dubbo-config/dubbo-config-spring/src/test/java/org/apache/dubbo/config/spring/initializer/DubboApplicationContextInitializerTest.java
@@ -38,8 +38,9 @@ public class DubboApplicationContextInitializerTest {
         context.addLifecycleListener(new ContextConfig());
         tomcat.getHost().addChild(context);
         tomcat.start();
-        // there should be 1 application listener
-        Assert.assertEquals(1, context.getApplicationLifecycleListeners().length);
+        // there should be 2 application listeners, one is spring context listener,
+        // the other is its wrapper dubbo introduces.
+        Assert.assertEquals(2, context.getApplicationLifecycleListeners().length);
         // the first one should be Spring's built in ContextLoaderListener.
         Assert.assertTrue(context.getApplicationLifecycleListeners()[0] instanceof ContextLoaderListener);
         tomcat.stop();
@@ -58,10 +59,10 @@ public class DubboApplicationContextInitializerTest {
         context.addLifecycleListener(new ContextConfig());
         tomcat.getHost().addChild(context);
         tomcat.start();
-        // there should be 1 application listener
+        // there should be 1 application listener, which is spring context listener's wrapper introduced by dubbo
         Assert.assertEquals(1, context.getApplicationLifecycleListeners().length);
         // the first one should be Spring's built in ContextLoaderListener.
-        Assert.assertTrue(context.getApplicationLifecycleListeners()[0] instanceof ContextLoaderListener);
+        Assert.assertTrue(context.getApplicationLifecycleListeners()[0] instanceof DubboContextListener);
         tomcat.stop();
         tomcat.destroy();
     }
