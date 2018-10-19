a9cd3096f2838246924ea21374bd5f96139cbd8c
小马哥 <mercyblitz@gmail.com>
2018-10-19 14:45:10 +0800

    @Service and @Reference Optimization (#2657)
    * Polish apache/incubator-dubbo#2235 apache/incubator-dubbo#2251 apache/incubator-dubbo-spring-boot-project#243
    
    * Fixed bugs and optimized imports

--- dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/ServiceBean.java
+++ dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/ServiceBean.java
@@ -24,7 +24,9 @@ import com.alibaba.dubbo.config.ProviderConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.config.ServiceConfig;
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.config.spring.context.event.ServiceBeanExportedEvent;
 import com.alibaba.dubbo.config.spring.extension.SpringExtensionFactory;
+
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.BeanNameAware;
@@ -32,6 +34,8 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -46,7 +50,9 @@ import java.util.Map;
  *
  * @export
  */
-public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean, DisposableBean, ApplicationContextAware, ApplicationListener<ContextRefreshedEvent>, BeanNameAware {
+public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean, DisposableBean,
+        ApplicationContextAware, ApplicationListener<ContextRefreshedEvent>, BeanNameAware,
+        ApplicationEventPublisherAware {
 
     private static final long serialVersionUID = 213195494150089726L;
 
@@ -59,6 +65,8 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
     private transient String beanName;
 
     private transient boolean supportedApplicationListener;
+
+    private ApplicationEventPublisher applicationEventPublisher;
 
     public ServiceBean() {
         super();
@@ -265,6 +273,34 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
         }
     }
 
+    /**
+     * Get the name of {@link ServiceBean}
+     *
+     * @return {@link ServiceBean}'s name
+     * @since 2.6.5
+     */
+    public String getBeanName() {
+        return this.beanName;
+    }
+
+    /**
+     * @since 2.6.5
+     */
+    @Override
+    public void export() {
+        super.export();
+        // Publish ServiceBeanExportedEvent
+        publishExportEvent();
+    }
+
+    /**
+     * @since 2.6.5
+     */
+    private void publishExportEvent() {
+        ServiceBeanExportedEvent exportEvent = new ServiceBeanExportedEvent(this);
+        applicationEventPublisher.publishEvent(exportEvent);
+    }
+
     @Override
     public void destroy() throws Exception {
         // This will only be called for singleton scope bean, and expected to be called by spring shutdown hook when BeanFactory/ApplicationContext destroys.
@@ -279,5 +315,14 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
             return AopUtils.getTargetClass(ref);
         }
         return super.getServiceClass(ref);
+    }
+
+    /**
+     * @param applicationEventPublisher
+     * @since 2.6.5
+     */
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
     }
 }
