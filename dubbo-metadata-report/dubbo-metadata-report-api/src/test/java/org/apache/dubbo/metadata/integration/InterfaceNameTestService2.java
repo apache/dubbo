a5352d0e2a8d85f9f640e6d5b58fdb9b0516ee31
cvictory <shenglicao2@gmail.com>
2018-10-22 14:15:38 +0800

    renew the module name, package name, class name

---
+++ dubbo-metadata-report/dubbo-metadata-report-api/src/test/java/org/apache/dubbo/metadata/integration/InterfaceNameTestService2.java
@@ -0,0 +1,9 @@
+package org.apache.dubbo.metadata.integration;
+
+/**
+ *  2018/9/19
+ */
+public interface InterfaceNameTestService2 {
+
+    public void test2();
+}
---
+++ dubbo-metadata-report/dubbo-metadata-report-api/src/test/java/org/apache/dubbo/metadata/metadata/builder/SingleEnum.java
@@ -0,0 +1,8 @@
+package org.apache.dubbo.metadata.metadata.builder;
+
+/**
+ *  2018/9/28
+ */
+public enum SingleEnum {
+    FIRST,SECOND
+}
