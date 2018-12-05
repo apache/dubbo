d082d6d09478611d3f933de8f65ac782284b327e
cvictory <shenglicao2@gmail.com>
2018-12-05 19:10:59 +0800

    fix unit test

--- dubbo-metadata-report/dubbo-metadata-report-api/src/test/java/org/apache/dubbo/metadata/integration/MetadataReportServiceTest.java
+++ dubbo-metadata-report/dubbo-metadata-report-api/src/test/java/org/apache/dubbo/metadata/integration/MetadataReportServiceTest.java
@@ -78,7 +78,7 @@ public class MetadataReportServiceTest {
 
         URL publishUrl = URL.valueOf("dubbo://" + NetUtils.getLocalAddress().getHostName() + ":4444/org.apache.dubbo.TestService?version=1.0.3&application=vicpubp&interface=org.apache.dubbo.metadata.integration.InterfaceNameTestService");
         metadataReportService1.publishProvider(publishUrl);
-        Thread.sleep(100);
+        Thread.sleep(300);
 
         Assert.assertTrue(metadataReportService1.metadataReport instanceof JTestMetadataReport4Test);
 
@@ -98,7 +98,7 @@ public class MetadataReportServiceTest {
 
         URL publishUrl = URL.valueOf("dubbo://" + NetUtils.getLocalAddress().getHostName() + ":4444/org.apache.dubbo.TestService?version=1.0.x&application=vicpubconsumer&side=consumer");
         metadataReportService1.publishConsumer(publishUrl);
-        Thread.sleep(100);
+        Thread.sleep(300);
 
         Assert.assertTrue(metadataReportService1.metadataReport instanceof JTestMetadataReport4Test);
 
