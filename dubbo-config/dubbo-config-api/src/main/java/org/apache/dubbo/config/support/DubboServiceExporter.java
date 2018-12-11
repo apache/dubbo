7260e3296f2d95d0a9f7dbde6eef3fcf5c5f7fd7
lvxiang <lvxiang@58ganji.com>
2018-12-11 13:59:43 +0800

    '修改'

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/support/DubboServiceExporter.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/support/DubboServiceExporter.java
@@ -4,13 +4,15 @@ import org.apache.dubbo.config.ServiceConfig;
 import org.apache.dubbo.config.spi.ServiceExporter;
 
 /**
- * Created by lvxiang@ganji.com 2018/12/10 18:14
+ * Created by lvxiang2018/12/10 18:14
  *
- * @author <a href="mailto:lvxiang@ganji.com">simple</a>
+ * @author <a href="mailto:278076999@qq.com">simple</a>
  */
 public class DubboServiceExporter implements ServiceExporter {
+
     @Override
     public void doExport(String beanName, Object bean) {
+        //todo 将Bean 暴露和引入的逻辑抽取为单独模块
         ((ServiceConfig)bean).export();
     }
 }
