ba2c16c9f1b20f409e081da2466cceaaf5be2208
Ian Luo <ian.luo@gmail.com>
2018-12-11 15:10:42 +0800

    enhance org.apache.dubbo.rpc.protocol.dubbo.telnet.InvokeTelnetHandler#isMatch (#2941)
    * enhance isMatch
    
    * remove useless imports

--- dubbo-rpc/dubbo-rpc-dubbo/src/main/java/org/apache/dubbo/rpc/protocol/dubbo/telnet/InvokeTelnetHandler.java
+++ dubbo-rpc/dubbo-rpc-dubbo/src/main/java/org/apache/dubbo/rpc/protocol/dubbo/telnet/InvokeTelnetHandler.java
@@ -17,7 +17,6 @@
 package org.apache.dubbo.rpc.protocol.dubbo.telnet;
 
 import org.apache.dubbo.common.extension.Activate;
-import org.apache.dubbo.common.utils.CompatibleTypeUtils;
 import org.apache.dubbo.common.utils.PojoUtils;
 import org.apache.dubbo.common.utils.ReflectUtils;
 import org.apache.dubbo.common.utils.StringUtils;
@@ -76,14 +75,11 @@ public class InvokeTelnetHandler implements TelnetHandler {
             }
 
             if (ReflectUtils.isPrimitive(arg.getClass())) {
+                // allow string arg to enum type, @see PojoUtils.realize0()
                 if (arg instanceof String && type.isEnum()) {
-                    try {
-                        CompatibleTypeUtils.compatibleTypeConvert(arg, type);
-                    } catch (RuntimeException e) {
-                        return false;
-                    }
                     continue;
                 }
+
                 if (!ReflectUtils.isPrimitive(type)) {
                     return false;
                 }
