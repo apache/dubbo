e6e7ea8b12d952a9ccb53e73ace18231effbf767
Ian Luo <ian.luo@gmail.com>
2018-12-17 16:55:07 +0800

    Merge pull request #2963, fix potential concurrency issue in a simpler way for ActiveLimitFilter and ExecuteLimitFilter.

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/RpcStatus.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/RpcStatus.java
@@ -16,12 +16,10 @@
  */
 package org.apache.dubbo.rpc;
 
-import org.apache.dubbo.common.Constants;
 import org.apache.dubbo.common.URL;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -46,18 +44,6 @@ public class RpcStatus {
     private final AtomicLong maxElapsed = new AtomicLong();
     private final AtomicLong failedMaxElapsed = new AtomicLong();
     private final AtomicLong succeededMaxElapsed = new AtomicLong();
-
-    /**
-     * Semaphore used to control concurrency limit set by `executes`
-     */
-    private volatile Semaphore executesLimit;
-    private volatile int executesPermits;
-
-    /**
-     * Semaphore used to control concurrency limit set by `actives`
-     */
-    private volatile Semaphore activesLimit;
-    private volatile int activesPermits;
 
     private RpcStatus() {
     }
@@ -115,16 +101,23 @@ public class RpcStatus {
         }
     }
 
+    public static void beginCount(URL url, String methodName) {
+        beginCount(url, methodName, Integer.MAX_VALUE);
+    }
+
     /**
      * @param url
      */
-    public static void beginCount(URL url, String methodName) {
-        beginCount(getStatus(url));
-        beginCount(getStatus(url, methodName));
-    }
-
-    private static void beginCount(RpcStatus status) {
-        status.active.incrementAndGet();
+    public static boolean beginCount(URL url, String methodName, int max) {
+        RpcStatus appStatus = getStatus(url);
+        RpcStatus methodStatus = getStatus(url, methodName);
+        if (methodStatus.active.incrementAndGet() > max) {
+            methodStatus.active.decrementAndGet();
+            return false;
+        } else {
+            appStatus.active.incrementAndGet();
+            return true;
+        }
     }
 
     /**
@@ -318,46 +311,5 @@ public class RpcStatus {
         return getTotal();
     }
 
-    /**
-     * Get the semaphore for thread number. Semaphore's permits is decided by {@link Constants#EXECUTES_KEY}
-     *
-     * @param maxThreadNum value of {@link Constants#EXECUTES_KEY}
-     * @return thread number semaphore
-     */
-    public Semaphore getSemaphore(int maxThreadNum) {
-        if(maxThreadNum <= 0) {
-            return null;
-        }
 
-        if (executesLimit == null || executesPermits != maxThreadNum) {
-            synchronized (this) {
-                if (executesLimit == null || executesPermits != maxThreadNum) {
-                    executesLimit = new Semaphore(maxThreadNum);
-                    executesPermits = maxThreadNum;
-                }
-            }
-        }
-
-        return executesLimit;
-    }
-
-
-    /**
-     * Get the semaphore for thread number. Semaphore's permits is decided by {@link Constants#ACTIVES_KEY}
-     *
-     * @param maxThreadNum value of {@link Constants#ACTIVES_KEY}
-     * @return thread number semaphore
-     */
-    public Semaphore getActivesSemaphore(int maxThreadNum) {
-        if (activesLimit == null || activesPermits != maxThreadNum) {
-            synchronized (this) {
-                if (activesLimit == null || activesPermits != maxThreadNum) {
-                    activesLimit = new Semaphore(maxThreadNum);
-                    activesPermits = maxThreadNum;
-                }
-            }
-        }
-
-        return activesLimit;
-    }
 }
