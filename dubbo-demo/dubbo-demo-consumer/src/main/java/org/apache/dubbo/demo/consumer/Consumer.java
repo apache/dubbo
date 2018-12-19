ffbbb5957a54279d5bcd3f35f82a967e9dc77b8e
beiwei30 <ian.luo@gmail.com>
2018-12-19 17:28:24 +0800

    fix more cycle dependency

--- dubbo-demo/dubbo-demo-consumer/src/main/java/org/apache/dubbo/demo/consumer/Consumer.java
+++ dubbo-demo/dubbo-demo-consumer/src/main/java/org/apache/dubbo/demo/consumer/Consumer.java
@@ -31,14 +31,7 @@ public class Consumer {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-consumer.xml"});
         context.start();
         DemoService demoService = (DemoService) context.getBean("demoService"); // get remote service proxy
-        while (true) {
-            try {
-                Thread.sleep(1000);
-                String hello = demoService.sayHello("world"); // call remote method
-                System.out.println(hello); // get result
-            } catch (Throwable throwable) {
-                throwable.printStackTrace();
-            }
-        }
+        String hello = demoService.sayHello("world"); // call remote method
+        System.out.println(hello);
     }
 }
