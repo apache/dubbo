2bd36f75f09662ee46e44ef456a4cd9acd17d637
ken.lj <ken.lj.hz@gmail.com>
2018-12-20 18:13:23 +0800

    update demo

--- dubbo-demo/dubbo-demo-consumer/src/main/java/org/apache/dubbo/demo/consumer/Consumer.java
+++ dubbo-demo/dubbo-demo-consumer/src/main/java/org/apache/dubbo/demo/consumer/Consumer.java
@@ -18,6 +18,7 @@ package org.apache.dubbo.demo.consumer;
 
 import org.apache.dubbo.demo.DemoService;
 import org.apache.dubbo.rpc.RpcContext;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Consumer {
@@ -31,6 +32,7 @@ public class Consumer {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-consumer.xml"});
         context.start();
         DemoService demoService = (DemoService) context.getBean("demoService"); // get remote service proxy
+//        DemoService demoService1 = (DemoService) context.getBean("demoService1"); // get remote service proxy
         while (true) {
             try {
                 Thread.sleep(1000);
