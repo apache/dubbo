678cdb4184b9397c70b94e1b696cf11bcf16b1e5
Ian Luo <ian.luo@gmail.com>
2018-12-21 22:00:58 +0800

    [DUBBO-2988] Unrecognized the other provider (#3013)
    *     #2988 [Dubbo - telnet] Unrecognized the other provider
    
    * remove useless files added by accident.
    
    * fix cycle dependency
    
    * fix more cycle dependency
    
    * revert unnecessary change in demo
    
    * make unit test pass
    
    * revert LF to the original CRLF
    
    * change to use lambda
    
    * fix cycle dependency
    
    * remove useless imports
    
    * make unit test stable
    
    * make refactor change according to code review comments
    
    * refactor as review comments suggest.

---
+++ dubbo-rpc/dubbo-rpc-dubbo/src/test/java/org/apache/dubbo/rpc/validation/ValidationParameter.java
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.dubbo.rpc.validation;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * ValidationParameter
+ */
+public class ValidationParameter implements Serializable {
+
+    private static final long serialVersionUID = 7158911668568000392L;
+
+    @NotNull(groups = ValidationService.Update.class)
+    private Integer id;
+
+    @NotNull
+    @Size(min = 2, max = 20)
+    private String name;
+
+    // not allow to save null, but allow to update with null which means not update the field
+    @NotNull(groups = ValidationService.Save.class)
+    @Pattern(regexp = "^\\s*\\w+(?:\\.{0,1}[\\w-]+)*@[a-zA-Z0-9]+(?:[-.][a-zA-Z0-9]+)*\\.[a-zA-Z]+\\s*$")
+    private String email;
+
+    @Min(18)
+    @Max(100)
+    private int age;
+
+    @Past
+    private Date loginDate;
+
+    @Future
+    private Date expiryDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getLoginDate() {
+        return loginDate;
+    }
+
+    public void setLoginDate(Date loginDate) {
+        this.loginDate = loginDate;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+}
