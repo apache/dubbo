60506f2b519806c8e05d3e851c4a417fc7259aea
beiwei30 <ian.luo@gmail.com>
2018-12-27 17:12:06 +0800

    avoid dup code in init

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/integration/AbstractConfiguratorListener.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/integration/AbstractConfiguratorListener.java
@@ -18,9 +18,11 @@ package org.apache.dubbo.registry.integration;
 
 import org.apache.dubbo.common.logger.Logger;
 import org.apache.dubbo.common.logger.LoggerFactory;
+import org.apache.dubbo.common.utils.StringUtils;
 import org.apache.dubbo.configcenter.ConfigChangeEvent;
 import org.apache.dubbo.configcenter.ConfigChangeType;
 import org.apache.dubbo.configcenter.ConfigurationListener;
+import org.apache.dubbo.configcenter.DynamicConfiguration;
 import org.apache.dubbo.rpc.cluster.Configurator;
 import org.apache.dubbo.rpc.cluster.configurator.parser.ConfigParser;
 
@@ -34,6 +36,16 @@ public abstract class AbstractConfiguratorListener implements ConfigurationListe
     private static final Logger logger = LoggerFactory.getLogger(AbstractConfiguratorListener.class);
 
     protected List<Configurator> configurators = Collections.emptyList();
+
+
+    protected final void initWith(String key) {
+        DynamicConfiguration dynamicConfiguration = DynamicConfiguration.getDynamicConfiguration();
+        dynamicConfiguration.addListener(key, this);
+        String rawConfig = dynamicConfiguration.getConfig(key);
+        if (!StringUtils.isEmpty(rawConfig)) {
+            process(new ConfigChangeEvent(key, rawConfig));
+        }
+    }
 
     @Override
     public void process(ConfigChangeEvent event) {
