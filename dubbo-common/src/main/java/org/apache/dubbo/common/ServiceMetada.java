26a954b34af3e1c0e12218376429596ce1cba65e
kongming <kongming.lrq@taobao.com>
2018-12-27 16:01:56 +0800

    import a pojo ServiceMeta which is used as service level info

---
+++ dubbo-common/src/main/java/org/apache/dubbo/common/ServiceMetada.java
@@ -0,0 +1,71 @@
+package org.apache.dubbo.common;/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * data related to service level such as name, version, classloader of business service,
+ * security info, etc. Also with a AttributeMap for extension.
+ */
+public class ServiceMetada {
+
+    private final String serviceName;
+    private final String version;
+    private final String group;
+
+    /* will be transferred to remote side */
+    private final Map<String, Object> attachments = new ConcurrentHashMap<String, Object>();
+    /* used locally*/
+    private final Map<String, Object> attributeMap = new ConcurrentHashMap<String, Object>();
+
+    public ServiceMetada(String serviceName, String version, String group) {
+        this.serviceName = serviceName;
+        this.version = version;
+        this.group = group;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public Map<String, Object> getAttachments() {
+        return attachments;
+    }
+
+    public Map<String, Object> getAttributeMap() {
+        return attributeMap;
+    }
+
+    public void addAttribute(String key, Object value) {
+        this.attributeMap.put(key, value);
+    }
+
+    public void addAttachment(String key, Object value) {
+        this.attributeMap.put(key, value);
+    }
+
+
+}
