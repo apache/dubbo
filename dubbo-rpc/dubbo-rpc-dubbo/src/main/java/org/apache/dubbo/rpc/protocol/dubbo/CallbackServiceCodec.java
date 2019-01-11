23d0a603e52d982a7b92212cfa76d854b597cddc
赵禹光 <zhaoyuguang@lianjia.com>
2019-01-11 19:09:26 +0800

    Optimize the code: fix name issue

--- dubbo-rpc/dubbo-rpc-dubbo/src/main/java/org/apache/dubbo/rpc/protocol/dubbo/CallbackServiceCodec.java
+++ dubbo-rpc/dubbo-rpc-dubbo/src/main/java/org/apache/dubbo/rpc/protocol/dubbo/CallbackServiceCodec.java
@@ -110,7 +110,7 @@ class CallbackServiceCodec {
                     Exporter<?> exporter = protocol.export(invoker);
                     // this is used for tracing if instid has published service or not.
                     channel.setAttribute(cacheKey, exporter);
-                    logger.info("export a callback service :" + exportUrl + ", on " + channel + ", url is: " + url);
+                    logger.info("Exporter a callback service :" + exportUrl + ", on " + channel + ", url is: " + url);
                     increaseInstanceCount(channel, countKey);
                 }
             }
