1b063ec6fe730d5fa010f98c8eed136eaae2a3ec
kongming <kongming.lrq@taobao.com>
2019-01-15 17:33:53 +0800

    License info should appear first for class ServiceMetada

--- dubbo-common/src/main/java/org/apache/dubbo/common/ServiceMetada.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/ServiceMetada.java
@@ -1,4 +1,4 @@
-package org.apache.dubbo.common;/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,6 +14,7 @@ package org.apache.dubbo.common;/*
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.dubbo.common;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
