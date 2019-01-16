f65530c4f47b552ed1b00e0661f39e5237636ab5
ken.lj <ken.lj.hz@gmail.com>
2019-01-16 19:09:34 +0800

    Change router order in case of getUrl empty

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/Router.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/Router.java
@@ -98,9 +98,6 @@ public interface Router extends Comparable<Router> {
             if (getUrl() == null) {
                 return -1;
             }
-            if (getUrl() == null) {
-                return 1;
-            }
             return getUrl().toFullString().compareTo(o.getUrl().toFullString());
         } else {
             return getPriority() > o.getPriority() ? 1 : -1;
