ebbab775d9e6a9238f302f6c953146d3ab51ede8
crazyhzm <crazyhzm@gmail.com>
2019-01-17 11:03:03 +0800

    catch NumberFormatException and return 'null' if necessary

--- dubbo-common/src/main/java/org/apache/dubbo/common/utils/ClassHelper.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/utils/ClassHelper.java
@@ -271,18 +271,23 @@ public class ClassHelper {
             return value.length() > 0 ? value.charAt(0) : '\0';
         } else if (type == boolean.class || type == Boolean.class) {
             return Boolean.valueOf(value);
-        } else if (type == byte.class || type == Byte.class) {
-            return StringUtils.isNumeric(value, false) ? Byte.valueOf(value) : null;
-        } else if (type == short.class || type == Short.class) {
-            return StringUtils.isNumeric(value, false) ? Short.valueOf(value) : null;
-        } else if (type == int.class || type == Integer.class) {
-            return StringUtils.isNumeric(value, false) ? Integer.valueOf(value) : null;
-        } else if (type == long.class || type == Long.class) {
-            return StringUtils.isNumeric(value, false) ? Long.valueOf(value) : null;
-        } else if (type == float.class || type == Float.class) {
-            return StringUtils.isNumeric(value, true) ? Float.valueOf(value) : null;
-        } else if (type == double.class || type == Double.class) {
-            return StringUtils.isNumeric(value, true) ? Double.valueOf(value) : null;
+        }
+        try {
+            if (type == byte.class || type == Byte.class) {
+                return Byte.valueOf(value);
+            } else if (type == short.class || type == Short.class) {
+                return Short.valueOf(value);
+            } else if (type == int.class || type == Integer.class) {
+                return Integer.valueOf(value);
+            } else if (type == long.class || type == Long.class) {
+                return Long.valueOf(value);
+            } else if (type == float.class || type == Float.class) {
+                return Float.valueOf(value);
+            } else if (type == double.class || type == Double.class) {
+                return Double.valueOf(value);
+            }
+        } catch (NumberFormatException e) {
+            return null;
         }
         return value;
     }
