a19cb2ab9426a76d68038634ccdb8488647f42d5
xiuyuhang <442367943@qq.com>
2019-01-24 11:41:04 +0800

    revert code

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/filter/TimeoutFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/filter/TimeoutFilter.java
@@ -25,10 +25,9 @@ import org.apache.dubbo.rpc.Invocation;
 import org.apache.dubbo.rpc.Invoker;
 import org.apache.dubbo.rpc.Result;
 import org.apache.dubbo.rpc.RpcException;
+import org.apache.dubbo.rpc.RpcInvocation;
 
 import java.util.Arrays;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Log any invocation timeout, but don't stop server from running
@@ -38,20 +37,28 @@ public class TimeoutFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(TimeoutFilter.class);
 
-    private static final Map<Invocation, Long> START_TIME_KEEPER = new ConcurrentHashMap<>();
+    private static final String TIMEOUT_FILTER_START_TIME = "timeout_filter_start_time";
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        long start = System.currentTimeMillis();
-        START_TIME_KEEPER.put(invocation, start);
+        if (invocation.getAttachments() != null) {
+            long start = System.currentTimeMillis();
+            invocation.getAttachments().put(TIMEOUT_FILTER_START_TIME, String.valueOf(start));
+        } else {
+            if (invocation instanceof RpcInvocation) {
+                RpcInvocation invc = (RpcInvocation) invocation;
+                long start = System.currentTimeMillis();
+                invc.setAttachment(TIMEOUT_FILTER_START_TIME, String.valueOf(start));
+            }
+        }
         return invoker.invoke(invocation);
     }
 
     @Override
     public Result onResponse(Result result, Invoker<?> invoker, Invocation invocation) {
-        Long startTime = START_TIME_KEEPER.remove(invocation);
-        if (startTime != null) {
-            long elapsed = System.currentTimeMillis() - startTime;
+        String startAttach = invocation.getAttachment(TIMEOUT_FILTER_START_TIME);
+        if (startAttach != null) {
+            long elapsed = System.currentTimeMillis() - Long.valueOf(startAttach);
             if (invoker.getUrl() != null
                     && elapsed > invoker.getUrl().getMethodParameter(invocation.getMethodName(),
                     "timeout", Integer.MAX_VALUE)) {
