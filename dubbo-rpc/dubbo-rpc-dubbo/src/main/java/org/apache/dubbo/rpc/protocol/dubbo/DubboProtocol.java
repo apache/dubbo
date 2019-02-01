9dc5c1b0b65df87653a194a0979f6230e307b964
manzhizhen <manzhizhen@163.com>
2019-02-01 22:44:48 +0800

    format code

--- dubbo-rpc/dubbo-rpc-dubbo/src/main/java/org/apache/dubbo/rpc/protocol/dubbo/DubboProtocol.java
+++ dubbo-rpc/dubbo-rpc-dubbo/src/main/java/org/apache/dubbo/rpc/protocol/dubbo/DubboProtocol.java
@@ -564,6 +564,7 @@ public class DubboProtocol extends AbstractProtocol {
             // connection should be lazy
             if (url.getParameter(Constants.LAZY_CONNECT_KEY, false)) {
                 client = new LazyConnectExchangeClient(url, requestHandler);
+
             } else {
                 client = Exchangers.connect(url, requestHandler);
             }
