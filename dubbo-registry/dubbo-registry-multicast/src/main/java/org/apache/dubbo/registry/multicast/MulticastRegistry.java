2140b9e5744ee89be65bb409701e0cee8bce0041
kexianjun <kexianjun1991@hotmail.com>
2019-02-11 18:17:49 +0800

    more improvement

--- dubbo-registry/dubbo-registry-multicast/src/main/java/org/apache/dubbo/registry/multicast/MulticastRegistry.java
+++ dubbo-registry/dubbo-registry-multicast/src/main/java/org/apache/dubbo/registry/multicast/MulticastRegistry.java
@@ -198,15 +198,16 @@ public class MulticastRegistry extends FailbackRegistry {
                 for (URL u : urls) {
                     if (UrlUtils.isMatch(url, u)) {
                         boolean isLocalhost = false;
-                        if (remoteAddress != null && remoteAddress.getAddress() != null && NetUtils.getLocalHost().equals(remoteAddress.getAddress().getHostAddress())) {
+                        String localHost = NetUtils.getLocalHost();
+                        if (remoteAddress != null && remoteAddress.getAddress() != null && localHost.equals(remoteAddress.getAddress().getHostAddress())) {
                             isLocalhost = true;
                         }
-                        if (!isLocalhost && NetUtils.getLocalHost().equals(url.getIp())) {
+                        if (!isLocalhost && localHost.equals(url.getIp())) {
                             isLocalhost = true;
                         }
-                        String host = remoteAddress != null && remoteAddress.getAddress() != null ? remoteAddress.getAddress().getHostAddress() : url.getIp();
                         if (url.getParameter("unicast", true) // Whether the consumer's machine has only one process
                                 && !isLocalhost) { // Multiple processes in the same machine cannot be unicast with unicast or there will be only one process receiving information
+                            String host = remoteAddress != null && remoteAddress.getAddress() != null ? remoteAddress.getAddress().getHostAddress() : url.getIp();
                             unicast(Constants.REGISTER + " " + u.toFullString(), host);
                         } else {
                             multicast(Constants.REGISTER + " " + u.toFullString());
