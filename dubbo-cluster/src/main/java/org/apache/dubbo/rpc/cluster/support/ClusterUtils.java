d11b0d396988e0d74700e02670b05c6e69c1abc2
kexianjun <kexianjun1991@hotmail.com>
2019-02-25 19:23:51 +0800

    fix UT failed

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/support/ClusterUtils.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/support/ClusterUtils.java
@@ -84,7 +84,7 @@ public class ClusterUtils {
             String remoteGroup = map.get(Constants.GROUP_KEY);
             String remoteRelease = map.get(Constants.RELEASE_KEY);
             map.putAll(localMap);
-            if (StringUtils.isNotEmpty(remoteGroup)) {
+            if (!map.containsKey(Constants.GROUP_KEY) || StringUtils.isNotEmpty(remoteGroup)) {
                 map.put(Constants.GROUP_KEY, remoteGroup);
             }
             // we should always keep the Provider RELEASE_KEY not overrode by the the value on Consumer side.
