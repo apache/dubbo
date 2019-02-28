c2e36122c1ae8605a4cd4ba1dc07287d9c1f8769
cvictory <shenglicao2@gmail.com>
2019-02-28 17:24:49 +0800

    fill license

--- dubbo-common/src/main/java/org/apache/dubbo/common/utils/CIDRUtils.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/utils/CIDRUtils.java
@@ -1,20 +1,4 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*
  * The MIT License
  *
  * Copyright (c) 2013 Edin Dazdarevic (edin.dazdarevic@gmail.com)
@@ -133,14 +117,14 @@ public class CIDRUtils {
         return ret;
     }
 
-//    public String getNetworkAddress() {
-//
-//        return this.startAddress.getHostAddress();
-//    }
-//
-//    public String getBroadcastAddress() {
-//        return this.endAddress.getHostAddress();
-//    }
+    public String getNetworkAddress() {
+
+        return this.startAddress.getHostAddress();
+    }
+
+    public String getBroadcastAddress() {
+        return this.endAddress.getHostAddress();
+    }
 
     public boolean isInRange(String ipAddress) throws UnknownHostException {
         InetAddress address = InetAddress.getByName(ipAddress);
