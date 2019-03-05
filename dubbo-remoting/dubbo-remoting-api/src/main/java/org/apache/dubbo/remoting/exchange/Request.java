db732ed847cb9ac90ce962de2bc33b6811fde5c1
xiuyuhang <442367943@qq.com>
2019-03-05 11:45:49 +0800

    remove failover in Request

--- dubbo-remoting/dubbo-remoting-api/src/main/java/org/apache/dubbo/remoting/exchange/Request.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/org/apache/dubbo/remoting/exchange/Request.java
@@ -41,11 +41,6 @@ public class Request {
 
     private boolean mBroken = false;
 
-    /**
-     * whether this request is a failover request.
-     */
-    private boolean failover = false;
-
     private Object mData;
 
     public Request() {
@@ -132,14 +127,6 @@ public class Request {
         if (isHeartbeat) {
             setEvent(HEARTBEAT_EVENT);
         }
-    }
-
-    public boolean isFailover() {
-        return failover;
-    }
-
-    public void setFailover(boolean failover) {
-        this.failover = failover;
     }
 
     @Override
