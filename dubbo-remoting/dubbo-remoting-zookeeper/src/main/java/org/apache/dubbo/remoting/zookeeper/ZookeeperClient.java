3038b92ace12872904e03204a90314def02ac48d
cvictory <shenglicao2@gmail.com>
2019-03-14 19:14:03 +0800

    Merge pull request #3603, configcenter share zookeeper connection with registry.
    Fixes #3288

--- dubbo-remoting/dubbo-remoting-zookeeper/src/main/java/org/apache/dubbo/remoting/zookeeper/ZookeeperClient.java
+++ dubbo-remoting/dubbo-remoting-zookeeper/src/main/java/org/apache/dubbo/remoting/zookeeper/ZookeeperClient.java
@@ -19,6 +19,7 @@ package org.apache.dubbo.remoting.zookeeper;
 import org.apache.dubbo.common.URL;
 
 import java.util.List;
+import java.util.concurrent.Executor;
 
 public interface ZookeeperClient {
 
@@ -29,6 +30,21 @@ public interface ZookeeperClient {
     List<String> getChildren(String path);
 
     List<String> addChildListener(String path, ChildListener listener);
+
+    /**
+     * @param path:    directory. All of child of path will be listened.
+     * @param listener
+     */
+    void addDataListener(String path, DataListener listener);
+
+    /**
+     * @param path:    directory. All of child of path will be listened.
+     * @param listener
+     * @param executor another thread
+     */
+    void addDataListener(String path, DataListener listener, Executor executor);
+
+    void removeDataListener(String path, DataListener listener);
 
     void removeChildListener(String path, ChildListener listener);
 
