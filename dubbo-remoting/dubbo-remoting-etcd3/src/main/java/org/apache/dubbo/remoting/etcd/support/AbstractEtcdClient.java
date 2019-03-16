a28ea34f1b9720531be923f41804cb529eceea42
Huxing Zhang <huxing.zhang@gmail.com>
2019-03-16 13:11:33 +0800

    [Dubbo-3653] etcd as config center (#3663)
    * Minor refactor, no functinoal change.
    
    * Separate ConnectionStateListener
    
    * Simplify code
    
    * Fix typo
    
    * Support get external config from etcd config center
    
    * Polish diamond operator
    
    * Initial etcd support as config center
    
    * Add a put interface for JEtcdClient
    
    * Enhanced Etcd config center support with the ability to watch and cancel watch
    
    * Polish code
    
    * Distinguish modification event and delete event
    
    * Add etcd registry and configcenter to dubbo-all
    
    * Watch again when connection is re-established

--- dubbo-remoting/dubbo-remoting-etcd3/src/main/java/org/apache/dubbo/remoting/etcd/support/AbstractEtcdClient.java
+++ dubbo-remoting/dubbo-remoting-etcd3/src/main/java/org/apache/dubbo/remoting/etcd/support/AbstractEtcdClient.java
@@ -57,7 +57,7 @@ public abstract class AbstractEtcdClient<WatcherListener> implements EtcdClient 
 
     private final Set<StateListener> stateListeners = new ConcurrentHashSet<>();
 
-    private final ConcurrentMap<String, ConcurrentMap<ChildListener, WatcherListener>> childListeners = new ConcurrentHashMap<String, ConcurrentMap<ChildListener, WatcherListener>>();
+    private final ConcurrentMap<String, ConcurrentMap<ChildListener, WatcherListener>> childListeners = new ConcurrentHashMap<>();
     private final List<String> categroies = Arrays.asList(Constants.PROVIDERS_CATEGORY
             , Constants.CONSUMERS_CATEGORY
             , Constants.ROUTERS_CATEGORY
@@ -99,7 +99,7 @@ public abstract class AbstractEtcdClient<WatcherListener> implements EtcdClient 
     public List<String> addChildListener(String path, final ChildListener listener) {
         ConcurrentMap<ChildListener, WatcherListener> listeners = childListeners.get(path);
         if (listeners == null) {
-            childListeners.putIfAbsent(path, new ConcurrentHashMap<ChildListener, WatcherListener>());
+            childListeners.putIfAbsent(path, new ConcurrentHashMap<>());
             listeners = childListeners.get(path);
         }
         WatcherListener targetListener = listeners.get(listener);
