d75d4852db6adb7d4256e1e39c472eb1a9c508eb
zhangfei <zhangfei_eason@163.com>
2019-03-26 13:41:08 +0800

    style: code tidy up

--- dubbo-serialization/dubbo-serialization-test/src/test/java/org/apache/dubbo/common/serialize/protostuff/ProtostuffObjectOutputTest.java
+++ dubbo-serialization/dubbo-serialization-test/src/test/java/org/apache/dubbo/common/serialize/protostuff/ProtostuffObjectOutputTest.java
@@ -1,16 +1,11 @@
 package org.apache.dubbo.common.serialize.protostuff;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.nullValue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import org.apache.dubbo.common.serialize.fastjson.FastJsonObjectInput;
-import org.apache.dubbo.common.serialize.fastjson.FastJsonObjectOutput;
-import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
