cad8d16a7481cc766af606573204de3f00025152
Ian Luo <ian.luo@gmail.com>
2019-05-10 14:39:18 +0800

    [DUBBO-3137]: step2 - seperate constants for cluster, common, filter, monitor, and registry

---
+++ dubbo-common/src/main/java/org/apache/dubbo/common/constants/FilterConstants.java
@@ -0,0 +1,7 @@
+package org.apache.dubbo.common.constants;
+
+public interface FilterConstants {
+    String CACHE_KEY = "cache";
+
+    String VALIDATION_KEY = "validation";
+}
---
+++ dubbo-common/src/main/java/org/apache/dubbo/common/constants/MonitorConstants.java
@@ -0,0 +1,29 @@
+package org.apache.dubbo.common.constants;
+
+public interface MonitorConstants {
+    String MONITOR_KEY = "monitor";
+
+    String LOGSTAT_PROTOCOL = "logstat";
+
+    String COUNT_PROTOCOL = "count";
+
+    String DUBBO_PROVIDER = "dubbo.provider";
+
+    String DUBBO_CONSUMER = "dubbo.consumer";
+
+    String DUBBO_PROVIDER_METHOD = "dubbo.provider.method";
+
+    String DUBBO_CONSUMER_METHOD = "dubbo.consumer.method";
+
+    String SERVICE = "service";
+
+    String METHOD = "method";
+
+    String DUBBO_GROUP = "dubbo";
+
+    String METRICS_KEY = "metrics";
+
+    String METRICS_PORT = "metrics.port";
+
+    String METRICS_PROTOCOL = "metrics.protocol";
+}
