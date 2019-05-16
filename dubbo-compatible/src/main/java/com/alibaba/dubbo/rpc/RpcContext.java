13cb6a12f38db65fb274b83bd84714d88c7064ba
cvictory <shenglicao2@gmail.com>
2019-05-16 10:10:46 +0800

    revert the licence and fix conflict

--- dubbo-compatible/src/main/java/com/alibaba/dubbo/rpc/RpcContext.java
+++ dubbo-compatible/src/main/java/com/alibaba/dubbo/rpc/RpcContext.java
@@ -1,9 +1,9 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
- * rpcContext work for additional information regarding copyright ownership.
- * The ASF licenses rpcContext file to You under the Apache License, Version 2.0
- * (the "License"); you may not use rpcContext file except in compliance with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
