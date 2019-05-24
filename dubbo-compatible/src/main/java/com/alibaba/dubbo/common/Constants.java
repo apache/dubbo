69d061ab3ec1c1bff6405ef5e01206ac0275da96
Ian Luo <ian.luo@gmail.com>
2019-05-24 10:25:50 +0800

    let compatible constants to reference all constants

--- dubbo-compatible/src/main/java/com/alibaba/dubbo/common/Constants.java
+++ dubbo-compatible/src/main/java/com/alibaba/dubbo/common/Constants.java
@@ -24,6 +24,20 @@ import org.apache.dubbo.common.constants.RegistryConstants;
 import org.apache.dubbo.common.constants.RemotingConstants;
 
 @Deprecated
-public class Constants implements CommonConstants, QosConstants, FilterConstants, RegistryConstants,
-        RemotingConstants, org.apache.dubbo.rpc.cluster.Constants, org.apache.dubbo.monitor.Constants {
+public class Constants implements CommonConstants,
+        QosConstants,
+        FilterConstants,
+        RegistryConstants,
+        RemotingConstants,
+        org.apache.dubbo.config.Constants,
+        org.apache.dubbo.remoting.Constants,
+        org.apache.dubbo.rpc.cluster.Constants,
+        org.apache.dubbo.monitor.Constants,
+        org.apache.dubbo.rpc.Constants,
+        org.apache.dubbo.rpc.protocol.dubbo.Constants,
+        org.apache.dubbo.common.serialize.Constants,
+        org.apache.dubbo.configcenter.Constants,
+        org.apache.dubbo.metadata.support.Constants,
+        org.apache.dubbo.rpc.protocol.rest.Constants,
+        org.apache.dubbo.registry.Constants {
 }
