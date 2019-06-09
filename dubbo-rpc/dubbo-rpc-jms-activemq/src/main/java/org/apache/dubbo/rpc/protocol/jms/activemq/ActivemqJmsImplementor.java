4a7226ad42c64b4a23e81acc7d866d26cc0a7f1a
Daniela Morais <dani.m@hotmail.co.uk>
2019-06-09 01:36:13 -0300

    Changes the name 'dubbo-rpc-jms-implementor-activemq' to 'dubbo-rpc-jms-activemq'

--- dubbo-rpc/dubbo-rpc-jms-activemq/src/main/java/org/apache/dubbo/rpc/protocol/jms/activemq/ActivemqJmsImplementor.java
+++ dubbo-rpc/dubbo-rpc-jms-activemq/src/main/java/org/apache/dubbo/rpc/protocol/jms/activemq/ActivemqJmsImplementor.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.dubbo.rpc.protocol.jms.implementor.activemq;
+package org.apache.dubbo.rpc.protocol.jms.activemq;
 
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.rpc.protocol.jms.implementor.JmsImplementor;
