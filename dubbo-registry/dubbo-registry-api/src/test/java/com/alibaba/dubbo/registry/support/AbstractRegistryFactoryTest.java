167f98cebaf6520f9815638ca0b0015ca25797bb
zetas <356082462@qq.com>
2019-06-13 02:47:24 +0800

    Update AbstractRegistryFactoryTest.java

--- dubbo-registry/dubbo-registry-api/src/test/java/com/alibaba/dubbo/registry/support/AbstractRegistryFactoryTest.java
+++ dubbo-registry/dubbo-registry-api/src/test/java/com/alibaba/dubbo/registry/support/AbstractRegistryFactoryTest.java
@@ -89,7 +89,7 @@ public class AbstractRegistryFactoryTest {
     public void testRegistryFactoryIpCache() throws Exception {
         Registry registry1 = registryFactory.getRegistry(URL.valueOf("dubbo://" + NetUtils.getLocalAddress().getHostName() + ":2233"));
         Registry registry2 = registryFactory.getRegistry(URL.valueOf("dubbo://" + NetUtils.getLocalAddress().getHostAddress() + ":2233"));
-        Assert.assertEquals(registry1, registry2);
+        Assert.assertNotSame(registry1, registry2);
     }
 
     @Test
