f19436cd3cc3f9a6b9f2e2ab15289e83d52b9119
Daniela Morais <dani.m@hotmail.co.uk>
2019-06-12 22:49:18 -0300

    Create cached gauge for lb

---
+++ dubbo-monitor/dubbo-monitor-default/src/main/java/org/apache/dubbo/monitor/dubbo/CpuUsageGauge.java
@@ -0,0 +1,32 @@
+package org.apache.dubbo.monitor.dubbo;
+
+import com.alibaba.metrics.CachedGauge;
+import com.alibaba.metrics.Clock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * A cached gauge for load balancer statistics
+ */
+public class CpuUsageGauge extends CachedGauge<AtomicLong> {
+    private AtomicLong cpuValue;
+
+    public CpuUsageGauge(long timeout, TimeUnit timeoutUnit) {
+        super(timeout, timeoutUnit);
+    }
+
+    public CpuUsageGauge(long timeout, TimeUnit timeoutUnit, AtomicLong value) {
+        super(timeout, timeoutUnit);
+        this.cpuValue = value;
+    }
+
+    @Override
+    protected AtomicLong loadValue() {
+        return cpuValue;
+    }
+
+    public void setCpuValue(AtomicLong cpuValue) {
+        this.cpuValue = cpuValue;
+    }
+}
