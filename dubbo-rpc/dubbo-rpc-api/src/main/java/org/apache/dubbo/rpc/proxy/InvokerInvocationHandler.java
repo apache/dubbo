8f6d5954c5649782ce46d79d60c0c0826b6a3160
ken.lj <ken.lj.hz@gmail.com>
2019-06-15 13:32:19 +0800

    remove method check

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/proxy/InvokerInvocationHandler.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/proxy/InvokerInvocationHandler.java
@@ -38,19 +38,19 @@ public class InvokerInvocationHandler implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         String methodName = method.getName();
-        Class<?>[] parameterTypes = method.getParameterTypes();
-        if (method.getDeclaringClass() == Object.class) {
-            return method.invoke(invoker, args);
-        }
-        if ("toString".equals(methodName) && parameterTypes.length == 0) {
-            return invoker.toString();
-        }
-        if ("hashCode".equals(methodName) && parameterTypes.length == 0) {
-            return invoker.hashCode();
-        }
-        if ("equals".equals(methodName) && parameterTypes.length == 1) {
-            return invoker.equals(args[0]);
-        }
+//        Class<?>[] parameterTypes = method.getParameterTypes();
+//        if (method.getDeclaringClass() == Object.class) {
+//            return method.invoke(invoker, args);
+//        }
+//        if ("toString".equals(methodName) && parameterTypes.length == 0) {
+//            return invoker.toString();
+//        }
+//        if ("hashCode".equals(methodName) && parameterTypes.length == 0) {
+//            return invoker.hashCode();
+//        }
+//        if ("equals".equals(methodName) && parameterTypes.length == 1) {
+//            return invoker.equals(args[0]);
+//        }
 
         return invoker.invoke(new RpcInvocation(method, invoker.getInterface().getName(), args)).recreate();
     }
