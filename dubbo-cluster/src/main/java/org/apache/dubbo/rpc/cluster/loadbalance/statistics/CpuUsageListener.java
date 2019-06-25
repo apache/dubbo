3e6c23817bd3fda9cd614ed39c5ec97b1a629e68
Daniela Morais <dani.m@hotmail.co.uk>
2019-06-25 19:45:06 -0300

    Remove unused import

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/statistics/CpuUsageListener.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/statistics/CpuUsageListener.java
@@ -1,7 +1,5 @@
 package org.apache.dubbo.rpc.cluster.loadbalance.statistics;
 
-import com.alibaba.metrics.Gauge;
-
 public interface CpuUsageListener {
     void cpuChanged(String ip, Float cpu);
 }
