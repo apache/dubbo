2819b82eeac979cb3765b8efe663be5e66000105
Daniela Morais <dani.m@hotmail.co.uk>
2019-06-25 01:31:50 -0300

    Create a new load balancer

---
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/StatisticsLoadBalance.java
@@ -0,0 +1,26 @@
+package org.apache.dubbo.rpc.cluster.loadbalance;
+
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.monitor.dubbo.CpuUsageService;
+import org.apache.dubbo.monitor.dubbo.CpuUsageServiceImpl;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StatisticsLoadBalance extends AbstractLoadBalance {
+
+    private Map<String, Float> cpuUsage = new HashMap<>();
+
+    public StatisticsLoadBalance() {
+        CpuUsageService cpuUsageService = new CpuUsageServiceImpl(100L, 500L);
+        cpuUsageService.addListener("statisticsloadbalance", (ip, cpu) -> cpuUsage.put(ip, cpu));
+    }
+
+    @Override
+    protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
+        throw new IllegalStateException("Method unimplemented");
+    }
+}
