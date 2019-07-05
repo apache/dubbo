5eeb0616dd340cd9eeac809211121f25c2376e62
zhangzhou <2023375439>
2019-07-05 17:15:56 +0800

    第一次修改

--- dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-consumer/src/main/java/org/apache/dubbo/demo/consumer/Application.java
+++ dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-consumer/src/main/java/org/apache/dubbo/demo/consumer/Application.java
@@ -16,9 +16,7 @@
  */
 package org.apache.dubbo.demo.consumer;
 
-import org.apache.dubbo.demo.DemoService;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     /**
