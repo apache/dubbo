0182e000fbefbfe299ce0235e3a1b7e33500a4dd
ken.lj <ken.lj.hz@gmail.com>
2019-07-09 15:55:08 +0800

    change if condition

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/FutureContext.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/FutureContext.java
@@ -99,8 +99,8 @@ public class FutureContext {
      */
     @Deprecated
     public void setCompatibleFuture(CompletableFuture<?> compatibleFuture) {
+        this.compatibleFuture = compatibleFuture;
         if (compatibleFuture != null) {
-            this.compatibleFuture = compatibleFuture;
             this.setFuture(new FutureAdapter(compatibleFuture));
         }
     }
