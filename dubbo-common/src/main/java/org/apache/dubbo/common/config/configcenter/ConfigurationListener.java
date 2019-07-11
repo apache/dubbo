f51b3944c36679500ce0da3eee79843e6dd467d9
mercyblitz <mercyblitz@gmail.com>
2019-07-11 15:13:08 +0800

    Polish apache/dubbo#4542 : [Enhancement] Adapt the Java standard Event/Listener mechanism

--- dubbo-common/src/main/java/org/apache/dubbo/common/config/configcenter/ConfigurationListener.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/config/configcenter/ConfigurationListener.java
@@ -16,10 +16,12 @@
  */
 package org.apache.dubbo.common.config.configcenter;
 
+import java.util.EventListener;
+
 /**
  * Config listener, will get notified when the config it listens on changes.
  */
-public interface ConfigurationListener {
+public interface ConfigurationListener extends EventListener {
 
     /**
      * Listener call back method. Listener gets notified by this method once there's any change happens on the config
