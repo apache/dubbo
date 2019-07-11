987f84f2896bd36e2d8f2e45d3be71ddbaff7e46
Daniela Morais <dani.m@hotmail.co.uk>
2019-07-10 21:35:37 -0300

    Add print for debug

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/StatisticsLoadBalance.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/StatisticsLoadBalance.java
@@ -17,15 +17,21 @@
 package org.apache.dubbo.rpc.cluster.loadbalance;
 
 import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.URLBuilder;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
 import org.apache.dubbo.rpc.cluster.CpuUsageFactory;
 import org.apache.dubbo.rpc.cluster.loadbalance.statistics.CpuUsageListener;
 import org.apache.dubbo.rpc.cluster.loadbalance.statistics.CpuUsageService;
-import org.apache.dubbo.rpc.Invocation;
-import org.apache.dubbo.rpc.Invoker;
 
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import static org.apache.dubbo.common.constants.CommonConstants.INTERFACE_KEY;
+import static org.apache.dubbo.common.constants.CommonConstants.METHODS_KEY;
+import static org.apache.dubbo.remoting.Constants.CHECK_KEY;
+import static org.apache.dubbo.rpc.Constants.REFERENCE_FILTER_KEY;
 
 public class StatisticsLoadBalance extends AbstractLoadBalance {
 
@@ -36,20 +42,36 @@ public class StatisticsLoadBalance extends AbstractLoadBalance {
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
         int index = (int)(invokers.size() * Math.random());
         Invoker<T> invoker = invokers.get(index);
-        cpuUsage.computeIfAbsent(invoker.getUrl().getAddress(), value -> {
-            CpuUsageService cpuUsageService = cpuUsageFactory.createCpuUsageService(invoker.getUrl());
-            CpuUsageListener cpuUsageListener = new CpuUsageListenerImpl();
-            cpuUsageService.addListener("statisticsloadbalance", cpuUsageListener);
-            return 0.0f;
-        });
+        if (invoker.getUrl() != null) {
+            print();
+            cpuUsage.computeIfAbsent(invoker.getUrl().getAddress(), value -> {
+                URLBuilder urlBuilder = URLBuilder.from(invoker.getUrl());
+                urlBuilder.setPath(CpuUsageService.class.getName());
+                urlBuilder.addParameter(INTERFACE_KEY, CpuUsageService.class.getName());
+                urlBuilder.addParameter("bean.name", CpuUsageService.class.getName());
+                urlBuilder.addParameter(METHODS_KEY, "addListener,removeListener");
+                urlBuilder.addParameters(CHECK_KEY, String.valueOf(false), REFERENCE_FILTER_KEY, "");
+                urlBuilder.addParameter("addListener.1.callback", true);
+                CpuUsageService cpuUsageService = cpuUsageFactory.createCpuUsageService(invoker.getUrl());
+
+                cpuUsageService.addListener("statisticsloadbalance", new CpuUsageListenerImpl());
+                return 0.0f;
+            });
+        }
         return invoker;
+    }
+
+    private void print() {
+        for (Map.Entry<String, Float> entry : cpuUsage.entrySet()) {
+            System.err.println(entry.getKey() + "/" + entry.getValue());
+        }
     }
 
     public void setCpuUsageFactory(CpuUsageFactory cpuUsageFactory) {
         this.cpuUsageFactory = cpuUsageFactory;
     }
 
-    private class CpuUsageListenerImpl implements CpuUsageListener {
+    public class CpuUsageListenerImpl implements CpuUsageListener {
         @Override
         public void cpuChanged(String ip, Float cpu) {
             cpuUsage.put(ip, cpu);
