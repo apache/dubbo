apache/dubbo

commit 2d323a9f87c8e0d5443a40b9ab65c58ad44dc262

[DUBBO-3137]: step3 - start to use RpcConstants (#4038)
* constants step3 rpc

* remove useless import

* remove useless import

* remove useless import

* remove useless import

dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/filter/AccessLogFilter.java
@@ -16,7 +16,6 @@
  */
 package org.apache.dubbo.rpc.filter;
 
-import org.apache.dubbo.common.Constants;
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.common.logger.Logger;
 import org.apache.dubbo.common.logger.LoggerFactory;
@@ -48,6 +47,7 @@ import java.util.concurrent.TimeUnit;
 import static org.apache.dubbo.common.constants.CommonConstants.GROUP_KEY;
 import static org.apache.dubbo.common.constants.CommonConstants.PROVIDER;
 import static org.apache.dubbo.common.constants.CommonConstants.VERSION_KEY;
+import static org.apache.dubbo.common.constants.RpcConstants.ACCESS_LOG_KEY;
 
 /**
  * Record access log for the service.
@@ -63,7 +63,7 @@ import static org.apache.dubbo.common.constants.CommonConstants.VERSION_KEY;
  * &lt;/logger&gt;
  * </pre></code>
  */
-@Activate(group = PROVIDER, value = Constants.ACCESS_LOG_KEY)
+@Activate(group = PROVIDER, value = ACCESS_LOG_KEY)
 public class AccessLogFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(AccessLogFilter.class);
@@ -102,7 +102,7 @@ public class AccessLogFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation inv) throws RpcException {
         try {
-            String accessLogKey = invoker.getUrl().getParameter(Constants.ACCESS_LOG_KEY);
+            String accessLogKey = invoker.getUrl().getParameter(ACCESS_LOG_KEY);
             if (ConfigUtils.isNotEmpty(accessLogKey)) {
                 AccessLogData logData = buildAccessLogData(invoker, inv);
                 log(accessLogKey, logData);

commit 05e44db43c829a7aea6cfc36ff522b981964f0ee

Fix bug #4374 for AccessLogFilter not working (#4566)
which introduced by #4038

dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/filter/AccessLogFilter.java
@@ -68,7 +68,7 @@ public class AccessLogFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(AccessLogFilter.class);
 
-    private static final String ACCESS_LOG_KEY = "dubbo.accesslog";
+    private static final String LOG_KEY = "dubbo.accesslog";
 
     private static final int LOG_MAX_BUFFER = 5000;
 
@@ -179,7 +179,7 @@ public class AccessLogFilter implements Filter {
              iterator.hasNext();
              iterator.remove()) {
             AccessLogData logData = iterator.next();
-            LoggerFactory.getLogger(ACCESS_LOG_KEY + "." + logData.getServiceName()).info(logData.getLogMessage());
+            LoggerFactory.getLogger(LOG_KEY + "." + logData.getServiceName()).info(logData.getLogMessage());
         }
     }
 

dubbo-rpc/dubbo-rpc-api/src/test/java/org/apache/dubbo/rpc/filter/AccessLogFilterTest.java
@@ -16,17 +16,23 @@
  */
 package org.apache.dubbo.rpc.filter;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Set;
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.utils.LogUtil;
 import org.apache.dubbo.rpc.Filter;
 import org.apache.dubbo.rpc.Invocation;
 import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.support.AccessLogData;
 import org.apache.dubbo.rpc.support.MockInvocation;
 import org.apache.dubbo.rpc.support.MyInvoker;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * AccessLogFilterTest.java
@@ -48,11 +54,23 @@ public class AccessLogFilterTest {
 
     // TODO how to assert thread action
     @Test
-    public void testDefault() {
+    @SuppressWarnings("unchecked")
+    public void testDefault() throws NoSuchFieldException, IllegalAccessException {
         URL url = URL.valueOf("test://test:11/test?accesslog=true&group=dubbo&version=1.1");
         Invoker<AccessLogFilterTest> invoker = new MyInvoker<AccessLogFilterTest>(url);
         Invocation invocation = new MockInvocation();
+
+        Field field = AccessLogFilter.class.getDeclaredField("LOG_ENTRIES");
+        field.setAccessible(true);
+        assertTrue(((Map) field.get(AccessLogFilter.class)).isEmpty());
+
         accessLogFilter.invoke(invoker, invocation);
+
+        Map<String, Set<AccessLogData>> logs = (Map<String, Set<AccessLogData>>) field.get(AccessLogFilter.class);
+        assertFalse(logs.isEmpty());
+        assertFalse(logs.get("true").isEmpty());
+        AccessLogData log = logs.get("true").iterator().next();
+        assertEquals("org.apache.dubbo.rpc.support.DemoService", log.getServiceName());
     }
 
     @Test