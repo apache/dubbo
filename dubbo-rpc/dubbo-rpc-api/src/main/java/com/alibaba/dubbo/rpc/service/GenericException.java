07b1a4d6ac33211458db3f185fdc0fdc3a5942ce
qixiaobo <qixiaobo.nj@gmail.com>
2019-07-26 17:30:00 +0800

    dubbo GenericInvoke throws exception local class incompatible

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/service/GenericException.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/service/GenericException.java
@@ -17,17 +17,46 @@
 
 package com.alibaba.dubbo.rpc.service;
 
+import org.apache.dubbo.common.utils.StringUtils;
+
 @Deprecated
-public class GenericException extends org.apache.dubbo.rpc.service.GenericException {
+public class GenericException extends RuntimeException {
+
+    private static final long serialVersionUID = -1182299763306599962L;
+
+    private String exceptionClass;
+
+    private String exceptionMessage;
 
     public GenericException() {
     }
 
     public GenericException(String exceptionClass, String exceptionMessage) {
-        super(exceptionClass, exceptionMessage);
+        super(exceptionMessage);
+        this.exceptionClass = exceptionClass;
+        this.exceptionMessage = exceptionMessage;
     }
 
     public GenericException(Throwable cause) {
-        super(cause);
+        super(StringUtils.toString(cause));
+        this.exceptionClass = cause.getClass().getName();
+        this.exceptionMessage = cause.getMessage();
     }
+
+    public String getExceptionClass() {
+        return exceptionClass;
+    }
+
+    public void setExceptionClass(String exceptionClass) {
+        this.exceptionClass = exceptionClass;
+    }
+
+    public String getExceptionMessage() {
+        return exceptionMessage;
+    }
+
+    public void setExceptionMessage(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+
 }
