0aa1b19efa8e56ea887be1d9ede0a88e7e483cd1
tswstarplanet <tswstarplanet@163.com>
2019-08-01 04:32:24 +0800

    refactor duplicate code

--- dubbo-config/dubbo-config-spring/src/main/java/org/apache/dubbo/config/spring/ReferenceBean.java
+++ dubbo-config/dubbo-config-spring/src/main/java/org/apache/dubbo/config/spring/ReferenceBean.java
@@ -111,36 +111,12 @@ public class ReferenceBean<T> extends ReferenceConfig<T> implements FactoryBean,
         if (getApplication() == null
                 && (getConsumer() == null || getConsumer().getApplication() == null)) {
             Map<String, ApplicationConfig> applicationConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, ApplicationConfig.class, false, false);
-            if (applicationConfigMap != null && applicationConfigMap.size() > 0) {
-                ApplicationConfig applicationConfig = null;
-                for (ApplicationConfig config : applicationConfigMap.values()) {
-                    if (applicationConfig != null) {
-                        throw new IllegalStateException("Duplicate application configs: " + applicationConfig + " and " + config);
-                    }
-                    applicationConfig = config;
-                }
-                if (applicationConfig != null) {
-                    setApplication(applicationConfig);
-                }
-            }
+            loadApplication(applicationConfigMap);
         }
         if (getModule() == null
                 && (getConsumer() == null || getConsumer().getModule() == null)) {
             Map<String, ModuleConfig> moduleConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, ModuleConfig.class, false, false);
-            if (moduleConfigMap != null && moduleConfigMap.size() > 0) {
-                ModuleConfig moduleConfig = null;
-                for (ModuleConfig config : moduleConfigMap.values()) {
-                    if (config.isDefault() == null || config.isDefault()) {
-                        if (moduleConfig != null) {
-                            throw new IllegalStateException("Duplicate module configs: " + moduleConfig + " and " + config);
-                        }
-                        moduleConfig = config;
-                    }
-                }
-                if (moduleConfig != null) {
-                    setModule(moduleConfig);
-                }
-            }
+            loadModule(moduleConfigMap);
         }
 
         if (StringUtils.isEmpty(getRegistryIds())) {
@@ -156,82 +132,29 @@ public class ReferenceBean<T> extends ReferenceConfig<T> implements FactoryBean,
                 && (getConsumer() == null || CollectionUtils.isEmpty(getConsumer().getRegistries()))
                 && (getApplication() == null || CollectionUtils.isEmpty(getApplication().getRegistries()))) {
             Map<String, RegistryConfig> registryConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, RegistryConfig.class, false, false);
-            if (registryConfigMap != null && registryConfigMap.size() > 0) {
-                List<RegistryConfig> registryConfigs = new ArrayList<>();
-                if (StringUtils.isNotEmpty(registryIds)) {
-                    Arrays.stream(COMMA_SPLIT_PATTERN.split(registryIds)).forEach(id -> {
-                        if (registryConfigMap.containsKey(id)) {
-                            registryConfigs.add(registryConfigMap.get(id));
-                        }
-                    });
-                }
-
-                if (registryConfigs.isEmpty()) {
-                    for (RegistryConfig config : registryConfigMap.values()) {
-                        if (StringUtils.isEmpty(registryIds)) {
-                            registryConfigs.add(config);
-                        }
-                    }
-                }
-                if (!registryConfigs.isEmpty()) {
-                    super.setRegistries(registryConfigs);
-                }
-            }
+            loadRegistries(registryConfigMap);
         }
 
         if (getMetadataReportConfig() == null) {
             Map<String, MetadataReportConfig> metadataReportConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, MetadataReportConfig.class, false, false);
-            if (metadataReportConfigMap != null && metadataReportConfigMap.size() == 1) {
-                // first elements
-                super.setMetadataReportConfig(metadataReportConfigMap.values().iterator().next());
-            } else if (metadataReportConfigMap != null && metadataReportConfigMap.size() > 1) {
-                throw new IllegalStateException("Multiple MetadataReport configs: " + metadataReportConfigMap);
-            }
+            loadMetadataReportConfig(metadataReportConfigMap);
         }
 
         if (getConfigCenter() == null) {
-            Map<String, ConfigCenterConfig> configenterMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, ConfigCenterConfig.class, false, false);
-            if (configenterMap != null && configenterMap.size() == 1) {
-                super.setConfigCenter(configenterMap.values().iterator().next());
-            } else if (configenterMap != null && configenterMap.size() > 1) {
-                throw new IllegalStateException("Multiple ConfigCenter found:" + configenterMap);
-            }
+            Map<String, ConfigCenterConfig> configCenterMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, ConfigCenterConfig.class, false, false);
+            loadConfigCenter(configCenterMap);
         }
 
         if (getMonitor() == null
                 && (getConsumer() == null || getConsumer().getMonitor() == null)
                 && (getApplication() == null || getApplication().getMonitor() == null)) {
             Map<String, MonitorConfig> monitorConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, MonitorConfig.class, false, false);
-            if (monitorConfigMap != null && monitorConfigMap.size() > 0) {
-                MonitorConfig monitorConfig = null;
-                for (MonitorConfig config : monitorConfigMap.values()) {
-                    if (config.isDefault() == null || config.isDefault()) {
-                        if (monitorConfig != null) {
-                            throw new IllegalStateException("Duplicate monitor configs: " + monitorConfig + " and " + config);
-                        }
-                        monitorConfig = config;
-                    }
-                }
-                if (monitorConfig != null) {
-                    setMonitor(monitorConfig);
-                }
-            }
+            loadMonitor(monitorConfigMap);
         }
 
         if (getMetrics() == null) {
             Map<String, MetricsConfig> metricsConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, MetricsConfig.class, false, false);
-            if (metricsConfigMap != null && metricsConfigMap.size() > 0) {
-                MetricsConfig metricsConfig = null;
-                for (MetricsConfig config : metricsConfigMap.values()) {
-                    if (metricsConfig != null) {
-                        throw new IllegalStateException("Duplicate metrics configs: " + metricsConfig + " and " + config);
-                    }
-                    metricsConfig = config;
-                }
-                if (metricsConfig != null) {
-                    setMetrics(metricsConfig);
-                }
-            }
+            loadMetrics(metricsConfigMap);
         }
 
         if (shouldInit()) {
