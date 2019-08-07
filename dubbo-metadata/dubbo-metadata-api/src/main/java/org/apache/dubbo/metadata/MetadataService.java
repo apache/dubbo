9f405d731688442e6532ae3985440b46b05637de
mercyblitz <mercyblitz@gmail.com>
2019-08-07 11:07:46 +0800

    Polish apache/dubbo#4759 : [Refactor] Change the signature of methods of MetadataService #4759

--- dubbo-metadata/dubbo-metadata-api/src/main/java/org/apache/dubbo/metadata/MetadataService.java
+++ dubbo-metadata/dubbo-metadata-api/src/main/java/org/apache/dubbo/metadata/MetadataService.java
@@ -19,8 +19,14 @@ package org.apache.dubbo.metadata;
 import org.apache.dubbo.common.URL;
 
 import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
+import static java.util.Collections.unmodifiableSortedSet;
 import static java.util.stream.StreamSupport.stream;
 
 /**
@@ -72,78 +78,88 @@ public interface MetadataService {
     /**
      * the list of String that presents all Dubbo subscribed {@link URL urls}
      *
-     * @return non-null read-only {@link List}
+     * @return the non-null read-only {@link SortedSet sorted set} of {@link URL#toFullString() strings} presenting the {@link URL URLs}
+     * @see #toSortedStrings(Stream)
+     * @see URL#toFullString()
      */
-    List<String> getSubscribedURLs();
+    SortedSet<String> getSubscribedURLs();
 
     /**
-     * Get the list of String that presents all Dubbo exported {@link URL urls}
+     * Get the {@link SortedSet sorted set} of String that presents all Dubbo exported {@link URL urls}
      *
-     * @return non-null read-only {@link List}
+     * @return the non-null read-only {@link SortedSet sorted set} of {@link URL#toFullString() strings} presenting the {@link URL URLs}
+     * @see #toSortedStrings(Stream)
+     * @see URL#toFullString()
      */
-    default List<String> getExportedURLs() {
+    default SortedSet<String> getExportedURLs() {
         return getExportedURLs(ALL_SERVICE_INTERFACES);
     }
 
     /**
-     * Get the list of String that presents the specified Dubbo exported {@link URL urls} by the <code>serviceInterface</code>
+     * Get the {@link SortedSet sorted set} of String that presents the specified Dubbo exported {@link URL urls} by the <code>serviceInterface</code>
      *
      * @param serviceInterface The class name of Dubbo service interface
-     * @return non-null read-only {@link List}
-     * @see URL
+     * @return the non-null read-only {@link SortedSet sorted set} of {@link URL#toFullString() strings} presenting the {@link URL URLs}
+     * @see #toSortedStrings(Stream)
+     * @see URL#toFullString()
      */
-    default List<String> getExportedURLs(String serviceInterface) {
+    default SortedSet<String> getExportedURLs(String serviceInterface) {
         return getExportedURLs(serviceInterface, null);
     }
 
     /**
-     * Get the list of String that presents the specified Dubbo exported {@link URL urls} by the
+     * Get the {@link SortedSet sorted set} of String that presents the specified Dubbo exported {@link URL urls} by the
      * <code>serviceInterface</code> and <code>group</code>
      *
      * @param serviceInterface The class name of Dubbo service interface
      * @param group            the Dubbo Service Group (optional)
-     * @return non-null read-only {@link List}
-     * @see URL
+     * @return the non-null read-only {@link SortedSet sorted set} of {@link URL#toFullString() strings} presenting the {@link URL URLs}
+     * @see #toSortedStrings(Stream)
+     * @see URL#toFullString()
      */
-    default List<String> getExportedURLs(String serviceInterface, String group) {
+    default SortedSet<String> getExportedURLs(String serviceInterface, String group) {
         return getExportedURLs(serviceInterface, group, null);
     }
 
     /**
-     * Get the list of String that presents the specified Dubbo exported {@link URL urls} by the
+     * Get the {@link SortedSet sorted set} of String that presents the specified Dubbo exported {@link URL urls} by the
      * <code>serviceInterface</code>, <code>group</code> and <code>version</code>
      *
      * @param serviceInterface The class name of Dubbo service interface
      * @param group            the Dubbo Service Group (optional)
      * @param version          the Dubbo Service Version (optional)
-     * @return non-null read-only {@link List}
-     * @see URL
+     * @return the non-null read-only {@link SortedSet sorted set} of {@link URL#toFullString() strings} presenting the {@link URL URLs}
+     * @see #toSortedStrings(Stream)
+     * @see URL#toFullString()
      */
-    default List<String> getExportedURLs(String serviceInterface, String group, String version) {
+    default SortedSet<String> getExportedURLs(String serviceInterface, String group, String version) {
         return getExportedURLs(serviceInterface, group, version, null);
     }
 
     /**
-     * Get the list of String that presents the specified Dubbo exported {@link URL urls} by the
+     * Get the sorted set of String that presents the specified Dubbo exported {@link URL urls} by the
      * <code>serviceInterface</code>, <code>group</code>, <code>version</code> and <code>protocol</code>
      *
      * @param serviceInterface The class name of Dubbo service interface
      * @param group            the Dubbo Service Group (optional)
      * @param version          the Dubbo Service Version (optional)
      * @param protocol         the Dubbo Service Protocol (optional)
-     * @return non-null read-only {@link List}
-     * @see URL
+     * @return the non-null read-only {@link SortedSet sorted set} of {@link URL#toFullString() strings} presenting the {@link URL URLs}
+     * @see #toSortedStrings(Stream)
+     * @see URL#toFullString()
      */
-    List<String> getExportedURLs(String serviceInterface, String group, String version, String protocol);
+    SortedSet<String> getExportedURLs(String serviceInterface, String group, String version, String protocol);
 
     /**
      * Interface definition.
+     *
      * @return
      */
     String getServiceDefinition(String interfaceName, String version, String group);
 
     /**
      * Interface definition.
+     *
      * @return
      */
     String getServiceDefinition(String serviceKey);
@@ -158,5 +174,29 @@ public interface MetadataService {
         return stream(urls.spliterator(), false)
                 .map(URL::valueOf)
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * Convert the specified {@link Iterable} of {@link URL URLs} to be the {@link URL#toFullString() strings} presenting
+     * the {@link URL URLs}
+     *
+     * @param iterable {@link Iterable} of {@link URL}
+     * @return the non-null read-only {@link SortedSet sorted set} of {@link URL#toFullString() strings} presenting
+     * @see URL#toFullString()
+     */
+    static SortedSet<String> toSortedStrings(Iterable<URL> iterable) {
+        return toSortedStrings(StreamSupport.stream(iterable.spliterator(), false));
+    }
+
+    /**
+     * Convert the specified {@link Stream} of {@link URL URLs} to be the {@link URL#toFullString() strings} presenting
+     * the {@link URL URLs}
+     *
+     * @param stream {@link Stream} of {@link URL}
+     * @return the non-null read-only {@link SortedSet sorted set} of {@link URL#toFullString() strings} presenting
+     * @see URL#toFullString()
+     */
+    static SortedSet<String> toSortedStrings(Stream<URL> stream) {
+        return unmodifiableSortedSet(stream.map(URL::toFullString).collect(TreeSet::new, Set::add, Set::addAll));
     }
 }
