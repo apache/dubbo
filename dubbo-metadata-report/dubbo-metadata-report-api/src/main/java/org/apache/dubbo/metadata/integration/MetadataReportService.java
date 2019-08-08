3f3639887bf02c57eb1515926a0f369c3ee8a22d
chengxingyuan <1817802738@qq.com>
2019-08-08 17:03:57 +0800

    Revert " [dubbo-4712] Fix [#4760](https://github.com/apache/dubbo/issues/4760)"
    fix add Thread ClassLoader check

--- dubbo-metadata-report/dubbo-metadata-report-api/src/main/java/org/apache/dubbo/metadata/integration/MetadataReportService.java
+++ dubbo-metadata-report/dubbo-metadata-report-api/src/main/java/org/apache/dubbo/metadata/integration/MetadataReportService.java
@@ -94,12 +94,15 @@ public class MetadataReportService {
         try {
             String interfaceName = providerUrl.getParameter(INTERFACE_KEY);
             if (StringUtils.isNotEmpty(interfaceName)) {
-                Class interfaceClass =Thread.currentThread().getContextClassLoader().loadClass(interfaceName);
-                FullServiceDefinition fullServiceDefinition = ServiceDefinitionBuilder.buildFullDefinition(interfaceClass, providerUrl.getParameters());
-                metadataReport.storeProviderMetadata(new MetadataIdentifier(providerUrl.getServiceInterface(),
-                        providerUrl.getParameter(VERSION_KEY), providerUrl.getParameter(GROUP_KEY),
-                        PROVIDER_SIDE, providerUrl.getParameter(APPLICATION_KEY)), fullServiceDefinition);
-                return;
+                Class interfaceClass =Class.forName(interfaceName);
+                ClassLoader classLoader = interfaceClass.getClassLoader();
+                if(isInterfaceAllowed(interfaceClass,classLoader)) {
+                    FullServiceDefinition fullServiceDefinition = ServiceDefinitionBuilder.buildFullDefinition(interfaceClass, providerUrl.getParameters());
+                    metadataReport.storeProviderMetadata(new MetadataIdentifier(providerUrl.getServiceInterface(),
+                            providerUrl.getParameter(VERSION_KEY), providerUrl.getParameter(GROUP_KEY),
+                            PROVIDER_SIDE, providerUrl.getParameter(APPLICATION_KEY)), fullServiceDefinition);
+                    return;
+                }
             }
             logger.error("publishProvider interfaceName is empty . providerUrl: " + providerUrl.toFullString());
         } catch (ClassNotFoundException e) {
