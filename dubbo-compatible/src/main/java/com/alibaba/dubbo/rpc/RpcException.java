d2dbc71d30e753515e9b54e3aeba88cc02695acc
qixiaobo <qixiaobo.nj@gmail.com>
2019-08-09 15:16:21 +0800

    dubbo GenericInvoke throws exception local class incompatible

--- dubbo-compatible/src/main/java/com/alibaba/dubbo/rpc/RpcException.java
+++ dubbo-compatible/src/main/java/com/alibaba/dubbo/rpc/RpcException.java
@@ -17,9 +17,26 @@
 
 package com.alibaba.dubbo.rpc;
 
+import javax.naming.LimitExceededException;
+
 @Deprecated
-public class RpcException extends org.apache.dubbo.rpc.RpcException {
-    
+public class RpcException extends RuntimeException {
+
+
+    public static final int UNKNOWN_EXCEPTION = 0;
+    public static final int NETWORK_EXCEPTION = 1;
+    public static final int TIMEOUT_EXCEPTION = 2;
+    public static final int BIZ_EXCEPTION = 3;
+    public static final int FORBIDDEN_EXCEPTION = 4;
+    public static final int SERIALIZATION_EXCEPTION = 5;
+    public static final int NO_INVOKER_AVAILABLE_AFTER_FILTER = 6;
+    public static final int LIMIT_EXCEEDED_EXCEPTION = 7;
+    private static final long serialVersionUID = 7815426752583648734L;
+    /**
+     * RpcException cannot be extended, use error code for exception type to keep compatibility
+     */
+    private int code;
+
     public RpcException() {
         super();
     }
@@ -37,18 +54,58 @@ public class RpcException extends org.apache.dubbo.rpc.RpcException {
     }
 
     public RpcException(int code) {
-        super(code);
+        super();
+        this.code = code;
     }
 
     public RpcException(int code, String message, Throwable cause) {
-        super(code, message, cause);
+        super(message, cause);
+        this.code = code;
     }
 
     public RpcException(int code, String message) {
-        super(code, message);
+        super(message);
+        this.code = code;
     }
 
     public RpcException(int code, Throwable cause) {
-        super(code, cause);
+        super(cause);
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public boolean isBiz() {
+        return code == BIZ_EXCEPTION;
+    }
+
+    public boolean isForbidded() {
+        return code == FORBIDDEN_EXCEPTION;
+    }
+
+    public boolean isTimeout() {
+        return code == TIMEOUT_EXCEPTION;
+    }
+
+    public boolean isNetwork() {
+        return code == NETWORK_EXCEPTION;
+    }
+
+    public boolean isSerialization() {
+        return code == SERIALIZATION_EXCEPTION;
+    }
+
+    public boolean isNoInvokerAvailableAfterFilter() {
+        return code == NO_INVOKER_AVAILABLE_AFTER_FILTER;
+    }
+
+    public boolean isLimitExceed() {
+        return code == LIMIT_EXCEEDED_EXCEPTION || getCause() instanceof LimitExceededException;
     }
 }
