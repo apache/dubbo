e6a8ef13725826cff1c03c9b11ec14c60c979b82
chengxingyuan <1817802738@qq.com>
2019-08-14 15:26:24 +0800

    fix bug of serialize or deserialie generic type use fastjson eg:result or paramete use List

--- dubbo-serialization/dubbo-serialization-fastjson/src/main/java/org/apache/dubbo/common/serialize/fastjson/FastJsonObjectInput.java
+++ dubbo-serialization/dubbo-serialization-fastjson/src/main/java/org/apache/dubbo/common/serialize/fastjson/FastJsonObjectInput.java
@@ -16,9 +16,13 @@
  */
 package org.apache.dubbo.common.serialize.fastjson;
 
+import com.alibaba.fastjson.parser.DefaultJSONParser;
+import com.alibaba.fastjson.parser.ParserConfig;
 import org.apache.dubbo.common.serialize.ObjectInput;
 
 import com.alibaba.fastjson.JSON;
+import org.apache.dubbo.common.utils.PojoUtils;
+import org.apache.dubbo.common.utils.ReflectUtils;
 
 import java.io.BufferedReader;
 import java.io.EOFException;
@@ -26,7 +30,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.lang.reflect.Field;
 import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * FastJson object input implementation
@@ -91,7 +98,15 @@ public class FastJsonObjectInput implements ObjectInput {
     @Override
     public Object readObject() throws IOException, ClassNotFoundException {
         String json = readLine();
-        return JSON.parse(json);
+        if (json == null) {
+            return null;
+        }
+        DefaultJSONParser parser = new DefaultJSONParser(json, getDubboFastjsonConfigParseConfig(), JSON.DEFAULT_PARSER_FEATURE);
+        Object value = parser.parse();
+        parser.handleResovleTask(value);
+        parser.close();
+
+        return value;
     }
 
     @Override
@@ -102,8 +117,42 @@ public class FastJsonObjectInput implements ObjectInput {
     @Override
     @SuppressWarnings("unchecked")
     public <T> T readObject(Class<T> cls, Type type) throws IOException, ClassNotFoundException {
-        String json = readLine();
-        return (T) JSON.parseObject(json, type);
+        Object value = null;
+        if (isConvertToJsonByType(cls, type)) {
+            String json = readLine();
+            value = JSON.parseObject(json, type);
+        } else {
+            value = readObject(cls, type);
+            return (T) value;
+        }
+        return (T) PojoUtils.realize(value, cls, type);
+    }
+
+    private static ParserConfig dubboFastjsonConfig =null;
+
+    private static ParserConfig getDubboFastjsonConfigParseConfig(){
+        if (dubboFastjsonConfig==null){
+            ParserConfig newConfig =new ParserConfig();
+            ParserConfig globalInstance = ParserConfig.getGlobalInstance();
+            Field[] declaredFields = ParserConfig.class.getDeclaredFields();
+            for (Field field : declaredFields) {
+                if (!field.isAccessible()){
+                    field.setAccessible(true);
+                }
+                try {
+                    Object globalValue = field.get(globalInstance);
+                    field.set(newConfig, globalValue);
+                } catch (IllegalArgumentException e) {
+                    //skip
+                } catch (IllegalAccessException e) {
+                    //skip
+                }
+            }
+            //必须是AutoTypeSupport=true
+            newConfig.setAutoTypeSupport(true);
+            dubboFastjsonConfig =newConfig;
+        }
+        return dubboFastjsonConfig;
     }
 
     private String readLine() throws IOException, EOFException {
@@ -118,4 +167,36 @@ public class FastJsonObjectInput implements ObjectInput {
         String json = readLine();
         return JSON.parseObject(json, cls);
     }
+
+
+    protected boolean isConvertToJsonByType(Class<?> clz, Type type) {
+        if (clz == null) {
+            return false;
+        }
+
+        if (type == null) {
+            return false;
+        }
+
+        if (clz.isEnum()) {
+            return false;
+        }
+
+        if (ReflectUtils.isPrimitives(clz)) {
+            return false;
+        }
+
+        if (clz.isArray()) {
+            return false;
+        }
+        if (Collection.class.isAssignableFrom(clz)) {
+            return false;
+        }
+
+        if (Map.class.isAssignableFrom(clz)) {
+            return false;
+        }
+        return true;
+    }
+
 }
