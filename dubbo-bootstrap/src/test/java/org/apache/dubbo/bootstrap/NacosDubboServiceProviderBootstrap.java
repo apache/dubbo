403858b8fb757617d71aeb59710e4264f5ed908f
cvictory <shenglicao2@gmail.com>
2019-08-22 11:12:34 +0800

    nacos test

--- dubbo-bootstrap/src/test/java/org/apache/dubbo/bootstrap/NacosDubboServiceProviderBootstrap.java
+++ dubbo-bootstrap/src/test/java/org/apache/dubbo/bootstrap/NacosDubboServiceProviderBootstrap.java
@@ -18,6 +18,7 @@ package org.apache.dubbo.bootstrap;
 
 import org.apache.dubbo.bootstrap.rest.UserService;
 import org.apache.dubbo.bootstrap.rest.UserServiceImpl;
+import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.MetadataReportConfig;
 
 /**
@@ -28,14 +29,16 @@ import org.apache.dubbo.config.MetadataReportConfig;
 public class NacosDubboServiceProviderBootstrap {
 
     public static void main(String[] args) {
+        ApplicationConfig applicationConfig = new ApplicationConfig("dubbo-nacos-provider-demo");
+        applicationConfig.setMetadataType("remote");
         new DubboBootstrap()
-                .application("dubbo-provider-demo")
+                .application(applicationConfig)
                 // Zookeeper in service registry type
-                .registry("zookeeper", builder -> builder.address("zookeeper://127.0.0.1:2181?registry.type=service"))
+                .registry("nacos", builder -> builder.address("nacos://127.0.0.1:8848?registry.type=service"))
                 // Nacos
 //                .registry("nacos", builder -> builder.address("nacos://127.0.0.1:8848?registry.type=service"))
 //                .registry(RegistryBuilder.newBuilder().address("etcd3://127.0.0.1:2379?registry.type=service").build())
-                .metadataReport(new MetadataReportConfig("zookeeper://127.0.0.1:2181"))
+                .metadataReport(new MetadataReportConfig("nacos://127.0.0.1:8848"))
                 .protocol("dubbo", builder -> builder.port(20885).name("dubbo"))
                 .protocol("rest", builder -> builder.port(9090).name("rest"))
                 .service(builder -> builder.id("echo").interfaceClass(EchoService.class).ref(new EchoServiceImpl()).protocolIds("dubbo"))
