bc3f35a1003710518df80488b4d09c44629706b6
ken.lj <ken.lj.hz@gmail.com>
2019-08-30 09:12:16 +0800

    consul service discovery support

--- dubbo-registry/dubbo-registry-consul/src/main/java/org/apache/dubbo/registry/consul/ConsulServiceDiscovery.java
+++ dubbo-registry/dubbo-registry-consul/src/main/java/org/apache/dubbo/registry/consul/ConsulServiceDiscovery.java
@@ -27,6 +27,7 @@ import org.apache.dubbo.registry.client.DefaultServiceInstance;
 import org.apache.dubbo.registry.client.ServiceDiscovery;
 import org.apache.dubbo.registry.client.ServiceInstance;
 import org.apache.dubbo.registry.client.event.ServiceInstancesChangedEvent;
+import org.apache.dubbo.registry.client.event.listener.ServiceInstancesChangedListener;
 
 import com.ecwid.consul.v1.ConsulClient;
 import com.ecwid.consul.v1.QueryParams;
@@ -75,7 +76,8 @@ public class ConsulServiceDiscovery implements ServiceDiscovery, EventListener<S
     private String tag;
     private ConsulClient client;
     private ExecutorService notifierExecutor = newCachedThreadPool(
-            new NamedThreadFactory("dubbo-consul-notifier", true));
+            new NamedThreadFactory("dubbo-service-discovery-consul-notifier", true));
+    private ConsulNotifier notifier;
     private TtlScheduler ttlScheduler;
     private long checkPassInterval;
     private URL url;
@@ -108,6 +110,10 @@ public class ConsulServiceDiscovery implements ServiceDiscovery, EventListener<S
 
     @Override
     public void destroy() {
+        notifier.stop();
+        notifier = null;
+        notifierExecutor.shutdownNow();
+        ttlScheduler.stop();
     }
 
     @Override
@@ -118,8 +124,20 @@ public class ConsulServiceDiscovery implements ServiceDiscovery, EventListener<S
     }
 
     @Override
-    public void update(ServiceInstance serviceInstance) throws RuntimeException {
+    public void addServiceInstancesChangedListener(ServiceInstancesChangedListener listener) throws NullPointerException, IllegalArgumentException {
+        if (notifier == null) {
+            String serviceName = listener.getServiceName();
+            Response<List<HealthService>> response = getHealthServices(serviceName, -1, buildWatchTimeout());
+            Long consulIndex = response.getConsulIndex();
+            notifier = new ConsulNotifier(serviceName, consulIndex);
+        }
+        notifierExecutor.execute(notifier);
+    }
 
+    @Override
+    public void update(ServiceInstance serviceInstance) throws RuntimeException {
+        // TODO
+        // client.catalogRegister(buildCatalogService(serviceInstance));
     }
 
     @Override
@@ -137,6 +155,10 @@ public class ConsulServiceDiscovery implements ServiceDiscovery, EventListener<S
     @Override
     public List<ServiceInstance> getInstances(String serviceName) throws NullPointerException {
         Response<List<HealthService>> response = getHealthServices(serviceName, -1, buildWatchTimeout());
+        Long consulIndex = response.getConsulIndex();
+        if (notifier == null) {
+            notifier = new ConsulNotifier(serviceName, consulIndex);
+        }
         return convert(response.getValue());
     }
 
@@ -145,6 +167,7 @@ public class ConsulServiceDiscovery implements ServiceDiscovery, EventListener<S
                 .map(HealthService::getService)
                 .map(service -> {
                     ServiceInstance instance = new DefaultServiceInstance(
+                            service.getId(),
                             service.getService(),
                             service.getAddress(),
                             service.getPort());
@@ -265,12 +288,12 @@ public class ConsulServiceDiscovery implements ServiceDiscovery, EventListener<S
     }
 
     private class ConsulNotifier implements Runnable {
-        private ServiceInstance serviceInstance;
+        private String serviceName;
         private long consulIndex;
         private boolean running;
 
-        ConsulNotifier(ServiceInstance serviceInstance, long consulIndex) {
-            this.serviceInstance = serviceInstance;
+        ConsulNotifier(String serviceName, long consulIndex) {
+            this.serviceName = serviceName;
             this.consulIndex = consulIndex;
             this.running = true;
         }
@@ -283,17 +306,14 @@ public class ConsulServiceDiscovery implements ServiceDiscovery, EventListener<S
         }
 
         private void processService() {
-//            String service = url.getServiceKey();
-//            Response<List<HealthService>> response = getHealthServices(service, consulIndex, buildWatchTimeout(url));
-//            Long currentIndex = response.getConsulIndex();
-//            if (currentIndex != null && currentIndex > consulIndex) {
-//                consulIndex = currentIndex;
-//                List<HealthService> services = response.getValue();
-//                List<URL> urls = convert(services, url);
-//                for (NotifyListener listener : getSubscribed().get(url)) {
-//                    doNotify(url, listener, urls);
-//                }
-//            }
+            Response<List<HealthService>> response = getHealthServices(serviceName, -1, Integer.MAX_VALUE);
+            Long currentIndex = response.getConsulIndex();
+            if (currentIndex != null && currentIndex > consulIndex) {
+                consulIndex = currentIndex;
+                List<HealthService> services = response.getValue();
+                List<ServiceInstance> serviceInstances = convert(services);
+                dispatchServiceInstancesChangedEvent(serviceName, serviceInstances);
+            }
         }
 
         void stop() {
@@ -308,7 +328,6 @@ public class ConsulServiceDiscovery implements ServiceDiscovery, EventListener<S
         private final Map<String, ScheduledFuture> serviceHeartbeats = new ConcurrentHashMap<>();
 
         private ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-        ;
 
         private long checkInterval;
 
@@ -363,6 +382,10 @@ public class ConsulServiceDiscovery implements ServiceDiscovery, EventListener<S
                 }
             }
 
+        }
+
+        public void stop() {
+            scheduler.shutdownNow();
         }
 
     }
