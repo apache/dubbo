6b9fc688a99f6cbbc828eacb29c514ca1692ad7c
licho <lecho.sun@gmail.com>
2019-09-06 14:19:29 +0800

    simplity code
    Signed-off-by: licho <lecho.sun@gmail.com>

--- dubbo-config/dubbo-config-spring/src/main/java/org/apache/dubbo/config/spring/util/AnnotationUtils.java
+++ dubbo-config/dubbo-config-spring/src/main/java/org/apache/dubbo/config/spring/util/AnnotationUtils.java
@@ -242,53 +242,31 @@ public class AnnotationUtils {
             List<A> annotationsList = new LinkedList<A>();
 
             switch (elementType) {
-
                 case PARAMETER:
-
                     Annotation[][] parameterAnnotations = method.getParameterAnnotations();
-
                     for (Annotation[] annotations : parameterAnnotations) {
-
                         for (Annotation annotation : annotations) {
-
                             if (annotationClass.equals(annotation.annotationType())) {
-
                                 annotationsList.add((A) annotation);
-
                             }
-
                         }
-
                     }
-
                     break;
-
                 case METHOD:
-
                     A annotation = findAnnotation(method, annotationClass);
-
                     if (annotation != null) {
-
                         annotationsList.add(annotation);
-
                     }
-
                     break;
-
                 case TYPE:
-
                     Class<?> beanType = method.getDeclaringClass();
-
                     A annotation2 = findAnnotation(beanType, annotationClass);
-
                     if (annotation2 != null) {
-
                         annotationsList.add(annotation2);
-
                     }
-
                     break;
-
+                default:
+                    break;
             }
 
             if (!annotationsList.isEmpty()) {
