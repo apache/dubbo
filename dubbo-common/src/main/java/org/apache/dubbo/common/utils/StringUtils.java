53132874700655c77d2c652fb1b051c78c17bb08
LiosWong <diy_os@163.com>
2019-09-11 23:38:50 +0800

    [*]代码还原

--- dubbo-common/src/main/java/org/apache/dubbo/common/utils/StringUtils.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/utils/StringUtils.java
@@ -16,7 +16,11 @@
  */
 package org.apache.dubbo.common.utils;
 
-import static org.apache.dubbo.common.constants.CommonConstants.*;
+import org.apache.dubbo.common.io.UnsafeStringWriter;
+import org.apache.dubbo.common.logger.Logger;
+import org.apache.dubbo.common.logger.LoggerFactory;
+
+import com.alibaba.fastjson.JSON;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -28,10 +32,15 @@ import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.alibaba.fastjson.JSON;
-import org.apache.dubbo.common.io.UnsafeStringWriter;
-import org.apache.dubbo.common.logger.Logger;
-import org.apache.dubbo.common.logger.LoggerFactory;
+import static org.apache.dubbo.common.constants.CommonConstants.COMMA_SEPARATOR;
+import static org.apache.dubbo.common.constants.CommonConstants.COMMA_SPLIT_PATTERN;
+import static org.apache.dubbo.common.constants.CommonConstants.DOT_REGEX;
+import static org.apache.dubbo.common.constants.CommonConstants.GROUP_KEY;
+import static org.apache.dubbo.common.constants.CommonConstants.HIDE_KEY_PREFIX;
+import static org.apache.dubbo.common.constants.CommonConstants.INTERFACE_KEY;
+import static org.apache.dubbo.common.constants.CommonConstants.SEPARATOR_REGEX;
+import static org.apache.dubbo.common.constants.CommonConstants.UNDERLINE_SEPARATOR;
+import static org.apache.dubbo.common.constants.CommonConstants.VERSION_KEY;
 
 /**
  * StringUtils
