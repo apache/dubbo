b39b325910db406c59bd6ec65c4b5cc76564dea0
licho <lecho.sun@gmail.com>
2019-09-12 09:31:11 +0800

    remove unused import
    Signed-off-by: licho <lecho.sun@gmail.com>

--- dubbo-common/src/main/java/org/apache/dubbo/common/extension/ExtensionLoader.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/extension/ExtensionLoader.java
@@ -41,7 +41,6 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
