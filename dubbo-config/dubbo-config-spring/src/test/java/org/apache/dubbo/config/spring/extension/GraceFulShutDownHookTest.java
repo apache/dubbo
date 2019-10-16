fb7e1ac3fcb993b5c3539bf12c411618497789f5
yuantiantian <yuantiantian@guazi.com>
2019-10-16 15:24:46 +0800

    add license

--- dubbo-config/dubbo-config-spring/src/test/java/org/apache/dubbo/config/spring/extension/GraceFulShutDownHookTest.java
+++ dubbo-config/dubbo-config-spring/src/test/java/org/apache/dubbo/config/spring/extension/GraceFulShutDownHookTest.java
@@ -1,6 +1,23 @@
 package org.apache.dubbo.config.spring.extension;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import org.apache.dubbo.config.DubboShutdownHook;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GraceFulShutDownHookTest {
@@ -8,6 +25,10 @@ public class GraceFulShutDownHookTest {
     @Test
     public void testGraceFulShutDownHook(){
         DubboShutdownHook hook = DubboShutdownHook.getDubboShutdownHook();
-        hook.doDestroy();
+        try {
+            hook.doDestroy();
+        }catch (Exception e){
+            Assertions.fail();
+        }
     }
 }
