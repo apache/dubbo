a3c616bc005fb07173ac51ef63f72c4221c40c93
Huang YunKun <htynkn@gmail.com>
2019-11-26 21:15:41 +0800

    Update Serialization.java

--- dubbo-serialization/dubbo-serialization-api/src/main/java/org/apache/dubbo/common/serialize/Serialization.java
+++ dubbo-serialization/dubbo-serialization-api/src/main/java/org/apache/dubbo/common/serialize/Serialization.java
@@ -37,7 +37,8 @@ public interface Serialization {
 
     /**
      * Get content type unique id, recommended that custom implementations use values different with
-     * any value of {@link Constants} and don't greater than ExchangeCodes.SERIALIZATION_MASK (31)
+     * any value of {@link Constants} and don't greater than ExchangeCodec.SERIALIZATION_MASK (31) 
+     * because dubbo protocol use 5 bits to record serialization ID in header.
      *
      * @return content type id
      */
