d272e58d3a783c04b02fd3ad24fdbda7b000e44a
pugwoo <pugwoo@gmail.com>
2019-12-02 10:16:41 +0800

    support : Custom exception error code for http protocol

---
+++ dubbo-rpc/dubbo-rpc-http/src/main/java/org/apache/dubbo/rpc/protocol/http/HttpProtocolErrorCode.java
@@ -0,0 +1,13 @@
+package org.apache.dubbo.rpc.protocol.http;
+
+/**
+ * Custom exception error code for http protocol.
+ */
+public interface HttpProtocolErrorCode {
+
+    /**
+     * @return custom error code of exception
+     */
+    int getErrorCode();
+
+}
