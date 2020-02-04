a9849983baff18fe4ea81ced1f7cd85a51829023
quzijing <quzijing@oppo.com>
2020-02-04 20:34:19 +0800

    set reference bean definition target type

--- dubbo-config/dubbo-config-spring/src/main/java/org/apache/dubbo/config/spring/schema/DubboBeanDefinitionParser.java
+++ dubbo-config/dubbo-config-spring/src/main/java/org/apache/dubbo/config/spring/schema/DubboBeanDefinitionParser.java
@@ -129,7 +129,13 @@ public class DubboBeanDefinitionParser implements BeanDefinitionParser {
                 parseProperties(element.getChildNodes(), classDefinition);
                 beanDefinition.getPropertyValues().addPropertyValue("ref", new BeanDefinitionHolder(classDefinition, id + "Impl"));
             }
-        } else if (ProviderConfig.class.equals(beanClass)) {
+        }  else if(ReferenceBean.class.equals(beanClass)){
+            String interfaceClassName = element.getAttribute("interface");
+            if(StringUtils.isNotEmpty(interfaceClassName)){
+                Class<?> interfaceClass = ReflectUtils.forName(interfaceClassName);
+                beanDefinition.setTargetType(interfaceClass);
+            }
+        }else if (ProviderConfig.class.equals(beanClass)) {
             parseNested(element, parserContext, ServiceBean.class, true, "service", "provider", id, beanDefinition);
         } else if (ConsumerConfig.class.equals(beanClass)) {
             parseNested(element, parserContext, ReferenceBean.class, false, "reference", "consumer", id, beanDefinition);
