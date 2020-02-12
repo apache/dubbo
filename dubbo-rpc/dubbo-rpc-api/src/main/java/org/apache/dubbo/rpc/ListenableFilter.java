c4920e4d5087f94ad9e2941fa33cff437c09addb
ken.lj <ken.lj.hz@gmail.com>
2020-02-12 22:31:51 +0800

    enhance ListenableFilter

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/ListenableFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/ListenableFilter.java
@@ -16,18 +16,36 @@
  */
 package org.apache.dubbo.rpc;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 /**
  * This abstract may probably be removed soon from one future release.
  * We recommend implementing Filter.Listener directly for callback registration,
  * check the default implementation, see {@link org.apache.dubbo.rpc.filter.ExceptionFilter}, for example.
  */
-@Deprecated
 public abstract class ListenableFilter implements Filter {
 
     protected Listener listener = null;
+    protected final ConcurrentMap<Invocation, Listener> listeners = new ConcurrentHashMap<>();
 
     public Listener listener() {
         return listener;
     }
 
+    public Listener listener(Invocation invocation) {
+        Listener invListener = listeners.get(invocation);
+        if (invListener == null) {
+            invListener = listener;
+        }
+        return invListener;
+    }
+
+    public void addListener(Invocation invocation, Listener listener) {
+        listeners.putIfAbsent(invocation, listener);
+    }
+
+    public void removeListener(Invocation invocation) {
+        listeners.remove(invocation);
+    }
 }
