20513a9dc79184651942ac2c34964bc857c9e5f5
gule <gule.gl@outlook.com>
2020-02-20 18:23:05 +0800

    修改函数返回类型、优化变量名

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/integration/RegistryDirectory.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/integration/RegistryDirectory.java
@@ -669,7 +669,7 @@ public class RegistryDirectory<T> extends AbstractDirectory<T> implements Notify
      * compare consumer methods and provider methods
      *
      * @param invoker invoker
-     * @return true:consumer <= provider ,false:consumer > provider
+     * @return void
      */
     private void isConMethodLessThanPro(Invoker<T> invoker) {
         String consumerMethod = queryMap.get(METHODS_KEY);
