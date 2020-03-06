7e6a4f1e45ed9afea20a8737f3a0010fe5ee6ebf
GungnirLaevatain <547992357@qq.com>
2020-03-06 14:58:53 +0800

    simplify OptionUtil#prefixEndOf

--- dubbo-remoting/dubbo-remoting-etcd3/src/main/java/org/apache/dubbo/remoting/etcd/option/OptionUtil.java
+++ dubbo-remoting/dubbo-remoting-etcd3/src/main/java/org/apache/dubbo/remoting/etcd/option/OptionUtil.java
@@ -28,13 +28,10 @@ public class OptionUtil {
 
     public static final ByteSequence prefixEndOf(ByteSequence prefix) {
         byte[] endKey = prefix.getBytes().clone();
-        for (int i = endKey.length - 1; i >= 0; i--) {
-            if (endKey[i] < 0xff) {
-                endKey[i] = (byte) (endKey[i] + 1);
-                return ByteSequence.from(Arrays.copyOf(endKey, i + 1));
-            }
+        if (prefix.size() > 0) {
+            endKey[endKey.length - 1] = (byte) (endKey[endKey.length - 1] + 1);
+            return ByteSequence.from(endKey);
         }
-
         return ByteSequence.from(NO_PREFIX_END);
     }
 
