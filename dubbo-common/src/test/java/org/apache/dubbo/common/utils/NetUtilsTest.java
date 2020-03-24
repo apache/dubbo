7efb3e7b1492f05e2a6d9bde48d7c0708e237338
Albumen <jhq0812@hotmail.com>
2020-03-24 11:22:39 +0800

    fix:in some platform ServerSocket getting random port is not base on 30000

--- dubbo-common/src/test/java/org/apache/dubbo/common/utils/NetUtilsTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/utils/NetUtilsTest.java
@@ -51,7 +51,7 @@ public class NetUtilsTest {
     public void testGetAvailablePort() throws Exception {
         assertThat(NetUtils.getAvailablePort(), greaterThan(0));
         assertThat(NetUtils.getAvailablePort(12345), greaterThanOrEqualTo(12345));
-        assertThat(NetUtils.getAvailablePort(-1), greaterThanOrEqualTo(30000));
+        assertThat(NetUtils.getAvailablePort(-1), greaterThanOrEqualTo(0));
     }
 
     @Test
