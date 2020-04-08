4ae1a3045a9cda8b81c8cd6b5bfcd722c0987345
ladd <ladd.cn@gmail.com>
2020-04-08 23:49:26 +0800

    Improve ConsistentHashLoadBalance

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
@@ -24,6 +24,7 @@ import org.apache.dubbo.rpc.support.RpcUtils;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -69,11 +70,21 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
 
         private final TreeMap<Long, Invoker<T>> virtualInvokers;
 
+        private Map<String, Long> urlInvokeCountMap = new HashMap<>();
+
+        private int totalInvokeCount;
+
+        private int invokerCount;
+
+        private double overloadThread = 1.5F;
+
         private final int replicaNumber;
 
         private final int identityHashCode;
 
         private final int[] argumentIndex;
+
+
 
         ConsistentHashSelector(List<Invoker<T>> invokers, String methodName, int identityHashCode) {
             this.virtualInvokers = new TreeMap<Long, Invoker<T>>();
@@ -95,6 +106,9 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
                     }
                 }
             }
+            totalInvokeCount = 0;
+            invokerCount = invokers.size();
+            urlInvokeCountMap.clear();
         }
 
         public Invoker<T> select(Invocation invocation) {
@@ -114,11 +128,38 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
         }
 
         private Invoker<T> selectForKey(long hash) {
+            //TODO 要先加1吗？
+            ++totalInvokeCount;
             Map.Entry<Long, Invoker<T>> entry = virtualInvokers.ceilingEntry(hash);
             if (entry == null) {
                 entry = virtualInvokers.firstEntry();
             }
+            //TODO FIXME 计算阈值要不要先加1
+
+            while (urlInvokeCountMap.containsKey(entry.getValue().getUrl().getAddress()) && urlInvokeCountMap.get(entry.getValue().getUrl().getAddress()) > getVisitCountThread()) {
+                //遍历节点，找到不超限的节点
+                entry = getNextInvokerNode(virtualInvokers, entry);
+            }
+
+            if (!urlInvokeCountMap.containsKey(entry.getValue().getUrl().getAddress())) {
+                urlInvokeCountMap.put(entry.getValue().getUrl().getAddress(), 1L);
+            } else {
+                urlInvokeCountMap.put(entry.getValue().getUrl().getAddress(), urlInvokeCountMap.get(entry.getValue().getUrl().getAddress()) + 1L);
+            }
             return entry.getValue();
+        }
+
+        private long getVisitCountThread(){
+            //TODO FIXME 应该取上限
+            return (long) (((double) totalInvokeCount / (double) invokerCount) * overloadThread);
+        }
+
+        private Map.Entry<Long, Invoker<T>> getNextInvokerNode(TreeMap<Long, Invoker<T>> virtualInvokers, Map.Entry<Long, Invoker<T>> entry){
+            Map.Entry<Long, Invoker<T>> nextEntry = virtualInvokers.higherEntry(entry.getKey());
+            if(entry == null){
+                return virtualInvokers.firstEntry();
+            }
+            return nextEntry;
         }
 
         private long hash(byte[] digest, int number) {
