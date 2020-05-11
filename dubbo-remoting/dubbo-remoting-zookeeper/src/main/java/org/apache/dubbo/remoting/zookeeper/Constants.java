2187fd42b5fff88ed4d4826196ce18d212f952af
horizonzy <1060026287@qq.com>
2020-05-11 13:11:47 +0800

    Fix-#6129 reWatch the /dubbo/xxx.xxx.XxxService/xxx node when it removed by manually

---
+++ dubbo-remoting/dubbo-remoting-zookeeper/src/main/java/org/apache/dubbo/remoting/zookeeper/Constants.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dubbo.remoting.zookeeper;
+
+public interface Constants {
+
+
+    /**
+     * Default value for the period of retry interval in milliseconds: 5000
+     */
+    int DEFAULT_REGISTRY_RETRY_PERIOD = 5 * 1000;
+
+    /**
+     * Period of registry center's retry interval
+     */
+    String SUBSCRIBE_RETRY_PERIOD_KEY = "retry.period";
+
+}
