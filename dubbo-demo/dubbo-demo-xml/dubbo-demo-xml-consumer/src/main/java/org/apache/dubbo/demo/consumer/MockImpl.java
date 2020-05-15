d31bfeb37584a4298b0e56910e112f00fe0cc1a9
ken.lj <ken.lj.hz@gmail.com>
2020-05-15 14:07:22 +0800

    add test code

--- dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-consumer/src/main/java/org/apache/dubbo/demo/consumer/MockImpl.java
+++ dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-consumer/src/main/java/org/apache/dubbo/demo/consumer/MockImpl.java
@@ -18,23 +18,12 @@ package org.apache.dubbo.demo.consumer;
 
 import org.apache.dubbo.demo.DemoService;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class Application {
-    /**
-     * In order to make sure multicast registry works, need to specify '-Djava.net.preferIPv4Stack=true' before
-     * launch the application
-     */
-    public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/dubbo-consumer.xml");
-        context.start();
-        DemoService demoService = context.getBean("demoService", DemoService.class);
-//        CompletableFuture<String> hello = demoService.sayHelloAsync("world");
-//        System.out.println("result: " + hello.get());
-
-        while (true) {
-            System.out.println(demoService.sayHello("world"));
-            Thread.sleep(300);
-        }
+/**
+ *
+ */
+public class MockImpl implements DemoService {
+    @Override
+    public String sayHello(String name) {
+        return "MockImpl";
     }
 }
