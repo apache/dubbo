8a31efa9e2b3adcd6c82f621b44c0bca9de97733
candyleer <lican@apache.org>
2020-05-21 23:42:01 +0800

    update AttachmentsAdapter

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/AttachmentsAdapter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/AttachmentsAdapter.java
@@ -16,8 +16,10 @@
  */
 package org.apache.dubbo.rpc;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * This class provides map adapters to support attachments in RpcContext, Invocation and Result switch from
@@ -25,12 +27,16 @@ import java.util.Map;
  */
 public class AttachmentsAdapter {
 
-    public static class ObjectToStringMap extends HashMap<String, String> {
+    public static class ObjectToStringMap implements Map<String, String> {
+
+        private Map<String, String> attachments;
 
         public ObjectToStringMap(Map<String, Object> attachments) {
+            Map<String, String> tmpMap = new HashMap<>();
             for (Entry<String, Object> entry : attachments.entrySet()) {
-                super.put(entry.getKey(), convert(entry.getValue()));
+                tmpMap.put(entry.getKey(), convert(entry.getValue()));
             }
+            this.attachments = tmpMap;
         }
 
         private String convert(Object obj) {
@@ -39,14 +45,158 @@ public class AttachmentsAdapter {
             }
             return null; // or JSON.toString(obj);
         }
+
+        @Override
+        public int size() {
+            return attachments.size();
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return attachments.isEmpty();
+        }
+
+        @Override
+        public boolean containsKey(Object key) {
+            return attachments.containsKey(key);
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return attachments.containsValue(value);
+        }
+
+        @Override
+        public String get(Object key) {
+            return attachments.get(key);
+        }
+
+        @Override
+        public String put(String key, String value) {
+            return attachments.put(key, value);
+        }
+
+        @Override
+        public String remove(Object key) {
+            return attachments.remove(key);
+        }
+
+        @Override
+        public void putAll(Map<? extends String, ? extends String> m) {
+            attachments.putAll(m);
+        }
+
+        @Override
+        public void clear() {
+            attachments.clear();
+        }
+
+        @Override
+        public Set<String> keySet() {
+            return attachments.keySet();
+        }
+
+        @Override
+        public Collection<String> values() {
+            return attachments.values();
+        }
+
+        @Override
+        public Set<Entry<String, String>> entrySet() {
+            return attachments.entrySet();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return attachments.equals(o);
+        }
+
+        @Override
+        public int hashCode() {
+            return attachments.hashCode();
+        }
     }
 
-    public static class StringToObjectMap extends HashMap<String, Object> {
+
+    public static class StringToObjectMap implements Map<String, Object> {
+        private Map<String, Object> attachments;
 
         public StringToObjectMap(Map<String, String> attachments) {
+            Map<String, Object> tmpMap = new HashMap<>();
             for (Entry<String, String> entry : attachments.entrySet()) {
-                super.put(entry.getKey(), entry.getValue());
+                tmpMap.put(entry.getKey(), entry.getValue());
             }
+            this.attachments = tmpMap;
+        }
+
+        @Override
+        public int size() {
+            return attachments.size();
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return attachments.isEmpty();
+        }
+
+        @Override
+        public boolean containsKey(Object key) {
+            return attachments.containsKey(key);
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return attachments.containsValue(value);
+        }
+
+        @Override
+        public Object get(Object key) {
+            return attachments.get(key);
+        }
+
+        @Override
+        public Object put(String key, Object value) {
+            return attachments.put(key, value);
+        }
+
+        @Override
+        public Object remove(Object key) {
+            return attachments.remove(key);
+        }
+
+        @Override
+        public void putAll(Map<? extends String, ? extends Object> m) {
+            attachments.putAll(m);
+        }
+
+        @Override
+        public void clear() {
+            attachments.clear();
+        }
+
+        @Override
+        public Set<String> keySet() {
+            return attachments.keySet();
+        }
+
+        @Override
+        public Collection<Object> values() {
+            return attachments.values();
+        }
+
+        @Override
+        public Set<Entry<String, Object>> entrySet() {
+            return attachments.entrySet();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return attachments.equals(o);
+        }
+
+        @Override
+        public int hashCode() {
+            return attachments.hashCode();
         }
     }
 }
