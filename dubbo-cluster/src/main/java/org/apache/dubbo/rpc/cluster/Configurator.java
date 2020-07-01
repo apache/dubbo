4df50a396bcc379654ffc1791219799750d4b9a7
zouyixian <zouyixian@shein.com>
2020-07-01 11:27:16 +0800

    optimize logic

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/Configurator.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/Configurator.java
@@ -86,7 +86,7 @@ public interface Configurator extends Comparable<Configurator> {
             Map<String, String> override = new HashMap<>(url.getParameters());
             //The anyhost parameter of override may be added automatically, it can't change the judgement of changing url
             override.remove(ANYHOST_KEY);
-            if (override.size() == 0) {
+            if (CollectionUtils.isEmptyMap(override)) {
                 continue;
             }
             configurators.add(configuratorFactory.getConfigurator(url));
