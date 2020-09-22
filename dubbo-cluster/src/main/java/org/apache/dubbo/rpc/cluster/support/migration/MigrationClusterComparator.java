5be82ed8560f2c106b7a386006716d639b4f0581
hongwei.quhw <hongwei.quhw@antfin.com>
2020-09-22 13:00:48 +0800

    修改Licensed

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/support/migration/MigrationClusterComparator.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/support/migration/MigrationClusterComparator.java
@@ -9,11 +9,21 @@ import org.apache.dubbo.rpc.Invoker;
 
 import java.util.List;
 
-/**
- * Copyright (C) 2013-2018 Ant Financial Services Group
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * @author quhongwei
- * @version : MigrationClusterComparator.java, v 0.1 2020年08月25日 20:42 quhongwei Exp $
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 @SPI
 public interface MigrationClusterComparator {
--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/support/migration/MigrationClusterInvoker.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/support/migration/MigrationClusterInvoker.java
@@ -9,11 +9,21 @@ import org.apache.dubbo.rpc.cluster.ClusterInvoker;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-/**
- * Copyright (C) 2013-2018 Ant Financial Services Group
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * @author quhongwei
- * @version : MigrationCluserInvoker.java, v 0.1 2020年08月25日 20:19 quhongwei Exp $
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public interface MigrationClusterInvoker<T> extends ClusterInvoker<T> {
 
