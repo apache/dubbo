50a1232a19e7996fae989c02937fec7c139a2463
贾言坤 <jiayankun@21tb.com>
2020-10-29 15:14:35 +0800

    添加 Apache LICENSE 2.0 授权

--- dubbo-serialization/dubbo-serialization-jackson/src/main/java/org/apache/dubbo/common/serialize/jackson/JacksonObjectOutput.java
+++ dubbo-serialization/dubbo-serialization-jackson/src/main/java/org/apache/dubbo/common/serialize/jackson/JacksonObjectOutput.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.dubbo.common.serialize.jackson;
 
 import org.apache.dubbo.common.serialize.ObjectOutput;
@@ -105,8 +121,8 @@ public class JacksonObjectOutput implements ObjectOutput {
         if (JacksonUtils.isArray(data)) {
             value = JacksonUtils.writeValueAsString(data);
         } else {
-            value =
-                    "{\"@c\":\"" + data.getClass().getName() + "\"," + JacksonUtils.writeValueAsString(data).substring(1);
+            value = "{\"@c\":\"" + data.getClass().getName() + "\","
+                    + JacksonUtils.writeValueAsString(data).substring(1);
         }
         char[] json = value.toCharArray();
         writer.write(json, 0, json.length);
