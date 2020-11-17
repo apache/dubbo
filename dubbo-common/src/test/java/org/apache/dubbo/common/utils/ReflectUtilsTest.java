714febf8df59b66954a5e73d92052f088919e0fa
赵延 <1060026287@qq.com>
2020-11-17 20:04:22 +0800

    modify the unitTest judge logic.

--- dubbo-common/src/test/java/org/apache/dubbo/common/utils/ReflectUtilsTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/utils/ReflectUtilsTest.java
@@ -43,6 +43,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class ReflectUtilsTest {
     @Test
@@ -256,27 +257,25 @@ public class ReflectUtilsTest {
 
     @Test
     public void testGetBeanPropertyFields() throws Exception {
-        Map<String, Field> map = ReflectUtils.getBeanPropertyFields(EmptyClass.class);
+        EmptyClass emptyClass = new EmptyClass();
+        Map<String, Field> map = ReflectUtils.getBeanPropertyFields(emptyClass.getClass());
         assertThat(map.size(), is(2));
         assertThat(map, hasKey("set"));
         assertThat(map, hasKey("property"));
         for (Field f : map.values()) {
-            if (!f.isAccessible()) {
-                fail();
-            }
+            assertDoesNotThrow(() -> f.get(emptyClass));
         }
     }
 
     @Test
     public void testGetBeanPropertyReadMethods() throws Exception {
-        Map<String, Method> map = ReflectUtils.getBeanPropertyReadMethods(EmptyClass.class);
+        EmptyClass emptyClass = new EmptyClass();
+        Map<String, Method> map = ReflectUtils.getBeanPropertyReadMethods(emptyClass.getClass());
         assertThat(map.size(), is(2));
         assertThat(map, hasKey("set"));
         assertThat(map, hasKey("property"));
         for (Method m : map.values()) {
-            if (!m.isAccessible()) {
-                fail();
-            }
+            assertDoesNotThrow(() -> m.invoke(emptyClass));
         }
     }
 
