6622deb43f2953dd6c65f6e320279395d9a3ea78
guohao <guohaoice@gmail.com>
2021-01-07 21:42:54 +0800

    Enhance grpc status

--- dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/GrpcStatus.java
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/GrpcStatus.java
@@ -3,29 +3,63 @@ package org.apache.dubbo.rpc.protocol.tri;
 /**
  * See https://github.com/grpc/grpc/blob/master/doc/statuscodes.md
  */
-public enum GrpcStatus {
-    OK(0),
-    UNKNOWN(2),
-    NOT_FOUND(5),
-    RESOURCE_EXHAUSTED(8),
-    UNIMPLEMENTED(12),
-    INTERNAL(13);
 
-    final int code;
+public class GrpcStatus {
+    public static final GrpcStatus OK = new GrpcStatus(Code.OK, null, "OK");
+    public final Code code;
+    public final Throwable cause;
+    public final String description;
 
-    GrpcStatus(int code){
-        this.code=code;
+    public GrpcStatus(Code code, Throwable cause, String description) {
+        this.code = code;
+        this.cause = cause;
+        this.description = description;
+    }
+    public static GrpcStatus fromCode(int code) {
+        return fromCode(Code.fromCode(code));
     }
 
-    public static boolean isOk(Integer status){
-        return status== OK.code;
+    public static GrpcStatus fromCode(Code code) {
+        return new GrpcStatus(code, null, null);
     }
-    public static GrpcStatus fromCode(int code){
-        for (GrpcStatus value : GrpcStatus.values()) {
-            if(value.code==code){
-                return value;
-            }
+
+    public GrpcStatus withCause(Throwable cause) {
+        return new GrpcStatus(this.code, cause, this.description);
+    }
+
+    public GrpcStatus withDescription(String description) {
+        return new GrpcStatus(this.code, this.cause, description);
+    }
+
+    public TripleRpcException asException(){
+        return new TripleRpcException(this);
+    }
+    enum Code {
+        OK(0),
+        UNKNOWN(2),
+        NOT_FOUND(5),
+        RESOURCE_EXHAUSTED(8),
+        UNIMPLEMENTED(12),
+        INTERNAL(13);
+
+        final int code;
+
+        Code(int code) {
+            this.code = code;
         }
-        throw new IllegalStateException("Can not find status for code: "+code);
+
+        public static boolean isOk(Integer status) {
+            return status == OK.code;
+        }
+
+        public static Code fromCode(int code) {
+            for (Code value : Code.values()) {
+                if (value.code == code) {
+                    return value;
+                }
+            }
+            throw new IllegalStateException("Can not find status for code: " + code);
+        }
     }
+
 }
--- dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/TripleRpcException.java
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/TripleRpcException.java
@@ -5,12 +5,8 @@ import org.apache.dubbo.rpc.RpcException;
 public class TripleRpcException extends RpcException {
     private final GrpcStatus status;
 
-    public TripleRpcException(GrpcStatus status, String msg) {
-        super(msg);
-        this.status=status;
-    }
-    public TripleRpcException(GrpcStatus status, Throwable t) {
-        super(t);
+    public TripleRpcException(GrpcStatus status) {
+        super(status.description, status.cause);
         this.status = status;
     }
 
