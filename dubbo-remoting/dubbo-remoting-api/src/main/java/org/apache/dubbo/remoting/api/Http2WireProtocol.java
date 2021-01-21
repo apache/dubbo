53fcb47c4334ac49391d682df55f2892b7deffdf
guohao <guohaoice@gmail.com>
2021-01-21 14:40:08 +0800

    Set h2 logger level to DEBUG

--- dubbo-remoting/dubbo-remoting-api/src/main/java/org/apache/dubbo/remoting/api/Http2WireProtocol.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/org/apache/dubbo/remoting/api/Http2WireProtocol.java
@@ -2,11 +2,11 @@ package org.apache.dubbo.remoting.api;
 
 import io.netty.handler.codec.http2.Http2FrameLogger;
 
-import static io.netty.handler.logging.LogLevel.INFO;
+import static io.netty.handler.logging.LogLevel.DEBUG;
 
 public abstract class Http2WireProtocol implements WireProtocol {
-    public static final Http2FrameLogger CLIENT_LOGGER = new Http2FrameLogger(INFO, "H2_CLIENT");
-    public static final Http2FrameLogger SERVER_LOGGER = new Http2FrameLogger(INFO, "H2_SERVER");
+    public static final Http2FrameLogger CLIENT_LOGGER = new Http2FrameLogger(DEBUG, "H2_CLIENT");
+    public static final Http2FrameLogger SERVER_LOGGER = new Http2FrameLogger(DEBUG, "H2_SERVER");
     private final ProtocolDetector detector = new Http2ProtocolDetector();
 
     @Override
