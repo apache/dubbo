a5887235f9a40b9bad4ac8c257db5511b80448ae
zhangxin1932 <32886647+zhangxin1932@users.noreply.github.com>
2021-03-11 22:42:37 +0800

    to fix something wrong with MergeableClusterInvoker (#7360)
    * as MergeableClusterInvoker was used, when 'resultList.size() == 1', the resultType of "resultList.iterator().next()" is AppResponse, which would throw an exception of UnsupportedOperationException
    
    * as MergeableClusterInvoker was used, when 'resultList.size() == 1', the resultType of "resultList.iterator().next()" is AppResponse, which would throw an exception of UnsupportedOperationException
    
    Co-authored-by: jerry <jerry@zy.com>

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/support/MergeableClusterInvoker.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/support/MergeableClusterInvoker.java
@@ -114,7 +114,7 @@ public class MergeableClusterInvoker<T> extends AbstractClusterInvoker<T> {
         if (resultList.isEmpty()) {
             return AsyncRpcResult.newDefaultAsyncResult(invocation);
         } else if (resultList.size() == 1) {
-            return resultList.iterator().next();
+            return AsyncRpcResult.newDefaultAsyncResult(resultList.get(0).getValue(), invocation);
         }
 
         if (returnType == void.class) {
