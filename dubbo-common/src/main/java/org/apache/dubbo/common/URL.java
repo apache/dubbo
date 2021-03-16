fc92f295f249b782d24fe6d1956c77d6ba6c21a7
changye <changye@startdt.com>
2021-03-16 10:53:31 +0800

    remove unused import

--- dubbo-common/src/main/java/org/apache/dubbo/common/URL.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/URL.java
@@ -39,7 +39,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
