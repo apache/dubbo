28e2f3bea7144a265e401581d8acce8cb8340812
skyguard1 <qhdxssm@qq.com>
2021-03-17 15:00:52 +0800

    Make keepalive parameters configurable on server (#6500)
    * Make keepalive parameters configurable
    
    * Update by review
    
    Co-authored-by: xingrufei <xingrufei@sogou-inc.com>

--- dubbo-common/src/main/java/org/apache/dubbo/common/constants/CommonConstants.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/constants/CommonConstants.java
@@ -110,6 +110,8 @@ public interface CommonConstants {
 
     String IO_THREADS_KEY = "iothreads";
 
+    String KEEP_ALIVE_KEY = "keep.alive";
+
     int DEFAULT_QUEUES = 0;
 
     int DEFAULT_ALIVE = 60 * 1000;
