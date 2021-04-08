593871cd616c577e6d95578ba88e8435d651868b
zifeihan <zifeihan@apache.org>
2021-04-08 11:18:36 +0800

    Polishing.

--- dubbo-common/src/main/java/org/apache/dubbo/common/bytecode/Wrapper.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/bytecode/Wrapper.java
@@ -155,7 +155,7 @@ public abstract class Wrapper {
         }
 
         final ClassPool classPool = new ClassPool(ClassPool.getDefault());
-        classPool.appendClassPath(new CustomizedLoaderClassPath(c.getClassLoader()));
+        classPool.appendClassPath(new CustomizedLoaderClassPath(cl));
 
         List<String> allMethod = new ArrayList<>();
         try {
