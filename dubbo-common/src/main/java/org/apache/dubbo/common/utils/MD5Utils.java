8f79fe173b1f9c648be9007e2e9faee2e2b355bb
xiaoheng1 <2018154970@qq.com>
2021-04-14 21:20:25 +0800

    ServiceNameMapping storage structure modification

--- dubbo-common/src/main/java/org/apache/dubbo/common/utils/MD5Utils.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/utils/MD5Utils.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -74,7 +73,7 @@ public class MD5Utils {
     }
 
     /**
-     * 将一个字节数组转化为可见的字符串.
+     * Convert a byte array into a visible string.
      */
     public static String encodeHexString(byte[] bytes) {
         int l = bytes.length;
