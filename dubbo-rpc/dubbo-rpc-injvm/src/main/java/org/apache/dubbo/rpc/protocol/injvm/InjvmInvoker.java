ee2099827cef22d24beeec4bd167337a4ab3775b
zhangyz-hd <zhangyzhd@163.com>
2021-05-08 15:35:56 +0800

    Update InjvmInvoker.java

--- dubbo-rpc/dubbo-rpc-injvm/src/main/java/org/apache/dubbo/rpc/protocol/injvm/InjvmInvoker.java
+++ dubbo-rpc/dubbo-rpc-injvm/src/main/java/org/apache/dubbo/rpc/protocol/injvm/InjvmInvoker.java
@@ -83,10 +83,7 @@ class InjvmInvoker<T> extends AbstractInvoker<T> {
         if (isAsync(exporter.getInvoker().getUrl(), getUrl())) {
             ((RpcInvocation) invocation).setInvokeMode(InvokeMode.ASYNC);
             // use consumer executor
-            ExecutorService executor = executorRepository.getExecutor(getUrl());
-            if (executor == null) {
-                executor = executorRepository.createExecutorIfAbsent(getUrl());
-            }
+            ExecutorService executor = executorRepository.createExecutorIfAbsent(getUrl());
             CompletableFuture<AppResponse> appResponseFuture = CompletableFuture.supplyAsync(() -> {
                 Result result = exporter.getInvoker().invoke(invocation);
                 if (result.hasException()) {
