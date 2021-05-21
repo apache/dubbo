1e132a3e9708ee2513b1b6c93c82786e5aae2281
zhenyu zhang <hadleyzhang@gmail.com>
2021-05-21 04:11:00 -0700

    modify unit test of MetricsConfig to pass the Check

--- dubbo-config/dubbo-config-api/src/test/java/org/apache/dubbo/config/MetricsConfigTest.java
+++ dubbo-config/dubbo-config-api/src/test/java/org/apache/dubbo/config/MetricsConfigTest.java
@@ -2,11 +2,8 @@ package org.apache.dubbo.config;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class MetricsConfigTest {
 
@@ -14,19 +11,13 @@ public class MetricsConfigTest {
     public void testPort() throws Exception{
         MetricsConfig metrics = new MetricsConfig();
         metrics.setPort("8080");
-        Map<String, String> parameters = new HashMap<>();
-        MetricsConfig.appendParameters(parameters, metrics);
-        assertThat(metrics.getPort(), equals("8080"));
-        assertThat(parameters.isEmpty(), is(true));
+        assertThat(metrics.getPort(), equalTo("8080"));
     }
 
     @Test
     public void testProtocol() throws Exception{
         MetricsConfig metrics = new MetricsConfig();
         metrics.setProtocol("protocol");
-        Map<String, String> parameters = new HashMap<>();
-        MetricsConfig.appendParameters(parameters, metrics);
-        assertThat(metrics.getProtocol(), equals("protocol"));
-        assertThat(parameters.isEmpty(), is(true));
+        assertThat(metrics.getProtocol(), equalTo("protocol"));
     }
 }
