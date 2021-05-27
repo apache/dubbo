8c845cb5f6d0dd79eaa390b0eafef9df49ff614b
Albumen Kevin <jhq0812@gmail.com>
2021-05-27 16:00:47 +0800

    Pick from #7303 497647bf

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/bootstrap/DubboBootstrap.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/bootstrap/DubboBootstrap.java
@@ -1251,7 +1251,7 @@ public class DubboBootstrap extends GenericEventListener {
                     unreferServices();
 
                     destroyRegistries();
-                    DubboShutdownHook.destroyProtocols();
+
                     destroyServiceDiscoveries();
 
                     clear();
