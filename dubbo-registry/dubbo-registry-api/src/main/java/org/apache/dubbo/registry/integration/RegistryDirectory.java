efd84a314da1eb4f5059800e140a516b1e8b88ba
zhongxiong.zeng <zhongxiong.zeng@za.group>
2021-05-28 15:27:52 +0800

    1 fix with RegistryDirectory.java: remove unused imports

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/integration/RegistryDirectory.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/integration/RegistryDirectory.java
@@ -39,7 +39,6 @@ import org.apache.dubbo.rpc.protocol.InvokerWrapper;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
