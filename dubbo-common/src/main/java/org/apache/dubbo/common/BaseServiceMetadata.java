a545a6000c78e32876b546edb903f6db154d385a
灼华 <43363120+BurningCN@users.noreply.github.com>
2021-05-28 17:13:17 +0800

    Reuse the existing method of generating ServiceKey (#7875)

--- dubbo-common/src/main/java/org/apache/dubbo/common/BaseServiceMetadata.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/BaseServiceMetadata.java
@@ -33,11 +33,11 @@ public class BaseServiceMetadata {
 
     public static String buildServiceKey(String path, String group, String version) {
         StringBuilder buf = new StringBuilder();
-        if (group != null && group.length() > 0) {
+        if (StringUtils.isNotEmpty(group)) {
             buf.append(group).append("/");
         }
         buf.append(path);
-        if (version != null && version.length() > 0) {
+        if (StringUtils.isNotEmpty(version)) {
             buf.append(":").append(version);
         }
         return buf.toString();
