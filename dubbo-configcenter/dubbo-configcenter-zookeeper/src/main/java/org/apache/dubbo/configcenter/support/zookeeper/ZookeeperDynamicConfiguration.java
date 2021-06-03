d5b46c3e6561b1c42d8f146f03e388342d9d787d
赵延 <1060026287@qq.com>
2021-06-03 17:23:30 +0800

    Use nodeCache to take place of treeCache (#7953)

--- dubbo-configcenter/dubbo-configcenter-zookeeper/src/main/java/org/apache/dubbo/configcenter/support/zookeeper/ZookeeperDynamicConfiguration.java
+++ dubbo-configcenter/dubbo-configcenter-zookeeper/src/main/java/org/apache/dubbo/configcenter/support/zookeeper/ZookeeperDynamicConfiguration.java
@@ -21,12 +21,13 @@ import org.apache.dubbo.common.config.configcenter.ConfigItem;
 import org.apache.dubbo.common.config.configcenter.ConfigurationListener;
 import org.apache.dubbo.common.config.configcenter.TreePathDynamicConfiguration;
 import org.apache.dubbo.common.threadpool.support.AbortPolicyWithReport;
+import org.apache.dubbo.common.utils.CollectionUtils;
 import org.apache.dubbo.common.utils.NamedThreadFactory;
 import org.apache.dubbo.remoting.zookeeper.ZookeeperClient;
 import org.apache.dubbo.remoting.zookeeper.ZookeeperTransporter;
 
 import java.util.Collection;
-import java.util.concurrent.CountDownLatch;
+import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -41,7 +42,6 @@ public class ZookeeperDynamicConfiguration extends TreePathDynamicConfiguration 
     // The final root path would be: /configRootPath/"config"
     private String rootPath;
     private final ZookeeperClient zkClient;
-    private CountDownLatch initializedLatch;
 
     private CacheListener cacheListener;
     private URL url;
@@ -55,8 +55,7 @@ public class ZookeeperDynamicConfiguration extends TreePathDynamicConfiguration 
         this.url = url;
         rootPath = getRootPath(url);
 
-        initializedLatch = new CountDownLatch(1);
-        this.cacheListener = new CacheListener(rootPath, initializedLatch);
+        this.cacheListener = new CacheListener(rootPath);
 
         final String threadName = this.getClass().getSimpleName();
         this.executor = new ThreadPoolExecutor(DEFAULT_ZK_EXECUTOR_THREADS_NUM, DEFAULT_ZK_EXECUTOR_THREADS_NUM,
@@ -66,17 +65,9 @@ public class ZookeeperDynamicConfiguration extends TreePathDynamicConfiguration 
                 new AbortPolicyWithReport(threadName, url));
 
         zkClient = zookeeperTransporter.connect(url);
-        zkClient.addDataListener(rootPath, cacheListener, executor);
-        try {
-            // Wait for connection
-            long timeout = url.getParameter("init.timeout", 5000);
-            boolean isCountDown = this.initializedLatch.await(timeout, TimeUnit.MILLISECONDS);
-            if (!isCountDown) {
-                throw new IllegalStateException("Failed to receive INITIALIZED event from zookeeper, pls. check if url "
-                        + url + " is correct");
-            }
-        } catch (InterruptedException e) {
-            logger.warn("Failed to build local cache for config center (zookeeper)." + url);
+        boolean isConnected = zkClient.isConnected();
+        if (!isConnected) {
+            throw new IllegalStateException("Failed to connect with zookeeper, pls check if url " + url + " is correct.");
         }
     }
 
@@ -132,11 +123,16 @@ public class ZookeeperDynamicConfiguration extends TreePathDynamicConfiguration 
     @Override
     protected void doAddListener(String pathKey, ConfigurationListener listener) {
         cacheListener.addListener(pathKey, listener);
+        zkClient.addDataListener(pathKey, cacheListener, executor);
     }
 
     @Override
     protected void doRemoveListener(String pathKey, ConfigurationListener listener) {
         cacheListener.removeListener(pathKey, listener);
+        Set<ConfigurationListener> configurationListeners = cacheListener.getConfigurationListeners(pathKey);
+        if (CollectionUtils.isNotEmpty(configurationListeners)) {
+            zkClient.removeDataListener(pathKey, cacheListener);
+        }
     }
 
     @Override
