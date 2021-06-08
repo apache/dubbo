ed88f43a1b8a462edca6c93facc604c8acea883d
灼华 <43363120+BurningCN@users.noreply.github.com>
2021-06-08 03:30:59 -0500

    Optimize URL#addParameters method to reuse existing methods (#8005)

--- dubbo-common/src/main/java/org/apache/dubbo/common/URL.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/URL.java
@@ -1244,18 +1244,10 @@ class URL implements Serializable {
     }
 
     public URL addParameters(String... pairs) {
-        if (pairs == null || pairs.length == 0) {
+        if (ArrayUtils.isEmpty(pairs)) {
             return this;
         }
-        if (pairs.length % 2 != 0) {
-            throw new IllegalArgumentException("Map pairs can not be odd number.");
-        }
-        Map<String, String> map = new HashMap<>();
-        int len = pairs.length / 2;
-        for (int i = 0; i < len; i++) {
-            map.put(pairs[2 * i], pairs[2 * i + 1]);
-        }
-        return addParameters(map);
+        return addParameters(CollectionUtils.toStringMap(pairs));
     }
 
     public URL addParameterString(String query) {
