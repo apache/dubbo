f0ee73e76fadb55c0c438fa278c84b45602a9725
ken.lj <ken.lj.hz@gmail.com>
2021-06-08 18:04:39 +0800

    [3.0] Address URL fault tolerance on getParameter usage (#7864)

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/client/migration/MigrationInvoker.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/client/migration/MigrationInvoker.java
@@ -489,4 +489,8 @@ public class MigrationInvoker<T> implements MigrationClusterInvoker<T> {
     public boolean checkInvokerAvailable(ClusterInvoker<T> invoker) {
         return invoker != null && !invoker.isDestroyed() && invoker.isAvailable();
     }
+
+    public ClusterInvoker<T> getCurrentAvailableInvoker() {
+        return currentAvailableInvoker;
+    }
 }
