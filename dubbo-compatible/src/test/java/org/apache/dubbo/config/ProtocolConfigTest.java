b548181d4fc39a812de646273f510b9fb896f5e8
Albumen Kevin <jhq0812@gmail.com>
2021-06-09 16:50:41 +0800

    fix end new line

--- dubbo-compatible/src/test/java/org/apache/dubbo/config/ProtocolConfigTest.java
+++ dubbo-compatible/src/test/java/org/apache/dubbo/config/ProtocolConfigTest.java
@@ -18,17 +18,16 @@
 package org.apache.dubbo.config;
 
 import com.alibaba.dubbo.config.ProtocolConfig;
-
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ProtocolConfigTest {
 
