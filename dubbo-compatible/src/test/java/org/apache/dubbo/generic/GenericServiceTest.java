f75d537a5e6cd9976c55696775b1f9b5fd09fca7
BurningCN <1015773611@qq.com>
2021-06-10 11:50:48 +0800

    Revert last commit

--- dubbo-compatible/src/test/java/org/apache/dubbo/generic/GenericServiceTest.java
+++ dubbo-compatible/src/test/java/org/apache/dubbo/generic/GenericServiceTest.java
@@ -51,10 +51,10 @@ public class GenericServiceTest {
         DemoService server = new DemoServiceImpl();
         ProxyFactory proxyFactory = ExtensionLoader.getExtensionLoader(ProxyFactory.class).getAdaptiveExtension();
         Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension();
-        URL url = URL.valueOf("dubbo://127.0.0.1:5342/" + DemoService.class.getName() + "?version=1.0.0&timeout=999999");
+        URL url = URL.valueOf("dubbo://127.0.0.1:5342/" + DemoService.class.getName() + "?version=1.0.0");
         Exporter<DemoService> exporter = protocol.export(proxyFactory.getInvoker(server, DemoService.class, url));
-
         Invoker<DemoService> invoker = protocol.refer(DemoService.class, url);
+
         GenericService client = (GenericService) proxyFactory.getProxy(invoker, true);
         Object result = client.$invoke("sayHello", new String[]{"java.lang.String"}, new Object[]{"haha"});
         Assertions.assertEquals("hello haha", result);
@@ -71,16 +71,16 @@ public class GenericServiceTest {
         DemoService server = new DemoServiceImpl();
         ProxyFactory proxyFactory = ExtensionLoader.getExtensionLoader(ProxyFactory.class).getAdaptiveExtension();
         Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension();
-        URL url = URL.valueOf("dubbo://127.0.0.1:5342/" + DemoService.class.getName() + "?version=1.0.0&generic=true&timeout=3000000");
+        URL url = URL.valueOf("dubbo://127.0.0.1:5342/" + DemoService.class.getName() + "?version=1.0.0&generic=true$timeout=3000");
         Exporter<DemoService> exporter = protocol.export(proxyFactory.getInvoker(server, DemoService.class, url));
-
-
         Invoker<GenericService> invoker = protocol.refer(GenericService.class, url);
+
         GenericService client = proxyFactory.getProxy(invoker, true);
         Object result = client.$invoke("sayHello", new String[]{"java.lang.String"}, new Object[]{"haha"});
         Assertions.assertEquals("hello haha", result);
 
         Invoker<DemoService> invoker2 = protocol.refer(DemoService.class, url);
+
         GenericService client2 = (GenericService) proxyFactory.getProxy(invoker2, true);
         Object result2 = client2.$invoke("sayHello", new String[]{"java.lang.String"}, new Object[]{"haha"});
         Assertions.assertEquals("hello haha", result2);
