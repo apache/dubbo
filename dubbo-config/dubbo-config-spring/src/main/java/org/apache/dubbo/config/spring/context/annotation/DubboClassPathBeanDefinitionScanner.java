4ba48f541fd39cb44e1a61f3dfaede04a6ee9243
zhang.jie <zhangjie@rivamed.cn>
2021-06-15 15:08:08 +0800

    add cache for scan result. (#7477)

--- dubbo-config/dubbo-config-spring/src/main/java/org/apache/dubbo/config/spring/context/annotation/DubboClassPathBeanDefinitionScanner.java
+++ dubbo-config/dubbo-config-spring/src/main/java/org/apache/dubbo/config/spring/context/annotation/DubboClassPathBeanDefinitionScanner.java
@@ -16,6 +16,7 @@
  */
 package org.apache.dubbo.config.spring.context.annotation;
 
+import org.apache.dubbo.common.utils.CollectionUtils;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
@@ -23,7 +24,10 @@ import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ResourceLoader;
 
+import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import static org.springframework.context.annotation.AnnotationConfigUtils.registerAnnotationConfigProcessors;
 
@@ -35,6 +39,11 @@ import static org.springframework.context.annotation.AnnotationConfigUtils.regis
  * @since 2.5.7
  */
 public class DubboClassPathBeanDefinitionScanner extends ClassPathBeanDefinitionScanner {
+
+    /**
+     * key is package to scan, value is BeanDefinition
+     */
+    private final ConcurrentMap<String, Set<BeanDefinition>> beanDefinitionMap = new ConcurrentHashMap<>();
 
 
     public DubboClassPathBeanDefinitionScanner(BeanDefinitionRegistry registry, boolean useDefaultFilters, Environment environment,
@@ -67,4 +76,15 @@ public class DubboClassPathBeanDefinitionScanner extends ClassPathBeanDefinition
         return super.checkCandidate(beanName, beanDefinition);
     }
 
+    @Override
+    public Set<BeanDefinition> findCandidateComponents(String basePackage) {
+        Set<BeanDefinition> beanDefinitions = beanDefinitionMap.get(basePackage);
+        // if beanDefinitions size eq 0 => don't scan
+        // if beanDefinitions size is null => scan
+        if (Objects.isNull(beanDefinitions)) {
+            beanDefinitions = super.findCandidateComponents(basePackage);
+            beanDefinitionMap.put(basePackage, beanDefinitions);
+        }
+        return beanDefinitions;
+    }
 }
