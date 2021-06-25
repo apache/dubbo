e73813b2a656bb25cf7a49b77597d52bd66c7160
ken.lj <ken.lj.hz@gmail.com>
2021-06-25 11:07:29 +0800

    add getter to RouterChain

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/RouterChain.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/RouterChain.java
@@ -149,6 +149,10 @@ public class RouterChain<T> {
         return routers;
     }
 
+    public List<StateRouter> getStateRouters() {
+        return stateRouters;
+    }
+
     private void sort() {
         Collections.sort(routers);
     }
