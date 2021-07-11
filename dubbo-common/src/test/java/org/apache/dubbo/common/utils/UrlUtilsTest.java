90457c75320e8ce034a7535cbfe0609fa1cf92d2
blake.qiu <blake.qiu@ly.com>
2021-07-12 00:15:06 +0800

    Fixed the line indentation caused by the last commit

--- dubbo-common/src/test/java/org/apache/dubbo/common/utils/UrlUtilsTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/utils/UrlUtilsTest.java
@@ -344,8 +344,8 @@ public class UrlUtilsTest {
     public void testIsServiceKeyMatch() throws Exception {
         URL url = URL.valueOf("test://127.0.0.1");
         URL pattern = url.addParameter(GROUP_KEY, "test")
-            .addParameter(INTERFACE_KEY, "test")
-            .addParameter(VERSION_KEY, "test");
+                .addParameter(INTERFACE_KEY, "test")
+                .addParameter(VERSION_KEY, "test");
         URL value = pattern;
         assertTrue(UrlUtils.isServiceKeyMatch(pattern, value));
 
