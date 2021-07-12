21a6b4d1a950e525eb860857dec2b0620569f220
djz段竞哲 <jzduan@trip.com>
2021-07-12 12:57:22 +0800

    fix ut

--- dubbo-common/src/test/java/org/apache/dubbo/config/context/ConfigManagerTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/config/context/ConfigManagerTest.java
@@ -267,7 +267,7 @@ public class ConfigManagerTest {
                 getConfigManager().addConfig(applicationConfig2);
                 fail("strict mode cannot add two application configs");
             } catch (Exception e) {
-                assertEquals(IllegalStateException.class, e.getClass());
+                assertEquals(RuntimeException.class, e.getClass());
                 assertTrue(e.getMessage().contains("please remove redundant configs and keep only one"));
             }
 
