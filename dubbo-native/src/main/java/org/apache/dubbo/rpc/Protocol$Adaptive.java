7018bd04ed74fcf9529e16be5454f857bde7a646
chenglu <1285823170@qq.com>
2021-07-25 22:19:04 +0800

    rollback the class generate in native.

--- dubbo-native/src/main/java/org/apache/dubbo/rpc/Protocol$Adaptive.java
+++ dubbo-native/src/main/java/org/apache/dubbo/rpc/Protocol$Adaptive.java
@@ -17,7 +17,6 @@
 package org.apache.dubbo.rpc;
 import org.apache.dubbo.common.extension.ExtensionLoader;
 public class Protocol$Adaptive implements org.apache.dubbo.rpc.Protocol {
-@Override
 public org.apache.dubbo.rpc.Exporter export(org.apache.dubbo.rpc.Invoker arg0) throws org.apache.dubbo.rpc.RpcException {
 if (arg0 == null) throw new IllegalArgumentException("org.apache.dubbo.rpc.Invoker argument == null");
 if (arg0.getUrl() == null) throw new IllegalArgumentException("org.apache.dubbo.rpc.Invoker argument getUrl() == null");
@@ -27,7 +26,6 @@ if(extName == null) throw new IllegalStateException("Failed to get extension (or
 org.apache.dubbo.rpc.Protocol extension = (org.apache.dubbo.rpc.Protocol)ExtensionLoader.getExtensionLoader(org.apache.dubbo.rpc.Protocol.class).getExtension(extName);
 return extension.export(arg0);
 }
-@Override
 public org.apache.dubbo.rpc.Invoker refer(java.lang.Class arg0, org.apache.dubbo.common.URL arg1) throws org.apache.dubbo.rpc.RpcException {
 if (arg1 == null) throw new IllegalArgumentException("url == null");
 org.apache.dubbo.common.URL url = arg1;
@@ -36,14 +34,12 @@ if(extName == null) throw new IllegalStateException("Failed to get extension (or
 org.apache.dubbo.rpc.Protocol extension = (org.apache.dubbo.rpc.Protocol)ExtensionLoader.getExtensionLoader(org.apache.dubbo.rpc.Protocol.class).getExtension(extName);
 return extension.refer(arg0, arg1);
 }
-@Override
 public java.util.List getServers()  {
 throw new UnsupportedOperationException("The method public default java.util.List org.apache.dubbo.rpc.Protocol.getServers() of interface org.apache.dubbo.rpc.Protocol is not adaptive method!");
 }
 public void destroy()  {
 throw new UnsupportedOperationException("The method public abstract void org.apache.dubbo.rpc.Protocol.destroy() of interface org.apache.dubbo.rpc.Protocol is not adaptive method!");
 }
-@Override
 public int getDefaultPort()  {
 throw new UnsupportedOperationException("The method public abstract int org.apache.dubbo.rpc.Protocol.getDefaultPort() of interface org.apache.dubbo.rpc.Protocol is not adaptive method!");
 }
