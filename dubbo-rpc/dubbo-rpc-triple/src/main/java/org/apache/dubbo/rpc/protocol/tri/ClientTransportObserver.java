c3c6f7176fdb8ab3df0ff72755a5ebc86bb734cd
earthchen <youwei.chen@duobei.com>
2021-08-17 02:47:59 -0500

    Fixed TLS using scheme HTTP when using TRI protocol (#8497)
    * Fixed TLS using scheme HTTP when using TRI protocol
    
    * change ssl config to channel attr
    
    * Reuse scheme variable & Set attr only if ssl enabled

--- dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/ClientTransportObserver.java
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/ClientTransportObserver.java
@@ -30,7 +30,7 @@ import io.netty.handler.codec.http2.Http2StreamChannelBootstrap;
 import io.netty.util.AsciiString;
 
 public class ClientTransportObserver implements TransportObserver {
-    private static final AsciiString SCHEME = AsciiString.of("http");
+    private final AsciiString SCHEME;
     private final ChannelHandlerContext ctx;
     private final Http2StreamChannel streamChannel;
     private final ChannelPromise promise;
@@ -41,6 +41,12 @@ public class ClientTransportObserver implements TransportObserver {
     public ClientTransportObserver(ChannelHandlerContext ctx, AbstractClientStream stream, ChannelPromise promise) {
         this.ctx = ctx;
         this.promise = promise;
+        Boolean ssl = ctx.channel().attr(TripleConstant.SSL_ATTRIBUTE_KEY).get();
+        if (ssl != null && ssl) {
+            SCHEME = TripleConstant.HTTPS_SCHEME;
+        } else {
+            SCHEME = TripleConstant.HTTP_SCHEME;
+        }
 
         final Http2StreamChannelBootstrap streamChannelBootstrap = new Http2StreamChannelBootstrap(ctx.channel());
         streamChannel = streamChannelBootstrap.open().syncUninterruptibly().getNow();
