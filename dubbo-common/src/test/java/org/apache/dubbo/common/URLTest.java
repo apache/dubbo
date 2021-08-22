a900299b1cada6a9afef8799c8a70e6781cde9a3
wangchengming <634749869@qq.com>
2021-08-22 22:28:36 +0800

    enhance URL test

--- dubbo-common/src/test/java/org/apache/dubbo/common/URLTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/URLTest.java
@@ -911,20 +911,24 @@ public class URLTest {
     public void testEquals() {
         URL url1 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
                 "dubbo-demo-api-consumer&category=consumers&check=false&dubbo=2.0.2&generic=true&interface=" +
-                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=1599556506417");
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=1599556506417" +
+                "&remote.timestamp=remote");
         URL url2 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
                 "dubbo-demo-api-consumer&category=consumers&check=false&dubbo=2.0.2&generic=true&interface=" +
-                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417");
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417" +
+                "&remote.timestamp=remote");
         assertEquals(url1, url2);
 
         URL url3 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
                 "dubbo-demo-api-consumer&category=consumers&check=false&dubbo=2.0.2&interface=" +
-                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417");
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417" +
+                "&remote.timestamp=remote");
         assertNotEquals(url2, url3);
 
         URL url4 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
                 "dubbo-demo-api-consumer&category=consumers&check=true&dubbo=2.0.2&interface=" +
-                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417");
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417" +
+                "&remote.timestamp=remote");
         assertNotEquals(url3, url4);
     }
 
@@ -932,20 +936,24 @@ public class URLTest {
     public void testHashcode() {
         URL url1 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
                 "dubbo-demo-api-consumer&category=consumers&check=false&dubbo=2.0.2&generic=true&interface=" +
-                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=1599556506417");
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=1599556506417" +
+                "&remote.timestamp=remote");
         URL url2 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
                 "dubbo-demo-api-consumer&category=consumers&check=false&dubbo=2.0.2&generic=true&interface=" +
-                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417");
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417" +
+                "&remote.timestamp=remote");
         assertEquals(url1.hashCode(), url2.hashCode());
 
         URL url3 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
                 "dubbo-demo-api-consumer&category=consumers&check=false&dubbo=2.0.2&interface=" +
-                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417");
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417" +
+                "&remote.timestamp=remote");
         assertNotEquals(url2.hashCode(), url3.hashCode());
 
         URL url4 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
                 "dubbo-demo-api-consumer&category=consumers&check=true&dubbo=2.0.2&interface=" +
-                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417");
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417" +
+                "&remote.timestamp=remote");
         assertNotEquals(url3.hashCode(), url4.hashCode());
     }
 
