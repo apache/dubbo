221b7a4150bcaf292b683e9177aed275c93c3c9c
Albumen Kevin <jhq0812@gmail.com>
2021-08-28 22:58:34 +0800

    exact ServiceModel from ProviderModel and ConsumerModel

--- dubbo-common/src/main/java/org/apache/dubbo/rpc/model/ConsumerModel.java
+++ dubbo-common/src/main/java/org/apache/dubbo/rpc/model/ConsumerModel.java
@@ -16,9 +16,7 @@
  */
 package org.apache.dubbo.rpc.model;
 
-import org.apache.dubbo.common.BaseServiceMetadata;
 import org.apache.dubbo.common.utils.Assert;
-import org.apache.dubbo.common.utils.ClassUtils;
 import org.apache.dubbo.config.ReferenceConfigBase;
 
 import java.lang.reflect.Method;
@@ -34,15 +32,11 @@ import java.util.TreeSet;
 /**
  * This model is bound to your reference's configuration, for example, group, version or method level configuration.
  */
-public class ConsumerModel {
-    private String serviceKey;
-    private final ServiceDescriptor serviceModel;
-    private final ReferenceConfigBase<?> referenceConfig;
+public class ConsumerModel extends ServiceModel {
     private final Set<String> apps = new TreeSet<>();
 
-    private Object proxyObject;
-
     private final Map<String, AsyncMethodInfo> methodConfigs;
+    private Map<Method, ConsumerMethodModel> methodModels = new HashMap<>();
 
     /**
      * This constructor create an instance of ConsumerModel and passed objects should not be null.
@@ -58,55 +52,27 @@ public class ConsumerModel {
                          ReferenceConfigBase<?> referenceConfig,
                          Map<String, AsyncMethodInfo> methodConfigs) {
 
+        super(proxyObject, serviceKey, serviceModel, referenceConfig);
         Assert.notEmptyString(serviceKey, "Service name can't be null or blank");
 
-        this.serviceKey = serviceKey;
-        this.proxyObject = proxyObject;
-        this.serviceModel = serviceModel;
-        this.referenceConfig = referenceConfig;
         this.methodConfigs = methodConfigs == null ? new HashMap<>() : methodConfigs;
     }
 
-    /**
-     * Return the proxy object used by called while creating instance of ConsumerModel
-     *
-     * @return
-     */
-    public Object getProxyObject() {
-        return proxyObject;
-    }
+    public ConsumerModel(String serviceKey,
+                         Object proxyObject,
+                         ServiceDescriptor serviceModel,
+                         ReferenceConfigBase<?> referenceConfig,
+                         ServiceMetadata metadata,
+                         Map<String, AsyncMethodInfo> methodConfigs) {
 
-    public void setProxyObject(Object proxyObject) {
-        this.proxyObject = proxyObject;
-    }
+        super(proxyObject, serviceKey, serviceModel, referenceConfig, metadata);
+        Assert.notEmptyString(serviceKey, "Service name can't be null or blank");
 
-    /**
-     * Return all method models for the current service
-     *
-     * @return method model list
-     */
-    public Set<MethodDescriptor> getAllMethods() {
-        return serviceModel.getAllMethods();
-    }
-
-    public Class<?> getServiceInterfaceClass() {
-        return serviceModel.getServiceInterfaceClass();
-    }
-
-    public String getServiceKey() {
-        return serviceKey;
+        this.methodConfigs = methodConfigs == null ? new HashMap<>() : methodConfigs;
     }
 
     public AsyncMethodInfo getMethodConfig(String methodName) {
         return methodConfigs.get(methodName);
-    }
-
-    public ServiceDescriptor getServiceModel() {
-        return serviceModel;
-    }
-
-    public ReferenceConfigBase getReferenceConfig() {
-        return referenceConfig;
     }
 
     public Set<String> getApps() {
@@ -117,59 +83,23 @@ public class ConsumerModel {
         return methodConfigs.get(methodName);
     }
 
-    /* *************** Start, metadata compatible **************** */
-
-    private ServiceMetadata serviceMetadata;
-    private Map<Method, ConsumerMethodModel> methodModels = new HashMap<>();
-
-    public ConsumerModel(String serviceKey,
-                         Object proxyObject,
-                         ServiceDescriptor serviceModel,
-                         ReferenceConfigBase<?> referenceConfig,
-                         ServiceMetadata metadata,
-                         Map<String, AsyncMethodInfo> methodConfigs) {
-
-        this(serviceKey, proxyObject, serviceModel, referenceConfig, methodConfigs);
-        this.serviceMetadata = metadata;
-    }
-
-    public void setServiceKey(String serviceKey) {
-        this.serviceKey = serviceKey;
-        if (serviceMetadata != null) {
-            serviceMetadata.setServiceKey(serviceKey);
-            serviceMetadata.setGroup(BaseServiceMetadata.groupFromServiceKey(serviceKey));
-        }
-    }
-
     public void initMethodModels() {
         Class<?>[] interfaceList;
-        if (proxyObject == null) {
-            Class<?> serviceInterfaceClass = referenceConfig.getServiceInterfaceClass();
+        if (getProxyObject() == null) {
+            Class<?> serviceInterfaceClass = getReferenceConfig().getServiceInterfaceClass();
             if (serviceInterfaceClass != null) {
                 interfaceList = new Class[]{serviceInterfaceClass};
             } else {
                 interfaceList = new Class[0];
             }
         } else {
-            interfaceList = proxyObject.getClass().getInterfaces();
+            interfaceList = getProxyObject().getClass().getInterfaces();
         }
         for (Class<?> interfaceClass : interfaceList) {
             for (Method method : interfaceClass.getMethods()) {
                 methodModels.put(method, new ConsumerMethodModel(method));
             }
         }
-    }
-
-    public ClassLoader getClassLoader() {
-        Class<?> serviceType = serviceMetadata.getServiceType();
-        return serviceType != null ? serviceType.getClassLoader() : ClassUtils.getClassLoader();
-    }
-
-    /**
-     * @return serviceMetadata
-     */
-    public ServiceMetadata getServiceMetadata() {
-        return serviceMetadata;
     }
 
     /**
@@ -214,10 +144,4 @@ public class ConsumerModel {
     public List<ConsumerMethodModel> getAllMethodModels() {
         return new ArrayList<>(methodModels.values());
     }
-
-    public String getServiceName() {
-        return this.serviceMetadata.getServiceKey();
-    }
-
-
 }
---
+++ dubbo-common/src/main/java/org/apache/dubbo/rpc/model/ServiceModel.java
@@ -0,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.dubbo.rpc.model;
+
+import org.apache.dubbo.common.BaseServiceMetadata;
+import org.apache.dubbo.common.utils.ClassUtils;
+import org.apache.dubbo.config.AbstractInterfaceConfig;
+import org.apache.dubbo.config.ReferenceConfigBase;
+import org.apache.dubbo.config.ServiceConfigBase;
+
+import java.util.Set;
+
+public class ServiceModel {
+    private String serviceKey;
+    private Object proxyObject;
+    private final ServiceDescriptor serviceModel;
+    private final AbstractInterfaceConfig config;
+
+    private ServiceMetadata serviceMetadata;
+
+    public ServiceModel(Object proxyObject, String serviceKey, ServiceDescriptor serviceModel, AbstractInterfaceConfig config) {
+        this.proxyObject = proxyObject;
+        this.serviceKey = serviceKey;
+        this.serviceModel = serviceModel;
+        this.config = config;
+    }
+
+    public ServiceModel(Object proxyObject, String serviceKey, ServiceDescriptor serviceModel, AbstractInterfaceConfig config, ServiceMetadata serviceMetadata) {
+        this.proxyObject = proxyObject;
+        this.serviceKey = serviceKey;
+        this.serviceModel = serviceModel;
+        this.config = config;
+        this.serviceMetadata = serviceMetadata;
+    }
+
+    public String getServiceKey() {
+        return serviceKey;
+    }
+
+    public void setProxyObject(Object proxyObject) {
+        this.proxyObject = proxyObject;
+    }
+
+    public Object getProxyObject() {
+        return proxyObject;
+    }
+
+    public ServiceDescriptor getServiceModel() {
+        return serviceModel;
+    }
+
+    public ClassLoader getClassLoader() {
+        Class<?> serviceType = serviceMetadata.getServiceType();
+        return serviceType != null ? serviceType.getClassLoader() : ClassUtils.getClassLoader();
+    }
+
+    /**
+     * Return all method models for the current service
+     *
+     * @return method model list
+     */
+    public Set<MethodDescriptor> getAllMethods() {
+        return serviceModel.getAllMethods();
+    }
+
+    public Class<?> getServiceInterfaceClass() {
+        return serviceModel.getServiceInterfaceClass();
+    }
+
+    public AbstractInterfaceConfig getConfig() {
+        return config;
+    }
+
+    public ReferenceConfigBase<?> getReferenceConfig() {
+        if(config instanceof ReferenceConfigBase) {
+            return (ReferenceConfigBase<?>) config;
+        } else {
+            throw new IllegalArgumentException("Current ServiceModel is not a ConsumerModel");
+        }
+    }
+
+    public ServiceConfigBase<?> getServiceConfig() {
+        if(config instanceof ServiceConfigBase) {
+            return (ServiceConfigBase<?>) config;
+        } else {
+            throw new IllegalArgumentException("Current ServiceModel is not a ProviderModel");
+        }
+    }
+
+    public void setServiceKey(String serviceKey) {
+        this.serviceKey = serviceKey;
+        if (serviceMetadata != null) {
+            serviceMetadata.setServiceKey(serviceKey);
+            serviceMetadata.setGroup(BaseServiceMetadata.groupFromServiceKey(serviceKey));
+        }
+    }
+
+    public String getServiceName() {
+        return this.serviceMetadata.getServiceKey();
+    }
+
+    /**
+     * @return serviceMetadata
+     */
+    public ServiceMetadata getServiceMetadata() {
+        return serviceMetadata;
+    }
+}
