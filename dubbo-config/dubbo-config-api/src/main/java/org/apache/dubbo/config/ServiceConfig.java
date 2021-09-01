067dba3afed44b5b91350e75e62de95ecce3c693
赵延 <horizonzy@apache.org>
2021-09-01 15:21:02 +0800

    When use delay to export service, catch the exception and log it. (#8584)
    * when use delay to export service, catch the exception and log it.
    
    * format file
    
    * enhance log info.

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/ServiceConfig.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/ServiceConfig.java
@@ -226,7 +226,7 @@ public class ServiceConfig<T> extends ServiceConfigBase<T> {
             }
 
             if (shouldDelay()) {
-                DELAY_EXPORT_EXECUTOR.schedule(this::doExport, getDelay(), TimeUnit.MILLISECONDS);
+                doDelayExport();
             } else {
                 doExport();
             }
@@ -235,6 +235,16 @@ public class ServiceConfig<T> extends ServiceConfigBase<T> {
                 this.bootstrap.start();
             }
         }
+    }
+
+    protected void doDelayExport() {
+        DELAY_EXPORT_EXECUTOR.schedule(() -> {
+            try {
+                doExport();
+            } catch (Exception e) {
+                logger.error("Failed to export service config: " + interfaceName, e);
+            }
+        }, getDelay(), TimeUnit.MILLISECONDS);
     }
 
     protected void exported() {
