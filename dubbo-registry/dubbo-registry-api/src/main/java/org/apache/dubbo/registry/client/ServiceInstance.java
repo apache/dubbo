5108758dc8c26e9bf40aa556e2440a21efe319f1
BurningCN <1015773611@qq.com>
2021-09-03 23:10:34 +0800

    Fix the comment of ServiceInstance

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/client/ServiceInstance.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/client/ServiceInstance.java
@@ -52,7 +52,7 @@ public interface ServiceInstance extends Serializable {
     String getAddress();
 
     /**
-     * The enable status of the registered service instance.
+     * The enabled status of the registered service instance.
      *
      * @return if <code>true</code>, indicates current instance is enabled, or disable, the client should remove this one.
      * The default value is <code>true</code>
@@ -64,7 +64,7 @@ public interface ServiceInstance extends Serializable {
     /**
      * The registered service instance is health or not.
      *
-     * @return if <code>true</code>, indicates current instance is enabled, or disable, the client may ignore this one.
+     * @return if <code>true</code>, indicates current instance is healthy, or unhealthy, the client may ignore this one.
      * The default value is <code>true</code>
      */
     default boolean isHealthy() {
