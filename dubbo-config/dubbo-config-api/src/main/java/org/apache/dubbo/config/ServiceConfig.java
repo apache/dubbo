b2118bd5549a53f3216b5e341067f2d4ffffcaf4
Albumen Kevin <jhq0812@gmail.com>
2021-09-07 22:54:05 +0800

    Add ApplicationModel to DubboBootstrap binding refer

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/ServiceConfig.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/ServiceConfig.java
@@ -204,7 +204,7 @@ public class ServiceConfig<T> extends ServiceConfigBase<T> {
     public void init() {
         if (this.initialized.compareAndSet(false, true)) {
             if (this.bootstrap == null) {
-                this.bootstrap = DubboBootstrap.getInstance();
+                this.bootstrap = DubboBootstrap.getInstance(getScopeModel().getApplicationModel());
                 this.bootstrap.initialize();
             }
             this.bootstrap.service(this);
