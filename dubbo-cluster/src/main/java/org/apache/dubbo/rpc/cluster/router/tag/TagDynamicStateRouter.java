b1ebfe81e57a5111ff6a5d8102208ff604ca2519
Wang Chengming <634749869@qq.com>
2021-09-10 21:14:03 +0800

    [3.0] remove duplicate catch block (#8750)
    * remove duplicate catch block
    
    * remove duplicate catch block

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/router/tag/TagDynamicStateRouter.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/router/tag/TagDynamicStateRouter.java
@@ -16,7 +16,6 @@
  */
 package org.apache.dubbo.rpc.cluster.router.tag;
 
-import java.net.UnknownHostException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -85,10 +84,10 @@ public class TagDynamicStateRouter extends AbstractStateRouter implements Config
 
     @Override
     public <T> BitList<Invoker<T>> route(BitList<Invoker<T>> invokers, RouterCache<T> cache, URL url,
-        Invocation invocation) throws RpcException {
+                                         Invocation invocation) throws RpcException {
 
 
-        final TagRouterRule tagRouterRuleCopy = (TagRouterRule)cache.getAddrMetadata();
+        final TagRouterRule tagRouterRuleCopy = (TagRouterRule) cache.getAddrMetadata();
 
         String tag = StringUtils.isEmpty(invocation.getAttachment(TAG_KEY)) ? url.getParameter(TAG_KEY) :
             invocation.getAttachment(TAG_KEY);
@@ -209,8 +208,6 @@ public class TagDynamicStateRouter extends AbstractStateRouter implements Config
                 if ((ANYHOST_VALUE + ":" + port).equals(address)) {
                     return true;
                 }
-            } catch (UnknownHostException e) {
-                logger.error("The format of ip address is invalid in tag route. Address :" + address, e);
             } catch (Exception e) {
                 logger.error("The format of ip address is invalid in tag route. Address :" + address, e);
             }
