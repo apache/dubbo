7757f28118b023128dd9ee557d75c7f759be6212
earthchen <youwei.chen@duobei.com>
2021-09-14 07:13:16 -0500

    [3.0-Triple] use promise catch server send big header exception (#8774)
    * use promise catch server send big header exception
    
    * remove duplicate set server stream key

--- dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/AbstractServerStream.java
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/AbstractServerStream.java
@@ -17,7 +17,6 @@
 
 package org.apache.dubbo.rpc.protocol.tri;
 
-import com.google.protobuf.Message;
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.common.extension.ExtensionLoader;
@@ -32,6 +31,8 @@ import org.apache.dubbo.rpc.model.ProviderModel;
 import org.apache.dubbo.rpc.model.ScopeModelUtil;
 import org.apache.dubbo.rpc.model.ServiceDescriptor;
 import org.apache.dubbo.triple.TripleWrapper;
+
+import com.google.protobuf.Message;
 
 import java.util.Arrays;
 import java.util.List;
