66c331767348b7d9b5d184e382a6e717c98b7b10
wuzhiguo <changye@startdt.com>
2021-09-18 14:29:08 +0800

    change method constant

--- dubbo-common/src/main/java/org/apache/dubbo/config/Constants.java
+++ dubbo-common/src/main/java/org/apache/dubbo/config/Constants.java
@@ -94,17 +94,17 @@ public interface Constants {
     String SCOPE_NONE = "none";
 
 
-    String ON_INVOKE_METHOD_KEY = "oninvoke.method";
+    String ON_INVOKE_METHOD_KEY = "oninvoke-method";
 
-    String ON_RETURN_METHOD_KEY = "onreturn.method";
+    String ON_RETURN_METHOD_KEY = "onreturn-method";
 
-    String ON_THROW_METHOD_KEY = "onthrow.method";
+    String ON_THROW_METHOD_KEY = "onthrow-method";
 
-    String ON_INVOKE_INSTANCE_KEY = "oninvoke.instance";
+    String ON_INVOKE_INSTANCE_KEY = "oninvoke-instance";
 
-    String ON_RETURN_INSTANCE_KEY = "onreturn.instance";
+    String ON_RETURN_INSTANCE_KEY = "onreturn-instance";
 
-    String ON_THROW_INSTANCE_KEY = "onthrow.instance";
+    String ON_THROW_INSTANCE_KEY = "onthrow-instance";
 
 
     // FIXME: is this still useful?
