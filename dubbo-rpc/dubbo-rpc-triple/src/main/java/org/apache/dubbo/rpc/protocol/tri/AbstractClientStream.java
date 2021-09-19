6aca758994093507e38dbe6e756a6c7103ab6894
wang.yubin <359474377@qq.com>
2021-09-20 01:04:53 +0800

    [3.0-Triple] change the access method of the compressor configuration

--- dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/AbstractClientStream.java
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/AbstractClientStream.java
@@ -25,6 +25,7 @@ import org.apache.dubbo.remoting.exchange.support.DefaultFuture2;
 import org.apache.dubbo.rpc.CancellationContext;
 import org.apache.dubbo.rpc.Constants;
 import org.apache.dubbo.rpc.RpcInvocation;
+import org.apache.dubbo.rpc.model.ApplicationModel;
 import org.apache.dubbo.rpc.model.ConsumerModel;
 import org.apache.dubbo.triple.TripleWrapper;
 
@@ -174,7 +175,7 @@ public abstract class AbstractClientStream extends AbstractStream implements Str
                         (String) inv.getObjectAttachments().remove(CommonConstants.REMOTE_APPLICATION_KEY))
                 .putIfNotNull(TripleHeaderEnum.SERVICE_GROUP.getHeader(), inv.getInvoker().getUrl().getGroup())
                 .putIfNotNull(TripleHeaderEnum.GRPC_ENCODING.getHeader(),
-                    ConfigurationUtils.getGlobalConfiguration(inv.getInvoker().getUrl().getScopeModel()).getString(Constants.COMPRESSOR_KEY, DEFAULT_COMPRESSOR))
+                    ConfigurationUtils.getGlobalConfiguration(ApplicationModel.defaultModel()).getString(Constants.COMPRESSOR_KEY, DEFAULT_COMPRESSOR))
                 .putIfNotNull(TripleHeaderEnum.GRPC_ACCEPT_ENCODING.getHeader(),
                     TripleUtil.calcAcceptEncoding(inv.getInvoker().getUrl()));
         final Map<String, Object> attachments = inv.getObjectAttachments();
