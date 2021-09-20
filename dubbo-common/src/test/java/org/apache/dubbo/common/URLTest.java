cf5eb3d3fbdcf54ac2a2709136b8ab7804ea1df4
zrlw <zrlw@sina.com>
2021-09-20 23:01:53 +0800

    fix StringIndexOutOfBoundsException at addParam() (#8799)

--- dubbo-common/src/test/java/org/apache/dubbo/common/URLTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/URLTest.java
@@ -1041,5 +1041,9 @@ public class URLTest {
         Assertions.assertEquals("registry", url.getParameter("protocol"));
     }
 
-
+    @Test
+    public void test_valueOfHasNameWithoutValue() throws Exception {
+        URL url = URL.valueOf("dubbo://admin:hello1234@10.20.130.230:20880/context/path?version=1.0.0&application=morgan&noValue");
+        Assertions.assertEquals("noValue", url.getParameter("noValue"));
+    }
 }
