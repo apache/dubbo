d86e01c32d819137cc014a2926a8c0e18c4d6e70
wangchengming <634749869@qq.com>
2021-09-23 12:46:53 +0800

    add MergeableClusterInvoker unit test

--- dubbo-cluster/src/test/java/org/apache/dubbo/rpc/cluster/support/MergeableClusterInvokerTest.java
+++ dubbo-cluster/src/test/java/org/apache/dubbo/rpc/cluster/support/MergeableClusterInvokerTest.java
@@ -218,6 +218,54 @@ public class MergeableClusterInvokerTest {
     }
 
     @Test
+    public void testAddMenu1() {
+
+        // setup
+        url = url.addParameter(MERGER_KEY, ".merge");
+
+        String menu = "first";
+        List<String> menuItems = new ArrayList<String>() {
+            {
+                add("1");
+                add("2");
+            }
+        };
+
+        given(invocation.getMethodName()).willReturn("addMenu");
+        given(invocation.getParameterTypes()).willReturn(new Class<?>[]{String.class, List.class});
+        given(invocation.getArguments()).willReturn(new Object[]{menu, menuItems});
+        given(invocation.getObjectAttachments()).willReturn(new HashMap<>());
+        given(invocation.getInvoker()).willReturn(firstInvoker);
+
+        given(firstInvoker.getUrl()).willReturn(url.addParameter(GROUP_KEY, "first"));
+        given(firstInvoker.getInterface()).willReturn(MenuService.class);
+        given(firstInvoker.invoke(invocation)).willReturn(new AppResponse());
+        given(firstInvoker.isAvailable()).willReturn(true);
+
+        given(secondInvoker.getUrl()).willReturn(url.addParameter(GROUP_KEY, "second"));
+        given(secondInvoker.getInterface()).willReturn(MenuService.class);
+        given(secondInvoker.invoke(invocation)).willReturn(new AppResponse());
+        given(secondInvoker.isAvailable()).willReturn(true);
+
+        given(directory.list(invocation)).willReturn(new ArrayList() {
+
+            {
+                add(firstInvoker);
+                add(secondInvoker);
+            }
+        });
+        given(directory.getUrl()).willReturn(url);
+        given(directory.getConsumerUrl()).willReturn(url);
+        given(directory.getConsumerUrl()).willReturn(url);
+        given(directory.getInterface()).willReturn(MenuService.class);
+
+        mergeableClusterInvoker = new MergeableClusterInvoker<MenuService>(directory);
+
+        Result result = mergeableClusterInvoker.invoke(invocation);
+        Assertions.assertNull(result.getValue());
+
+    }
+    @Test
     public void testInvokerToNoInvokerAvailableException() {
         String menu = "first";
         List<String> menuItems = new ArrayList<String>() {
@@ -436,7 +484,7 @@ public class MergeableClusterInvokerTest {
 
     @Disabled
     @Test
-    public void testGetMenuSuccessfullyWithApplicationModel() {
+    public void testGetMenuWithApplicationModel() {
 
         // setup
         url = url.addParameter(MERGER_KEY, "list");
