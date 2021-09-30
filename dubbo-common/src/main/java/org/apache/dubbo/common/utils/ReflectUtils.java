5fd9981bbbb96d89f4fac11409dcdaf4a2fc8775
yangzan <yangzan0816@gmail.com>
2021-09-30 12:39:59 +0800

    恢复判断是否为java文件和拥有空构造的方法

--- dubbo-common/src/main/java/org/apache/dubbo/common/utils/ReflectUtils.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/utils/ReflectUtils.java
@@ -1316,4 +1316,17 @@ public final class ReflectUtils {
 
         return types;
     }
+
+    public static boolean checkZeroArgConstructor(Class clazz) {
+        try {
+            clazz.getDeclaredConstructor();
+            return true;
+        } catch (NoSuchMethodException e) {
+            return false;
+        }
+    }
+
+    public static boolean isJdk(Class clazz) {
+        return clazz.getName().startsWith("java.") || clazz.getName().startsWith("javax.");
+    }
 }
