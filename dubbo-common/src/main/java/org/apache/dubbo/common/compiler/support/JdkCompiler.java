23a4ff0a0d52ba3faec75029f3b46accbdf6a59f
Albumen Kevin <jhq0812@gmail.com>
2021-10-10 17:28:14 +0800

    Fix Concurrent issue when creating SPIs' Adaptive class (#8998)
    * Fix Concurrent issue when creating SPIs' Adaptive class
    
    * Fix ut

--- dubbo-common/src/main/java/org/apache/dubbo/common/compiler/support/JdkCompiler.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/compiler/support/JdkCompiler.java
@@ -110,7 +110,7 @@ public class JdkCompiler extends AbstractCompiler {
     }
 
     @Override
-    public Class<?> doCompile(String name, String sourceCode) throws Throwable {
+    public Class<?> doCompile(ClassLoader ignored, String name, String sourceCode) throws Throwable {
         int i = name.lastIndexOf('.');
         String packageName = i < 0 ? "" : name.substring(0, i);
         String className = i < 0 ? name : name.substring(i + 1);
