870045118a8a4912c51857938a569df1c4e81523
zrlw <zrlw@sina.com>
2021-10-14 14:54:06 +0800

    add close method of ZookeeperTransporter$Adaptive

--- dubbo-native/src/main/java/org/apache/dubbo/remoting/zookeeper/ZookeeperTransporter$Adaptive.java
+++ dubbo-native/src/main/java/org/apache/dubbo/remoting/zookeeper/ZookeeperTransporter$Adaptive.java
@@ -17,12 +17,17 @@
 package org.apache.dubbo.remoting.zookeeper;
 import org.apache.dubbo.common.extension.ExtensionLoader;
 public class ZookeeperTransporter$Adaptive implements org.apache.dubbo.remoting.zookeeper.ZookeeperTransporter {
+private org.apache.dubbo.remoting.zookeeper.ZookeeperTransporter extension;
 public org.apache.dubbo.remoting.zookeeper.ZookeeperClient connect(org.apache.dubbo.common.URL arg0)  {
 if (arg0 == null) throw new IllegalArgumentException("url == null");
 org.apache.dubbo.common.URL url = arg0;
 String extName = url.getParameter("client", url.getParameter("transporter", "curator"));
 if(extName == null) throw new IllegalStateException("Failed to get extension (org.apache.dubbo.remoting.zookeeper.ZookeeperTransporter) name from url (" + url.toString() + ") use keys([client, transporter])");
-org.apache.dubbo.remoting.zookeeper.ZookeeperTransporter extension = (org.apache.dubbo.remoting.zookeeper.ZookeeperTransporter)ExtensionLoader.getExtensionLoader(org.apache.dubbo.remoting.zookeeper.ZookeeperTransporter.class).getExtension(extName);
+extension = (org.apache.dubbo.remoting.zookeeper.ZookeeperTransporter)ExtensionLoader.getExtensionLoader(org.apache.dubbo.remoting.zookeeper.ZookeeperTransporter.class).getExtension(extName);
 return extension.connect(arg0);
+}
+public void close(ZookeeperClient arg0, String arg1) {
+if (extension == null) throw new IllegalStateException("Close zookeeper client should not be called before connection created for url: (" + arg0.getUrl().toString() + ")");
+extension.close(arg0, arg1);
 }
 }
