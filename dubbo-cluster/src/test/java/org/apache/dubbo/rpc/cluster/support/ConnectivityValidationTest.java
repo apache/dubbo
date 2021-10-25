b69c13871b9d900f095fb7c19c90a6b56be49d27
Albumen Kevin <jhq0812@gmail.com>
2021-10-25 11:02:41 +0800

    add ut

--- dubbo-cluster/src/test/java/org/apache/dubbo/rpc/cluster/support/ConnectivityValidationTest.java
+++ dubbo-cluster/src/test/java/org/apache/dubbo/rpc/cluster/support/ConnectivityValidationTest.java
@@ -50,6 +50,16 @@ public class ConnectivityValidationTest {
     private Invoker invoker3;
     private Invoker invoker4;
     private Invoker invoker5;
+    private Invoker invoker6;
+    private Invoker invoker7;
+    private Invoker invoker8;
+    private Invoker invoker9;
+    private Invoker invoker10;
+    private Invoker invoker11;
+    private Invoker invoker12;
+    private Invoker invoker13;
+    private Invoker invoker14;
+    private Invoker invoker15;
 
     private List<Invoker> invokerList;
 
@@ -63,18 +73,32 @@ public class ConnectivityValidationTest {
         invoker3 = Mockito.mock(Invoker.class);
         invoker4 = Mockito.mock(Invoker.class);
         invoker5 = Mockito.mock(Invoker.class);
+        invoker6 = Mockito.mock(Invoker.class);
+        invoker7 = Mockito.mock(Invoker.class);
+        invoker8 = Mockito.mock(Invoker.class);
+        invoker9 = Mockito.mock(Invoker.class);
+        invoker10 = Mockito.mock(Invoker.class);
+        invoker11 = Mockito.mock(Invoker.class);
+        invoker12 = Mockito.mock(Invoker.class);
+        invoker13 = Mockito.mock(Invoker.class);
+        invoker14 = Mockito.mock(Invoker.class);
+        invoker15 = Mockito.mock(Invoker.class);
 
-        when(invoker1.isAvailable()).thenReturn(true);
-        when(invoker2.isAvailable()).thenReturn(true);
-        when(invoker3.isAvailable()).thenReturn(true);
-        when(invoker4.isAvailable()).thenReturn(true);
-        when(invoker5.isAvailable()).thenReturn(true);
-
-        when(invoker1.getUrl()).thenReturn(URL.valueOf(""));
-        when(invoker2.getUrl()).thenReturn(URL.valueOf(""));
-        when(invoker3.getUrl()).thenReturn(URL.valueOf(""));
-        when(invoker4.getUrl()).thenReturn(URL.valueOf(""));
-        when(invoker5.getUrl()).thenReturn(URL.valueOf(""));
+        configInvoker(invoker1);
+        configInvoker(invoker2);
+        configInvoker(invoker3);
+        configInvoker(invoker4);
+        configInvoker(invoker5);
+        configInvoker(invoker6);
+        configInvoker(invoker7);
+        configInvoker(invoker8);
+        configInvoker(invoker9);
+        configInvoker(invoker10);
+        configInvoker(invoker11);
+        configInvoker(invoker12);
+        configInvoker(invoker13);
+        configInvoker(invoker14);
+        configInvoker(invoker15);
 
         invokerList = new LinkedList<>();
         invokerList.add(invoker1);
@@ -85,6 +109,11 @@ public class ConnectivityValidationTest {
 
         directory = new StaticDirectory(invokerList);
         clusterInvoker = new ConnectivityClusterInvoker(directory);
+    }
+
+    private void configInvoker(Invoker invoker) {
+        when(invoker.getUrl()).thenReturn(URL.valueOf(""));
+        when(invoker.isAvailable()).thenReturn(true);
     }
 
     @BeforeAll
@@ -104,7 +133,7 @@ public class ConnectivityValidationTest {
 
         Assertions.assertEquals(5, directory.list(invocation).size());
 
-        Assertions.assertNotNull(clusterInvoker.select(loadBalance, invocation, invokerList, Collections.emptyList()));
+        Assertions.assertNotNull(clusterInvoker.select(loadBalance, invocation, directory.list(invocation), Collections.emptyList()));
 
         when(invoker1.isAvailable()).thenReturn(false);
         when(invoker2.isAvailable()).thenReturn(false);
@@ -112,32 +141,120 @@ public class ConnectivityValidationTest {
         when(invoker4.isAvailable()).thenReturn(false);
         when(invoker5.isAvailable()).thenReturn(false);
 
-        clusterInvoker.select(loadBalance, invocation, invokerList, Collections.emptyList());
+        clusterInvoker.select(loadBalance, invocation, directory.list(invocation), Collections.emptyList());
         Assertions.assertEquals(0, directory.list(invocation).size());
 
         when(invoker1.isAvailable()).thenReturn(true);
         waitRefresh();
         Assertions.assertEquals(1, directory.list(invocation).size());
-        Assertions.assertNotNull(clusterInvoker.select(loadBalance, invocation, invokerList, Collections.emptyList()));
+        Assertions.assertNotNull(clusterInvoker.select(loadBalance, invocation, directory.list(invocation), Collections.emptyList()));
 
         when(invoker2.isAvailable()).thenReturn(true);
         waitRefresh();
         Assertions.assertEquals(2, directory.list(invocation).size());
-        Assertions.assertNotNull(clusterInvoker.select(loadBalance, invocation, invokerList, Collections.emptyList()));
+        Assertions.assertNotNull(clusterInvoker.select(loadBalance, invocation, directory.list(invocation), Collections.emptyList()));
 
         invokerList.remove(invoker5);
         directory.notify(invokerList);
         when(invoker2.isAvailable()).thenReturn(true);
         waitRefresh();
         Assertions.assertEquals(2, directory.list(invocation).size());
-        Assertions.assertNotNull(clusterInvoker.select(loadBalance, invocation, invokerList, Collections.emptyList()));
+        Assertions.assertNotNull(clusterInvoker.select(loadBalance, invocation, directory.list(invocation), Collections.emptyList()));
 
         when(invoker3.isAvailable()).thenReturn(true);
         when(invoker4.isAvailable()).thenReturn(true);
 
         waitRefresh();
         Assertions.assertEquals(4, directory.list(invocation).size());
-        Assertions.assertNotNull(clusterInvoker.select(loadBalance, invocation, invokerList, Collections.emptyList()));
+        Assertions.assertNotNull(clusterInvoker.select(loadBalance, invocation, directory.list(invocation), Collections.emptyList()));
+    }
+
+    @Test
+    public void testRetry() throws InterruptedException {
+        Invocation invocation = new RpcInvocation();
+        LoadBalance loadBalance = new RandomLoadBalance();
+
+        invokerList.clear();
+        invokerList.add(invoker1);
+        invokerList.add(invoker2);
+        directory.notify(invokerList);
+
+        Assertions.assertEquals(2, directory.list(invocation).size());
+
+        when(invoker1.isAvailable()).thenReturn(false);
+        for (int i = 0; i < 5; i++) {
+            Assertions.assertEquals(invoker2, clusterInvoker.select(loadBalance, invocation, directory.list(invocation), Collections.emptyList()));
+        }
+        Assertions.assertEquals(1, directory.list(invocation).size());
+
+        when(invoker1.isAvailable()).thenReturn(true);
+        waitRefresh();
+        Assertions.assertEquals(2, directory.list(invocation).size());
+    }
+
+    @Test
+    public void testRandomSelect() throws InterruptedException {
+        Invocation invocation = new RpcInvocation();
+        LoadBalance loadBalance = new RandomLoadBalance();
+
+        invokerList.add(invoker6);
+        invokerList.add(invoker7);
+        invokerList.add(invoker8);
+        invokerList.add(invoker9);
+        invokerList.add(invoker10);
+        invokerList.add(invoker11);
+        invokerList.add(invoker12);
+        invokerList.add(invoker13);
+        invokerList.add(invoker14);
+        invokerList.add(invoker15);
+
+        directory.notify(invokerList);
+
+        Assertions.assertEquals(15, directory.list(invocation).size());
+
+        when(invoker2.isAvailable()).thenReturn(false);
+        when(invoker3.isAvailable()).thenReturn(false);
+        when(invoker4.isAvailable()).thenReturn(false);
+        when(invoker5.isAvailable()).thenReturn(false);
+        when(invoker6.isAvailable()).thenReturn(false);
+        when(invoker7.isAvailable()).thenReturn(false);
+        when(invoker8.isAvailable()).thenReturn(false);
+        when(invoker9.isAvailable()).thenReturn(false);
+        when(invoker10.isAvailable()).thenReturn(false);
+        when(invoker11.isAvailable()).thenReturn(false);
+        when(invoker12.isAvailable()).thenReturn(false);
+        when(invoker13.isAvailable()).thenReturn(false);
+        when(invoker14.isAvailable()).thenReturn(false);
+        when(invoker15.isAvailable()).thenReturn(false);
+        for (int i = 0; i < 15; i++) {
+            clusterInvoker.select(loadBalance, invocation, directory.list(invocation), Collections.emptyList());
+        }
+        for (int i = 0; i < 5; i++) {
+            Assertions.assertEquals(invoker1, clusterInvoker.select(loadBalance, invocation, directory.list(invocation), Collections.emptyList()));
+        }
+
+        when(invoker1.isAvailable()).thenReturn(false);
+        clusterInvoker.select(loadBalance, invocation, directory.list(invocation), Collections.emptyList());
+        Assertions.assertEquals(0, directory.list(invocation).size());
+
+        when(invoker1.isAvailable()).thenReturn(true);
+        when(invoker2.isAvailable()).thenReturn(true);
+        when(invoker3.isAvailable()).thenReturn(true);
+        when(invoker4.isAvailable()).thenReturn(true);
+        when(invoker5.isAvailable()).thenReturn(true);
+        when(invoker6.isAvailable()).thenReturn(true);
+        when(invoker7.isAvailable()).thenReturn(true);
+        when(invoker8.isAvailable()).thenReturn(true);
+        when(invoker9.isAvailable()).thenReturn(true);
+        when(invoker10.isAvailable()).thenReturn(true);
+        when(invoker11.isAvailable()).thenReturn(true);
+        when(invoker12.isAvailable()).thenReturn(true);
+        when(invoker13.isAvailable()).thenReturn(true);
+        when(invoker14.isAvailable()).thenReturn(true);
+        when(invoker15.isAvailable()).thenReturn(true);
+
+        waitRefresh();
+        Assertions.assertTrue(directory.list(invocation).size() > 1);
     }
 
     private static class ConnectivityClusterInvoker<T> extends AbstractClusterInvoker<T> {
