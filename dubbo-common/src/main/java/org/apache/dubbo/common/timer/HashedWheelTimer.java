d8856dc69917b558fba447d46f022d1d24004925
zrlw <zrlw@sina.com>
2021-10-25 11:16:46 +0800

    reset state if expire task executes failed

--- dubbo-common/src/main/java/org/apache/dubbo/common/timer/HashedWheelTimer.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/timer/HashedWheelTimer.java
@@ -690,6 +690,8 @@ public class HashedWheelTimer implements Timer {
                     logger.warn("An exception was thrown while submit " + TimerTask.class.getSimpleName()
                             + " for execution.", t);
                 }
+                // reset state to try again.
+                compareAndSetState(ST_EXPIRED, ST_INIT);
             }
         }
 
