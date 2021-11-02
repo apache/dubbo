41d7caf10b7eeff60aa3f2e9c8af8b25e1b3ec56
gongdewei <kylixs@qq.com>
2021-11-02 21:49:37 +0800

    Fix MetadataService referProxy error #9190

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/client/metadata/StandardMetadataServiceURLBuilder.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/client/metadata/StandardMetadataServiceURLBuilder.java
@@ -56,10 +56,12 @@ public class StandardMetadataServiceURLBuilder implements MetadataServiceURLBuil
     public static final String NAME = "standard";
 
     private ApplicationModel applicationModel;
+    private Integer metadataServicePort;
 
     @Override
     public void setApplicationModel(ApplicationModel applicationModel) {
         this.applicationModel = applicationModel;
+        metadataServicePort = applicationModel.getCurrentConfig().getMetadataServicePort();
     }
 
     /**
@@ -109,7 +111,7 @@ public class StandardMetadataServiceURLBuilder implements MetadataServiceURLBuil
     }
 
     private URL generateUrlWithoutMetadata(String serviceName, String host, Integer instancePort) {
-        Integer port = ApplicationModel.defaultModel().getCurrentConfig().getMetadataServicePort();
+        Integer port = metadataServicePort;
         if (port == null || port < 1) {
             logger.warn("Metadata Service Port is not provided, since DNS is not able to negotiate the metadata port " +
                     "between Provider and Consumer, will try to use instance port as the default metadata port.");
