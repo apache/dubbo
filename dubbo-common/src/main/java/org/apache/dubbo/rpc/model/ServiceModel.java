d01bb2798237755c9d2506e8a01eb60a5b5134a6
Albumen Kevin <jhq0812@gmail.com>
2021-11-23 14:40:14 +0800

    revert ServiceModel

--- dubbo-common/src/main/java/org/apache/dubbo/rpc/model/ServiceModel.java
+++ dubbo-common/src/main/java/org/apache/dubbo/rpc/model/ServiceModel.java
@@ -110,7 +110,7 @@ public class ServiceModel {
         if (config instanceof ReferenceConfigBase) {
             return (ReferenceConfigBase<?>) config;
         } else {
-            return null;
+            throw new IllegalArgumentException("Current ServiceModel is not a ConsumerModel");
         }
     }
 
@@ -121,7 +121,7 @@ public class ServiceModel {
         if (config instanceof ServiceConfigBase) {
             return (ServiceConfigBase<?>) config;
         } else {
-            return null;
+            throw new IllegalArgumentException("Current ServiceModel is not a ProviderModel");
         }
     }
 
