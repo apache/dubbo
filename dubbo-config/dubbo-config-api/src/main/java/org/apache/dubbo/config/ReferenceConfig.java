503563be32e0374d75ee0988f3fce0243c46c856
Albumen Kevin <jhq0812@gmail.com>
2021-12-02 19:11:48 +0800

    revert invoker check failed log (#9345)

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/ReferenceConfig.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/ReferenceConfig.java
@@ -520,8 +520,16 @@ public class ReferenceConfig<T> extends ReferenceConfigBase<T> {
     private void checkInvokerAvailable() throws IllegalStateException {
         if (shouldCheck() && !invoker.isAvailable()) {
             invoker.destroy();
-            throw new IllegalStateException("Should has at least one way to know which services this interface belongs to," +
-                " subscription url: " + invoker.getUrl());
+            throw new IllegalStateException("Failed to check the status of the service "
+                + interfaceName
+                + ". No provider available for the service "
+                + (group == null ? "" : group + "/")
+                + interfaceName +
+                (version == null ? "" : ":" + version)
+                + " from the url "
+                + invoker.getUrl()
+                + " to the consumer "
+                + NetUtils.getLocalHost() + " use dubbo version " + Version.getVersion());
         }
     }
 
