46680486b820196711b087b08e38464e8137261c
ken.lj <ken.lj.hz@gmail.com>
2021-12-25 01:15:11 +0800

    do not copy

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/AsyncContextImpl.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/AsyncContextImpl.java
@@ -31,7 +31,7 @@ public class AsyncContextImpl implements AsyncContext {
     private ClassLoader stagedClassLoader;
 
     public AsyncContextImpl() {
-        restoreContext = RpcContext.storeContext(true);
+        restoreContext = RpcContext.storeContext(false);
         restoreClassLoader = Thread.currentThread().getContextClassLoader();
     }
 
