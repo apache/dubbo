e5f309a322b850db172e3f93eb657aa2ee013235
viper <38645977+xiaochangbai@users.noreply.github.com>
2022-01-23 16:58:52 +0800

    fix hessian2 deserialize fail (#9533) (#9536)
    * fix hessian2 deserialize fail (#9533)
    
    * refactor: Delete redundant import items

--- dubbo-serialization/dubbo-serialization-api/src/main/java/org/apache/dubbo/common/serialize/DefaultMultipleSerialization.java
+++ dubbo-serialization/dubbo-serialization-api/src/main/java/org/apache/dubbo/common/serialize/DefaultMultipleSerialization.java
@@ -18,7 +18,6 @@
 package org.apache.dubbo.common.serialize;
 
 import org.apache.dubbo.common.URL;
-import org.apache.dubbo.common.utils.ClassUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,9 +38,8 @@ public class DefaultMultipleSerialization implements MultipleSerialization {
     public Object deserialize(URL url, String serializeType, String clz, InputStream os) throws IOException, ClassNotFoundException {
         serializeType = convertHessian(serializeType);
         final Serialization serialization = url.getOrDefaultFrameworkModel().getExtensionLoader(Serialization.class).getExtension(serializeType);
-        final Class<?> aClass = ClassUtils.forName(clz);
         final ObjectInput in = serialization.deserialize(null, os);
-        return in.readObject(aClass);
+        return in.readObject();
     }
 
     private String convertHessian(String ser) {
