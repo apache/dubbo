23c02d81cf500800b4d9abfe5219a9b84cc46647
w4n92h3n <wangzhenxyz1@msn.com>
2022-02-06 11:34:33 +0800

    Bugfix - ExtensionLoader can not inject by type properly. (#9187)
    * bugfix ExtensionLoader can not inject by type properly.
    refactor DisableInject Annotation to the generic Inject Annotation.
    
    * update Inject Annotation disabled to enable.
    
    * update Inject Annotation disabled to enable.
    
    * update Inject Annotation disabled to enable.
    
    * update Inject Annotation disabled to enable.
    
    * bugfix ExtensionLoader can not inject by type properly.
    refactor DisableInject Annotation to the generic Inject Annotation.
    
    * Compatible with @Inject & @DisableInject, and tag @DisableInject with @Deprecated
    
    * Compatible with @Inject & @DisableInject, and tag @DisableInject with @Deprecated

--- dubbo-common/src/main/java/org/apache/dubbo/common/extension/Inject.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/extension/Inject.java
@@ -22,9 +22,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static org.apache.dubbo.common.extension.Inject.InjectType.ByName;
+
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
-@Deprecated
-public @interface DisableInject {
+public @interface Inject {
+    // whether enable injection or not
+    boolean enable() default true;
+
+    // inject type default by name injection
+    InjectType type() default ByName;
+
+    enum InjectType{
+        ByName,
+        ByType
+    }
 }
