651df4869089a899445bd553954f1aec3d7aaa94
GuoHao <guohaoice@gmail.com>
2022-03-29 14:54:12 +0800

    [3.0] Fix shutdown hang when use port unification handler (#9852)
    * Fix shutdown hang when use port unification handler
    
    * Fix ut
    
    * Fix sync

--- dubbo-remoting/dubbo-remoting-api/src/main/java/org/apache/dubbo/remoting/api/PortUnificationServer.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/org/apache/dubbo/remoting/api/PortUnificationServer.java
@@ -175,8 +175,8 @@ public class PortUnificationServer {
                 long quietPeriod = Math.min(2000L, timeout);
                 Future<?> bossGroupShutdownFuture = bossGroup.shutdownGracefully(quietPeriod, timeout, MILLISECONDS);
                 Future<?> workerGroupShutdownFuture = workerGroup.shutdownGracefully(quietPeriod, timeout, MILLISECONDS);
-                bossGroupShutdownFuture.syncUninterruptibly();
-                workerGroupShutdownFuture.syncUninterruptibly();
+                bossGroupShutdownFuture.awaitUninterruptibly(timeout, MILLISECONDS);
+                workerGroupShutdownFuture.awaitUninterruptibly(timeout, MILLISECONDS);
             }
         } catch (Throwable e) {
             logger.warn(e.getMessage(), e);
