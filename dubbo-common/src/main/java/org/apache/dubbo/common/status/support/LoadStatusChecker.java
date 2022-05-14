f03156f5c8b058dfcc7199c34f10aae91782f436
yanhom <yanhom1314@foxmail.com>
2022-05-14 10:24:39 +0800

    support for ibm J9

--- dubbo-common/src/main/java/org/apache/dubbo/common/status/support/LoadStatusChecker.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/status/support/LoadStatusChecker.java
@@ -19,10 +19,7 @@ package org.apache.dubbo.common.status.support;
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.common.status.Status;
 import org.apache.dubbo.common.status.StatusChecker;
-
-import java.lang.management.ManagementFactory;
-import java.lang.management.OperatingSystemMXBean;
-import java.lang.reflect.Method;
+import org.apache.dubbo.common.system.OperatingSystemBeanManager;
 
 /**
  * Load Status
@@ -30,24 +27,27 @@ import java.lang.reflect.Method;
 @Activate
 public class LoadStatusChecker implements StatusChecker {
 
+    private static final double NA_VAL = -1d;
+
     @Override
     public Status check() {
-        OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
-        double load;
-        try {
-            Method method = OperatingSystemMXBean.class.getMethod("getSystemLoadAverage", new Class<?>[0]);
-            load = (Double) method.invoke(operatingSystemMXBean, new Object[0]);
-            if (load == -1) {
-                com.sun.management.OperatingSystemMXBean bean =
-                        (com.sun.management.OperatingSystemMXBean) operatingSystemMXBean;
-                load = bean.getSystemCpuLoad();
-            }
-        } catch (Throwable e) {
-            load = -1;
+        double load = OperatingSystemBeanManager.getOperatingSystemBean().getSystemLoadAverage();
+        if (load == NA_VAL) {
+            load = OperatingSystemBeanManager.getSystemCpuUsage();
         }
-        int cpu = operatingSystemMXBean.getAvailableProcessors();
-        return new Status(load < 0 ? Status.Level.UNKNOWN : (load < cpu ? Status.Level.OK : Status.Level.WARN),
-                (load < 0 ? "" : "load:" + load + ",") + "cpu:" + cpu);
-    }
 
+        int cpu = OperatingSystemBeanManager.getOperatingSystemBean().getAvailableProcessors();
+        Status.Level level;
+        if (load < 0) {
+            level = Status.Level.UNKNOWN;
+        } else if (load < cpu) {
+            level = Status.Level.OK;
+        } else {
+            level = Status.Level.WARN;
+        }
+
+        String message = (load < 0 ? "" : "load:" + load + ",") + "cpu:" + cpu;
+        return new Status(level, message);
+    }
 }
+
