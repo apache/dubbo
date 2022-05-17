87ef1de5bfe7b4de63e64e5fe23693c23f53e538
呈铭 <beck.wcm@antgroup.com>
2022-05-17 17:53:31 +0800

    fix #10042, ignore overlapping lock exception and print log

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/support/AbstractRegistry.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/support/AbstractRegistry.java
@@ -223,18 +223,13 @@ public abstract class AbstractRegistry implements Registry {
                 if (e instanceof OverlappingFileLockException) {
                     logger.info("Failed to save registry cache file for file overlapping lock exception, file name " + file.getName());
                 } else {
-                    throw new Throwable(e);
+                    throw e;
                 }
             }
         } catch (Throwable e) {
             savePropertiesRetryTimes.incrementAndGet();
             if (savePropertiesRetryTimes.get() >= MAX_RETRY_TIMES_SAVE_PROPERTIES) {
-                if (e instanceof OverlappingFileLockException) {
-                    // fix #9341, ignore OverlappingFileLockException
-                    logger.info("Failed to save registry cache file for file overlapping lock exception, file name " + file.getName());
-                } else {
-                    logger.warn("Failed to save registry cache file after retrying " + MAX_RETRY_TIMES_SAVE_PROPERTIES + " times, cause: " + e.getMessage(), e);
-                }
+                logger.warn("Failed to save registry cache file after retrying " + MAX_RETRY_TIMES_SAVE_PROPERTIES + " times, cause: " + e.getMessage(), e);
                 savePropertiesRetryTimes.set(0);
                 return;
             }
