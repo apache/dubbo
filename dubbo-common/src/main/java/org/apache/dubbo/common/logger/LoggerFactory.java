372f6a697a0a4ebfdbf637c5f7e3dde8662bbe53
Albumen Kevin <jhq0812@gmail.com>
2022-06-09 18:54:27 +0800

    [3.0] Add some logger related command (#10132)

--- dubbo-common/src/main/java/org/apache/dubbo/common/logger/LoggerFactory.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/logger/LoggerFactory.java
@@ -26,6 +26,8 @@ import org.apache.dubbo.rpc.model.FrameworkModel;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -60,15 +62,17 @@ public class LoggerFactory {
                 break;
             default:
                 List<Class<? extends LoggerAdapter>> candidates = Arrays.asList(
-                        Log4jLoggerAdapter.class,
-                        Slf4jLoggerAdapter.class,
-                        Log4j2LoggerAdapter.class,
-                        JclLoggerAdapter.class,
-                        JdkLoggerAdapter.class
+                    Log4jLoggerAdapter.class,
+                    Slf4jLoggerAdapter.class,
+                    Log4j2LoggerAdapter.class,
+                    JclLoggerAdapter.class,
+                    JdkLoggerAdapter.class
                 );
                 for (Class<? extends LoggerAdapter> clazz : candidates) {
                     try {
-                        setLoggerAdapter(clazz.newInstance());
+                        LoggerAdapter loggerAdapter = clazz.newInstance();
+                        loggerAdapter.getLogger(LoggerFactory.class);
+                        setLoggerAdapter(loggerAdapter);
                         break;
                     } catch (Throwable ignored) {
                     }
@@ -148,6 +152,50 @@ public class LoggerFactory {
      */
     public static File getFile() {
         return LOGGER_ADAPTER.getFile();
+    }
+
+    /**
+     * Get the available adapter names
+     *
+     * @return available adapter names
+     */
+    public static List<String> getAvailableAdapter() {
+        Map<Class<? extends LoggerAdapter>, String> candidates = new HashMap<>();
+        candidates.put(Log4jLoggerAdapter.class, "log4j");
+        candidates.put(Slf4jLoggerAdapter.class, "slf4j");
+        candidates.put(Log4j2LoggerAdapter.class, "log4j2");
+        candidates.put(JclLoggerAdapter.class, "jcl");
+        candidates.put(JdkLoggerAdapter.class, "jdk");
+        List<String> result = new LinkedList<>();
+        for (Map.Entry<Class<? extends LoggerAdapter>, String> entry : candidates.entrySet()) {
+            try {
+                LoggerAdapter loggerAdapter = entry.getKey().newInstance();
+                loggerAdapter.getLogger(LoggerFactory.class);
+                result.add(entry.getValue());
+            } catch (Throwable ignored) {
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Get the current adapter name
+     *
+     * @return current adapter name
+     */
+    public static String getCurrentAdapter() {
+        Map<Class<? extends LoggerAdapter>, String> candidates = new HashMap<>();
+        candidates.put(Log4jLoggerAdapter.class, "log4j");
+        candidates.put(Slf4jLoggerAdapter.class, "slf4j");
+        candidates.put(Log4j2LoggerAdapter.class, "log4j2");
+        candidates.put(JclLoggerAdapter.class, "jcl");
+        candidates.put(JdkLoggerAdapter.class, "jdk");
+
+        String name = candidates.get(LOGGER_ADAPTER.getClass());
+        if (name == null) {
+            name = LOGGER_ADAPTER.getClass().getSimpleName();
+        }
+        return name;
     }
 
 }
