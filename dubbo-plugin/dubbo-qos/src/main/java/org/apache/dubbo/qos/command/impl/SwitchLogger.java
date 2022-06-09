639d5be18c5adc7237f97be7533898ff074e570c
Albumen Kevin <jhq0812@gmail.com>
2022-06-09 16:36:38 +0800

    [3.0] Add some logger related command

---
+++ dubbo-plugin/dubbo-qos/src/main/java/org/apache/dubbo/qos/command/impl/SwitchLogger.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.dubbo.qos.command.impl;
+
+import org.apache.dubbo.common.logger.LoggerFactory;
+import org.apache.dubbo.qos.command.BaseCommand;
+import org.apache.dubbo.qos.command.CommandContext;
+import org.apache.dubbo.qos.command.annotation.Cmd;
+import org.apache.dubbo.rpc.model.FrameworkModel;
+
+@Cmd(name = "switchLogger", summary = "Switch logger", example = {
+    "switchLogger slf4j"
+})
+public class SwitchLogger implements BaseCommand {
+    private FrameworkModel frameworkModel;
+
+    @Override
+    public String execute(CommandContext commandContext, String[] args) {
+        if (args.length != 1) {
+            return "Unexpected argument length.";
+        }
+        LoggerFactory.setLoggerAdapter(frameworkModel, args[0]);
+        return "OK";
+    }
+}
