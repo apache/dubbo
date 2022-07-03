3ebdc4d387bd5cfdd0ec2f4b052ce3e145a71b78
cheese8 <yinwerhong@163.com>
2022-07-03 14:50:52 +0800

    fix UnsupportedOperationException 10257

--- dubbo-common/src/main/java/org/apache/dubbo/config/AbstractConfig.java
+++ dubbo-common/src/main/java/org/apache/dubbo/config/AbstractConfig.java
@@ -823,8 +823,8 @@ public abstract class AbstractConfig implements Serializable {
         if (CollectionUtils.isEmptyMap(values)) {
             return;
         }
-        Map<String, String> map = invokeGetParameters(obj.getClass(), obj);
-        map = map == null ? new HashMap<>() : map;
+        Map<String, String> map = new HashMap<>();
+        map.putAll(invokeGetParameters(obj.getClass(), obj));
         map.putAll(values);
         invokeSetParameters(obj.getClass(), obj, map);
     }
