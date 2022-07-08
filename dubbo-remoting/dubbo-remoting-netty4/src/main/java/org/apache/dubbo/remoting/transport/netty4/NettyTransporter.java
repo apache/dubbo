1d59565578e5f2d98135d0c91d73f78a28ef745b
trueabc <547551933@qq.com>
2022-07-08 12:21:27 +0800

    revoke protocol detector and wire protocol in rpc-dubbo

--- dubbo-remoting/dubbo-remoting-netty4/src/main/java/org/apache/dubbo/remoting/transport/netty4/NettyTransporter.java
+++ dubbo-remoting/dubbo-remoting-netty4/src/main/java/org/apache/dubbo/remoting/transport/netty4/NettyTransporter.java
@@ -33,8 +33,7 @@ public class NettyTransporter implements Transporter {
 
     @Override
     public RemotingServer bind(URL url, ChannelHandler handler) throws RemotingException {
-//        return new NettyServer(url, handler);
-        return new NettyPortUnificationServer(url, handler);
+        return new NettyServer(url, handler);
     }
 
     @Override
