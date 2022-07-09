a6b2eea596ed1ca1562de84e3134fe0d3beb422e
Andy Cheung <micro_zx@msn.cn>
2022-07-09 17:42:06 +0800

    Add error type definition.

---
+++ dubbo-common/src/main/java/org/apache/dubbo/common/logger/ErrorType.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dubbo.common.logger;
+
+/**
+ * Represents an error type, which can be displayed by the logger like:
+ * ".... This may be caused by [error cause], go to [URL] to find instructions."
+ */
+public enum ErrorType {
+
+    // Temporarily empty. The only one entry is for testing.
+    REGISTRY_CENTER_OFFLINE("registry center offline", "http://dubbo.apache.org/");
+
+    /**
+     * Error cause.
+     */
+    private final String cause;
+
+    /**
+     * Link to instructions.
+     */
+    private final String errorUrl;
+
+    ErrorType(String cause, String errorUrl) {
+        this.cause = cause;
+        this.errorUrl = errorUrl;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public String getErrorUrl() {
+        return errorUrl;
+    }
+}
