82387ef7cd48e1ae3f8edb0e35b627d12e7457b2
Andy Cheung <micro_zx@msn.cn>
2022-07-23 20:17:00 +0800

    Returns an empty string when NumberFormatException occurs. (in getErrorUrl in FailsafeErrorTypeAwareLogger)

--- dubbo-common/src/main/java/org/apache/dubbo/common/logger/support/FailsafeErrorTypeAwareLogger.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/logger/support/FailsafeErrorTypeAwareLogger.java
@@ -66,6 +66,8 @@ public class FailsafeErrorTypeAwareLogger extends FailsafeLogger implements Erro
         } catch (NumberFormatException numberFormatException) {
             error("Invalid error code: " + code + ", the format of error code is: X-X (where X is a number).",
                 numberFormatException);
+
+            return "";
         }
 
         return String.format(INSTRUCTIONS_URL, errorCodeSegments[0], errorCodeSegments[1]);
@@ -80,6 +82,7 @@ public class FailsafeErrorTypeAwareLogger extends FailsafeLogger implements Erro
         try {
             getLogger().warn(appendContextMessageWithInstructions(code, cause, extendedInformation, msg));
         } catch (Throwable t) {
+            // ignored.
         }
     }
 
@@ -92,6 +95,7 @@ public class FailsafeErrorTypeAwareLogger extends FailsafeLogger implements Erro
         try {
             getLogger().warn(appendContextMessageWithInstructions(code, cause, extendedInformation, msg), e);
         } catch (Throwable t) {
+            // ignored.
         }
     }
 
@@ -104,6 +108,7 @@ public class FailsafeErrorTypeAwareLogger extends FailsafeLogger implements Erro
         try {
             getLogger().error(appendContextMessageWithInstructions(code, cause, extendedInformation, msg));
         } catch (Throwable t) {
+            // ignored.
         }
     }
 
@@ -116,6 +121,7 @@ public class FailsafeErrorTypeAwareLogger extends FailsafeLogger implements Erro
         try {
             getLogger().error(appendContextMessageWithInstructions(code, cause, extendedInformation, msg), e);
         } catch (Throwable t) {
+            // ignored.
         }
     }
 }
