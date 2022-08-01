4b3197848167f469eb0e539c696f1cabe28f6a0c
Owen.Cai <89424516@qq.com>
2022-08-01 11:01:57 +0800

    Cache file add module name (#10338)
    * cache_file_add_module_name & support config file cache
    
    * add application name for file name
    
    * fix compile bug & meta report support file cache
    
    * meta report support config file cache
    
    * fix compile bug
    
    * change param name to enableFileCached
    
    * rename from enableFileCached to enableFileCache

--- dubbo-common/src/main/java/org/apache/dubbo/rpc/service/GenericException.java
+++ dubbo-common/src/main/java/org/apache/dubbo/rpc/service/GenericException.java
@@ -16,7 +16,11 @@
  */
 package org.apache.dubbo.rpc.service;
 
+import org.apache.dubbo.common.utils.JsonUtils;
 import org.apache.dubbo.common.utils.StringUtils;
+
+import java.beans.Transient;
+
 
 /**
  * GenericException
@@ -27,33 +31,48 @@ public class GenericException extends RuntimeException {
 
     private static final long serialVersionUID = -1182299763306599962L;
 
+    private boolean useCause;
+
     private String exceptionClass;
 
     private String exceptionMessage;
 
     public GenericException() {
+        this.useCause = false;
     }
 
     public GenericException(String exceptionClass, String exceptionMessage) {
         super(exceptionMessage);
+        this.useCause = false;
+        this.exceptionClass = exceptionClass;
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    public GenericException(String exceptionClass, String exceptionMessage, String message) {
+        super(message);
+        this.useCause = false;
         this.exceptionClass = exceptionClass;
         this.exceptionMessage = exceptionMessage;
     }
 
     public GenericException(Throwable cause) {
         super(StringUtils.toString(cause));
+        this.useCause = false;
         this.exceptionClass = cause.getClass().getName();
         this.exceptionMessage = cause.getMessage();
     }
 
+    @Transient
     public String getExceptionClass() {
         return exceptionClass;
     }
+
 
     public void setExceptionClass(String exceptionClass) {
         this.exceptionClass = exceptionClass;
     }
 
+    @Transient
     public String getExceptionMessage() {
         return exceptionMessage;
     }
@@ -62,4 +81,66 @@ public class GenericException extends RuntimeException {
         this.exceptionMessage = exceptionMessage;
     }
 
+    @Override
+    public String getMessage() {
+        if(this.useCause) {
+            Throwable cause = getCause();
+            if(cause != null) {
+                return cause.getMessage();
+            }
+        }
+        GenericExceptionInfo genericExceptionInfo = new GenericExceptionInfo(exceptionClass, exceptionMessage, getMessage());
+        return JsonUtils.getJson().toJson(genericExceptionInfo);
+    }
+
+    public void setMessage(String json) {
+        GenericExceptionInfo info = JsonUtils.getJson().toJavaObject(json, GenericExceptionInfo.class);
+        this.useCause = true;
+        initCause(new GenericException(info.getExClass(), info.getExMsg(), info.getMsg()));
+    }
+
+    @Override
+    @Transient
+    public String getLocalizedMessage() {
+        return getMessage();
+    }
+
+    /**
+     * create generic exception info
+     */
+    public static class GenericExceptionInfo {
+        private String exClass;
+        private String exMsg;
+        private String msg;
+
+        public GenericExceptionInfo(String exceptionClass, String exceptionMessage, String message) {
+            this.exClass = exceptionClass;
+            this.exMsg = exceptionMessage;
+            this.msg = message;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public String getExClass() {
+            return exClass;
+        }
+
+        public String getExMsg() {
+            return exMsg;
+        }
+
+        public void setExClass(String exClass) {
+            this.exClass = exClass;
+        }
+
+        public void setExMsg(String exMsg) {
+            this.exMsg = exMsg;
+        }
+
+        public void setMsg(String msg) {
+            this.msg = msg;
+        }
+    }
 }
