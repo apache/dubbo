beeca4a4ab10faeb0489b5e0eb6b881cc1afbe4d
Kunshuai Zhu <jookunshuai@gmail.com>
2022-08-04 20:24:53 +0800

    Abstract the disableAutoFlowControl method of CallStreamObserver (#10406)
    * Abstract the disableAutoFlowControl method of CallStreamObserver
    
    * Abstract the disableAutoFlowControl method of CallStreamObserver

--- dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/observer/ServerCallToObserverAdapter.java
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/observer/ServerCallToObserverAdapter.java
@@ -105,7 +105,7 @@ public class ServerCallToObserverAdapter<T> extends CancelableStreamObserver<T> 
     }
 
     @Override
-    public void disableAutoInboundFlowControl() {
+    public void disableAutoFlowControl() {
         call.disableAutoRequestN();
     }
 
