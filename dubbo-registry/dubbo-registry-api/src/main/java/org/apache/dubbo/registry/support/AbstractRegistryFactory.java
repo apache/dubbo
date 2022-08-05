02d9ecb636b5ced24ff74f04e23f1944ee6a1282
Andy Cheung <micro_zx@msn.cn>
2022-08-06 00:57:47 +0800

    Optimize javadoc.

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/support/AbstractRegistryFactory.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/support/AbstractRegistryFactory.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.dubbo.registry.support;
 
 import org.apache.dubbo.common.URL;
@@ -69,9 +70,11 @@ public abstract class AbstractRegistryFactory implements RegistryFactory, ScopeM
             .removeAttribute(EXPORT_KEY)
             .removeAttribute(REFER_KEY)
             .build();
+
         String key = createRegistryCacheKey(url);
         Registry registry = null;
         boolean check = url.getParameter(CHECK_KEY, true) && url.getPort() != 0;
+
         // Lock the registry access process to ensure a single instance of the registry
         registryManager.getRegistryLock().lock();
         try {
@@ -81,11 +84,13 @@ public abstract class AbstractRegistryFactory implements RegistryFactory, ScopeM
             if (null != defaultNopRegistry) {
                 return defaultNopRegistry;
             }
+
             registry = registryManager.getRegistry(key);
             if (registry != null) {
                 return registry;
             }
-            //create registry by spi/ioc
+
+            // create registry by spi/ioc
             registry = createRegistry(url);
             if (check && registry == null) {
                 throw new IllegalStateException("Can not create registry " + url);
