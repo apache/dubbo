e86b28ce9f755bec95c2180d5081173cbc0fd67f
trueabc <547551933@qq.com>
2022-08-11 20:19:15 +0800

    add telnet ayt detect

--- dubbo-plugin/dubbo-qos/src/main/java/org/apache/dubbo/qos/pu/TelnetDetector.java
+++ dubbo-plugin/dubbo-qos/src/main/java/org/apache/dubbo/qos/pu/TelnetDetector.java
@@ -21,15 +21,20 @@ import org.apache.dubbo.qos.command.CommandContext;
 import org.apache.dubbo.qos.command.decoder.TelnetCommandDecoder;
 import org.apache.dubbo.remoting.api.ProtocolDetector;
 import org.apache.dubbo.remoting.buffer.ChannelBuffer;
+import org.apache.dubbo.remoting.buffer.ChannelBuffers;
+import org.apache.dubbo.remoting.buffer.HeapChannelBuffer;
 import org.apache.dubbo.rpc.model.FrameworkModel;
 
 import io.netty.util.CharsetUtil;
+
+import static java.lang.Math.min;
 
 
 public class TelnetDetector implements ProtocolDetector {
 
     private FrameworkModel frameworkModel;
     private final int MaxSize = 2048;
+    private final ChannelBuffer AytPreface = new HeapChannelBuffer(new byte[]{(byte) 0xff, (byte) 0xf6});
 
     public void setFrameworkModel(FrameworkModel frameworkModel) {
         this.frameworkModel = frameworkModel;
@@ -40,7 +45,22 @@ public class TelnetDetector implements ProtocolDetector {
         if (in.readableBytes() >= MaxSize) {
             return Result.UNRECOGNIZED;
         }
-        // if no \n is found and in.len() is ok, NEED_MORE_DATA
+        Result resCommand = CommandDetect(in);
+        if (resCommand.equals(Result.RECOGNIZED)){
+            return resCommand;
+        }
+        Result resAyt = TelnetAytDetect(in);
+        if (resAyt.equals(Result.RECOGNIZED)) {
+            return resAyt;
+        }
+        if (resAyt.equals(Result.UNRECOGNIZED) && resCommand.equals(Result.UNRECOGNIZED)) {
+            return Result.UNRECOGNIZED;
+        }
+        return Result.NEED_MORE_DATA;
+    }
+
+    private Result CommandDetect(ChannelBuffer in) {
+        // detect if remote channel send a qos command to server
         ChannelBuffer back = in.copy();
         byte[] backBytes = new byte[back.readableBytes()];
         back.getBytes(back.readerIndex(), backBytes);
@@ -54,4 +74,22 @@ public class TelnetDetector implements ProtocolDetector {
         }
         return Result.UNRECOGNIZED;
     }
+
+    private Result TelnetAytDetect(ChannelBuffer in) {
+        // detect if remote channel send a telnet ayt command to server
+        int prefaceLen = AytPreface.readableBytes();
+        int bytesRead = min(in.readableBytes(), prefaceLen);
+        if(bytesRead == 0 || !ChannelBuffers.prefixEquals(in, AytPreface, bytesRead)) {
+            return Result.UNRECOGNIZED;
+        }
+        if(bytesRead == prefaceLen) {
+            // we need to consume preface because it's not a qos command
+            // consume and remember to mark, pu server handler reset reader index
+            in.readBytes(AytPreface.readableBytes());
+            in.markReaderIndex();
+            return Result.RECOGNIZED;
+        }
+        return Result.NEED_MORE_DATA;
+    }
+
 }
