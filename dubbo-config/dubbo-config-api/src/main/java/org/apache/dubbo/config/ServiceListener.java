4da9bec266afba524da24991edc722a2bc5a6b5d
灼华 <1015773611@qq.com>
2022-08-18 15:54:53 +0800

    Rollback

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/ServiceListener.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/ServiceListener.java
@@ -16,13 +16,12 @@
  */
 package org.apache.dubbo.config;
 
-import org.apache.dubbo.common.extension.ExtensionScope;
 import org.apache.dubbo.common.extension.SPI;
 
 /**
  * Listener for service config
  */
-@SPI(scope = ExtensionScope.MODULE)
+@SPI
 public interface ServiceListener {
 
     /**
