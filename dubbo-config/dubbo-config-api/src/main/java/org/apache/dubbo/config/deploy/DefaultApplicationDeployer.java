2cf4447347993185f5655d02561a5d85fb2154d1
xuhao <467046520@qq.com>
2022-09-13 23:10:29 +0800

    fix: Fix unregisterServiceInstance not work in shutdown hook (#10610)

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/deploy/DefaultApplicationDeployer.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/deploy/DefaultApplicationDeployer.java
@@ -764,15 +764,14 @@ public class DefaultApplicationDeployer extends AbstractDeployer<ApplicationMode
             }
             onStopping();
 
+            unregisterServiceInstance();
             destroyRegistries();
-            destroyServiceDiscoveries();
             destroyMetadataReports();
 
             unRegisterShutdownHook();
             if (asyncMetadataFuture != null) {
                 asyncMetadataFuture.cancel(true);
             }
-            unregisterServiceInstance();
         }
     }
 
