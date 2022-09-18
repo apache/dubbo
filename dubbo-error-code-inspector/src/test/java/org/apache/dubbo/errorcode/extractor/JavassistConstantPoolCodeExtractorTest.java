b06d2389a3a1f814388dd63cb7db777e2520a8e2
Andy Cheung <micro_zx@msn.cn>
2022-09-18 18:14:07 +0800

    Workaround of the testing resource file missing.

--- dubbo-error-code-inspector/src/test/java/org/apache/dubbo/errorcode/extractor/JavassistConstantPoolCodeExtractorTest.java
+++ dubbo-error-code-inspector/src/test/java/org/apache/dubbo/errorcode/extractor/JavassistConstantPoolCodeExtractorTest.java
@@ -27,9 +27,14 @@ class JavassistConstantPoolCodeExtractorTest {
 
     private static final JavassistConstantPoolErrorCodeExtractor ERROR_CODE_EXTRACTOR = new JavassistConstantPoolErrorCodeExtractor();
 
+    /**
+     * Use a pre-compiled class file to check error code.
+     * Ignore the .jcls extension, it is a Java class file, actually.
+     * It's a workaround of the testing resource file missing.
+     */
     @Test
     void testGettingErrorCodes() {
-        String resourceFilePath = getClass().getClassLoader().getResource("FileCacheStore.class").toString();
+        String resourceFilePath = getClass().getClassLoader().getResource("FileCacheStore.jcls").toString();
 
         if (resourceFilePath.startsWith("file:/")) {
             resourceFilePath = resourceFilePath.replace("file:/", "");
