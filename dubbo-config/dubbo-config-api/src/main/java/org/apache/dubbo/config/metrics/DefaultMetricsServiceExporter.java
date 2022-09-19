a20207321e07a6b8c5ba5c97c9d7e92025b731cc
jojocodex <571943037@qq.com>
2022-09-19 22:14:28 +0800

    ut test

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/metrics/DefaultMetricsServiceExporter.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/metrics/DefaultMetricsServiceExporter.java
@@ -20,7 +20,6 @@ import static org.apache.dubbo.common.constants.CommonConstants.METRICS_SERVICE_
 import static org.apache.dubbo.common.constants.CommonConstants.METRICS_SERVICE_PROTOCOL_KEY;
 import static org.apache.dubbo.common.constants.MetricsConstants.PROTOCOL_PROMETHEUS;
 
-import org.apache.dubbo.common.extension.ExtensionLoader;
 import org.apache.dubbo.common.logger.Logger;
 import org.apache.dubbo.common.logger.LoggerFactory;
 import org.apache.dubbo.common.metrics.service.MetricsService;
@@ -51,14 +50,9 @@ public class DefaultMetricsServiceExporter implements MetricsServiceExporter, Sc
     private void initialize() {
         MetricsConfig metricsConfig = applicationModel.getApplicationConfigManager().getMetrics().orElse(null);
         // TODO compatible with old usage of metrics, remove protocol check after new metrics is ready for use.
-        if (metricsConfig != null) {
-            if (PROTOCOL_PROMETHEUS.equals(metricsConfig.getProtocol()) &&  metricsService == null) {
-                ExtensionLoader<MetricsService> extensionLoader = applicationModel.getExtensionLoader(MetricsService.class);
-                if (!extensionLoader.hasExtension(MetricsService.DEFAULT_EXTENSION_NAME)) {
-                    throw new IllegalStateException("Metrics config exist, but the dubbo-metrics-api dependency is missing. Please check your project dependencies.");
-                } else {
-                    this.metricsService = extensionLoader.getDefaultExtension();
-                }
+        if (metricsConfig != null &&  metricsService == null) {
+            if (PROTOCOL_PROMETHEUS.equals(metricsConfig.getProtocol()) ) {
+                this.metricsService  = applicationModel.getExtensionLoader(MetricsService.class).getDefaultExtension();
             } else {
                 logger.warn("Protocol " + metricsConfig.getProtocol() + " not support for new metrics mechanism. " +
                     "Using old metrics mechanism instead.");
--- dubbo-config/dubbo-config-spring/src/test/java/org/apache/dubbo/config/spring/boot/configprops/SpringBootConfigPropsTest.java
+++ dubbo-config/dubbo-config-spring/src/test/java/org/apache/dubbo/config/spring/boot/configprops/SpringBootConfigPropsTest.java
@@ -99,6 +99,7 @@ public class SpringBootConfigPropsTest {
     @Autowired
     private ModuleModel moduleModel;
 
+
     @Test
     public void testConfigProps() {
 
