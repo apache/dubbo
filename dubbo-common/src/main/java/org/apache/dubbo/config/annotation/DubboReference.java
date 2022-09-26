ed39d4e4b8eda1d64828e9c72e50b31b5279caf6
niejian <niejian@bluemoon.com.cn>
2022-09-26 10:30:53 +0800

    assign provider namespace

--- dubbo-common/src/main/java/org/apache/dubbo/config/annotation/DubboReference.java
+++ dubbo-common/src/main/java/org/apache/dubbo/config/annotation/DubboReference.java
@@ -357,6 +357,13 @@ public @interface DubboReference {
     int providerPort() default -1;
 
     /**
+     * assign the namespace that provider belong to
+     * @see AbstractReferenceConfig#providerNamespace
+     * @since 3.1.1
+     */
+    String providerNamespace() default "";
+
+    /**
      * the scope for referring/exporting a service, if it's local, it means searching in current JVM only.
      *
      * @see org.apache.dubbo.rpc.Constants#SCOPE_LOCAL
