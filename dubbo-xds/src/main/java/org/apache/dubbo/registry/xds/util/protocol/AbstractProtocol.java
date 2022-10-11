42d99a9804fa0736fd7f14d4abbd25c9f91c28c1
Albumen Kevin <jhq0812@gmail.com>
2022-10-11 10:53:40 +0800

    Fix xds long time wait

--- dubbo-xds/src/main/java/org/apache/dubbo/registry/xds/util/protocol/AbstractProtocol.java
+++ dubbo-xds/src/main/java/org/apache/dubbo/registry/xds/util/protocol/AbstractProtocol.java
@@ -226,17 +226,22 @@ public abstract class AbstractProtocol<T, S extends DeltaResource<T>> implements
                 return;
             }
             observer.onNext(buildDiscoveryRequest(Collections.emptySet(), value));
-            CompletableFuture<T> future = streamResult.get(requestId);
-            if (future == null) {
-                return;
-            }
-            future.complete(result);
+            returnResult(result);
         }
 
         @Override
         public void onError(Throwable t) {
             logger.error("xDS Client received error message! detail:", t);
             clear();
+            returnResult(null);
+        }
+
+        private void returnResult(T result) {
+            CompletableFuture<T> future = streamResult.get(requestId);
+            if (future == null) {
+                return;
+            }
+            future.complete(result);
         }
 
         @Override
