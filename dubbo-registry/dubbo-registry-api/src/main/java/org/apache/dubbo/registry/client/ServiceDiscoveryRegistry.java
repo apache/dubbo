36521dcfe08e9131c071727e75065c09e7995a52
czq <89424516@qq.com>
2022-11-01 20:58:50 +0800

    fix_providerby_change_discovery_error

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/client/ServiceDiscoveryRegistry.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/client/ServiceDiscoveryRegistry.java
@@ -17,9 +17,11 @@
 package org.apache.dubbo.registry.client;
 
 import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.constants.RegistryConstants;
 import org.apache.dubbo.common.logger.ErrorTypeAwareLogger;
 import org.apache.dubbo.common.logger.LoggerFactory;
 import org.apache.dubbo.common.utils.CollectionUtils;
+import org.apache.dubbo.common.utils.StringUtils;
 import org.apache.dubbo.metadata.AbstractServiceNameMapping;
 import org.apache.dubbo.metadata.MappingChangedEvent;
 import org.apache.dubbo.metadata.MappingListener;
@@ -33,6 +35,7 @@ import org.apache.dubbo.rpc.model.ApplicationModel;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.locks.Lock;
@@ -192,11 +195,21 @@ public class ServiceDiscoveryRegistry extends FailbackRegistry {
 
         boolean check = url.getParameter(CHECK_KEY, false);
 
+        String providedBy = url.getParameter(RegistryConstants.PROVIDED_BY);
+
         String key = ServiceNameMapping.buildMappingKey(url);
         Lock mappingLock = serviceNameMapping.getMappingLock(key);
         try {
             mappingLock.lock();
-            Set<String> subscribedServices = serviceNameMapping.getCachedMapping(url);
+
+            Set<String> subscribedServices = null;
+            if(StringUtils.isBlank(providedBy)) {
+                subscribedServices = serviceNameMapping.getCachedMapping(url);
+            }
+            else {
+                subscribedServices = new TreeSet();
+                subscribedServices.add(providedBy);
+            }
             try {
                 MappingListener mappingListener = new DefaultMappingListener(url, subscribedServices, listener);
                 subscribedServices = serviceNameMapping.getAndListen(this.getUrl(), url, mappingListener);
