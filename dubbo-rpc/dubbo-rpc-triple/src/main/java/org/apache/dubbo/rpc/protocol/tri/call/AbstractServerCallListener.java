4cf4aa4e4a771ad2b485b339f91ebe217c642052
aamingaa <49740762+aamingaa@users.noreply.github.com>
2022-11-08 15:28:53 +0800

    Update AbstractServerCallListener.java

--- dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/call/AbstractServerCallListener.java
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/call/AbstractServerCallListener.java
@@ -54,8 +54,7 @@ public abstract class AbstractServerCallListener implements AbstractServerCall.L
         String remoteApp = (String) invocation.getAttributes()
             .remove(TripleHeaderEnum.CONSUMER_APP_NAME_KEY);
         if (null != remoteApp) {
-            RpcContext.getServerResponseContext().setRemoteApplicationName(remoteApp);
-            RpcContext.getServerContext().setRemoteApplicationName(remoteApp);
+            RpcContext.getServiceContext().setRemoteApplicationName(remoteApp);
         }
         final long stInMillis = System.currentTimeMillis();
         try {
