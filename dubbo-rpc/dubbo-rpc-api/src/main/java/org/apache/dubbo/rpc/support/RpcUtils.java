305d290e5fa8491803f9fc4f0d1088a316eaf3f5
Albumen Kevin <jhq0812@gmail.com>
2022-11-08 11:34:05 +0800

    Fix dubbo get wrong return type when generic impl invoke (#10891)

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/support/RpcUtils.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/support/RpcUtils.java
@@ -78,10 +78,22 @@ public class RpcUtils {
                 && invocation.getInvoker().getUrl() != null
                 && invocation.getInvoker().getInterface() != GenericService.class
                 && !invocation.getMethodName().startsWith("$")) {
+                Type[] returnTypes = null;
+                if (invocation instanceof RpcInvocation) {
+                    returnTypes = ((RpcInvocation) invocation).getReturnTypes();
+                    if (returnTypes != null) {
+                        return returnTypes;
+                    }
+                }
                 String service = invocation.getInvoker().getUrl().getServiceInterface();
                 if (StringUtils.isNotEmpty(service)) {
                     Method method = getMethodByService(invocation, service);
-                    return ReflectUtils.getReturnTypes(method);
+                    if (method != null) {
+                        returnTypes = ReflectUtils.getReturnTypes(method);
+                    }
+                }
+                if (returnTypes != null) {
+                    return returnTypes;
                 }
             }
         } catch (Throwable t) {
