6b8a1717455983967aca41f7424b3ecfec092343
songxiaosheng <songxiaosheng@elastic.link>
2022-11-15 10:59:11 +0800

    dubbo metrics to springboot endpoints

---
+++ dubbo-spring-boot/dubbo-spring-boot-autoconfigure/src/main/java/org/apache/dubbo/spring/boot/autoconfigure/DubboMetricsAutoConfiguration.java
@@ -0,0 +1,28 @@
+package org.apache.dubbo.spring.boot.autoconfigure;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.apache.dubbo.metrics.DubboMetrics;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author songxiaosheng
+ */
+@Configuration(
+    proxyBeanMethods = false
+)
+@ConditionalOnWebApplication
+@ConditionalOnClass({DubboMetrics.class})
+public class DubboMetricsAutoConfiguration {
+    @Bean
+    @ConditionalOnBean({MeterRegistry.class})
+    @ConditionalOnMissingBean({DubboMetrics.class, DubboMetricsBinder.class})
+    public DubboMetricsBinder tomcatMetricsBinder(MeterRegistry meterRegistry) {
+        return new DubboMetricsBinder(meterRegistry);
+    }
+}
