ef1fb6bea40727365885929e6ee8412588dfe26c
xin gu <418294249@qq.com>
2022-11-18 09:16:28 +0800

    Add definition to spring xsd
    Add definition to spring xsd

--- dubbo-xds/src/main/java/org/apache/dubbo/registry/xds/util/XdsChannel.java
+++ dubbo-xds/src/main/java/org/apache/dubbo/registry/xds/util/XdsChannel.java
@@ -49,13 +49,17 @@ public class XdsChannel {
 
     private static final String USE_AGENT = "use-agent";
 
+    private static final String SECURE = "secure";
+
+    private static final String PLAINTEXT = "plaintext";
+
     private final ManagedChannel channel;
 
     protected XdsChannel(URL url) {
         ManagedChannel managedChannel = null;
         try {
             if (!url.getParameter(USE_AGENT, false)) {
-                if("plaintext".equals(url.getParameter("secure"))){
+                if(PLAINTEXT.equals(url.getParameter(SECURE))){
                     managedChannel = NettyChannelBuilder.forAddress(url.getHost(), url.getPort()).usePlaintext()
                         .build();
                 }else{
