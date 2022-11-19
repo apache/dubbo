6d2a250777d1cfc6834f989acae390a3bfa86587
aamingaa <774066625@qq.com>
2022-11-19 15:57:59 +0800

    feat: temp change

--- dubbo-xds/src/main/java/org/apache/dubbo/registry/xds/util/protocol/AbstractProtocol.java
+++ dubbo-xds/src/main/java/org/apache/dubbo/registry/xds/util/protocol/AbstractProtocol.java
@@ -33,11 +33,9 @@ import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 
 import static org.apache.dubbo.common.constants.LoggerCodeConstants.REGISTRY_ERROR_REQUEST_XDS;
@@ -62,21 +60,7 @@ public abstract class AbstractProtocol<T, S extends DeltaResource<T>> implements
      */
     private final LinkedHashSet<StreamObserver<DiscoveryRequest>> requestObserverSet = new LinkedHashSet<>();
 
-    /**
-     * Store Delta-ADS Request Observer ( StreamObserver in Streaming Request )
-     * K - requestId, V - StreamObserver
-     */
-    private final Map<Long, ScheduledFuture<?>> observeScheduledMap = new ConcurrentHashMap<>();
-
-    /**
-     * Store CompletableFuture for Request ( used to fetch async result in ResponseObserver )
-     * K - requestId, V - CompletableFuture
-     */
-    private final Map<Long, CompletableFuture<T>> streamResult = new ConcurrentHashMap<>();
-
     private final int pollingTimeout;
-
-//    protected final static AtomicLong requestId = new AtomicLong(0);
 
     public AbstractProtocol(XdsChannel xdsChannel, Node node, int pollingPoolSize, int pollingTimeout) {
         this.xdsChannel = xdsChannel;
@@ -91,90 +75,50 @@ public abstract class AbstractProtocol<T, S extends DeltaResource<T>> implements
      */
     public abstract String getTypeUrl();
 
-    public abstract Set<Set<String>> getResouceNamesList();
-    public abstract void addResouceNames(Set<String> resourceNaems);
+    public abstract Set<String> getAllResouceNames();
+    public abstract void addResouceNames(Set<String> resourceNames);
     public abstract boolean isExistResource(Set<String> resourceNames);
+
+    public abstract T getCacheResource(Set<String> resourceNames);
     @Override
     public T getResource(Set<String> resourceNames) {
-//        long request = requestId.getAndIncrement();
         resourceNames = resourceNames == null ? Collections.emptySet() : resourceNames;
-        if (!resourceNames.isEmpty() && !isExistResource(resourceNames)) {
-            addResouceNames(resourceNames);
+        if (!resourceNames.isEmpty() && isExistResource(resourceNames)) {
+            return getCacheResource(resourceNames);
         }
-
-        // Store Request Parameter, which will be used for ACK
-//        requestParam.put(request, resourceNames);
-
-        // create observer
-        StreamObserver<DiscoveryRequest> requestObserver = xdsChannel.createDeltaDiscoveryRequest(new ResponseObserver(null, null));
-
         // use future to get async result
         CompletableFuture<T> future = new CompletableFuture<>();
+        // create observer
+        StreamObserver<DiscoveryRequest> requestObserver = xdsChannel.createDeltaDiscoveryRequest(new ResponseObserver(future, null));
         requestObserverSet.add(requestObserver);
-//        requestObserverMap.put(request, requestObserver);
-//        streamResult.put(request, future);
-
         // send request to control panel
         requestObserver.onNext(buildDiscoveryRequest(resourceNames));
-
         try {
             // get result
             return future.get();
         } catch (InterruptedException | ExecutionException e) {
             logger.error(REGISTRY_ERROR_REQUEST_XDS, "", "", "Error occur when request control panel.");
             return null;
-        } finally {
-            // close observer
-            //requestObserver.onCompleted();
-
-            // remove temp
-//            streamResult.remove(request);
-//            requestObserverMap.remove(request);
-//            requestParam.remove(request);
         }
     }
 
     @Override
     public void observeResource(Set<String> resourceNames, Consumer<T> consumer) {
-//        long request = requestId.getAndIncrement();
         resourceNames = resourceNames == null ? Collections.emptySet() : resourceNames;
-        if (!resourceNames.isEmpty() &&  !isExistResource(resourceNames)) {
-            addResouceNames(resourceNames);
-        }
-        // Store Request Parameter, which will be used for ACK
-//        requestParam.put(request, resourceNames);
-
         // call once for full data
         consumer.accept(getResource(resourceNames));
-
-        // channel reused
-        StreamObserver<DiscoveryRequest> requestObserver = xdsChannel.createDeltaDiscoveryRequest(new ResponseObserver(null, consumer));
-//        requestObserverMap.put(request, requestObserver);
+        // use future to get async result, future complete on StreamObserver onNext
+        CompletableFuture<T> future = new CompletableFuture<>();
+        StreamObserver<DiscoveryRequest> requestObserver = xdsChannel.createDeltaDiscoveryRequest(new ResponseObserver(future, consumer));
         try {
-            int requestObserverSetSize = requestObserverSet.size();
-            // origin request, may changed by updateObserve
-            Set<String> names = requestParam.get(requestObserverSetSize >= 2 ?
-                requestObserverSet.stream().skip(requestObserverSetSize - 2).findFirst().get(): null
-            );
-
-            // use future to get async result, future complete on StreamObserver onNext
-            CompletableFuture<T> future = new CompletableFuture<>();
-//            streamResult.put(request, future);
-
-            // observer reused
-            StreamObserver<DiscoveryRequest> observer = requestObserverSet.iterator().next();
-
-            if (observer == null) {
-                observer = xdsChannel.createDeltaDiscoveryRequest(new ResponseObserver(future, consumer));
-//                requestObserverMap.put(request, observer);
+            if (!isExistResource(resourceNames)) {
+                addResouceNames(resourceNames);
+                // send request to control panel
+                requestObserver.onNext(buildDiscoveryRequest(getAllResouceNames()));
             }
-
-            // send request to control panel
-            observer.onNext(buildDiscoveryRequest(names));
         } catch (Throwable t) {
             logger.error("Error when requesting observe data. Type: " + getTypeUrl(), t);
         }
-//        return request;
     }
 
     @Override
