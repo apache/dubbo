bfce75cf66689874b5cf8a6136546385ffbf3ba0
Albumen Kevin <jhq0812@gmail.com>
2022-11-21 11:31:29 +0800

    Revert "Revert "Local references support mergeable (#9645)" (#10707)"
    This reverts commit 67d94aea5804e2a057537a04f4b509ef63200d56.

--- dubbo-rpc/dubbo-rpc-injvm/src/test/java/org/apache/dubbo/rpc/protocol/injvm/InjvmProtocolTest.java
+++ dubbo-rpc/dubbo-rpc-injvm/src/test/java/org/apache/dubbo/rpc/protocol/injvm/InjvmProtocolTest.java
@@ -29,18 +29,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import static org.apache.dubbo.common.constants.CommonConstants.GROUP_KEY;
-import static org.apache.dubbo.common.constants.CommonConstants.INTERFACE_KEY;
-import static org.apache.dubbo.common.constants.CommonConstants.VERSION_KEY;
+import static org.apache.dubbo.common.constants.CommonConstants.*;
 import static org.apache.dubbo.rpc.Constants.ASYNC_KEY;
 import static org.apache.dubbo.rpc.Constants.GENERIC_KEY;
 import static org.apache.dubbo.rpc.Constants.LOCAL_PROTOCOL;
 import static org.apache.dubbo.rpc.Constants.SCOPE_KEY;
 import static org.apache.dubbo.rpc.Constants.SCOPE_LOCAL;
 import static org.apache.dubbo.rpc.Constants.SCOPE_REMOTE;
+import static org.apache.dubbo.rpc.Constants.MERGER_KEY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -76,7 +74,7 @@ public class InjvmProtocolTest {
         assertEquals(service.getSize(new String[]{"", "", ""}), 3);
         service.invoke("injvm://127.0.0.1/TestService", "invoke");
 
-        InjvmInvoker<?> injvmInvoker = new InjvmInvoker<>(DemoService.class, URL.valueOf("injvm://127.0.0.1/TestService"), null, new HashMap<>());
+        InjvmInvoker<?> injvmInvoker = new InjvmInvoker<>(DemoService.class, URL.valueOf("injvm://127.0.0.1/TestService"), null, null);
         assertFalse(injvmInvoker.isAvailable());
 
     }
@@ -135,6 +133,38 @@ public class InjvmProtocolTest {
         exporters.add(exporter);
         service = proxy.getProxy(protocol.refer(DemoService.class, url));
         assertNull(service.getAsyncResult());
+    }
+
+    @Test
+    public void testLocalProtocolForMergeResult() throws Exception {
+        HelloService helloService1 = new Hello1ServiceImpl();
+        URL url = URL.valueOf("injvm://127.0.0.1/HelloService")
+            .addParameter(INTERFACE_KEY, HelloService.class.getName())
+            .addParameter(APPLICATION_KEY, "consumer")
+            .addParameter(GROUP_KEY, "g1");
+        Invoker<?> invoker1 = proxy.getInvoker(helloService1, HelloService.class, url);
+        assertTrue(invoker1.isAvailable());
+        Exporter<?> exporter1 = protocol.export(invoker1);
+        exporters.add(exporter1);
+
+        URL url2 = URL.valueOf("injvm://127.0.0.1/HelloService")
+            .addParameter(INTERFACE_KEY, HelloService.class.getName())
+            .addParameter(APPLICATION_KEY, "consumer")
+            .addParameter(GROUP_KEY, "g2");
+        HelloService helloService2 = new Hello2ServiceImpl();
+        Invoker<?> invoker2 = proxy.getInvoker(helloService2, HelloService.class, url2);
+        assertTrue(invoker2.isAvailable());
+        Exporter<?> exporter2 = protocol.export(invoker2);
+        exporters.add(exporter2);
+
+
+        URL referUrl = URL.valueOf("injvm://127.0.0.1/HelloService")
+            .addParameter(INTERFACE_KEY, HelloService.class.getName())
+            .addParameter(APPLICATION_KEY, "consumer")
+            .addParameter(GROUP_KEY, "*")
+            .addParameter(MERGER_KEY, "list");
+        List<String> list = proxy.getProxy(protocol.refer(HelloService.class, referUrl)).hellos();
+        assertEquals(2, list.size());
     }
 
 }
