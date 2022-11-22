3a0a79f205ede424d9a276c1d48e4685bae332f0
Albumen Kevin <jhq0812@gmail.com>
2022-11-22 09:35:36 +0800

    Remove public modifier in dubbo-common

--- dubbo-common/src/test/java/org/apache/dubbo/common/utils/MemberUtilsTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/utils/MemberUtilsTest.java
@@ -29,7 +29,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  *
  * @since 2.7.6
  */
-class MemberUtilsTest {
+public class MemberUtilsTest {
 
     @Test
     void testIsStatic() throws NoSuchMethodException {
