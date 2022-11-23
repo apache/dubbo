d2e7e995b72231d96ea9e1d8d38da62d34194e3d
Albumen Kevin <jhq0812@gmail.com>
2022-11-23 11:28:20 +0800

    update catch

--- dubbo-common/src/main/java/org/apache/dubbo/common/logger/LoggerFactory.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/logger/LoggerFactory.java
@@ -81,7 +81,7 @@ public class LoggerFactory {
                             found = true;
                             break;
                         }
-                    } catch (Exception ignored) {
+                    } catch (Exception | LinkageError ignored) {
                         // ignore
                     }
                 }
