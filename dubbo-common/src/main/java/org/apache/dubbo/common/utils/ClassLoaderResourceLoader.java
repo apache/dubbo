1f1995ac8c9bcf2e701677e298a348930cf77a8f
Albumen Kevin <jhq0812@gmail.com>
2022-11-23 16:24:38 +0800

    Fix logger in ClassLoaderResourceLoader (#10995)

--- dubbo-common/src/main/java/org/apache/dubbo/common/utils/ClassLoaderResourceLoader.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/utils/ClassLoaderResourceLoader.java
@@ -16,6 +16,8 @@
  */
 package org.apache.dubbo.common.utils;
 
+import org.apache.dubbo.common.logger.Logger;
+import org.apache.dubbo.common.logger.LoggerFactory;
 import org.apache.dubbo.common.resource.GlobalResourcesRepository;
 
 import java.io.IOException;
@@ -34,7 +36,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 
 public class ClassLoaderResourceLoader {
-
+    private static Logger logger = LoggerFactory.getLogger(ClassLoaderResourceLoader.class);
     private static SoftReference<Map<ClassLoader, Map<String, Set<URL>>>> classLoaderResourcesCache = null;
 
     static {
@@ -86,7 +88,7 @@ public class ClassLoaderResourceLoader {
                     }
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.error("Exception occurred when reading SPI definitions. SPI path: " + fileName + " ClassLoader name: " + currentClassLoader, e);
             }
             urlCache.put(fileName, set);
         }
