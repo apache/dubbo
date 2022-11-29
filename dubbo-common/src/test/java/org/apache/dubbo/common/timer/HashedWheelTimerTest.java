f670fa33ad8e1284b0aa552e32c48aadd8836fae
Albumen Kevin <jhq0812@gmail.com>
2022-11-29 19:50:30 +0800

    Enhance unit test cases (#11037)

--- dubbo-common/src/test/java/org/apache/dubbo/common/timer/HashedWheelTimerTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/timer/HashedWheelTimerTest.java
@@ -28,6 +28,8 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import static org.awaitility.Awaitility.await;
+
 class HashedWheelTimerTest {
     private CountDownLatch tryStopTaskCountDownLatch = new CountDownLatch(1);
     private CountDownLatch errorTaskCountDownLatch = new CountDownLatch(1);
@@ -140,10 +142,11 @@ class HashedWheelTimerTest {
                 TimeUnit.MILLISECONDS,
                 8, 8);
 
+        EmptyTask emptyTask = new EmptyTask();
         Assertions.assertThrows(RuntimeException.class,
                 () -> timer.newTimeout(null, 5, TimeUnit.SECONDS));
         Assertions.assertThrows(RuntimeException.class,
-                () -> timer.newTimeout(new EmptyTask(), 5, null));
+                () -> timer.newTimeout(emptyTask, 5, null));
 
         Timeout timeout = timer.newTimeout(new ErrorTask(), 10, TimeUnit.MILLISECONDS);
         errorTaskCountDownLatch.await();
@@ -152,27 +155,27 @@ class HashedWheelTimerTest {
         Assertions.assertNotNull(timeout.toString());
         Assertions.assertEquals(timeout.timer(), timer);
 
-        timeout = timer.newTimeout(new EmptyTask(), 1000, TimeUnit.SECONDS);
+        timeout = timer.newTimeout(emptyTask, 1000, TimeUnit.SECONDS);
         timeout.cancel();
         Assertions.assertTrue(timeout.isCancelled());
 
         List<Timeout> timeouts = new LinkedList<>();
-        for (; timer.pendingTimeouts() < 8; ) {
+        BlockTask blockTask = new BlockTask();
+        while (timer.pendingTimeouts() < 8) {
             // to trigger maxPendingTimeouts
-            timeout = timer.newTimeout(new BlockTask(), -1, TimeUnit.MILLISECONDS);
+            timeout = timer.newTimeout(blockTask, -1, TimeUnit.MILLISECONDS);
             timeouts.add(timeout);
             Assertions.assertNotNull(timeout.toString());
         }
-        Assertions.assertEquals(timer.pendingTimeouts(), 8);
+        Assertions.assertEquals(8, timer.pendingTimeouts());
 
         // this will throw an exception because of maxPendingTimeouts
         Assertions.assertThrows(RuntimeException.class,
-                () -> timer.newTimeout(new BlockTask(), 1, TimeUnit.MILLISECONDS));
+                () -> timer.newTimeout(blockTask, 1, TimeUnit.MILLISECONDS));
 
-        timeout = timeouts.get(2);
+        Timeout secondTimeout = timeouts.get(2);
         // wait until the task expired
-        Thread.sleep(100);
-        Assertions.assertTrue(timeout.isExpired());
+        await().until(secondTimeout::isExpired);
 
         timer.stop();
     }
