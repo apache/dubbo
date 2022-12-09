cbec2a99da6da318bb0de96fd5c434fa1f36e9de
suncr <suncairong@moresec.cn>
2022-12-09 16:43:47 +0800

    remove mvc module

--- dubbo-rpc/dubbo-rpc-mvc/src/main/java/org/apache/dubbo/rpc/protocol/mvc/annotation/consumer/HttpConnectionPreBuildIntercept.java
+++
@@ -1,8 +0,0 @@
-package org.apache.dubbo.rpc.protocol.mvc.annotation.consumer;
-
-import org.apache.dubbo.common.extension.SPI;
-
-@SPI()
-public interface HttpConnectionPreBuildIntercept {
-    void intercept(HttpConnectionCreateContext connectionCreateContext);
-}
