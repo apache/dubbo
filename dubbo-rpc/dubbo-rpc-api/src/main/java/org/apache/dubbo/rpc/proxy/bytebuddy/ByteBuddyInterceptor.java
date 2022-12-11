0f089f990081a530fa5f237b8ead2e1d0c70e16d
icodening <cleverbn96@gmail.com>
2022-12-11 20:17:35 +0800

    remove unused import

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/proxy/bytebuddy/ByteBuddyInterceptor.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/proxy/bytebuddy/ByteBuddyInterceptor.java
@@ -23,7 +23,6 @@ import net.bytebuddy.implementation.bind.annotation.This;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 public class ByteBuddyInterceptor {
 
