e3b0b041545a64fb490efa6a991196d67bfa3f94
suncr <suncairong@moresec.cn>
2022-12-18 14:46:22 +0800

    RestProtocol consumer

--- dubbo-rpc/dubbo-rpc-rest/src/main/java/org/apache/dubbo/rpc/protocol/rest/request/convert/RequestConvert.java
+++ dubbo-rpc/dubbo-rpc-rest/src/main/java/org/apache/dubbo/rpc/protocol/rest/request/convert/RequestConvert.java
@@ -1,10 +1,18 @@
 package org.apache.dubbo.rpc.protocol.rest.request.convert;
 
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.extension.Adaptive;
+import org.apache.dubbo.common.extension.SPI;
+import org.apache.dubbo.remoting.Constants;
 import org.apache.dubbo.remoting.RemotingException;
 import org.apache.dubbo.rpc.protocol.rest.annotation.consumer.RequestTemplate;
 import org.apache.dubbo.rpc.protocol.rest.request.client.ClientFacade;
 
+@SPI
 public interface RequestConvert<REQ, RES> extends ClientFacade<REQ, RES> {
+
+    @Adaptive({Constants.CLIENT_KEY})
+    RequestConvert createRequestConvert(URL url);
 
     REQ convert(RequestTemplate requestTemplate);
 
