18742f0ff864fbeb9e4a7c08d8bf7c224f9a68d0
Andy Cheung <win120a@163.com>
2022-12-21 01:26:06 +0800

    Add cause.

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/client/metadata/ProtocolPortsMetadataCustomizer.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/client/metadata/ProtocolPortsMetadataCustomizer.java
@@ -56,8 +56,8 @@ public class ProtocolPortsMetadataCustomizer implements ServiceInstanceCustomize
             Integer oldPort = protocols.get(protocol);
             int newPort = url.getPort();
             if (oldPort != null) {
-                LOGGER.warn(LoggerCodeConstants.PROTOCOL_INCORRECT_PARAMETER_VALUES, "same protocol " + "[" + protocol + "]" + " listen on different ports " + "[" + oldPort + "," + newPort + "]" + " will override with each other" +
-                    ".Override port [" + oldPort + "] with port [" + newPort + "]");
+                LOGGER.warn(LoggerCodeConstants.PROTOCOL_INCORRECT_PARAMETER_VALUES, "the protocol is listening multiple ports", "", "Same protocol " + "[" + protocol + "]" + " listens on different ports " + "[" + oldPort + "," + newPort + "]" + " will override with each other" +
+                    ". The port [" + oldPort + "] is overridden with port [" + newPort + "].");
             }
             protocols.put(protocol, newPort);
         });
