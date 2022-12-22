ad3d8dda7732416b780c7432e1ffa0c7fceb3f5c
水手 <xieshouyu@cai-inc.com>
2022-12-22 09:25:53 +0800

    1.update LICENSE-2.0 to first row

--- dubbo-common/src/test/java/org/apache/dubbo/common/model/person/PersonMap.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/model/person/PersonMap.java
@@ -1,4 +1,3 @@
-package org.apache.dubbo.common.model.person;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -15,6 +14,8 @@ package org.apache.dubbo.common.model.person;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.dubbo.common.model.person;
+
 import java.util.HashMap;
 
 public class PersonMap extends HashMap<String, String> {
