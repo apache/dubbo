175e836847b29a73a29509ff52acd22b453379cc
Albumen Kevin <jhq0812@gmail.com>
2023-01-11 14:46:40 +0800

    enhance fastjson2 log

--- dubbo-serialization/dubbo-serialization-fastjson2/src/main/java/org/apache/dubbo/common/serialize/fastjson2/Fastjson2SecurityManager.java
+++ dubbo-serialization/dubbo-serialization-fastjson2/src/main/java/org/apache/dubbo/common/serialize/fastjson2/Fastjson2SecurityManager.java
@@ -34,6 +34,7 @@ import com.alibaba.fastjson2.util.TypeUtils;
 
 import static com.alibaba.fastjson2.util.TypeUtils.loadClass;
 import static org.apache.dubbo.common.constants.LoggerCodeConstants.PROTOCOL_UNTRUSTED_SERIALIZE_CLASS;
+import static org.apache.dubbo.common.utils.SerializeCheckStatus.STRICT;
 
 public class Fastjson2SecurityManager implements AllowClassNotifyListener {
     private Filter securityFilter = new Handler(AllowClassNotifyListener.DEFAULT_STATUS, new String[0]);
@@ -66,29 +67,40 @@ public class Fastjson2SecurityManager implements AllowClassNotifyListener {
 
         @Override
         public Class<?> apply(String typeName, Class<?> expectClass, long features) {
-            switch (status) {
-                case STRICT:
-                    return super.apply(typeName, expectClass, features);
-                case WARN:
-                    Class<?> tryLoad = super.apply(typeName, expectClass, features);
-                    if (tryLoad != null) {
-                        return tryLoad;
-                    }
-                case DISABLED:
-                    Class<?> localClass = loadClassDirectly(typeName);
-                    if (localClass != null) {
-                        if (status == SerializeCheckStatus.WARN && warnedClasses.add(typeName)) {
-                            logger.error(PROTOCOL_UNTRUSTED_SERIALIZE_CLASS, "", "",
-                                "[Serialization Security] Serialized class " + localClass.getName() + " is not in allow list. " +
-                                "Current mode is `WARN`, will allow to deserialize it by default. " +
-                                "Dubbo will set to `STRICT` mode by default in the future. " +
-                                "Please add it into security/serialize.allowlist or follow FAQ to configure it.");
-                        }
-                        return localClass;
-                    }
-                default:
-                    return null;
+            Class<?> tryLoad = super.apply(typeName, expectClass, features);
+
+            // 1. in allow list, return
+            if (tryLoad != null) {
+                return tryLoad;
             }
+
+            // 2. check if in strict mode
+            if (status == STRICT) {
+                String msg = "[Serialization Security] Serialized class " + typeName + " is not in allow list. " +
+                    "Current mode is `STRICT`, will disallow to deserialize it by default. " +
+                    "Please add it into security/serialize.allowlist or follow FAQ to configure it.";
+                if (warnedClasses.add(typeName)) {
+                    logger.error(PROTOCOL_UNTRUSTED_SERIALIZE_CLASS, "", "", msg);
+                }
+
+                return null;
+            }
+
+            // 3. try load
+            Class<?> localClass = loadClassDirectly(typeName);
+            if (localClass != null) {
+                if (status == SerializeCheckStatus.WARN && warnedClasses.add(typeName)) {
+                    logger.error(PROTOCOL_UNTRUSTED_SERIALIZE_CLASS, "", "",
+                        "[Serialization Security] Serialized class " + localClass.getName() + " is not in allow list. " +
+                            "Current mode is `WARN`, will allow to deserialize it by default. " +
+                            "Dubbo will set to `STRICT` mode by default in the future. " +
+                            "Please add it into security/serialize.allowlist or follow FAQ to configure it.");
+                }
+                return localClass;
+            }
+
+            // 4. class not found
+            return null;
         }
 
         public Class<?> loadClassDirectly(String typeName) {
