53e608672e950a9ca1a6f928965b7725b1f37487
Albumen Kevin <jhq0812@gmail.com>
2023-01-11 13:59:38 +0800

    Switch default check level

--- dubbo-common/src/main/java/org/apache/dubbo/common/utils/AllowClassNotifyListener.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/utils/AllowClassNotifyListener.java
@@ -20,7 +20,7 @@ import java.util.Set;
 
 public interface AllowClassNotifyListener {
 
-    SerializeCheckStatus DEFAULT_STATUS = SerializeCheckStatus.STRICT;
+    SerializeCheckStatus DEFAULT_STATUS = SerializeCheckStatus.WARN;
 
     void notify(SerializeCheckStatus status, Set<String> prefixList);
 }
