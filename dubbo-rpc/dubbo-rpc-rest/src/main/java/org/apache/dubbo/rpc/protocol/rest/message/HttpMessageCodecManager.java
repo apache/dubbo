4a3c594229b81a40ccc1b318b381270101b1535d
suncr <suncairong@moresec.cn>
2023-01-29 14:14:15 +0800

    add rest http message encode

---
+++ dubbo-rpc/dubbo-rpc-rest/src/main/java/org/apache/dubbo/rpc/protocol/rest/message/HttpMessageCodecManager.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.dubbo.rpc.protocol.rest.message;
+
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.metadata.rest.media.MediaType;
+import org.apache.dubbo.rpc.model.ApplicationModel;
+import org.apache.dubbo.rpc.protocol.rest.exception.UnSupportContentTypeException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Set;
+
+public class HttpMessageCodecManager {
+    private static final Set<HttpMessageCodec> httpMessageCodecs =
+        ApplicationModel.defaultModel().getExtensionLoader(HttpMessageCodec.class).getSupportedExtensionInstances();
+
+
+    public static Object httpMessageDecode(InputStream inputStream, Class type, MediaType mediaType) throws Exception {
+        for (HttpMessageCodec httpMessageCodec : httpMessageCodecs) {
+            if (httpMessageCodec.contentTypeSupport(mediaType)) {
+                return httpMessageCodec.decode(inputStream, type);
+            }
+        }
+        throw new UnSupportContentTypeException("UnSupport content-type :" + mediaType.value);
+    }
+
+    public static void httpMessageEncode(ByteArrayOutputStream outputStream, Object unSerializedBody, URL url, MediaType mediaType) throws Exception {
+        for (HttpMessageCodec httpMessageCodec : httpMessageCodecs) {
+            if (httpMessageCodec.contentTypeSupport(mediaType)) {
+                httpMessageCodec.encode(outputStream, unSerializedBody, url);
+                return;
+            }
+        }
+        throw new UnSupportContentTypeException("UnSupport content-type :" + mediaType.value);
+    }
+
+}
