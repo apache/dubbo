ab50d3954e24d431202bb4e56392ae6e4a530e96
kamtohung <kamtohung@gmail.com>
2023-01-30 15:37:31 +0800

    Add JRE utils (#11393)

---
+++ dubbo-common/src/test/java/org/apache/dubbo/common/utils/JRETest.java
@@ -0,0 +1,28 @@
+package org.apache.dubbo.common.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class JRETest {
+
+    @Test
+    void blankSystemVersion() {
+        System.setProperty("java.version", "");
+        JRE jre = JRE.currentVersion();
+        Assertions.assertEquals(JRE.JAVA_8, jre);
+    }
+
+    @Test
+    void java8Version() {
+        JRE jre = JRE.currentVersion();
+        Assertions.assertEquals(JRE.JAVA_8, jre);
+    }
+
+    @Test
+    void java19Version() {
+        JRE jre = JRE.currentVersion();
+        Assertions.assertNotEquals(JRE.JAVA_19, jre);
+    }
+
+}
