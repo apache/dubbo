a4b001768abbb3a4f02d69c9e0a7f254e4fef7f7
wxbty <wxb_101@163.com>
2023-02-03 10:59:42 +0800

    resolve sonar bug

--- dubbo-metrics/dubbo-metrics-api/src/main/java/org/apache/dubbo/metrics/event/SimpleMetricsEventMulticaster.java
+++ dubbo-metrics/dubbo-metrics-api/src/main/java/org/apache/dubbo/metrics/event/SimpleMetricsEventMulticaster.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class SimpleMetricsEventMulticaster implements MetricsEventMulticaster {
+public final class SimpleMetricsEventMulticaster implements MetricsEventMulticaster {
 
     private static volatile SimpleMetricsEventMulticaster instance;
 
