9fd7ae2ff08e1df786b20485870530eaca2c4412
jojocodeX <571943037@qq.com>
2023-02-08 17:37:15 +0800

    ci test

--- dubbo-plugin/dubbo-spring-security/src/main/java/org/apache/dubbo/spring/security/filter/ContextHolderAuthenticationResolverFilter.java
+++ dubbo-plugin/dubbo-spring-security/src/main/java/org/apache/dubbo/spring/security/filter/ContextHolderAuthenticationResolverFilter.java
@@ -34,7 +34,7 @@ import static org.apache.dubbo.spring.security.utils.SecurityNames.SECURITY_CONT
 @Activate(group = CommonConstants.PROVIDER, order = -10000,onClass = SECURITY_CONTEXT_HOLDER_CLASS_NAME)
 public class ContextHolderAuthenticationResolverFilter implements Filter {
 
-    private final ObjectMapperCodec MAPPER = new ObjectMapperCodec();
+    private ObjectMapperCodec MAPPER = new ObjectMapperCodec();
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
