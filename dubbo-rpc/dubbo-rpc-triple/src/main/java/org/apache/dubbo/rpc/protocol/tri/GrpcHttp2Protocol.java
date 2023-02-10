fcc1e6bb0a77e4c0e2a5cdf8d562d61a14cda939
earthchen <earthchen@apache.org>
2023-02-10 19:01:39 +0800

    remove grpc module

---
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/GrpcHttp2Protocol.java
@@ -0,0 +1,7 @@
+package org.apache.dubbo.rpc.protocol.tri;
+
+import org.apache.dubbo.common.extension.Activate;
+
+@Activate
+public class GrpcHttp2Protocol extends TripleHttp2Protocol {
+}
---
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/GrpcProtocol.java
@@ -0,0 +1,10 @@
+package org.apache.dubbo.rpc.protocol.tri;
+
+import org.apache.dubbo.rpc.model.FrameworkModel;
+
+public class GrpcProtocol extends TripleProtocol {
+
+    public GrpcProtocol(FrameworkModel frameworkModel) {
+        super(frameworkModel);
+    }
+}
