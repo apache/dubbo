3ead6f038f5bde9d144d403c7c94b25224e0907a
Andy Cheung <micro_zx@msn.cn>
2023-02-10 22:01:18 +0800

    Add DeprecatedMethodInvocationCounterTest (2).

--- dubbo-common/src/test/java/org/apache/dubbo/common/DeprecatedMethodInvocationCounterTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/DeprecatedMethodInvocationCounterTest.java
@@ -9,14 +9,21 @@ import java.util.Map;
  * Tests of DeprecatedMethodInvocationCounter.
  */
 class DeprecatedMethodInvocationCounterTest {
+
+    private static final String METHOD_DEFINITION = "org.apache.dubbo.common.URL.getServiceName()";
+
     @Test
     void testRealInvocation() {
+        Assertions.assertFalse(DeprecatedMethodInvocationCounter.hasThisMethodInvoked(METHOD_DEFINITION));
+
         // Invoke a deprecated method.
         URL url = URL.valueOf("dubbo://admin:hello1234@10.20.130.230:20880/context/path#index?version=1.0.0&id=org.apache.dubbo.config.RegistryConfig#0");
         url.getServiceName();
         url.getServiceName();
 
+        Assertions.assertTrue(DeprecatedMethodInvocationCounter.hasThisMethodInvoked(METHOD_DEFINITION));
+
         Map<String, Integer> record = DeprecatedMethodInvocationCounter.getInvocationRecord();
-        Assertions.assertEquals(2, record.get("org.apache.dubbo.common.URL.getServiceName()"));
+        Assertions.assertEquals(2, record.get(METHOD_DEFINITION));
     }
 }
