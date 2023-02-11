153390f273bc9a1a4e6be8cb5763281f08c31929
Albumen Kevin <jhq0812@gmail.com>
2023-02-11 11:46:35 +0800

    Fix concurrent issue in scope model

--- dubbo-common/src/main/java/org/apache/dubbo/rpc/model/ScopeModel.java
+++ dubbo-common/src/main/java/org/apache/dubbo/rpc/model/ScopeModel.java
@@ -26,13 +26,9 @@ import org.apache.dubbo.common.logger.LoggerFactory;
 import org.apache.dubbo.common.utils.ConcurrentHashSet;
 import org.apache.dubbo.common.utils.StringUtils;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.apache.dubbo.common.constants.LoggerCodeConstants.CONFIG_UNABLE_DESTROY_MODEL;
@@ -62,21 +58,23 @@ public abstract class ScopeModel implements ExtensionAccessor {
 
     private String desc;
 
-    private Set<ClassLoader> classLoaders;
+    private final Set<ClassLoader> classLoaders = new ConcurrentHashSet<>();
 
     private final ScopeModel parent;
     private final ExtensionScope scope;
 
-    private ExtensionDirector extensionDirector;
+    private volatile ExtensionDirector extensionDirector;
 
-    private ScopeBeanFactory beanFactory;
-    private List<ScopeModelDestroyListener> destroyListeners;
+    private volatile ScopeBeanFactory beanFactory;
+    private final List<ScopeModelDestroyListener> destroyListeners = new CopyOnWriteArrayList<>();
 
-    private List<ScopeClassLoaderListener> classLoaderListeners;
+    private final List<ScopeClassLoaderListener> classLoaderListeners = new CopyOnWriteArrayList<>();
 
-    private Map<String, Object> attributes;
+    private final Map<String, Object> attributes = new ConcurrentHashMap<>();
     private final AtomicBoolean destroyed = new AtomicBoolean(false);
     private final boolean internalScope;
+
+    protected final Object instLock = new Object();
 
     public ScopeModel(ScopeModel parent, ExtensionScope scope, boolean isInternal) {
         this.parent = parent;
@@ -94,18 +92,16 @@ public abstract class ScopeModel implements ExtensionAccessor {
      * </ol>
      */
     protected void initialize() {
-        this.extensionDirector = new ExtensionDirector(parent != null ? parent.getExtensionDirector() : null, scope, this);
-        this.extensionDirector.addExtensionPostProcessor(new ScopeModelAwareExtensionProcessor(this));
-        this.beanFactory = new ScopeBeanFactory(parent != null ? parent.getBeanFactory() : null, extensionDirector);
-        this.destroyListeners = new LinkedList<>();
-        this.classLoaderListeners = new LinkedList<>();
-        this.attributes = new ConcurrentHashMap<>();
-        this.classLoaders = new ConcurrentHashSet<>();
+        synchronized (instLock) {
+            this.extensionDirector = new ExtensionDirector(parent != null ? parent.getExtensionDirector() : null, scope, this);
+            this.extensionDirector.addExtensionPostProcessor(new ScopeModelAwareExtensionProcessor(this));
+            this.beanFactory = new ScopeBeanFactory(parent != null ? parent.getBeanFactory() : null, extensionDirector);
 
-        // Add Framework's ClassLoader by default
-        ClassLoader dubboClassLoader = ScopeModel.class.getClassLoader();
-        if (dubboClassLoader != null) {
-            this.addClassLoader(dubboClassLoader);
+            // Add Framework's ClassLoader by default
+            ClassLoader dubboClassLoader = ScopeModel.class.getClassLoader();
+            if (dubboClassLoader != null) {
+                this.addClassLoader(dubboClassLoader);
+            }
         }
     }
 
@@ -134,39 +130,51 @@ public abstract class ScopeModel implements ExtensionAccessor {
     }
 
     protected void notifyDestroy() {
-        for (ScopeModelDestroyListener destroyListener : destroyListeners) {
-            destroyListener.onDestroy(this);
-        }
-    }
-
-    protected void notifyProtocolDestroy() {
-        for (ScopeModelDestroyListener destroyListener : destroyListeners) {
-            if (destroyListener.isProtocol()) {
+        synchronized (instLock) {
+            for (ScopeModelDestroyListener destroyListener : destroyListeners) {
                 destroyListener.onDestroy(this);
             }
         }
     }
 
+    protected void notifyProtocolDestroy() {
+        synchronized (instLock) {
+            for (ScopeModelDestroyListener destroyListener : destroyListeners) {
+                if (destroyListener.isProtocol()) {
+                    destroyListener.onDestroy(this);
+                }
+            }
+        }
+    }
+
     protected void notifyClassLoaderAdd(ClassLoader classLoader) {
-        for (ScopeClassLoaderListener classLoaderListener : classLoaderListeners) {
-            classLoaderListener.onAddClassLoader(this, classLoader);
+        synchronized (instLock) {
+            for (ScopeClassLoaderListener classLoaderListener : classLoaderListeners) {
+                classLoaderListener.onAddClassLoader(this, classLoader);
+            }
         }
     }
 
     protected void notifyClassLoaderDestroy(ClassLoader classLoader) {
-        for (ScopeClassLoaderListener classLoaderListener : classLoaderListeners) {
-            classLoaderListener.onRemoveClassLoader(this, classLoader);
+        synchronized (instLock) {
+            for (ScopeClassLoaderListener classLoaderListener : classLoaderListeners) {
+                classLoaderListener.onRemoveClassLoader(this, classLoader);
+            }
         }
     }
 
     protected abstract void onDestroy();
 
     public final void addDestroyListener(ScopeModelDestroyListener listener) {
-        destroyListeners.add(listener);
+        synchronized (instLock) {
+            destroyListeners.add(listener);
+        }
     }
 
     public final void addClassLoaderListener(ScopeClassLoaderListener listener) {
-        classLoaderListeners.add(listener);
+        synchronized (instLock) {
+            classLoaderListeners.add(listener);
+        }
     }
 
     public Map<String, Object> getAttributes() {
@@ -203,22 +211,26 @@ public abstract class ScopeModel implements ExtensionAccessor {
     }
 
     public void addClassLoader(ClassLoader classLoader) {
-        this.classLoaders.add(classLoader);
-        if (parent != null) {
-            parent.addClassLoader(classLoader);
+        synchronized (instLock) {
+            this.classLoaders.add(classLoader);
+            if (parent != null) {
+                parent.addClassLoader(classLoader);
+            }
+            extensionDirector.removeAllCachedLoader();
+            notifyClassLoaderAdd(classLoader);
         }
-        extensionDirector.removeAllCachedLoader();
-        notifyClassLoaderAdd(classLoader);
     }
 
     public void removeClassLoader(ClassLoader classLoader) {
-        if (checkIfClassLoaderCanRemoved(classLoader)) {
-            this.classLoaders.remove(classLoader);
-            if (parent != null) {
-                parent.removeClassLoader(classLoader);
+        synchronized (instLock) {
+            if (checkIfClassLoaderCanRemoved(classLoader)) {
+                this.classLoaders.remove(classLoader);
+                if (parent != null) {
+                    parent.removeClassLoader(classLoader);
+                }
+                extensionDirector.removeAllCachedLoader();
+                notifyClassLoaderDestroy(classLoader);
             }
-            extensionDirector.removeAllCachedLoader();
-            notifyClassLoaderDestroy(classLoader);
         }
     }
 
@@ -227,7 +239,9 @@ public abstract class ScopeModel implements ExtensionAccessor {
     }
 
     public Set<ClassLoader> getClassLoaders() {
-        return Collections.unmodifiableSet(classLoaders);
+        synchronized (instLock) {
+            return Collections.unmodifiableSet(classLoaders);
+        }
     }
 
     public abstract Environment getModelEnvironment();
