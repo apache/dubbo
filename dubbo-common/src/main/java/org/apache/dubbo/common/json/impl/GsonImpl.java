84dcbcfefeddc476a75121d30b4ceee763bb74e1
Albumen Kevin <jhq0812@gmail.com>
2023-02-11 15:57:48 +0800

    fix import

--- dubbo-common/src/main/java/org/apache/dubbo/common/json/impl/GsonImpl.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/json/impl/GsonImpl.java
@@ -21,7 +21,11 @@ import org.apache.dubbo.common.utils.ClassUtils;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.util.List;
