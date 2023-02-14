e1baaae71b8f73fd248e7348c24f28554cf1b2e6
Mengyang Tang <mengyang.t@163.com>
2023-02-15 06:57:20 +0800

    Metrics code opt: redundant, modifier, magic (#11553)
    * Remove redundant code
    
    * Optimize modifier
    
    * Optimize magic constant
    
    * Resolve conflicts
    
    * Delete empty file

--- dubbo-metrics/dubbo-metrics-default/src/main/java/org/apache/dubbo/metrics/collector/DefaultMetricsCollector.java
+++ dubbo-metrics/dubbo-metrics-default/src/main/java/org/apache/dubbo/metrics/collector/DefaultMetricsCollector.java
@@ -43,13 +43,13 @@ import static org.apache.dubbo.metrics.model.MetricsKey.APPLICATION_METRIC_INFO;
  */
 public class DefaultMetricsCollector implements MetricsCollector {
 
-    private AtomicBoolean collectEnabled = new AtomicBoolean(false);
+    private final AtomicBoolean collectEnabled = new AtomicBoolean(false);
     private final SimpleMetricsEventMulticaster eventMulticaster;
-    private MethodMetricsSampler methodSampler = new MethodMetricsSampler(this);
-    private ThreadPoolMetricsSampler threadPoolSampler = new ThreadPoolMetricsSampler(this);
+    private final MethodMetricsSampler methodSampler = new MethodMetricsSampler(this);
+    private final ThreadPoolMetricsSampler threadPoolSampler = new ThreadPoolMetricsSampler(this);
     private String applicationName;
     private ApplicationModel applicationModel;
-    private List<MetricsSampler> samplers = new ArrayList<>();
+    private final List<MetricsSampler> samplers = new ArrayList<>();
 
     public DefaultMetricsCollector() {
         this.eventMulticaster = SimpleMetricsEventMulticaster.getInstance();
@@ -66,11 +66,11 @@ public class DefaultMetricsCollector implements MetricsCollector {
         return this.applicationName;
     }
 
-    public ApplicationModel getApplicationModel(){
+    public ApplicationModel getApplicationModel() {
         return this.applicationModel;
     }
 
-    public SimpleMetricsEventMulticaster getEventMulticaster(){
+    public SimpleMetricsEventMulticaster getEventMulticaster() {
         return this.eventMulticaster;
     }
 
@@ -82,14 +82,14 @@ public class DefaultMetricsCollector implements MetricsCollector {
         return collectEnabled.get();
     }
 
-    public MethodMetricsSampler getMethodSampler(){
+    public MethodMetricsSampler getMethodSampler() {
         return this.methodSampler;
     }
 
     public void collectApplication(ApplicationModel applicationModel) {
         this.setApplicationName(applicationModel.getApplicationName());
         this.applicationModel = applicationModel;
-        applicationSampler.inc(applicationName,MetricsEvent.Type.APPLICATION_INFO);
+        applicationSampler.inc(applicationName, MetricsEvent.Type.APPLICATION_INFO);
     }
 
     @Override
@@ -106,13 +106,13 @@ public class DefaultMetricsCollector implements MetricsCollector {
         this.eventMulticaster.addListener(listener);
     }
 
-    public SimpleMetricsCountSampler<String,MetricsEvent.Type, ApplicationMetric> applicationSampler = new SimpleMetricsCountSampler<String,MetricsEvent.Type,ApplicationMetric>(){
+    public SimpleMetricsCountSampler<String, MetricsEvent.Type, ApplicationMetric> applicationSampler = new SimpleMetricsCountSampler<String, MetricsEvent.Type, ApplicationMetric>() {
         @Override
         public List<MetricSample> sample() {
             List<MetricSample> samples = new ArrayList<>();
-            this.getCount(MetricsEvent.Type.APPLICATION_INFO).filter(e->!e.isEmpty())
-                    .ifPresent(map -> map.forEach((k, v) -> samples.add(new GaugeMetricSample(APPLICATION_METRIC_INFO, k.getTags(),
-                            APPLICATION, v::get))));
+            this.getCount(MetricsEvent.Type.APPLICATION_INFO).filter(e -> !e.isEmpty())
+                .ifPresent(map -> map.forEach((k, v) -> samples.add(new GaugeMetricSample(APPLICATION_METRIC_INFO, k.getTags(),
+                    APPLICATION, v::get))));
             return samples;
         }
 
