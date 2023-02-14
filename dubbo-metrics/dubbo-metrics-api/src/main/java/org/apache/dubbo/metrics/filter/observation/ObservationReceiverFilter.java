3ad532fa0c9ddf99506d218ceb88874094c8c1bf
jojocodeX <571943037@qq.com>
2023-02-14 14:45:47 +0800

    classNotFound (#11552)

--- dubbo-metrics/dubbo-metrics-api/src/main/java/org/apache/dubbo/metrics/filter/observation/ObservationReceiverFilter.java
+++ dubbo-metrics/dubbo-metrics-api/src/main/java/org/apache/dubbo/metrics/filter/observation/ObservationReceiverFilter.java
@@ -33,7 +33,7 @@ import static org.apache.dubbo.common.constants.CommonConstants.PROVIDER;
 /**
  * A {@link Filter} that creates an {@link Observation} around the incoming message.
  */
-@Activate(group = PROVIDER, order = -1)
+@Activate(group = PROVIDER, order = -1,onClass = "io.micrometer.observation.NoopObservationRegistry")
 public class ObservationReceiverFilter implements Filter, BaseFilter.Listener, ScopeModelAware {
 
     private ObservationRegistry observationRegistry = ObservationRegistry.NOOP;
