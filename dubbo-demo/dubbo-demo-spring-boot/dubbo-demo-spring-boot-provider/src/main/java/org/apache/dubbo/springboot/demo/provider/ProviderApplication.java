cc4becdb9d3eeb48cecfc7b516f100553d43660f
shenfeng <shenfeng197@gmail.com>
2023-02-15 16:34:12 +0800

    add License and fix codestyle

--- dubbo-demo/dubbo-demo-spring-boot/dubbo-demo-spring-boot-provider/src/main/java/org/apache/dubbo/springboot/demo/provider/ProviderApplication.java
+++ dubbo-demo/dubbo-demo-spring-boot/dubbo-demo-spring-boot-provider/src/main/java/org/apache/dubbo/springboot/demo/provider/ProviderApplication.java
@@ -18,7 +18,6 @@
 package org.apache.dubbo.springboot.demo.provider;
 
 
-import io.micrometer.tracing.Tracer;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
