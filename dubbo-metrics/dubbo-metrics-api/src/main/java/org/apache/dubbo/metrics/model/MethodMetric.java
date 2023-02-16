630e3fd49642a2964646247a4851df32eaafbfd4
songxiaosheng <songxiaosheng@elastic.link>
2023-02-16 17:31:10 +0800

    :recycle: refactor consumer meter collect

--- dubbo-metrics/dubbo-metrics-api/src/main/java/org/apache/dubbo/metrics/model/MethodMetric.java
+++ dubbo-metrics/dubbo-metrics-api/src/main/java/org/apache/dubbo/metrics/model/MethodMetric.java
@@ -17,16 +17,13 @@
 
 package org.apache.dubbo.metrics.model;
 
-import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.rpc.Invocation;
-import org.apache.dubbo.rpc.Invoker;
 import org.apache.dubbo.rpc.RpcContext;
 import org.apache.dubbo.rpc.RpcInvocation;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 
 import static org.apache.dubbo.common.constants.CommonConstants.*;
 import static org.apache.dubbo.common.constants.CommonConstants.CONSUMER_SIDE;
