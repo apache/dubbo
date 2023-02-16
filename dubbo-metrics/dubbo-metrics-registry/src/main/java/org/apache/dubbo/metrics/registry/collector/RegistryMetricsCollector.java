1cc6e8e82eb1594e6d76d80b5b4a4512173f9990
x-shadow-man <1494445739@qq.com>
2023-02-16 21:35:52 +0800

    add pom

--- dubbo-metrics/dubbo-metrics-registry/src/main/java/org/apache/dubbo/metrics/registry/collector/RegistryMetricsCollector.java
+++ dubbo-metrics/dubbo-metrics-registry/src/main/java/org/apache/dubbo/metrics/registry/collector/RegistryMetricsCollector.java
@@ -58,6 +58,7 @@ public class RegistryMetricsCollector implements ApplicationMetricsCollector<Reg
         }
     }
 
+    @Override
     public boolean isCollectEnabled() {
         if (collectEnabled == null) {
             ConfigManager configManager = applicationModel.getApplicationConfigManager();
