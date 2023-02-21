70ceb218ba5d3491f076435004943dab3e635a54
suncr <suncairong@moresec.cn>
2023-02-21 12:47:16 +0800

    code style

--- dubbo-rpc/dubbo-rpc-rest/src/test/java/org/apache/dubbo/rpc/protocol/rest/rest/AnotherUserRestService.java
+++ dubbo-rpc/dubbo-rpc-rest/src/test/java/org/apache/dubbo/rpc/protocol/rest/rest/AnotherUserRestService.java
@@ -18,7 +18,12 @@ package org.apache.dubbo.rpc.protocol.rest.rest;
 
 import org.apache.dubbo.rpc.protocol.rest.User;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("u")
