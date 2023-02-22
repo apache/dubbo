9cf1bfeed80b0fb7de74ca13c0f843b39f2d00d1
jojocodeX <571943037@qq.com>
2023-02-22 22:53:33 +0800

    Run CI

--- dubbo-plugin/dubbo-spring-security/src/main/java/org/apache/dubbo/spring/security/model/SecurityScopeModelInitializer.java
+++ dubbo-plugin/dubbo-spring-security/src/main/java/org/apache/dubbo/spring/security/model/SecurityScopeModelInitializer.java
@@ -46,7 +46,6 @@ public class SecurityScopeModelInitializer implements ScopeModelInitializer {
 
     @Override
     public void initializeApplicationModel(ApplicationModel applicationModel) {
-
     }
 
     @Override
