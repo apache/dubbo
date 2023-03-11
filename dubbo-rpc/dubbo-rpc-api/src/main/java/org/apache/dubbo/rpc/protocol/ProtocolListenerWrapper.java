54084a0ef5f3008f65d291be22de4ac9864b8831
MieAh <100568933+MieAh@users.noreply.github.com>
2023-03-11 16:16:06 +0800

    remove unused import

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/protocol/ProtocolListenerWrapper.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/protocol/ProtocolListenerWrapper.java
@@ -37,7 +37,6 @@ import java.util.List;
 import static org.apache.dubbo.common.constants.CommonConstants.EXPORTER_LISTENER_KEY;
 import static org.apache.dubbo.common.constants.CommonConstants.INVOKER_LISTENER_KEY;
 import static org.apache.dubbo.common.constants.RegistryConstants.REGISTRY_CLUSTER_TYPE_KEY;
-import static org.apache.dubbo.rpc.Constants.LOCAL_PROTOCOL;
 
 /**
  * ListenerProtocol
@@ -66,9 +65,6 @@ public class ProtocolListenerWrapper implements Protocol {
         }
         List<ExporterListener> exporterListeners = ScopeModelUtil.getExtensionLoader(ExporterListener.class, invoker.getUrl().getScopeModel())
             .getActivateExtension(invoker.getUrl(), EXPORTER_LISTENER_KEY);
-        if (LOCAL_PROTOCOL.equals(invoker.getUrl().getProtocol())) {
-            exporterListeners.add(ScopeModelUtil.getExtensionLoader(ExporterListener.class,invoker.getUrl().getScopeModel()).getExtension(LOCAL_PROTOCOL));
-        }
         return new ListenerExporterWrapper<T>(protocol.export(invoker),
                 Collections.unmodifiableList(exporterListeners));
     }
