234f1c64abb34cfc49bf233cbf37a4f6e2953857
Albumen Kevin <jhq0812@gmail.com>
2023-03-12 10:26:18 +0800

    Fix lock acquire blocked

--- dubbo-common/src/main/java/org/apache/dubbo/rpc/model/ModuleModel.java
+++ dubbo-common/src/main/java/org/apache/dubbo/rpc/model/ModuleModel.java
@@ -32,6 +32,7 @@ import org.apache.dubbo.config.context.ModuleConfigManager;
 
 import java.util.HashMap;
 import java.util.Set;
+import java.util.concurrent.locks.Lock;
 
 /**
  * Model of a service module
@@ -170,6 +171,11 @@ public class ModuleModel extends ScopeModel {
 
     public void setDeployer(ModuleDeployer deployer) {
         this.deployer = deployer;
+    }
+
+    @Override
+    protected Lock acquireDestroyLock() {
+        return getApplicationModel().getFrameworkModel().acquireDestroyLock();
     }
 
     /**
