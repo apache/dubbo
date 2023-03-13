ce4d6b93cf2b8f7906b6ec58aa6d0744bf645349
crazyhzm <crazyhzm@gmail.com>
2023-03-13 10:46:22 +0800

    remove useless import
    Signed-off-by: crazyhzm <crazyhzm@gmail.com>

--- dubbo-native/src/main/java/org/apache/dubbo/aot/generate/NativeClassSourceWriter.java
+++ dubbo-native/src/main/java/org/apache/dubbo/aot/generate/NativeClassSourceWriter.java
@@ -25,7 +25,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 
