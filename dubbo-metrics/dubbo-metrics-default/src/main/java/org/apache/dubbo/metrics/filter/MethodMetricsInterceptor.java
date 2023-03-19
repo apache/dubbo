8db8928f3ba825404c583e33b07165cff89eb2c5
songxiaosheng <songxiaosheng@elastic.link>
2023-03-19 22:23:29 +0800

    :bug: if it has exception processing request need end too.

--- dubbo-metrics/dubbo-metrics-default/src/main/java/org/apache/dubbo/metrics/filter/MethodMetricsInterceptor.java
+++ dubbo-metrics/dubbo-metrics-default/src/main/java/org/apache/dubbo/metrics/filter/MethodMetricsInterceptor.java
@@ -87,12 +87,7 @@ public class MethodMetricsInterceptor {
             sampler.incOnEvent(invocation, eventType.getNameByType(side));
         }
 
-        if (throwable instanceof RpcException && ((RpcException) throwable).isBiz()) {
-            onCompleted(invocation);
-        }else{
-            rtTime(invocation);
-        }
-
+        onCompleted(invocation);
         sampler.incOnEvent(invocation, MetricsEvent.Type.TOTAL_FAILED.getNameByType(side));
     }
 
