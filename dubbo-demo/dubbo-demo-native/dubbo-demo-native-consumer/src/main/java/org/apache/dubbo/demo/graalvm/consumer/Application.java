c0dd8650acdd509b07797751c4829b7fe78acc73
namelessssssssssss <1544669126@qq.com>
2023-04-01 22:54:52 +0800

    Update demos (#11970)

--- dubbo-demo/dubbo-demo-native/dubbo-demo-native-consumer/src/main/java/org/apache/dubbo/demo/graalvm/consumer/Application.java
+++ dubbo-demo/dubbo-demo-native/dubbo-demo-native-consumer/src/main/java/org/apache/dubbo/demo/graalvm/consumer/Application.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.apache.dubbo.demo.graalvm.consumer;
+package org.apache.dubbo.demo.graalvm.consumer;
 
 import org.apace.dubbo.graalvm.demo.DemoService;
 import org.apache.dubbo.common.constants.CommonConstants;
