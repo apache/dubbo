cd074252bf915c9e7c30486d3647def33cd1b6f0
suncairong163 <suncairong163@163.com>
2023-04-11 22:14:15 +0800

    fix License format

--- dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-rest-provider/src/main/java/org/apache/dubbo/samples/rest/api/impl/HttpMethodServiceImpl.java
+++ dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-rest-provider/src/main/java/org/apache/dubbo/samples/rest/api/impl/HttpMethodServiceImpl.java
@@ -1,4 +1,4 @@
-package org.apache.dubbo.samples.rest.api.impl;/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,7 +14,7 @@ package org.apache.dubbo.samples.rest.api.impl;/*
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+package org.apache.dubbo.samples.rest.api.impl;
 
 import org.apache.dubbo.samples.rest.api.HttpMethodService;
 import org.springframework.stereotype.Service;
