6bd53ac7178020de54f8efe726227dd64b51587e
suncairong163 <suncairong163@163.com>
2023-04-25 00:48:08 +0800

    add jaxrs rest demo

---
+++ dubbo-demo/dubbo-demo-xml/dubbo-demo-jaxrs-rest-consumer/src/main/java/org/apache/dubbo/demo/rest/api/config/DubboConfig.java
@@ -0,0 +1,7 @@
+package org.apache.dubbo.demo.rest.api.config;
+
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
+
+@DubboComponentScan("org.apache.dubbo.demo.rest")
+public class DubboConfig {
+}
