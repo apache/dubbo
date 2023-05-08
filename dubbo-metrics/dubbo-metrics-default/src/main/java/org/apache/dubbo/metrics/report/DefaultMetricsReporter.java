01500a14dcdedf1164f6ec68464c9a18cf281c12
hujun <hujun3@xiaomi.com>
2023-05-08 12:40:53 +0800

    check style

--- dubbo-metrics/dubbo-metrics-default/src/main/java/org/apache/dubbo/metrics/report/DefaultMetricsReporter.java
+++ dubbo-metrics/dubbo-metrics-default/src/main/java/org/apache/dubbo/metrics/report/DefaultMetricsReporter.java
@@ -1,9 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.dubbo.metrics.report;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.Timer;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.rpc.model.ApplicationModel;
 
@@ -13,18 +31,18 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class DefaultMetricsReporter extends AbstractMetricsReporter {
-    
+
     SimpleMeterRegistry meterRegistry = new SimpleMeterRegistry();
-    
+
     protected DefaultMetricsReporter(URL url, ApplicationModel applicationModel) {
         super(url, applicationModel);
     }
-    
+
     @Override
     public String getResponse() {
         return null;
     }
-    
+
     @Override
     public String getResponseWithName(String metricsName) {
         //name->tags->value
@@ -56,19 +74,19 @@ public class DefaultMetricsReporter extends AbstractMetricsReporter {
             sb.append("##metrics name=").append(name).append(System.lineSeparator());
             tags.forEach((tag, value) -> {
                 sb.append(" #tags:").append(tag).append(System.lineSeparator()).append(" #value：").append(value)
-                        .append(System.lineSeparator());
+                    .append(System.lineSeparator());
             });
         });
         return sb.toString();
     }
-    
+
     @Override
     protected void doInit() {
         addMeterRegistry(meterRegistry);
     }
-    
+
     @Override
     protected void doDestroy() {
-    
+
     }
 }
--- dubbo-metrics/dubbo-metrics-default/src/main/java/org/apache/dubbo/metrics/report/DefaultMetricsReporterCmd.java
+++ dubbo-metrics/dubbo-metrics-default/src/main/java/org/apache/dubbo/metrics/report/DefaultMetricsReporterCmd.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.dubbo.metrics.report;
 
 import org.apache.dubbo.common.utils.JsonUtils;
@@ -17,19 +34,19 @@ import java.util.Optional;
 
 @Cmd(name = "metrics_default", summary = "reuse qos report")
 public class DefaultMetricsReporterCmd implements BaseCommand {
-    
+
     public FrameworkModel frameworkModel;
-    
+
     public DefaultMetricsReporterCmd(FrameworkModel frameworkModel) {
         this.frameworkModel = frameworkModel;
     }
-    
+
     @Override
     public String execute(CommandContext commandContext, String[] args) {
         List<ApplicationModel> models = frameworkModel.getApplicationModels();
         String result = "There is no application with data";
         if (notSpecifyApplication(args)) {
-            result = useFirst(models, result,null);
+            result = useFirst(models, result, null);
         } else if (args.length == 1) {
             result = specifyApplication(args[0], models, null);
         } else if (args.length == 2) {
@@ -37,11 +54,11 @@ public class DefaultMetricsReporterCmd implements BaseCommand {
         }
         return result;
     }
-    
+
     private boolean notSpecifyApplication(String[] args) {
         return args == null || args.length == 0;
     }
-    
+
     private String useFirst(List<ApplicationModel> models, String result, String metricsName) {
         for (ApplicationModel model : models) {
             String current = getResponseByApplication(model, metricsName);
@@ -52,25 +69,25 @@ public class DefaultMetricsReporterCmd implements BaseCommand {
         }
         return result;
     }
-    
+
     private String specifyApplication(String appName, List<ApplicationModel> models, String metricsName) {
         if ("application_all".equals(appName)) {
             return allApplication(models);
         } else {
-            return specifySingleApplication(appName, models,metricsName);
+            return specifySingleApplication(appName, models, metricsName);
         }
     }
-    
+
     private String specifySingleApplication(String appName, List<ApplicationModel> models, String metricsName) {
         Optional<ApplicationModel> modelOptional = models.stream()
-                .filter(applicationModel -> appName.equals(applicationModel.getApplicationName())).findFirst();
+            .filter(applicationModel -> appName.equals(applicationModel.getApplicationName())).findFirst();
         if (modelOptional.isPresent()) {
-            return getResponseByApplication(modelOptional.get(),metricsName);
+            return getResponseByApplication(modelOptional.get(), metricsName);
         } else {
             return "Not exist application: " + appName;
         }
     }
-    
+
     private String allApplication(List<ApplicationModel> models) {
         Map<String, String> appResultMap = new HashMap<>();
         for (ApplicationModel model : models) {
@@ -78,7 +95,7 @@ public class DefaultMetricsReporterCmd implements BaseCommand {
         }
         return JsonUtils.toJson(appResultMap);
     }
-    
+
     private String getResponseByApplication(ApplicationModel applicationModel, String metricsName) {
         String response = "MetricsReporter not init";
         MetricsReporter metricsReporter = applicationModel.getBeanFactory().getBean(MetricsReporter.class);
@@ -88,8 +105,8 @@ public class DefaultMetricsReporterCmd implements BaseCommand {
         }
         return response;
     }
-    
-    
+
+
     private static long getLineNumber(String content) {
         LineNumberReader lnr = new LineNumberReader(new CharArrayReader(content.toCharArray()));
         try {
