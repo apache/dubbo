563a0e472ab50d2d697e2f0415c80b68ad6b17bd
Albumen Kevin <jhq0812@gmail.com>
2023-06-29 10:31:36 +0800

    Enable spring bean autowired to get scope model in advance (#12608)
    * Enable spring bean autowired to get scope model in advance
    
    * Fix uts
    
    * Fix uts

--- dubbo-config/dubbo-config-spring/src/main/java/org/apache/dubbo/config/spring/beans/factory/annotation/ServiceAnnotationPostProcessor.java
+++ dubbo-config/dubbo-config-spring/src/main/java/org/apache/dubbo/config/spring/beans/factory/annotation/ServiceAnnotationPostProcessor.java
@@ -428,6 +428,7 @@ public class ServiceAnnotationPostProcessor implements BeanDefinitionRegistryPos
         BeanDefinitionBuilder builder = rootBeanDefinition(ServiceBean.class);
 
         AbstractBeanDefinition beanDefinition = builder.getBeanDefinition();
+        beanDefinition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_CONSTRUCTOR);
 
         MutablePropertyValues propertyValues = beanDefinition.getPropertyValues();
 
