b316f2bc7b2508600078be0a3bb97ab3dbeecf96
Albumen Kevin <jhq0812@gmail.com>
2023-07-02 15:43:48 +0800

    Fix server shutdown await timeout

--- dubbo-remoting/dubbo-remoting-netty4/src/main/java/org/apache/dubbo/remoting/transport/netty4/NettyPortUnificationServer.java
+++ dubbo-remoting/dubbo-remoting-netty4/src/main/java/org/apache/dubbo/remoting/transport/netty4/NettyPortUnificationServer.java
@@ -178,7 +178,7 @@ public class NettyPortUnificationServer extends AbstractPortUnificationServer {
 
         try {
             if (bootstrap != null) {
-                long timeout = serverShutdownTimeoutMills;
+                long timeout = ConfigurationUtils.reCalShutdownTime(serverShutdownTimeoutMills);
                 long quietPeriod = Math.min(2000L, timeout);
                 Future<?> bossGroupShutdownFuture = bossGroup.shutdownGracefully(quietPeriod,
                     timeout, MILLISECONDS);
