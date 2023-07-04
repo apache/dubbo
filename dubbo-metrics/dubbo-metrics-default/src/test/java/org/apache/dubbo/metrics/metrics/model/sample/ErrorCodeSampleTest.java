8e9dace26f104bbe8547d0818b79dbddd458d750
namelessssssssssss <100946116+namelessssssssssss@users.noreply.github.com>
2023-07-04 16:25:54 +0800

    Update ErrorCodeSampleTest.java

--- dubbo-metrics/dubbo-metrics-default/src/test/java/org/apache/dubbo/metrics/metrics/model/sample/ErrorCodeSampleTest.java
+++ dubbo-metrics/dubbo-metrics-default/src/test/java/org/apache/dubbo/metrics/metrics/model/sample/ErrorCodeSampleTest.java
@@ -62,8 +62,8 @@ public class ErrorCodeSampleTest {
 
         List<MetricSample> samples = defaultMetricsCollector.collect();
 
-        Assert.assertTrue(samples.size() == 4,"样本数量错误");
-        samples.forEach(metricSample ->  Assert.assertTrue(((AtomicLong)((CounterMetricSample<?>)metricSample).getValue()).get() == 2L,"样本计数错误"));
+        Assert.assertTrue(samples.size() == 4,"Wrong number of samples.");
+        samples.forEach(metricSample ->  Assert.assertTrue(((AtomicLong)((CounterMetricSample<?>)metricSample).getValue()).get() == 2L,"Sample count error."));
 
         System.out.println(samples);
     }
