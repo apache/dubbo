cdc7c3d19ada0e70a9bb227d5c0e1ae4fb3aa28e
crazyhzm <crazyhzm@gmail.com>
2023-07-25 21:36:38 +0800

    revert TpsLimitFilter activate annotation value
    Signed-off-by: crazyhzm <crazyhzm@gmail.com>

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/filter/TpsLimitFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/filter/TpsLimitFilter.java
@@ -29,6 +29,8 @@ import org.apache.dubbo.rpc.filter.tps.DefaultTPSLimiter;
 import org.apache.dubbo.rpc.filter.tps.TPSLimiter;
 import org.apache.dubbo.rpc.support.RpcUtils;
 
+import static org.apache.dubbo.rpc.Constants.TPS_LIMIT_RATE_KEY;
+
 /**
  * TpsLimitFilter limit the TPS (transaction per second) for all method of a service or a particular method.
  * Service or method url can define <b>tps</b> or <b>tps.interval</b> to control this control.It use {@link DefaultTPSLimiter}
@@ -36,7 +38,7 @@ import org.apache.dubbo.rpc.support.RpcUtils;
  * if invocation count exceed the configured <b>tps</b> value (default is -1 which means unlimited) then invocation will get
  * RpcException.
  */
-@Activate(group = CommonConstants.PROVIDER)
+@Activate(group = CommonConstants.PROVIDER, value = TPS_LIMIT_RATE_KEY)
 public class TpsLimitFilter implements Filter {
 
     private final TPSLimiter tpsLimiter = new DefaultTPSLimiter();
