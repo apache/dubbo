30133e2ff9c9f00b6337d4567f42ebacc0c4fb7b
hjyp <2594297576@qq.com>
2023-08-02 18:19:57 +0800

    update color class

--- dubbo-common/src/test/java/org/apache/dubbo/common/utils/json/Color.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/utils/json/Color.java
@@ -2,15 +2,15 @@ package org.apache.dubbo.common.utils.json;
 
 public enum Color {
     RED("红色", 1), GREEN("绿色", 2), BLANK("白色", 3), YELLO("黄色", 4);
-    // 成员变量
+
     private String name;
     private int index;
-    // 构造方法
+
     private Color(String name, int index) {
         this.name = name;
         this.index = index;
     }
-    // 普通方法
+
     public static String getName(int index) {
         for (Color c : Color.values()) {
             if (c.getIndex() == index) {
@@ -19,7 +19,7 @@ public enum Color {
         }
         return null;
     }
-    // get set 方法
+
     public String getName() {
         return name;
     }
