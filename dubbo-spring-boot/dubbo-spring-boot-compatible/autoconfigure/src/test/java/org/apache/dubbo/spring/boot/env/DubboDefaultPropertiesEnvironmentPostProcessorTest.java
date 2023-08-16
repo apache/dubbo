7290ae59bc69577b5b902dc0f6e161eb4c74db6b
xixingya <2679431923@qq.com>
2023-08-16 21:01:28 +0800

    remove spring default qos-enable (#12861)
    fix #12779

--- dubbo-spring-boot/dubbo-spring-boot-compatible/autoconfigure/src/test/java/org/apache/dubbo/spring/boot/env/DubboDefaultPropertiesEnvironmentPostProcessorTest.java
+++ dubbo-spring-boot/dubbo-spring-boot-compatible/autoconfigure/src/test/java/org/apache/dubbo/spring/boot/env/DubboDefaultPropertiesEnvironmentPostProcessorTest.java
@@ -53,7 +53,7 @@ public class DubboDefaultPropertiesEnvironmentPostProcessorTest {
         PropertySource defaultPropertySource = propertySources.get("defaultProperties");
         Assert.assertNotNull(defaultPropertySource);
         Assert.assertEquals("true", defaultPropertySource.getProperty("dubbo.config.multiple"));
-        Assert.assertEquals("true", defaultPropertySource.getProperty("dubbo.application.qos-enable"));
+        // Assert.assertEquals("true", defaultPropertySource.getProperty("dubbo.application.qos-enable"));
 
         // Case 2 :  Only set property "spring.application.name"
         environment.setProperty("spring.application.name", "demo-dubbo-application");
