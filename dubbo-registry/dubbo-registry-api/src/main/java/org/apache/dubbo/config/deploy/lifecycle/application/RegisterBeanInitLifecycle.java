c7cb9e2d77dc1191129f9f30c0bc7e93c454c3bd
namelessssssssssss <1544669126@qq.com>
2023-09-20 11:05:21 +0800

    Change MetadataUpdater init process

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/config/deploy/lifecycle/application/RegisterBeanInitLifecycle.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/config/deploy/lifecycle/application/RegisterBeanInitLifecycle.java
@@ -18,6 +18,7 @@ package org.apache.dubbo.config.deploy.lifecycle.application;
 
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.config.deploy.context.ApplicationContext;
+import org.apache.dubbo.registry.ApplicationMetadataUpdater;
 import org.apache.dubbo.registry.DefaultMetadataUpdater;
 
 @Activate(order = -10000)
@@ -30,6 +31,6 @@ public class RegisterBeanInitLifecycle implements ApplicationLifecycle{
 
     @Override
     public void initialize(ApplicationContext context) {
-        context.getModel().getBeanFactory().getOrRegisterBean(DefaultMetadataUpdater.class);
+        context.getModel().getBeanFactory().getOrRegisterBean(ApplicationMetadataUpdater.class,clz-> new DefaultMetadataUpdater());
     }
 }
