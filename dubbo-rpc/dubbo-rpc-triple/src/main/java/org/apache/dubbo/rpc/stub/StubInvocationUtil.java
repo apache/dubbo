b0f1317673c96cd27a2024e476468a9779e13cbf
namelessssssssssss <1544669126@qq.com>
2023-10-17 16:41:43 +0800

    Fix StubMethodDescriptor cover issue

--- dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/stub/StubInvocationUtil.java
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/stub/StubInvocationUtil.java
@@ -24,6 +24,8 @@ import org.apache.dubbo.rpc.TriRpcStatus;
 import org.apache.dubbo.rpc.model.MethodDescriptor;
 import org.apache.dubbo.rpc.proxy.InvocationUtil;
 
+import java.lang.reflect.Type;
+
 public class StubInvocationUtil {
 
     public static <T, R> R unaryCall(Invoker<?> invoker, MethodDescriptor methodDescriptor,
@@ -54,12 +56,27 @@ public class StubInvocationUtil {
 
     private static Object call(Invoker<?> invoker, MethodDescriptor methodDescriptor,
         Object[] arguments) {
+        //Due to the design of ServiceDescriptor, the MethodDescriptor with the same name will override each other in ServiceDescriptor.
+        //The sync and async version of MethodDescriptor share the same method name, and usually the sync version will replace another one
+        //(Because it's declared after the async version).
+        //So this RpcInvocation always uses MethodDescriptor of sync version to initialize.
         RpcInvocation rpcInvocation = new RpcInvocation(invoker.getUrl().getServiceModel(),
             methodDescriptor.getMethodName(), invoker.getInterface().getName(),
             invoker.getUrl().getProtocolServiceKey(), methodDescriptor.getParameterClasses(),
             arguments);
+
         //When there are multiple MethodDescriptors with the same method name, the return type will be wrong
         rpcInvocation.setReturnType(methodDescriptor.getReturnClass());
+
+        Type[] returnTypes = methodDescriptor.getReturnTypes();
+        //If this MethodDescriptor from an async method, its return class should be CompletableFuture.
+        //If this MethodDescriptor from a sync method, its return class is real return type.
+        //The definition of returnTypes:
+        //returnTypes[0], the concrete type.
+        //returnTypes[1], the parameterized type.
+        returnTypes[0] = methodDescriptor.getReturnClass();
+        rpcInvocation.setReturnTypes(returnTypes);
+
         try {
             return InvocationUtil.invoke(invoker, rpcInvocation);
         } catch (Throwable e) {
