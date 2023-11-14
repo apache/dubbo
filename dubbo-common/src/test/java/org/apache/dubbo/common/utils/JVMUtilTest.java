2a7d17660a7f1abd14bd4f0fd8fbb8de854ae27d
Linchen Xu <592789211@qq.com>
2023-11-15 02:02:13 +0800

    add tests in JVMUtilTest, fix output in JVMUtil

--- dubbo-common/src/test/java/org/apache/dubbo/common/utils/JVMUtilTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/utils/JVMUtilTest.java
@@ -16,4 +16,60 @@
  */
 package org.apache.dubbo.common.utils;
 
-class JVMUtilTest {}
+import org.apache.dubbo.common.logger.ErrorTypeAwareLogger;
+import org.apache.dubbo.common.logger.LoggerFactory;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import static org.apache.dubbo.common.constants.CommonConstants.DUBBO_JSTACK_MAXLINE;
+import static org.apache.dubbo.common.constants.CommonConstants.OS_NAME_KEY;
+import static org.apache.dubbo.common.constants.CommonConstants.OS_WIN_PREFIX;
+import static org.apache.dubbo.common.constants.LoggerCodeConstants.COMMON_UNEXPECTED_CREATE_DUMP;
+import static org.apache.dubbo.common.utils.JVMUtil.jstack;
+
+class JVMUtilTest {
+
+    protected static final ErrorTypeAwareLogger logger = LoggerFactory.getErrorTypeAwareLogger(JVMUtilTest.class);
+
+    @Test
+    void testPrintStackTrace() {
+        test(10);
+    }
+
+    @Test
+    void testPrintStackTraceWithSpecifiedDepth() {
+        test(-1);
+    }
+
+    private void test(Integer depth) {
+        SimpleDateFormat sdf;
+        String os = System.getProperty(OS_NAME_KEY).toLowerCase();
+        // window system don't support ":" in file name
+        if (os.contains(OS_WIN_PREFIX)) {
+            sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss .SSS");
+        } else {
+            sdf = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss .SSS");
+        }
+        String dateStr = sdf.format(new Date());
+
+        String property = System.getProperty(DUBBO_JSTACK_MAXLINE);
+        System.setProperty(DUBBO_JSTACK_MAXLINE, depth.toString());
+        try (FileOutputStream jStackStream =
+                new FileOutputStream(new File("/tmp", "Dubbo_JStack.log" + "." + dateStr))) {
+            jstack(jStackStream);
+        } catch (Exception e) {
+            logger.error(COMMON_UNEXPECTED_CREATE_DUMP, "", "", "dump jStack error", e);
+        } finally {
+            if (property == null) {
+                System.clearProperty(DUBBO_JSTACK_MAXLINE);
+            } else {
+                System.setProperty(DUBBO_JSTACK_MAXLINE, property);
+            }
+        }
+    }
+}
