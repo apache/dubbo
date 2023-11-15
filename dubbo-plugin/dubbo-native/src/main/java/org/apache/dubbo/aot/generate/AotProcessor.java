44558cf7024b1aae1652f071584af7c193e2ec86
hjyp <2594297576@qq.com>
2023-11-15 21:59:59 +0800

    unify code style

--- dubbo-plugin/dubbo-native/src/main/java/org/apache/dubbo/aot/generate/AotProcessor.java
+++ dubbo-plugin/dubbo-native/src/main/java/org/apache/dubbo/aot/generate/AotProcessor.java
@@ -43,22 +43,22 @@ public class AotProcessor {
 
         ResourceConfigMetadataRepository resourceRepository = new ResourceConfigMetadataRepository();
         resourceRepository.registerIncludesPatterns(
-            ResourceScanner.INSTANCE.distinctSpiResource().toArray(new String[]{}));
+                ResourceScanner.INSTANCE.distinctSpiResource().toArray(new String[] {}));
         resourceRepository.registerIncludesPatterns(
-            ResourceScanner.INSTANCE.distinctSecurityResource().toArray(new String[]{}));
+                ResourceScanner.INSTANCE.distinctSecurityResource().toArray(new String[] {}));
         writer.writeResourceConfig(resourceRepository);
 
         ReflectConfigMetadataRepository reflectRepository = new ReflectConfigMetadataRepository();
         reflectRepository
-            .registerSpiExtensionType(new ArrayList<>(ClassSourceScanner.INSTANCE
-                .distinctSpiExtensionClasses(ResourceScanner.INSTANCE.distinctSpiResource())
-                .values()))
-            .registerAdaptiveType(new ArrayList<>(
-                ClassSourceScanner.INSTANCE.adaptiveClasses().values()))
-            .registerBeanType(ClassSourceScanner.INSTANCE.scopeModelInitializer())
-            .registerConfigType(ClassSourceScanner.INSTANCE.configClasses())
-            .registerConfigType(getCustomClasses())
-            .registerTypeDescriber(getTypes());
+                .registerSpiExtensionType(new ArrayList<>(ClassSourceScanner.INSTANCE
+                        .distinctSpiExtensionClasses(ResourceScanner.INSTANCE.distinctSpiResource())
+                        .values()))
+                .registerAdaptiveType(new ArrayList<>(
+                        ClassSourceScanner.INSTANCE.adaptiveClasses().values()))
+                .registerBeanType(ClassSourceScanner.INSTANCE.scopeModelInitializer())
+                .registerConfigType(ClassSourceScanner.INSTANCE.configClasses())
+                .registerConfigType(getCustomClasses())
+                .registerTypeDescriber(getTypes());
         writer.writeReflectionConfig(reflectRepository);
 
         ProxyConfigMetadataRepository proxyRepository = new ProxyConfigMetadataRepository();
@@ -69,22 +69,22 @@ public class AotProcessor {
     private static List<TypeDescriber> getTypes() {
         List<TypeDescriber> typeDescribers = new ArrayList<>();
         FrameworkModel.defaultModel()
-            .defaultApplication()
-            .getExtensionLoader(ReflectionTypeDescriberRegistrar.class)
-            .getSupportedExtensionInstances()
-            .forEach(reflectionTypeDescriberRegistrar -> {
-                List<TypeDescriber> describers = new ArrayList<>();
-                try {
-                    describers = reflectionTypeDescriberRegistrar.getTypeDescribers();
-                } catch (Throwable e) {
-                    // The ReflectionTypeDescriberRegistrar implementation classes are shaded, causing some unused
-                    // classes to be loaded.
-                    // When loading a dependent class may appear that cannot be found, it does not affect.
-                    // ignore
-                }
+                .defaultApplication()
+                .getExtensionLoader(ReflectionTypeDescriberRegistrar.class)
+                .getSupportedExtensionInstances()
+                .forEach(reflectionTypeDescriberRegistrar -> {
+                    List<TypeDescriber> describers = new ArrayList<>();
+                    try {
+                        describers = reflectionTypeDescriberRegistrar.getTypeDescribers();
+                    } catch (Throwable e) {
+                        // The ReflectionTypeDescriberRegistrar implementation classes are shaded, causing some unused
+                        // classes to be loaded.
+                        // When loading a dependent class may appear that cannot be found, it does not affect.
+                        // ignore
+                    }
 
-                typeDescribers.addAll(describers);
-            });
+                    typeDescribers.addAll(describers);
+                });
 
         return typeDescribers;
     }
@@ -92,18 +92,18 @@ public class AotProcessor {
     private static List<JdkProxyDescriber> getProxyDescribers() {
         List<JdkProxyDescriber> jdkProxyDescribers = new ArrayList<>();
         FrameworkModel.defaultModel()
-            .defaultApplication()
-            .getExtensionLoader(ProxyDescriberRegistrar.class)
-            .getSupportedExtensionInstances()
-            .forEach(reflectionTypeDescriberRegistrar -> {
-                jdkProxyDescribers.addAll(reflectionTypeDescriberRegistrar.getJdkProxyDescribers());
-            });
+                .defaultApplication()
+                .getExtensionLoader(ProxyDescriberRegistrar.class)
+                .getSupportedExtensionInstances()
+                .forEach(reflectionTypeDescriberRegistrar -> {
+                    jdkProxyDescribers.addAll(reflectionTypeDescriberRegistrar.getJdkProxyDescribers());
+                });
 
         return jdkProxyDescribers;
     }
 
     private static List<Class<?>> getCustomClasses() {
-        Class<?>[] configClasses = new Class[]{
+        Class<?>[] configClasses = new Class[] {
             CommonConstants.SystemProperty.class,
             CommonConstants.ThirdPartyProperty.class,
             CommonConstants.DubboProperty.class
