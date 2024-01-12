3c0f6bf62359e427eddc7e7fbf98739c1e4ae354
jianbin <jianbin@apache.org>
2024-01-12 16:14:19 +0800

    DefaultProviderFirstParams add THREADPOOL_KEY

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/support/DefaultProviderFirstParams.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/support/DefaultProviderFirstParams.java
@@ -34,7 +34,8 @@ import static org.apache.dubbo.common.constants.CommonConstants.TIMESTAMP_KEY;
 public class DefaultProviderFirstParams implements ProviderFirstParams {
     private static final Set<String> PARAMS = Collections.unmodifiableSet(new HashSet<String>() {
         {
-            addAll(Arrays.asList(RELEASE_KEY, DUBBO_VERSION_KEY, METHODS_KEY, TIMESTAMP_KEY, TAG_KEY, THREADPOOL_KEY, EXECUTOR_MANAGEMENT_MODE));
+            addAll(Arrays.asList(RELEASE_KEY, DUBBO_VERSION_KEY, METHODS_KEY, TIMESTAMP_KEY, TAG_KEY, THREADPOOL_KEY,
+                EXECUTOR_MANAGEMENT_MODE));
         }
     });
 
