4daff4297ac6add0de4d2f525a9f7175e44e7367
icodening <cleverbn96@gmail.com>
2024-03-21 22:09:11 +0800

    fix memory leak of tri protocol

--- dubbo-remoting/dubbo-remoting-http12/src/main/java/org/apache/dubbo/remoting/http12/netty4/h2/NettyHttp2ProtocolSelectorHandler.java
+++ dubbo-remoting/dubbo-remoting-http12/src/main/java/org/apache/dubbo/remoting/http12/netty4/h2/NettyHttp2ProtocolSelectorHandler.java
@@ -71,8 +71,7 @@ public class NettyHttp2ProtocolSelectorHandler extends SimpleChannelInboundHandl
         ChannelPipeline pipeline = ctx.pipeline();
         Http2TransportListener http2TransportListener = factory.newInstance(h2StreamChannel, url, frameworkModel);
         ctx.channel().closeFuture().addListener(future -> http2TransportListener.onStreamClosed());
-        pipeline.addLast(
-                new NettyHttp2FrameHandler(h2StreamChannel, http2TransportListener));
+        pipeline.addLast(new NettyHttp2FrameHandler(h2StreamChannel, http2TransportListener));
         pipeline.remove(this);
         ctx.fireChannelRead(metadata);
     }
