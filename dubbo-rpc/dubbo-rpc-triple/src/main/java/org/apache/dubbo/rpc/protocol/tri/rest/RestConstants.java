96b82c288995fa8bcb45eb2c9cb290794125fe03
liu <a2506991786@qq.com>
2024-04-08 12:41:06 +0800

    feat(): add cors key

--- dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/rest/RestConstants.java
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/rest/RestConstants.java
@@ -48,6 +48,22 @@ public final class RestConstants {
     public static final String PRODUCIBLE_MEDIA_TYPES_ATTRIBUTE =
             "org.springframework.web.servlet.HandlerMapping.producibleMediaTypes";
 
+    /* Cors Config */
+    public static final String VARY = "Vary";
+    public static final String ORIGIN = "Origin";
+    public static final String ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
+    public static final String ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
+    public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    public static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+    public static final String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
+    public static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    public static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    public static final String HTTP = "http";
+    public static final String HTTPS = "https";
+    public static final String WS = "ws";
+    public static final String WSS = "wss";
+
     /* Configuration Key */
     public static final String CONFIG_PREFIX = "dubbo.rpc.rest.";
     public static final String MAX_BODY_SIZE_KEY = CONFIG_PREFIX + "max-body-size";
