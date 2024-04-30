60dbdc03d3908593945a92392bcfc5f0f8b59c19
fanlobu <2840697550@qq.com>
2024-04-30 23:30:31 +0800

    fix():fix codestyle

--- dubbo-plugin/dubbo-rest-jaxrs/src/main/java/org/apache/dubbo/rpc/protocol/tri/rest/support/jaxrs/JaxrsParamConverter.java
+++ dubbo-plugin/dubbo-rest-jaxrs/src/main/java/org/apache/dubbo/rpc/protocol/tri/rest/support/jaxrs/JaxrsParamConverter.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.dubbo.rpc.protocol.tri.rest.support.jaxrs;
 
 import org.apache.dubbo.common.extension.ExtensionDirector;
@@ -18,25 +34,23 @@ import java.util.Map;
 @SuppressWarnings({"rawtypes"})
 public class JaxrsParamConverter {
 
-    private final Map<Pair<Class<?>, Class<?>>, ParamConverter> cacheConverter =
-            CollectionUtils.newConcurrentHashMap();
-
+    private final Map<Pair<Class<?>, Class<?>>, ParamConverter> cacheConverter = CollectionUtils.newConcurrentHashMap();
 
     private final List<ParamConverter> converters;
 
     JaxrsParamConverter(FrameworkModel frameworkModel) {
-        this.converters= getExtensionLoader(ParamConverter.class,frameworkModel).getActivateExtensions();
+        this.converters =
+                getExtensionLoader(ParamConverter.class, frameworkModel).getActivateExtensions();
     }
 
-
     boolean canConvert(Class<?> sourceType, Class<?> targetType) {
-        return getConvert(sourceType,targetType) != null;
+        return getConvert(sourceType, targetType) != null;
     }
 
     @SuppressWarnings("unchecked")
     Object convert(Class<?> sourceType, Class<?> targetType, Object value) {
         ParamConverter paramConverter = getConvert(sourceType, targetType);
-        if(paramConverter == null){
+        if (paramConverter == null) {
             return null;
         }
 
@@ -50,13 +64,13 @@ public class JaxrsParamConverter {
         return result;
     }
 
-    private  ParamConverter getConvert(Class<?> sourceType, Class<?> targetType){
+    private ParamConverter getConvert(Class<?> sourceType, Class<?> targetType) {
         ParamConverter converter = cacheConverter.get(Pair.of(sourceType, targetType));
         if (converter != null) {
             return converter;
         }
 
-        return cacheConverter.computeIfAbsent(Pair.of(sourceType,targetType),k->{
+        return cacheConverter.computeIfAbsent(Pair.of(sourceType, targetType), k -> {
             for (ParamConverter paramConverter : converters) {
                 Class<?> supportType = TypeUtils.getSuperGenericType(paramConverter.getClass(), 0);
                 if (supportType == null) {
@@ -70,19 +84,19 @@ public class JaxrsParamConverter {
         });
     }
 
-
-    private  <T> ExtensionLoader<T> getExtensionLoader(Class<T> converterClass,FrameworkModel frameworkModel){
+    private <T> ExtensionLoader<T> getExtensionLoader(Class<T> converterClass, FrameworkModel frameworkModel) {
         ExtensionLoader<T> instance = null;
         try {
             Class<ExtensionLoader> clazz = ExtensionLoader.class;
-            Constructor<ExtensionLoader> constructor = clazz.getDeclaredConstructor(Class.class, ExtensionDirector.class, ScopeModel.class);
+            Constructor<ExtensionLoader> constructor =
+                    clazz.getDeclaredConstructor(Class.class, ExtensionDirector.class, ScopeModel.class);
             constructor.setAccessible(true);
             instance = constructor.newInstance(converterClass, frameworkModel.getExtensionDirector(), frameworkModel);
 
         } catch (NoSuchMethodException
-                 | InstantiationException
-                 | IllegalAccessException
-                 | InvocationTargetException e) {
+                | InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
         return instance;
