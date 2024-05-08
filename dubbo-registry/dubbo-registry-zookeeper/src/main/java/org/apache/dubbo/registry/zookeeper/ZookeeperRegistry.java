96298d860fd7d40a12e561b85dabb33903b84e6e
walkinggo <40360529+walkinggo@users.noreply.github.com>
2024-05-08 21:01:42 +0800

    try to continue the string without warn.

--- dubbo-registry/dubbo-registry-zookeeper/src/main/java/org/apache/dubbo/registry/zookeeper/ZookeeperRegistry.java
+++ dubbo-registry/dubbo-registry-zookeeper/src/main/java/org/apache/dubbo/registry/zookeeper/ZookeeperRegistry.java
@@ -203,7 +203,8 @@ public class ZookeeperRegistry extends CacheableFailbackRegistry {
                         listeners, listener, k -> (parentPath, currentChildren) -> {
                             for (String child : currentChildren) {
                                 if (!child.startsWith("{") && !child.startsWith("[")) {
-                                    logger.warn(PROTOCOL_ERROR_DESERIALIZE, "", "", child + "is not json");
+//                                    logger.warn(PROTOCOL_ERROR_DESERIALIZE, "", "", child + "is not json");
+                                    continue;
                                 }
                                 child = URL.decode(child);
                                 if (!anyServices.contains(child)) {
