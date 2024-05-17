d22e58eba602c4dbface26443d464ca95854c3ab
aofall <ihacat3004@gmail.com>
2024-05-17 12:03:28 +0800

    revert the default extension name

--- dubbo-plugin/dubbo-filter-validation/src/main/java/org/apache/dubbo/validation/Validation.java
+++ dubbo-plugin/dubbo-filter-validation/src/main/java/org/apache/dubbo/validation/Validation.java
@@ -25,7 +25,7 @@ import static org.apache.dubbo.common.constants.FilterConstants.VALIDATION_KEY;
 /**
  * Instance of Validation interface provide instance of {@link Validator} based on the value of <b>validation</b> attribute.
  */
-@SPI("adapter")
+@SPI("jvalidation")
 public interface Validation {
 
     /**
