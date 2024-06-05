52b51968f21698fda6e9fc38a13e50511037b11a
star <653945222@qq.com>
2024-06-05 10:46:40 +0800

    fix: outclass name error question (#14286)

--- dubbo-compiler/src/main/java/org/apache/dubbo/gen/AbstractGenerator.java
+++ dubbo-compiler/src/main/java/org/apache/dubbo/gen/AbstractGenerator.java
@@ -98,10 +98,11 @@ public abstract class AbstractGenerator extends Generator {
                 serviceContext.protoName = fileProto.getName();
                 serviceContext.packageName = extractPackageName(fileProto);
                 String protoBaseName = Files.getNameWithoutExtension(fileProto.getName());
-                if (!Strings.isNullOrEmpty(fileProto.getOptions().getJavaOuterClassname()) {
+                if (!Strings.isNullOrEmpty(fileProto.getOptions().getJavaOuterClassname())) {
                     serviceContext.outerClassName = fileProto.getOptions().getJavaOuterClassname();
                 } else if (!fileProto.getName().equals(serviceContext.serviceName)) {
-                    serviceContext.outerClassName = protoBaseName.substring(0, 1).toUpperCase() + protoBaseName.substring(1);
+                    serviceContext.outerClassName = protoBaseName.substring(0, 1).toUpperCase()
+                            + protoBaseName.substring(1);
                 }
                 serviceContext.commonPackageName = extractCommonPackageName(fileProto);
                 serviceContext.multipleFiles = fileProto.getOptions().getJavaMultipleFiles();
