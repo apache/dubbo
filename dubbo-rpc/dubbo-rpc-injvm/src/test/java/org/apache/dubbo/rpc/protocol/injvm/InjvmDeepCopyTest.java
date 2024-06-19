8dd74d5d831103e7239b2c6a1261fc0fbc0ffec7
caoyanan <caoyanan@growingio.com>
2024-06-19 16:30:23 +0800

    fix unit test

--- dubbo-rpc/dubbo-rpc-injvm/src/test/java/org/apache/dubbo/rpc/protocol/injvm/InjvmDeepCopyTest.java
+++ dubbo-rpc/dubbo-rpc-injvm/src/test/java/org/apache/dubbo/rpc/protocol/injvm/InjvmDeepCopyTest.java
@@ -30,6 +30,7 @@ import org.apache.dubbo.rpc.model.ProviderModel;
 import org.apache.dubbo.rpc.model.ServiceDescriptor;
 
 import java.io.Serializable;
+import java.net.URLClassLoader;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.Assertions;
@@ -77,6 +78,7 @@ class InjvmDeepCopyTest {
                 ClassUtils.getClassLoader(DemoInterface.class));
         URL consumerUrl = url.setScopeModel(moduleModel).setServiceModel(consumerModel);
 
+        Thread.currentThread().setContextClassLoader(new URLClassLoader(new java.net.URL[] {}));
         DemoInterface stub = proxyFactory.getProxy(protocol.refer(DemoInterface.class, consumerUrl));
 
         Data request = new Data();
