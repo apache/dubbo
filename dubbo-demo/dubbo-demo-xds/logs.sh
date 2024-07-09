d3044d79cc7f809f8d1a02a22af4cfd6ad33a91e
wuyuhao <wuyuhao1@xiaohongshu.com>
2024-07-09 11:30:40 +0800

    Add log script

--- dubbo-demo/dubbo-demo-xds/logs.sh
+++ dubbo-demo/dubbo-demo-xds/logs.sh
@@ -1,7 +1,14 @@
 #!bash
 
-echo "Starting logs"
+echo "Starting logs consumer"
 kubectl get pods -n default
 POD_NAME=$(kubectl get pods -n default | grep dubbo-demo-xds-consumer | awk '{print $1}')
 echo $POD_NAME
-kubectl logs $POD_NAME -n default -f
+kubectl logs $POD_NAME -n default
+
+
+echo "Starting logs provider"
+kubectl get pods -n default
+POD_NAME=$(kubectl get pods -n default | grep dubbo-demo-xds-provider | awk '{print $1}')
+echo $POD_NAME
+kubectl logs $POD_NAME -n default
