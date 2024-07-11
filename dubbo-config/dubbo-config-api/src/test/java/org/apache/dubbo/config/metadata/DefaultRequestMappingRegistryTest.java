528f87d0920037c8a350a7d32c74ed77a9b3071e
heliang <3596006474@qq.com>
2024-07-11 20:25:55 +0800

    code format

--- dubbo-config/dubbo-config-api/src/test/java/org/apache/dubbo/config/metadata/DefaultRequestMappingRegistryTest.java
+++ dubbo-config/dubbo-config-api/src/test/java/org/apache/dubbo/config/metadata/DefaultRequestMappingRegistryTest.java
@@ -17,16 +17,8 @@
 package org.apache.dubbo.config.metadata;
 
 import org.apache.dubbo.common.URL;
-import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.common.utils.ClassUtils;
-import org.apache.dubbo.common.utils.NetUtils;
 import org.apache.dubbo.config.ApplicationConfig;
-import org.apache.dubbo.config.ProtocolConfig;
-import org.apache.dubbo.config.RegistryConfig;
-import org.apache.dubbo.config.ServiceConfig;
-import org.apache.dubbo.config.api.DemoService;
-import org.apache.dubbo.config.bootstrap.DubboBootstrap;
-import org.apache.dubbo.config.provider.impl.DemoServiceImpl;
 import org.apache.dubbo.registry.client.metadata.MetadataServiceDelegation;
 import org.apache.dubbo.rpc.Invoker;
 import org.apache.dubbo.rpc.Protocol;
@@ -38,10 +30,6 @@ import org.apache.dubbo.rpc.model.ServiceDescriptor;
 import org.apache.dubbo.rpc.model.ServiceMetadata;
 import org.apache.dubbo.rpc.protocol.tri.TripleProtocol;
 
-import org.junit.Test;
-
-import static org.apache.dubbo.common.constants.CommonConstants.REMOTE_METADATA_STORAGE_TYPE;
-
 public class DefaultRequestMappingRegistryTest {
 
     public static void main(String[] args) throws InterruptedException {
@@ -51,32 +39,37 @@ public class DefaultRequestMappingRegistryTest {
 
         URL providerUrl = URL.valueOf("tri://127.0.0.1:" + 8081 + "/" + IGreeter.class.getName());
 
-        ModuleServiceRepository serviceRepository = applicationModel.getDefaultModule().getServiceRepository();
+        ModuleServiceRepository serviceRepository =
+                applicationModel.getDefaultModule().getServiceRepository();
         ServiceDescriptor serviceDescriptor = serviceRepository.registerService(IGreeter.class);
 
-        ProviderModel providerModel = new ProviderModel(providerUrl.getServiceKey(), serviceImpl, serviceDescriptor, new ServiceMetadata(), ClassUtils.getClassLoader(IGreeter.class));
+        ProviderModel providerModel = new ProviderModel(
+                providerUrl.getServiceKey(),
+                serviceImpl,
+                serviceDescriptor,
+                new ServiceMetadata(),
+                ClassUtils.getClassLoader(IGreeter.class));
         serviceRepository.registerProvider(providerModel);
         providerUrl = providerUrl.setServiceModel(providerModel);
 
         Protocol protocol = new TripleProtocol(providerUrl.getOrDefaultFrameworkModel());
-        ProxyFactory proxyFactory = applicationModel.getExtensionLoader(ProxyFactory.class).getAdaptiveExtension();
+        ProxyFactory proxyFactory =
+                applicationModel.getExtensionLoader(ProxyFactory.class).getAdaptiveExtension();
         Invoker<IGreeter> invoker = proxyFactory.getInvoker(serviceImpl, IGreeter.class, providerUrl);
         protocol.export(invoker);
-        MetadataServiceDelegation metadataServiceDelegation = applicationModel.getDefaultModule()
-                .getBeanFactory()
-                .getBean(MetadataServiceDelegation.class);
+        MetadataServiceDelegation metadataServiceDelegation =
+                applicationModel.getDefaultModule().getBeanFactory().getBean(MetadataServiceDelegation.class);
 
         ApplicationConfig config = new ApplicationConfig();
         config.setProtocol("tri");
         config.setMetadataServicePort(9000);
         applicationModel.getApplicationConfigManager().setApplication(config);
 
-        ConfigurableMetadataServiceExporter metadataServiceExporter = new ConfigurableMetadataServiceExporter(applicationModel, metadataServiceDelegation);
+        ConfigurableMetadataServiceExporter metadataServiceExporter =
+                new ConfigurableMetadataServiceExporter(applicationModel, metadataServiceDelegation);
         metadataServiceExporter.export();
         // 检查元数据服务代理的OpenAPI
 
         System.out.println(metadataServiceDelegation.getOpenAPISchema());
-
-
     }
 }
