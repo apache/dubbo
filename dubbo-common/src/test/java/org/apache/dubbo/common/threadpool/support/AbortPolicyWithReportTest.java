90520b0237f282f341cfcfffe628def4349c6efb
小东dylanli <dylanli666@qq.com>
2024-07-29 09:57:38 +0800

    fix AbortPolicyWithReport may repeatedly jstack when threadPool is exhausted (#14468)

--- dubbo-common/src/test/java/org/apache/dubbo/common/threadpool/support/AbortPolicyWithReportTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/threadpool/support/AbortPolicyWithReportTest.java
@@ -17,18 +17,26 @@
 package org.apache.dubbo.common.threadpool.support;
 
 import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.threadlocal.NamedInternalThreadFactory;
 import org.apache.dubbo.common.threadpool.event.ThreadPoolExhaustedEvent;
 import org.apache.dubbo.common.threadpool.event.ThreadPoolExhaustedListener;
 
 import java.io.FileOutputStream;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.apache.dubbo.common.constants.CommonConstants.OS_NAME_KEY;
@@ -37,6 +45,11 @@ import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AbortPolicyWithReportTest {
+    @BeforeEach
+    public void setUp() {
+        AbortPolicyWithReport.lastPrintTime = 0;
+    }
+
     @Test
     void jStackDumpTest() {
         URL url = URL.valueOf(
@@ -58,6 +71,61 @@ class AbortPolicyWithReportTest {
 
         await().until(() -> AbortPolicyWithReport.guard.availablePermits() == 1);
         Assertions.assertNotNull(fileOutputStream.get());
+    }
+
+    @Test
+    void jStack_ConcurrencyDump_Silence_10Min() {
+        URL url = URL.valueOf(
+                "dubbo://admin:hello1234@10.20.130.230:20880/context/path?dump.directory=/tmp&version=1.0.0&application=morgan&noValue=");
+        AtomicInteger jStackCount = new AtomicInteger(0);
+        AtomicInteger failureCount = new AtomicInteger(0);
+        AtomicInteger finishedCount = new AtomicInteger(0);
+        AtomicInteger timeoutCount = new AtomicInteger(0);
+        AbortPolicyWithReport abortPolicyWithReport = new AbortPolicyWithReport("Test", url) {
+            @Override
+            protected void jstack(FileOutputStream jStackStream) {
+                jStackCount.incrementAndGet();
+                // try to simulate the jstack cost long time, so that AbortPolicyWithReport may jstack repeatedly.
+                long startTime = System.currentTimeMillis();
+                await().atLeast(200, TimeUnit.MILLISECONDS).until(() -> System.currentTimeMillis() - startTime >= 300);
+            }
+        };
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                4,
+                4,
+                0,
+                TimeUnit.MILLISECONDS,
+                new SynchronousQueue<>(),
+                new NamedInternalThreadFactory("jStack_ConcurrencyDump_Silence_10Min", false),
+                abortPolicyWithReport);
+        int runTimes = 100;
+        List<Future<?>> futureList = new LinkedList<>();
+        for (int i = 0; i < runTimes; i++) {
+            try {
+                futureList.add(threadPoolExecutor.submit(() -> {
+                    finishedCount.incrementAndGet();
+                    long start = System.currentTimeMillis();
+                    // try to await 1s to make sure jstack dump thread scheduled
+                    await().atLeast(300, TimeUnit.MILLISECONDS).until(() -> System.currentTimeMillis() - start >= 300);
+                }));
+            } catch (Exception ignored) {
+                failureCount.incrementAndGet();
+            }
+        }
+        futureList.forEach(f -> {
+            try {
+                f.get(500, TimeUnit.MILLISECONDS);
+            } catch (Exception ignored) {
+                timeoutCount.incrementAndGet();
+            }
+        });
+
+        System.out.printf(
+                "jStackCount: %d, finishedCount: %d, failureCount: %d, timeoutCount: %d %n",
+                jStackCount.get(), finishedCount.get(), failureCount.get(), timeoutCount.get());
+        Assertions.assertEquals(
+                runTimes, finishedCount.get() + failureCount.get(), "all the test thread should be run completely");
+        Assertions.assertEquals(1, jStackCount.get(), "'jstack' should be called only once in 10 minutes");
     }
 
     @Test
