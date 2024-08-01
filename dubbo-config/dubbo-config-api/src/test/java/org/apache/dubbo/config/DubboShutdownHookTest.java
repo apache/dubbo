1c7e698754b90280b3bc5c75ee64943ad92d7ea0
Chenjp <ch_jp@msn.com>
2024-08-01 15:55:13 +0800

    Update DubboShutdownHookTest.java
    FIX: testDestoryWithModuleManagedExternally - add log assertion to ensure.

--- dubbo-config/dubbo-config-api/src/test/java/org/apache/dubbo/config/DubboShutdownHookTest.java
+++ dubbo-config/dubbo-config-api/src/test/java/org/apache/dubbo/config/DubboShutdownHookTest.java
@@ -16,17 +16,29 @@
  */
 package org.apache.dubbo.config;
 
-import org.apache.dubbo.common.constants.CommonConstants;
+import org.apache.dubbo.common.logger.ErrorTypeAwareLogger;
+import org.apache.dubbo.common.logger.Logger;
+import org.apache.dubbo.common.logger.support.FailsafeLogger;
 import org.apache.dubbo.rpc.model.ApplicationModel;
 import org.apache.dubbo.rpc.model.FrameworkModel;
 import org.apache.dubbo.rpc.model.ModuleModel;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class DubboShutdownHookTest {
     private DubboShutdownHook dubboShutdownHook;
@@ -34,7 +46,6 @@ public class DubboShutdownHookTest {
 
     @BeforeEach
     public void init() {
-        SysProps.setProperty(CommonConstants.IGNORE_LISTEN_SHUTDOWN_HOOK, "false");
         FrameworkModel frameworkModel = new FrameworkModel();
         applicationModel = frameworkModel.newApplication();
         ModuleModel moduleModel = applicationModel.newModule();
@@ -61,14 +72,50 @@ public class DubboShutdownHookTest {
     }
 
     @Test
-    public void testDestoryWithModuleManagedExternally() throws InterruptedException {
+    public void testDestoryWithModuleManagedExternally()
+            throws InterruptedException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException,
+                    SecurityException, InvocationTargetException {
+        Field loggerField = ReflectionUtils.findFields(
+                        DubboShutdownHook.class,
+                        f -> f.getName().equals("logger"),
+                        ReflectionUtils.HierarchyTraversalMode.TOP_DOWN)
+                .get(0);
+        loggerField.setAccessible(true);
+        ErrorTypeAwareLogger hookLogger = (ErrorTypeAwareLogger) loggerField.get(dubboShutdownHook);
+        Method getLogger = ReflectionUtils.findMethod(FailsafeLogger.class, "getLogger", new Class<?>[0])
+                .get();
+        Method setLogger = ReflectionUtils.findMethod(FailsafeLogger.class, "setLogger", Logger.class)
+                .get();
+        Logger internalLogger = (Logger) getLogger.invoke(hookLogger, new Object[0]);
+
+        Logger spyLogger = Mockito.spy(internalLogger);
+        when(spyLogger.isInfoEnabled()).thenReturn(true);
+        setLogger.invoke(hookLogger, spyLogger);
+
+        ArgumentCaptor<String> loggerCaptor = ArgumentCaptor.forClass(String.class);
+
         applicationModel.getModuleModels().get(0).setLifeCycleManagedExternally(true);
         new Thread(() -> {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(100);
+                    } catch (InterruptedException e) {
+                    }
                     applicationModel.getModuleModels().get(0).destroy();
                 })
                 .start();
         TimeUnit.MILLISECONDS.sleep(10);
         dubboShutdownHook.run();
         Assertions.assertTrue(applicationModel.isDestroyed());
+
+        verify(spyLogger, atLeastOnce()).info(loggerCaptor.capture());
+        StringBuffer logBuf = new StringBuffer();
+        for (String row : loggerCaptor.getAllValues()) {
+            logBuf.append(row).append("\n");
+        }
+        String logs = logBuf.toString();
+        Assertions.assertTrue(logs.contains("Waiting for modules"));
+        Assertions.assertTrue(logs.contains("managed by Spring to be shutdown failed")
+                || logs.contains("managed by Spring has been destroyed successfully."));
+        Assertions.assertTrue(logs.contains("Complete shutdown hook now."));
     }
 }
