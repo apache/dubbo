fc1fc84e749db8f278b3d7ac1d04f1f15e0ba009
jianbin <jianbin@apache.org>
2024-08-14 11:46:32 +0800

    optimize: the handling logic of PathAndInvokerMapper

--- dubbo-rpc/dubbo-rpc-rest/src/main/java/org/apache/dubbo/rpc/protocol/rest/PathAndInvokerMapper.java
+++ dubbo-rpc/dubbo-rpc-rest/src/main/java/org/apache/dubbo/rpc/protocol/rest/PathAndInvokerMapper.java
@@ -53,19 +53,14 @@ public class PathAndInvokerMapper {
      */
     public void addPathAndInvoker(Map<PathMatcher, RestMethodMetadata> metadataMap, Invoker invoker) {
 
-        metadataMap.entrySet().stream().forEach(entry -> {
-            PathMatcher pathMatcher = entry.getKey();
-            if (pathMatcher.hasPathVariable()) {
-                addPathMatcherToPathMap(
-                        pathMatcher,
-                        pathToServiceMapContainPathVariable,
-                        InvokerAndRestMethodMetadataPair.pair(invoker, entry.getValue()));
-            } else {
-                addPathMatcherToPathMap(
-                        pathMatcher,
-                        pathToServiceMapNoPathVariable,
-                        InvokerAndRestMethodMetadataPair.pair(invoker, entry.getValue()));
-            }
+        metadataMap.forEach((pathMatcher, value) -> {
+	        if (pathMatcher.hasPathVariable()) {
+		        addPathMatcherToPathMap(pathMatcher, pathToServiceMapContainPathVariable,
+			        InvokerAndRestMethodMetadataPair.pair(invoker, value));
+	        } else {
+		        addPathMatcherToPathMap(pathMatcher, pathToServiceMapNoPathVariable,
+			        InvokerAndRestMethodMetadataPair.pair(invoker, value));
+	        }
         });
     }
 
@@ -78,16 +73,14 @@ public class PathAndInvokerMapper {
     public InvokerAndRestMethodMetadataPair getRestMethodMetadata(PathMatcher pathMatcher) {
 
         // first search from pathToServiceMapNoPathVariable
-        if (pathToServiceMapNoPathVariable.containsKey(pathMatcher)) {
-            return pathToServiceMapNoPathVariable.get(pathMatcher);
+        InvokerAndRestMethodMetadataPair pair;
+        pair = pathToServiceMapNoPathVariable.get(pathMatcher);
+        if (pair == null) {
+            // second search from pathToServiceMapContainPathVariable
+            pair = pathToServiceMapContainPathVariable.get(pathMatcher);
         }
 
-        // second search from pathToServiceMapContainPathVariable
-        if (pathToServiceMapContainPathVariable.containsKey(pathMatcher)) {
-            return pathToServiceMapContainPathVariable.get(pathMatcher);
-        }
-
-        return null;
+        return pair;
     }
 
     /**
@@ -117,12 +110,10 @@ public class PathAndInvokerMapper {
             Map<PathMatcher, InvokerAndRestMethodMetadataPair> pathMatcherPairMap,
             InvokerAndRestMethodMetadataPair invokerRestMethodMetadataPair) {
 
-        if (pathMatcherPairMap.containsKey(pathMatcher)) {
-
+        InvokerAndRestMethodMetadataPair beforeMetadata = pathMatcherPairMap.get(pathMatcher);
+        if (beforeMetadata != null) {
             // cover the old service metadata when  current interface is old interface & current method desc equals
             // old`s method desc,else ,throw double check exception
-
-            InvokerAndRestMethodMetadataPair beforeMetadata = pathMatcherPairMap.get(pathMatcher);
             // true when reExport
             if (!invokerRestMethodMetadataPair.compareServiceMethod(beforeMetadata)) {
                 throw new DoublePathCheckException("dubbo rest double path check error, current path is: " + pathMatcher
@@ -145,15 +136,12 @@ public class PathAndInvokerMapper {
 
         PathMatcher newPathMatcher = PathMatcher.convertPathMatcher(pathMatcher);
 
-        if (!pathMatcherToHttpMethodMap.containsKey(newPathMatcher)) {
-            HashSet<String> httpMethods = new HashSet<>();
+        Set<String> httpMethods = pathMatcherToHttpMethodMap.computeIfAbsent(newPathMatcher, k -> {
+            HashSet<String> methods = new HashSet<>();
 
-            httpMethods.add(pathMatcher.getHttpMethod());
-
-            pathMatcherToHttpMethodMap.put(newPathMatcher, httpMethods);
-        }
-
-        Set<String> httpMethods = pathMatcherToHttpMethodMap.get(newPathMatcher);
+            methods.add(pathMatcher.getHttpMethod());
+            return methods;
+        });
 
         httpMethods.add(newPathMatcher.getHttpMethod());
     }
