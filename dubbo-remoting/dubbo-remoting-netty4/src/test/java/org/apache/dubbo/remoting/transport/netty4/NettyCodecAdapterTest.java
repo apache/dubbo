93b90c0efb32237d9b6b58d12f9fe3c82fd7ae37
jiangyuan <joe469391363@gmail.com>
2024-08-14 22:04:10 +0800

    fix NettyCodecAdapter decoder memory leak

--- dubbo-remoting/dubbo-remoting-netty4/src/test/java/org/apache/dubbo/remoting/transport/netty4/NettyCodecAdapterTest.java
+++ dubbo-remoting/dubbo-remoting-netty4/src/test/java/org/apache/dubbo/remoting/transport/netty4/NettyCodecAdapterTest.java
@@ -16,15 +16,32 @@
  */
 package org.apache.dubbo.remoting.transport.netty4;
 
+import io.netty.buffer.AbstractByteBufAllocator;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.embedded.EmbeddedChannel;
+
+import io.netty.handler.codec.DecoderException;
+
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.remoting.ChannelHandler;
 import org.apache.dubbo.remoting.Codec2;
 
 import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.MessageToByteEncoder;
+
+import org.apache.dubbo.remoting.Constants;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 /**
  * {@link NettyCodecAdapter}
@@ -41,5 +58,31 @@ class NettyCodecAdapterTest {
         io.netty.channel.ChannelHandler encoder = nettyCodecAdapter.getEncoder();
         Assertions.assertTrue(decoder instanceof ByteToMessageDecoder);
         Assertions.assertTrue(encoder instanceof MessageToByteEncoder);
+    }
+
+    @Test
+    void testDecodeException() throws IOException {
+        Codec2 codec2 = Mockito.mock(Codec2.class);
+        doThrow(new IOException("testDecodeIllegalPacket")).when(codec2).decode(any(), any());
+
+        URL url = Mockito.mock(URL.class);
+        doReturn("default").when(url).getParameter(eq(Constants.CODEC_KEY));
+
+        ChannelHandler handler = Mockito.mock(ChannelHandler.class);
+        NettyCodecAdapter nettyCodecAdapter = new NettyCodecAdapter(codec2, url, handler);
+        io.netty.channel.ChannelHandler decoder = nettyCodecAdapter.getDecoder();
+        EmbeddedChannel embeddedChannel = new EmbeddedChannel();
+        embeddedChannel.pipeline().addLast(decoder);
+
+        // 模拟问题数据包
+        ByteBuf input = AbstractByteBufAllocator.DEFAULT.buffer();
+        input.writeBytes("testDecodeIllegalPacket".getBytes(StandardCharsets.UTF_8));
+
+        DecoderException decoderException = Assertions.assertThrows(DecoderException.class, () -> {
+            embeddedChannel.writeInbound(input);
+        });
+        Assertions.assertTrue( decoderException.getCause() instanceof IOException);
+
+        Assertions.assertEquals(0, input.refCnt());
     }
 }
