58f5bf97ebc5574562e208ed9ef72586fb1f8eb4
Mac <songxiaosheng@apache.org>
2025-02-25 23:01:06 +0800

    default format need't convert avoid data loss

--- dubbo-metrics/dubbo-metrics-default/src/main/java/org/apache/dubbo/metrics/report/DefaultMetricsReporter.java
+++ dubbo-metrics/dubbo-metrics-default/src/main/java/org/apache/dubbo/metrics/report/DefaultMetricsReporter.java
@@ -45,47 +45,7 @@ public class DefaultMetricsReporter extends AbstractMetricsReporter {
 
     @Override
     public String getResponseWithName(String metricsName) {
-        Map<String, List<Tag>> metricsTags = new HashMap<>();
-        Map<String, Object> metricsValue = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
-        meterRegistry.getMeters().stream()
-                .filter(meter -> {
-                    if (meter == null || meter.getId() == null || meter.getId().getName() == null) {
-                        return false;
-                    }
-                    if (metricsName != null) {
-                        return meter.getId().getName().contains(metricsName);
-                    }
-                    return true;
-                })
-                .forEach(meter -> {
-                    Object value = null;
-                    if (meter instanceof Counter) {
-                        Counter counter = (Counter) meter;
-                        value = counter.count();
-                    }
-                    if (meter instanceof Gauge) {
-                        Gauge gauge = (Gauge) meter;
-                        value = gauge.value();
-                    }
-                    if (meter instanceof Timer) {
-                        Timer timer = (Timer) meter;
-                        value = timer.totalTime(TimeUnit.MILLISECONDS);
-                    }
-                    metricsTags.put(meter.getId().getName(), meter.getId().getTags());
-                    metricsValue.put(meter.getId().getName(), value);
-                });
-        metricsValue.forEach((key, value) -> {
-            sb.append(key).append("{");
-            List<Tag> tags = metricsTags.get(key);
-            if (tags != null && tags.size() > 0) {
-                tags.forEach(tag -> {
-                    sb.append(tag.getKey()).append("=").append(tag.getValue()).append(",");
-                });
-            }
-            sb.append("} ").append(value).append(System.lineSeparator());
-        });
-        return sb.toString();
+        return meterRegistry.getMetersAsString();
     }
 
     @Override
