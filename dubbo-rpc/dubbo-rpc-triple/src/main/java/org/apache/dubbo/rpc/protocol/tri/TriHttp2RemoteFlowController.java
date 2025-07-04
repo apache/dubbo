ccd3d8af636fafae96e92b9a484781d8d20c72f3
zrlw <zrlw@sina.com>
2025-07-04 11:34:16 +0800

    Remove overflow check process to align with Netty http2 remote flow controller

--- dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/TriHttp2RemoteFlowController.java
+++ dubbo-rpc/dubbo-rpc-triple/src/main/java/org/apache/dubbo/rpc/protocol/tri/TriHttp2RemoteFlowController.java
@@ -407,10 +407,8 @@ public class TriHttp2RemoteFlowController implements Http2RemoteFlowController {
                 if (cancelled) {
                     cancel(INTERNAL_ERROR, cause);
                 }
-                if (monitor.isOverFlowControl()) {
-                    // Let receiver continue receiving the pending bytes.
-                    logger.warn("TotalPendingBytes size overflow for stream: " + this.stream().id());
-                }
+
+                // does not check overflow anymore: Let receiver continue receiving the pending bytes.
             }
             return writtenBytes;
         }
@@ -670,14 +668,6 @@ public class TriHttp2RemoteFlowController implements Http2RemoteFlowController {
         final boolean isWritableConnection() {
             return connectionState.windowSize() - totalPendingBytes > 0 && isChannelWritable();
         }
-
-        final boolean isOverFlowControl() {
-            if (connectionState.windowSize() == 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
     }
 
     /**
@@ -777,9 +767,8 @@ public class TriHttp2RemoteFlowController implements Http2RemoteFlowController {
                 checkAllWritabilityChanged();
             } else if (isWritable(state) != state.markedWritability()) {
                 notifyWritabilityChanged(state);
-            } else if (isOverFlowControl()) {
-                logger.warn("TotalPendingBytes size overflow for stream: " + state.stream().id());
             }
+            // does not check overflow anymore: Let receiver continue receiving the pending bytes.
         }
 
         private void checkAllWritabilityChanged() throws Http2Exception {
