c203f6991d1b92a8ea66c8da39d5c3801e8a9ed9
Arena <me@biu.kim>
2025-07-10 21:15:39 +0800

    fix code format and add server/client unit tests

--- dubbo-plugin/dubbo-mutiny/src/test/java/org/apache/dubbo/mutiny/TripleMutinyPublisherTest.java
+++ dubbo-plugin/dubbo-mutiny/src/test/java/org/apache/dubbo/mutiny/TripleMutinyPublisherTest.java
@@ -1,14 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.dubbo.mutiny;
 
 import org.apache.dubbo.rpc.protocol.tri.observer.CallStreamObserver;
-
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Flow;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -39,16 +55,13 @@ public class TripleMutinyPublisherTest {
             }
 
             @Override
-            public void onNext(String item) {
-            }
+            public void onNext(String item) {}
 
             @Override
-            public void onError(Throwable t) {
-            }
+            public void onError(Throwable t) {}
 
             @Override
-            public void onComplete() {
-            }
+            public void onComplete() {}
         };
 
         publisher.subscribe(subscriber);
@@ -72,8 +85,8 @@ public class TripleMutinyPublisherTest {
     public void testCancelTriggersShutdownHook() {
         AtomicBoolean shutdown = new AtomicBoolean(false);
 
-        AbstractTripleMutinyPublisher<String> publisher = new AbstractTripleMutinyPublisher<>(
-                null, () -> shutdown.set(true)) {};
+        AbstractTripleMutinyPublisher<String> publisher =
+                new AbstractTripleMutinyPublisher<>(null, () -> shutdown.set(true)) {};
 
         publisher.cancel();
         assertTrue(publisher.isCancelled());
@@ -89,8 +102,7 @@ public class TripleMutinyPublisherTest {
 
         publisher.subscribe(new Flow.Subscriber<>() {
             @Override
-            public void onSubscribe(Flow.Subscription s) {
-            }
+            public void onSubscribe(Flow.Subscription s) {}
 
             @Override
             public void onNext(String item) {
@@ -98,8 +110,7 @@ public class TripleMutinyPublisherTest {
             }
 
             @Override
-            public void onError(Throwable t) {
-            }
+            public void onError(Throwable t) {}
 
             @Override
             public void onComplete() {
