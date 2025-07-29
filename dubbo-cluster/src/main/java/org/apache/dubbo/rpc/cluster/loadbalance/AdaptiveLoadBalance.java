ea8699be4f3335aacb73e395f9ccc484ea4ac9e9
co63oc <co63oc@users.noreply.github.com>
2025-07-29 12:29:34 +0800

    fix

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/AdaptiveLoadBalance.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/AdaptiveLoadBalance.java
@@ -33,9 +33,6 @@ import java.util.concurrent.ThreadLocalRandom;
 import static org.apache.dubbo.common.constants.CommonConstants.DEFAULT_TIMEOUT;
 import static org.apache.dubbo.common.constants.CommonConstants.LOADBALANCE_KEY;
 
-/**
- * AdaptiveLoadBalance
- */
 public class AdaptiveLoadBalance extends AbstractLoadBalance {
 
     public static final String NAME = "adaptive";
