84011a7511b05e7722bebc1d5864acb7124cde9f
crackhead6474 <abdullahsheikh6474760@gmail.com>
2025-08-21 19:20:57 +0600

    calculator.java

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+    // Method to add two numbers
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    // Method to subtract two numbers
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // Method to multiply two numbers
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // Method to divide two numbers
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
