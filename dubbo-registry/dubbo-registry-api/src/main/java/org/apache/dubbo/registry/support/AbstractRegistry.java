4596b75733e9a3dcd17e6652eb15d85b47b48797
Pretham G <unknownhawk804@gmail.com>
2025-12-23 11:44:31 +0530

    ran mvn spotless:apply

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/support/AbstractRegistry.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/support/AbstractRegistry.java
@@ -463,21 +463,17 @@ public abstract class AbstractRegistry implements Registry {
         Set<NotifyListener> listeners =
                 ConcurrentHashMapUtils.computeIfAbsent(subscribed, url, n -> new ConcurrentHashSet<>());
         listeners.add(listener);
-        if(localCacheEnabled && !isAvailable()){
-            try{
-                List<URL> cached=getCacheUrls(url);
-                if(CollectionUtils.isNotEmpty(cached)){
+        if (localCacheEnabled && !isAvailable()) {
+            try {
+                List<URL> cached = getCacheUrls(url);
+                if (CollectionUtils.isNotEmpty(cached)) {
                     listener.notify(cached);
                     logger.info("Registry unavailable or not yet ready. Loaded cached URLs for " + url.getServiceKey()
                             + " : " + cached.size());
                 }
             } catch (Throwable t) {
                 logger.warn(
-                        INTERNAL_ERROR,
-                        "failed to load cached URLs",
-                        "",
-                        "Failed to load cached URLs for " + url,
-                        t);
+                        INTERNAL_ERROR, "failed to load cached URLs", "", "Failed to load cached URLs for " + url, t);
                 throw t;
             }
         }
