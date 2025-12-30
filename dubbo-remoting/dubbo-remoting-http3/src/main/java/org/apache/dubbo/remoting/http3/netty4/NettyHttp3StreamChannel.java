75f85f8bbd183b5db54a1e56fc51cb6225243b85
earthchen <earthchen@apache.org>
2025-12-30 23:30:35 +0800

    fix

--- dubbo-remoting/dubbo-remoting-http3/src/main/java/org/apache/dubbo/remoting/http3/netty4/NettyHttp3StreamChannel.java
+++ dubbo-remoting/dubbo-remoting-http3/src/main/java/org/apache/dubbo/remoting/http3/netty4/NettyHttp3StreamChannel.java
@@ -52,6 +52,11 @@ public class NettyHttp3StreamChannel implements H2StreamChannel {
     }
 
     @Override
+    public void consumeBytes(int numBytes) throws Exception {
+        // HTTP/3 flow control is handled by Netty and QUIC implementation.
+    }
+
+    @Override
     public CompletableFuture<Void> writeHeader(HttpMetadata httpMetadata) {
         NettyHttpChannelFutureListener futureListener = new NettyHttpChannelFutureListener();
         http3StreamChannel.write(httpMetadata).addListener(futureListener);
