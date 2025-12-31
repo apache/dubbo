e99935e510bdff1e5dfed6e905e25c48f7748a74
LI123456mo <conellimo@gmail.com>
2025-12-31 15:34:14 +0300

    fix: restore ProtocolUtils to original state to fix compilation

--- dubbo-common/src/main/java/org/apache/dubbo/rpc/support/ProtocolUtils.java
+++ dubbo-common/src/main/java/org/apache/dubbo/rpc/support/ProtocolUtils.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -53,11 +53,11 @@ public final class ProtocolUtils {
     public static boolean isGeneric(String generic) {
         return StringUtils.isNotEmpty(generic)
                 && (GENERIC_SERIALIZATION_DEFAULT.equalsIgnoreCase(generic)
-                || GENERIC_SERIALIZATION_NATIVE_JAVA.equalsIgnoreCase(generic)
-                || GENERIC_SERIALIZATION_BEAN.equalsIgnoreCase(generic)
-                || GENERIC_SERIALIZATION_PROTOBUF.equalsIgnoreCase(generic)
-                || GENERIC_SERIALIZATION_GSON.equalsIgnoreCase(generic)
-                || GENERIC_RAW_RETURN.equalsIgnoreCase(generic));
+                        || GENERIC_SERIALIZATION_NATIVE_JAVA.equalsIgnoreCase(generic)
+                        || GENERIC_SERIALIZATION_BEAN.equalsIgnoreCase(generic)
+                        || GENERIC_SERIALIZATION_PROTOBUF.equalsIgnoreCase(generic)
+                        || GENERIC_SERIALIZATION_GSON.equalsIgnoreCase(generic)
+                        || GENERIC_RAW_RETURN.equalsIgnoreCase(generic));
     }
 
     public static boolean isValidGenericValue(String generic) {
