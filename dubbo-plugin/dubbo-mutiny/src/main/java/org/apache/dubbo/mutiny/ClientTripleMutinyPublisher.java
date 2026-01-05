b4bd5d6471abe6a6daae4bd9f50d38050e3b58d3
earthchen <earthchen@apache.org>
2026-01-05 22:55:47 +0800

    fix

--- dubbo-plugin/dubbo-mutiny/src/main/java/org/apache/dubbo/mutiny/ClientTripleMutinyPublisher.java
+++ dubbo-plugin/dubbo-mutiny/src/main/java/org/apache/dubbo/mutiny/ClientTripleMutinyPublisher.java
@@ -17,7 +17,7 @@
 package org.apache.dubbo.mutiny;
 
 import org.apache.dubbo.common.stream.CallStreamObserver;
-import org.apache.dubbo.rpc.protocol.tri.observer.ClientCallToObserverAdapter;
+import org.apache.dubbo.common.stream.ClientCallStreamObserver;
 
 import java.util.function.Consumer;
 
@@ -36,7 +36,7 @@ public class ClientTripleMutinyPublisher<T> extends AbstractTripleMutinyPublishe
     }
 
     @Override
-    public void beforeStart(ClientCallToObserverAdapter<T> clientCallToObserverAdapter) {
+    public void beforeStart(ClientCallStreamObserver<T> clientCallToObserverAdapter) {
         super.onSubscribe(clientCallToObserverAdapter);
     }
 }
