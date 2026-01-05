686e5be20b26bf64d7551d89833783278f08a1ef
earthchen <earthchen@apache.org>
2026-01-05 22:49:16 +0800

    fix dubbo plugin spotless

--- dubbo-plugin/dubbo-mutiny/src/main/java/org/apache/dubbo/mutiny/AbstractTripleMutinyPublisher.java
+++ dubbo-plugin/dubbo-mutiny/src/main/java/org/apache/dubbo/mutiny/AbstractTripleMutinyPublisher.java
@@ -16,8 +16,8 @@
  */
 package org.apache.dubbo.mutiny;
 
-import org.apache.dubbo.rpc.protocol.tri.CancelableStreamObserver;
 import org.apache.dubbo.common.stream.CallStreamObserver;
+import org.apache.dubbo.rpc.protocol.tri.CancelableStreamObserver;
 
 import java.util.concurrent.Flow;
 import java.util.concurrent.atomic.AtomicBoolean;
