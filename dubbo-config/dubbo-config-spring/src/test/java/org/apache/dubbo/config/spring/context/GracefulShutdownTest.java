675b2a4b0a5660a780c4c1db62a7b9e5739a6e35
arunikayadav42 <arunikayadav42@users.noreply.github.com>
2026-01-06 19:27:09 +0530

    Style: Apply Spotless formatting

--- dubbo-config/dubbo-config-spring/src/test/java/org/apache/dubbo/config/spring/context/GracefulShutdownTest.java
+++ dubbo-config/dubbo-config-spring/src/test/java/org/apache/dubbo/config/spring/context/GracefulShutdownTest.java
@@ -1,7 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.dubbo.config.spring.context;
 
 import org.apache.dubbo.config.spring.util.DubboBeanUtils;
 import org.apache.dubbo.rpc.model.ModuleModel;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.DisposableBean;
@@ -10,8 +29,6 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class GracefulShutdownTest {
 
@@ -22,7 +39,7 @@ public class GracefulShutdownTest {
         context.refresh();
 
         MyTask myTask = context.getBean(MyTask.class);
-        
+
         // Ensure Dubbo is running
         ModuleModel moduleModel = DubboBeanUtils.getModuleModel(context);
         Assertions.assertFalse(moduleModel.isDestroyed(), "Module should be running");
@@ -31,7 +48,8 @@ public class GracefulShutdownTest {
         context.close();
         System.out.println("Context closed.");
 
-        Assertions.assertTrue(myTask.verified.get(), "MyTask.destroy() should have been called and verified Dubbo status");
+        Assertions.assertTrue(
+                myTask.verified.get(), "MyTask.destroy() should have been called and verified Dubbo status");
     }
 
     @Configuration
@@ -64,7 +82,7 @@ public class GracefulShutdownTest {
             // But we want it to be FALSE (still alive).
             boolean destroyed = moduleModel.isDestroyed();
             System.out.println("Dubbo destroyed status in MyTask: " + destroyed);
-            
+
             if (!destroyed) {
                 verified.set(true);
             }
