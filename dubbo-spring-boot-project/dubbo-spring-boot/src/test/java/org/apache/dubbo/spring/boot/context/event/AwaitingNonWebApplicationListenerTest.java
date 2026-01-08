5633c86eb9b6b8ae9afcc5a7851a41239f2ab37b
ishankhemani <ishankhemani1@gmail.com>
2026-01-08 23:19:56 +0530

    Fix AwaitingNonWebApplicationListener await with parent context fixes #13722

--- dubbo-spring-boot-project/dubbo-spring-boot/src/test/java/org/apache/dubbo/spring/boot/context/event/AwaitingNonWebApplicationListenerTest.java
+++ dubbo-spring-boot-project/dubbo-spring-boot/src/test/java/org/apache/dubbo/spring/boot/context/event/AwaitingNonWebApplicationListenerTest.java
@@ -18,14 +18,18 @@ package org.apache.dubbo.spring.boot.context.event;
 
 import org.apache.dubbo.config.bootstrap.DubboBootstrap;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * {@link AwaitingNonWebApplicationListener} Test
  */
-@Disabled
 class AwaitingNonWebApplicationListenerTest {
 
     @BeforeEach
@@ -58,14 +62,15 @@ class AwaitingNonWebApplicationListenerTest {
     //        });
     //    }
     //
-    //    @Test
-    //    void testMultipleContextNonWebApplication() {
-    //        new SpringApplicationBuilder(Object.class)
-    //                .parent(Object.class)
-    //                .web(false)
-    //                .run().close();
-    //        AtomicBoolean awaited = AwaitingNonWebApplicationListener.getAwaited();
-    //        assertFalse(awaited.get());
-    //    }
-
+    @Test
+    void testMultipleContextNonWebApplication() {
+        new SpringApplicationBuilder(Object.class)
+                .parent(Object.class)
+                .properties("spring.main.web-application-type=none")
+                .run()
+                .close();
+        AwaitingNonWebApplicationListener listener = new AwaitingNonWebApplicationListener();
+        AtomicBoolean awaited = listener.getAwaited();
+        assertFalse(awaited.get());
+    }
 }
