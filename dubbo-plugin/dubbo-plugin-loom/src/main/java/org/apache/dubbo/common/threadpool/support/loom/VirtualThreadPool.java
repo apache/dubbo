8a257d7ebcf40745855bf6f667499db4a4e73a13
jianbin <jianbin@apache.org>
2026-01-27 14:54:27 +0800

    feat: enhance VirtualThreadPool to support configurable thread pool size

--- dubbo-plugin/dubbo-plugin-loom/src/main/java/org/apache/dubbo/common/threadpool/support/loom/VirtualThreadPool.java
+++ dubbo-plugin/dubbo-plugin-loom/src/main/java/org/apache/dubbo/common/threadpool/support/loom/VirtualThreadPool.java
@@ -21,8 +21,12 @@ import org.apache.dubbo.common.threadpool.ThreadPool;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 
+import static org.apache.dubbo.common.constants.CommonConstants.DEFAULT_THREADS;
 import static org.apache.dubbo.common.constants.CommonConstants.DEFAULT_THREAD_NAME;
+import static org.apache.dubbo.common.constants.CommonConstants.THREADS_KEY;
 import static org.apache.dubbo.common.constants.CommonConstants.THREAD_NAME_KEY;
 
 /**
@@ -35,7 +39,18 @@ public class VirtualThreadPool implements ThreadPool {
     public Executor getExecutor(URL url) {
         String name =
                 url.getParameter(THREAD_NAME_KEY, (String) url.getAttribute(THREAD_NAME_KEY, DEFAULT_THREAD_NAME));
-        return Executors.newThreadPerTaskExecutor(
-                Thread.ofVirtual().name(name, 1).factory());
+        int threads = url.getParameter(THREADS_KEY, DEFAULT_THREADS);
+        if (threads > 0) {
+            return new ThreadPoolExecutor(
+                    threads,
+                    Integer.MAX_VALUE,
+                    0L,
+                    java.util.concurrent.TimeUnit.MILLISECONDS,
+                    new SynchronousQueue<>(),
+                    Thread.ofVirtual().name(name, 1).factory());
+        } else {
+            return Executors.newThreadPerTaskExecutor(
+                    Thread.ofVirtual().name(name, 1).factory());
+        }
     }
 }
